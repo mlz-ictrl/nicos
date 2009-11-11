284bb8a58ffa69ba31d8fe69dc9e576c396989cd
Georg Brandl <georg.brandl@frm2.tum.de>
2009-11-11 12:36:22 +0000

    Use standard file header in all modules.

--- nicm/coder.py
+++ nicm/coder.py
@@ -1,9 +1,37 @@
-# -*- coding: utf-8 -*-
-"""
-    nicm.coder
-    ~~~~~~~~~~
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# Module:
+#   $Id$
+#
+# Description:
+#   NICOS coder definition
+#
+# Author:
+#   Jens Krüger <jens.krueger@frm2.tum.de>
+#   $Author$
+#
+#   The basic NICOS methods for the NICOS daemon (http://nicos.sf.net)
+#
+#   Copyright (C) 2009 Jens Krüger <jens.krueger@frm2.tum.de>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# *****************************************************************************
 
-    NICOS coder definition.
+"""
+NICOS coder definition.
 """
 
 __author__ = "Jens Krüger <jens.krueger@frm2.tum.de>"
