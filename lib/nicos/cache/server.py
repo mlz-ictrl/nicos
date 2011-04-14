a01217e423b614ac0b8674404e64ea4e4c8c3a93
Georg Brandl <georg.brandl@frm2.tum.de>
2011-04-14 08:56:09 +0000

    Add missing future statement.

--- lib/nicos/cache/server.py
+++ lib/nicos/cache/server.py
@@ -26,6 +26,8 @@
 #
 # *****************************************************************************
 
+from __future__ import with_statement
+
 """NICOS cache server."""
 
 __author__  = "$Author$"
