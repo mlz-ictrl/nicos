ccdf28cf4484b4a80eefa44ebe495d81b77e3c9f
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2011-06-30 15:07:25 +0200

    TEST: Use a data dir inside the testroot path
    During tests the setup references a datapath in '/data' which is not
    present on all systems.
    
    Solution: Change it.
    
    Change-Id: I3a3c17c1b25f3d3f70bd31d6019aec2ff46e705d
    Bug:NICOS 14
    Reviewed-on: https://trac.frm2.tum.de/review/165
    Tested-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
    Tested-by: Georg Brandl <georg.brandl@frm2.tum.de>
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>

--- test/setups/system.py
+++ test/setups/system.py
@@ -40,7 +40,7 @@ devices = dict(
     Exp = device('nicos.experiment.Experiment',
         sample = 'sample',
         loglevel = 'info',
-        datapath = ['/data/testdata'],
+        datapath = ['./data/testdata'],
         scriptdir = '.',
         lowlevel = False,
     ),
