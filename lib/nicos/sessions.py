21e60ae08e526587274c4f8e927dafed4e097a15
Georg Brandl <georg.brandl@frm2.tum.de>
2011-07-06 11:21:23 +0200

    Fix the completer hide condition.

--- lib/nicos/sessions.py
+++ lib/nicos/sessions.py
@@ -826,13 +826,13 @@ class NicosCompleter(rlcompleter.Completer):
     """
 
     stopwords = set(['attached_devices', 'parameters', 'hardware_access',
-                     'temporary', 'log'])
+                     'temporary', 'log', 'valuetype'])
 
     def attr_matches(self, text):
         matches = rlcompleter.Completer.attr_matches(self, text)
         textlen = len(text)
-        return [m for m in matches if not m[textlen:].startswith(('_', 'do')) or
-                m[textlen:] in self.stopwords]
+        return [m for m in matches if not (m[textlen:].startswith(('_', 'do')) or
+                m[textlen:] in self.stopwords)]
 
 
 class NicosInteractiveStop(BaseException):
