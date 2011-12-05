ccd95f4c301857142c1161933f8eb95c13adfe9d
Georg Brandl <georg.brandl@frm2.tum.de>
2011-12-05 18:39:30 +0100

    Fix slits test.

--- test/test_slit.py
+++ test/test_slit.py
@@ -42,35 +42,35 @@ def test_slit():
     motor_top = session.getDevice('motor_top')
 
     slit.opmode = '4blades'
-    slit.doStart((1, 2, 3, 4))
+    slit.doStart([1, 2, 3, 4])
     slit.doWait()
     assert motor_right.doRead() == 1
     assert motor_left.doRead() == 2
     assert motor_bottom.doRead() == 3
     assert motor_top.doRead() == 4
-    assert slit.doRead() == (motor_right.doRead(),
+    assert slit.doRead() == [motor_right.doRead(),
                              motor_left.doRead(),
                              motor_bottom.doRead(),
-                             motor_top.doRead())
+                             motor_top.doRead()]
 
-    slit.doStart((8, 7, 6, 5))
+    slit.doStart([8, 7, 6, 5])
     slit.doWait()
-    assert slit.doRead() == (8, 7, 6, 5)
+    assert slit.doRead() == [8, 7, 6, 5]
 
-    assert raises(LimitError, slit.doStart, (8000, 7, 6, 5))
+    assert raises(LimitError, slit.doStart, [8000, 7, 6, 5])
 
-    slit.doStart((8, 4, 3, 5))
+    slit.doStart([8, 4, 3, 5])
     slit.doWait()
-    assert slit.doRead() == (8, 4, 3, 5)
+    assert slit.doRead() == [8, 4, 3, 5]
 
     slit.opmode = 'centered'
-    assert slit.doRead() == (-4, 2)
+    assert slit.doRead() == [-4, 2]
 
     slit.opmode = 'offcentered'
-    assert slit.doRead() == (6, 4, -4, 2)
+    assert slit.doRead() == [6, 4, -4, 2]
 
-    slit.doStart((4, 2, 3, 5))
+    slit.doStart([4, 2, 3, 5])
     slit.doWait()
-    assert slit.doRead() == (4, 2, 3, 5)
+    assert slit.doRead() == [4, 2, 3, 5]
     slit.opmode = '4blades'
-    assert slit.doRead() == (2.5, 5.5, -0.5, 4.5)
+    assert slit.doRead() == [2.5, 5.5, -0.5, 4.5]
