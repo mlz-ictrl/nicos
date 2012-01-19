a44e2832bc52342a0a7d3cf04739cb98201c3bd3
Georg Brandl <georg.brandl@frm2.tum.de>
2012-01-19 19:02:27 +0100

    Add Julabo and setup for it.

---
+++ custom/toftof/setups/biofurnace.py
@@ -0,0 +1,26 @@
+description = 'bio furnace aka Julabo '
+includes = ['system']
+
+devices = dict(
+    julabobus = device('nicos.toftof.julabo.HaakeRS232Driver',
+                       tacodevice = '//toftofsrv/toftof/rs232/ifhubermot1',
+                       maxtries = 5,
+                       lowlevel = False,
+                   ),
+
+    bio = device('nicos.toftof.julabo.Julabo',
+                 bus = 'julabobus',
+                 thermostat_type = 'JulaboF32HD',
+		 userlimits = (-40, 160),
+                 abslimits = (-50, 200),
+                 rampType = 1,
+                 rampRate = 0.1,
+                 tolerance = 0.2,
+                 unit = 'degC',
+                 fmtstr = '%g'),
+)
+
+startupcode = """
+Ts = bio
+T = bio
+"""
