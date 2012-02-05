f8484cf2c95cafd1ec76ac454f55d98ced95d2b6
Georg Brandl <georg.brandl@frm2.tum.de>
2012-02-05 18:23:46 +0100

    TOFTOF: monitor setup update

--- custom/toftof/setups/monitor.py
+++ custom/toftof/setups/monitor.py
@@ -4,57 +4,61 @@ group = 'special'
 _expcolumn = [
     ('Experiment', [
         [{'name': 'Proposal', 'key': 'exp/proposal', 'width': 7},
-         {'name': 'Title', 'key': 'exp/title', 'width': 20,
-          'istext': True, 'maxlen': 20},
+         {'name': 'Title', 'key': 'exp/title', 'width': 30,
+          'istext': True, 'maxlen': 40},
          {'name': 'Sample', 'key': 'sample/samplename', 'width': 30,
-          'istext': True},]]),
+          'istext': True},
+         {'name': 'Remark', 'key': 'exp/remark', 'width': 30,
+          'istext': True}]]),
 ]
 
 _reactorblock = ('Reactor', [
     [{'name': 'Power', 'dev': 'ReactorPower'}]
-], 'reactor')
+])
 
 _chopperblock = ('Chopper system', [
-    [{'name': 'Wavelength', 'dev': 'chWL'},
-     {'name': 'Nom. Speed', 'dev': 'chSpeed'},
-     {'name': 'Ratio', 'dev': 'chRatio'},
+    [{'name': 'Wavelength', 'dev': 'chWL', 'format': '%.1f'},
+     {'name': 'Nom. Speed', 'dev': 'chSpeed', 'format': '%.0f'},
+     {'name': 'Act. Speed', 'dev': 'ch', 'format': '%.0f'}],
+    [{'name': 'Ratio', 'dev': 'chRatio'},
      {'name': 'CRC', 'dev': 'chCRC'},
      {'name': 'Slit type', 'dev': 'chST'}],
-    [{'name': 'Disk 1', 'dev': 'chDS', 'item': 0},
-     {'name': 'Disk 2', 'dev': 'chDS', 'item': 1},
-     {'name': 'Disk 3', 'dev': 'chDS', 'item': 2},
-     {'name': 'Disk 4', 'dev': 'chDS', 'item': 3},
-     {'name': 'Disk 5', 'dev': 'chDS', 'item': 4},
-     {'name': 'Disk 6', 'dev': 'chDS', 'item': 5},
-     {'name': 'Disk 7', 'dev': 'chDS', 'item': 6}],
-], 'chopper')
+    '---',
+    [{'name': 'Disk 1', 'dev': 'chDS', 'item': 0, 'format': '%.1f'},
+     {'name': 'Disk 2', 'dev': 'chDS', 'item': 1, 'format': '%.1f'},
+     {'name': 'Disk 3', 'dev': 'chDS', 'item': 2, 'format': '%.1f'},
+     {'name': 'Disk 4', 'dev': 'chDS', 'item': 3, 'format': '%.1f'}],
+    [{'name': 'Disk 5', 'dev': 'chDS', 'item': 4, 'format': '%.1f'},
+     {'name': 'Disk 6', 'dev': 'chDS', 'item': 5, 'format': '%.1f'},
+     {'name': 'Disk 7', 'dev': 'chDS', 'item': 6, 'format': '%.1f'}],
+])
 
 _vacuumblock = ('Vacuum', [
-    [{'name': 'Gauge 1', 'dev': 'vac0'},
-     {'name': 'Gauge 2', 'dev': 'vac1'},
-     {'name': 'Gauge 3', 'dev': 'vac2'},
-     {'name': 'Gauge 4', 'dev': 'vac3'},
+    [{'name': 'Gauge 1', 'dev': 'vac0', 'format': '%.2g'},
+     {'name': 'Gauge 2', 'dev': 'vac1', 'format': '%.2g'},
+     {'name': 'Gauge 3', 'dev': 'vac2', 'format': '%.2g'},
+     {'name': 'Gauge 4', 'dev': 'vac3', 'format': '%.2g'},
      ]
-], 'vacuum')
+])
 
 _tableblock = ('Sample table', [
-    [{'dev': 'gx', 'dev': 'gy', 'dev': 'gz'}],
-    [{'dev': 'gcx', 'dev': 'gcy', 'dev': 'gphi'}],
-], 'table')
+    [{'dev': 'gx'}, {'dev': 'gy'}, {'dev': 'gz'}],
+    [{'dev': 'gcx'}, {'dev': 'gcy'}, {'dev': 'gphi'}],
+])
 
 _slitblock = ('Sample slit', [
-    [{'dev': 'slit', 'istext': True, 'width': 24}]
-], 'slit')
+    [{'dev': 'slit', 'istext': True, 'width': 24, 'name': 'Slit'}]
+])
 
 _measblock = ('Measurement', [
     [{'key': 'm/timechannels', 'name': 'Time channels'},
      {'name': 'Last file', 'key': 'm/lastfilenumber'}],
-    [{'key': 'm/laststats', 'item': 0, 'name': 'Time'},
-     {'key': 'm/laststats', 'item': 1, 'name': 'Monitor'},
-     {'key': 'm/laststats', 'item': 2, 'name': 'Counts'}],
-    [{'key': 'm/laststats', 'item': 0, 'name': 'Monitor rate'},
-     {'key': 'm/laststats', 'item': 0, 'name': 'Count rate'}],
-], 'measurement')
+    [{'key': 'm/laststats', 'item': 0, 'name': 'Time', 'format': '%.1f'},
+     {'key': 'm/laststats', 'item': 1, 'name': 'Monitor', 'format': '%d'},
+     {'key': 'm/laststats', 'item': 2, 'name': 'Counts', 'format': '%d'}],
+    [{'key': 'm/laststats', 'item': 0, 'name': 'Monitor rate', 'format': '%.1f'},
+     {'key': 'm/laststats', 'item': 0, 'name': 'Count rate', 'format': '%.1f'}],
+])
 
 _cryoblock = ('Cryo', [
     [{'key': 'cryo/setpoint', 'name': 'Setpoint'},
@@ -62,13 +66,7 @@ _cryoblock = ('Cryo', [
      {'dev': 'cryo_a', 'name': 'Sensor A'}],
     [{'key': 'cryo/p', 'name': 'P'}, {'key': 'cryo/i', 'name': 'I'},
      {'key': 'cryo/d', 'name': 'D'}, {'dev': 'cryo_p', 'name': 'pressure'}]
-], 'cryo')
-
-_otherblock = (
-    'Other devices',
-    [[{'dev': 'slit', 'width': 20, 'name': 'Slit'}],
-     [{'dev': 'sw', 'width': 4, 'name': 'Switcher'}]],
-    'misc')
+])
 
 _col1 = [
     _reactorblock,
@@ -79,6 +77,7 @@ _col1 = [
 _col2 = [
     _tableblock,
     _slitblock,
+    _measblock,
 ]
 
 _col3 = [
@@ -98,8 +97,8 @@ devices = dict(
                      loglevel = 'info',
                      server = 'cpci1.toftof.frm2:14869',
                      prefix = 'nicos/',
-                     font = 'Luxi Sans',
-                     valuefont = 'Consolas',
+                     font = 'Ubuntu',
+                     valuefont = 'DejaVu Sans Mono',
                      padding = 5,
                      layout = [[_expcolumn], [_col1, _col2, _col3]],
                      warnings = _warnings,
