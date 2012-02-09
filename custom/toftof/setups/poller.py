156906a8049572bd0d028e123c666c5db9387ad1
Jens Krueger <jens.krueger@frm2.tum.de>
2012-02-09 17:46:08 +0100

    Save currently used TOFTOF setups.
    Change-Id: Idf7df852a867df86ff113e4181b4fa13b7c4cc8d
    Reviewed-on: https://trac.frm2.tum.de/review/675
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/toftof/setups/poller.py
+++ custom/toftof/setups/poller.py
@@ -9,7 +9,7 @@ devices = dict(
     Poller = device('nicos.poller.Poller',
                     autosetup = False,  # important! do not poll everything
                     poll = ['chopper', 'power', 'vacuum', 'voltage'] +
-                           ['he3', 'htf', 'ls', 'biofurnace'],
+                           ['he3', 'htf', 'ls', 'biofurnace', 'cryo'],
                     alwayspoll = [],
                     blacklist = []),
 )
