715a64e89fba2a79826e96edc3b923241f49247a
Jens Krüger <jens.krueger@frm2.tum.de>
2012-03-02 16:10:00 +0100

    Change (REFSANS) power setup to MIRA setup.
    Change-Id: Ib84b68f0fe05da38c8cecbed11cde74efd7dd127
    Reviewed-on: https://trac.frm2.tum.de/review/762
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/refsans/setups/reactor.py
+++ custom/refsans/setups/reactor.py
@@ -4,6 +4,12 @@ nethost = '//tacodb.taco.frm2/'
 
 devices = dict(
         power = device('nicos.taco.io.AnalogInput',
+                       description = 'FRM II reactor power',
                        tacodevice = nethost + 'frm2/reactor/power',
+                       tacotimeout = 0.5,
+                       pollinterval = 600,
+                       maxage = 3600,
+                       fmtstr = '%.1f',
+                       unit = 'MW'
                       ),
 )
