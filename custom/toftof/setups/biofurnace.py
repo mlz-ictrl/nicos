0622e5388f1bd46abd9c55614b8d734d28ac1a80
Georg Brandl <georg.brandl@frm2.tum.de>
2012-03-20 18:05:07 +0100

    TOFTOF: update biofurnace setup

--- custom/toftof/setups/biofurnace.py
+++ custom/toftof/setups/biofurnace.py
@@ -2,14 +2,9 @@ description = 'Julabo bio furnace'
 includes = ['system']
 
 devices = dict(
-    julabobus = device('nicos.toftof.julabo.HaakeRS232Driver',
-                 tacodevice = '//toftofsrv/toftof/rs232/ifbiofurnace',
-                 maxtries = 5,
-                 lowlevel = False),
-
     bio = device('nicos.toftof.julabo.Julabo',
-                 bus = 'julabobus',
-		 intern_extern = 0,
+                 tacodevice = '//toftofsrv/toftof/rs232/ifbiofurnace',
+                 intern_extern = 0,
                  userlimits = (-40, 160),
                  abslimits = (-50, 200),
                  unit = 'degC',
