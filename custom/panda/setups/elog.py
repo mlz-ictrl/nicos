9449c24c04521c0c3d97ae926dd29cdafcfa1a94
Georg Brandl <georg.brandl@frm2.tum.de>
2012-03-29 11:04:17 +0200

    setups: remove empty cache definition for elog setups

--- custom/panda/setups/elog.py
+++ custom/panda/setups/elog.py
@@ -1,10 +1,6 @@
 description = 'setup for the electronic logbook'
 group = 'special'
 
-sysconfig = dict(
-    cache = None,
-)
-
 devices = dict(
     Logbook = device('nicos.elog.Logbook',
                      prefix = 'logbook/',
