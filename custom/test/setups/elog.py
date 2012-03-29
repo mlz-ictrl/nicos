1a59e1560d61c412ec08c48a8fb283c1785d54a0
Georg Brandl <georg.brandl@frm2.tum.de>
2012-03-29 11:05:31 +0200

    setups: add hint to configure cache for elog

--- custom/test/setups/elog.py
+++ custom/test/setups/elog.py
@@ -25,6 +25,9 @@
 description = 'setup for the electronic logbook'
 group = 'special'
 
+# If you want to use the electronic logbook, make sure the "system" setup has
+# also a cache configured.
+
 devices = dict(
     Logbook = device('nicos.elog.Logbook',
                      prefix = 'logbook/',
