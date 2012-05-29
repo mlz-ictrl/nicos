8859549a338436d748134a0a5cb98e51795a9f77
Georg Brandl <georg.brandl@frm2.tum.de>
2012-05-29 16:54:24 +0200

    device: fix error when using read(0.1) on a device where the entry has ttl=None

--- lib/nicos/core/device.py
+++ lib/nicos/core/device.py
@@ -779,7 +779,8 @@ class Readable(Device):
             val = None
         elif maxage is not None:
             time, ttl, val = self._cache.get_explicit(self, name)
-            if val is not None and time + min(maxage, ttl) < currenttime():
+            if val is not None and \
+                time + min(maxage, ttl or 3600) < currenttime():
                 val = None
         else:
             val = self._cache.get(self, name)
