0b772e2fa75f0d46089184b8fe9b6f405c8db831
Georg Brandl <georg.brandl@frm2.tum.de>
2012-07-12 11:37:47 +0200

    Fix typo.

--- test/test_scripts.py
+++ test/test_scripts.py
@@ -3,7 +3,7 @@
 # NICOS, the Networked Instrument Control System of the FRM-II
 # Copyright (c) 2009-2012 by the NICOS contributors (see AUTHORS)
 #
-x# This program is free software; you can redistribute it and/or modify it under
+# This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
 # Foundation; either version 2 of the License, or (at your option) any later
 # version.
