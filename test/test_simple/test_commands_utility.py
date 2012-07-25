aed496346f91f453bded53ee5cbb90c94859848e
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2012-07-25 08:23:16 +0200

    [TASK] Add range utility functions to the user commands
    Add range functions for lin- and log spaced values as user commands.
    The main user interface is the floatrange-function,
    that can generate list specified by step or number of points.
    
    These are kept in a separate utility class.
    
    Change-Id: Iea6b54a081da3b1eb4dc67abdec4fcbcccf08149
    Reviewed-on: https://trac.frm2.tum.de/review/1294
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

---
+++ test/test_simple/test_commands_utility.py
@@ -0,0 +1,139 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2012 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Georg Brandl <georg.brandl@frm2.tum.de>
+#
+# *****************************************************************************
+
+"""NICOS commands tests."""
+
+from __future__ import with_statement
+
+from nicos import session
+from nicos.core import UsageError
+
+from nicos.commands.utility import RangeListByStep, RangeListByCount, RangeListLog, \
+     floatrange
+from nicos.commands.output import printdebug, printinfo, printwarning, \
+     printerror, printexception
+
+from test.utils import ErrorLogged, raises, assertAlmostEqual
+
+import numpy
+
+def setup_module():
+    pass
+def teardown_module():
+    pass
+
+def test_list_commands():
+    # start/stop/step
+    l1 = RangeListByStep(1, 2, 0.5)
+    assert len(l1) == 3
+    assert l1[0] == 1.
+    assert l1[-1] == 2.
+    assert l1 == [1., 1.5, 2.]
+
+    l1 = RangeListByStep(1, 2, 0.7)
+    assert len(l1) == 3
+    assert l1[0] == 1.
+    assert l1[-1] == 2.
+    assert l1 == [1., 1.7, 2.]
+
+    # start/stop/step
+    l1 = RangeListByStep(-1, -2, -0.5)
+    assert len(l1) == 3
+    assert l1[0] == -1.
+    assert l1[-1] == -2.
+    assert l1 == [-1., -1.5, -2.]
+
+    # start/stop
+    l2 = RangeListByStep(1, 3)
+    assert len(l2) == 3
+    assert l2[0] == 1.
+    assert l2[-1] == 3.
+
+    # start/stop
+    l2 = RangeListByStep(3, 1)
+    assert len(l2) == 3
+    assert l2[0] == 3.
+    assert l2[-1] == 1.
+
+    #stop
+    l2 = RangeListByStep(3)
+    assert len(l2) == 4
+    assert l2[0] == 0.
+    assert l2[-1] == 3.
+
+    assert raises(UsageError, RangeListByStep, 1 , 2, -0.5)
+    assert raises(UsageError, RangeListByStep, 1 , 2, 0)
+
+    # start/stop/num
+    l2 = RangeListByCount(1, 2, 3)
+    assert len(l2) == 3
+    assert l2[0] == 1.
+    assert l2[-1] == 2.
+
+    # start/stop
+    l2a = RangeListByCount(3, 7)
+    assert len(l2a) == 2
+    assert l2a[0] == 3.
+    assert l2a[-1] == 7.
+
+    # stop
+    l2a = RangeListByCount(3)
+    assert len(l2a) == 2
+    assert l2a[0] == 0.
+    assert l2a[-1] == 3.
+
+    l4 = floatrange(1, 2, step=0.5)
+    assert len(l4) == 3
+    assert l4[0] == 1.
+    assert l4[-1] == 2.
+    assert type(l4[0]) == float
+    assert type(l4[-1]) == float
+
+    l4 = floatrange(1, 2, num=5)
+    assert len(l4) == 5
+    assert l4[0] == 1.
+    assert l4[-1] == 2.
+    assert type(l4[0]) == numpy.float64
+    assert type(l4[-1]) == numpy.float64
+
+    l3 = RangeListLog(1., 2., 3)
+    print l3
+    assert len (l3) == 3
+    assert l3[0] == 1.
+    assert l3[-1] == 2.
+    assert numpy.allclose(l3, [1.0, 1.4142135623730949, 2.0])
+
+
+    l3a = RangeListLog(0.1, 200., 3)
+    assert len (l3a) == 3
+    assertAlmostEqual(l3a[0], 0.1, 5)
+    assertAlmostEqual(l3a[-1], 200., 5)
+    assert numpy.allclose(l3a, [0.10000000000000001, 4.4721359549995787, 200.0])
+
+    l3b = RangeListLog(200, 2., 5)
+    assert len (l3b) == 5
+    assertAlmostEqual(l3b[0], 200., 5)
+    assertAlmostEqual(l3b[-1], 2., 5)
+
+    assert raises(UsageError, RangeListLog, -1, 2, 10)
