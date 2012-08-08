0102f8e0983aadedfedcecc4db23a13bbc711726
Georg Brandl <georg.brandl@frm2.tum.de>
2012-08-08 22:11:48 +0200

    spacegroup conversion script: minor fixes

--- etc/conv_pcwspgr.py
+++ etc/conv_pcwspgr.py
@@ -9,20 +9,24 @@ for line in f:
         sp = line.split(None, 8)
         spnum = int(sp[1])
         spvar = int(sp[2])
-        sp_hm = sp[8].replace(' ', '').strip()
+        hm = sp[8].strip()
+
+        sp_hm = hm.replace(' ', '')
         sp_cond = f.next()
         conditions = [int(sp_cond[2*i:2*i+2]) for i in range(14)]
         if sp_hm in sg_by_hm:
-            if sp_hm not in ('R3', 'R-3', 'R32', 'R3m', 'R3c', 'R-32m', 'R-32/m', 'R-32/c'):
+            if sp_hm not in ('R3', 'R-3', 'R32', 'R3m', 'R3c',
+                             'R-32m', 'R-32/m', 'R-32/c'):
                 assert sg_by_num[sg_by_hm[sp_hm]] == conditions, sp_hm
-        sg_by_hm[sp_hm] = spnum, spvar
+        else:
+            sg_by_hm[sp_hm] = spnum, spvar
         sg_by_num[spnum, spvar] = conditions
 
 print 'sg_by_num = {'
 for k in sorted(sg_by_num):
-    print ' %-9s: %s,' % (k, sg_by_num[k])
+    print '   %-9s: %s,' % (k, sg_by_num[k])
 print '}\n'
 print 'sg_by_hm = {'
 for k in sorted(sg_by_hm):
-    print ' %-9r: %s,' % (k, sg_by_hm[k])
+    print '   %-9r: %s,' % (k, sg_by_hm[k])
 print '}'
