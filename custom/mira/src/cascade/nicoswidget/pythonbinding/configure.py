d1e88b7fe9abead5ddf2150758592830a6b0060e
Georg Brandl <georg.brandl@frm2.tum.de>
2012-08-28 09:57:01 +0200

    MIRA: change qwt library name and include dir for Ubuntu conventions

--- custom/mira/src/cascade/nicoswidget/pythonbinding/configure.py
+++ custom/mira/src/cascade/nicoswidget/pythonbinding/configure.py
@@ -15,7 +15,7 @@ os.system(" ".join([config.sip_bin, "-t", "Qwt_5_2_0", "-c", ".", "-b", build_fi
 print "Generating makefile..."
 makefile = pyqtconfig.QtGuiModuleMakefile(configuration=config, build_file=build_file, debug=0)
 
-makefile.extra_include_dirs = ["/usr/include/qwt", "/usr/include/qwt5", ".."]
-makefile.extra_libs = ["cascadewidget", "Minuit2", "gomp", "QtNetwork", "qwt", "xml++-2.6"]
+makefile.extra_include_dirs = ["/usr/include/qwt", "/usr/include/qwt5", "/usr/include/qwt-qt4", ".."]
+makefile.extra_libs = ["cascadewidget", "Minuit2", "gomp", "QtNetwork", "qwt-qt4", "xml++-2.6"]
 makefile.extra_lib_dirs = [".."]
 makefile.generate()
