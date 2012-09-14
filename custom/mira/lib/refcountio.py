07e887dea64b27cc4869f1b3dc7c0ec9f1433a27
Georg Brandl <georg.brandl@frm2.tum.de>
2012-09-14 08:15:37 +0200

    MIRA: add "reference counting I/O" implementation

---
+++ custom/mira/lib/refcountio.py
@@ -0,0 +1,14 @@
+
+from nicos.taco import DigitalOutput
+
+class RefcountDigitalOutput(DigitalOutput):
+    def doInit(self, mode):
+        self._counter = 0
+        #DigitalOutput.doInit(self, mode)
+
+    def doStart(self, target):
+        if target:
+            self._counter += 1
+        else:
+            self._counter -= 1
+        self._taco_guard(self._dev.write, self._counter >= 1)
