f7715f091649485e4660f5db92808dc20597e2a8
Georg Brandl <georg.brandl@frm2.tum.de>
2012-11-25 19:15:42 +0100

    count: remove count loop delay in simulation mode
    This needlessly made simulation of scans quite slow (about 40 steps
    per second).  Now they are more than 10 times faster.
    
    Change-Id: I6ba08c19607c94d0ae439a430a682d44b334bc8a
    Reviewed-on: https://trac.frm2.tum.de/review/1732
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>

--- lib/nicos/commands/measure.py
+++ lib/nicos/commands/measure.py
@@ -49,8 +49,9 @@ def _count(detlist, preset, result):
     for det in detlist:
         det.start(**preset)
     i = 0
-    delay = getattr(session.instrument, 'countloopdelay', 0.025)
-    sleep(0.02)
+    delay = (getattr(session.instrument, 'countloopdelay', 0.025)
+             if session.mode != 'simulation' else 0.0)
+    sleep(delay)
     session.beginActionScope('Counting')
     try:
         while True:
