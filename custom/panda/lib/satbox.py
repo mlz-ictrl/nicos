467409d647b1e34eb746923343136ce297eade46
Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
2013-01-17 10:40:18 +0100

    PANDA: tiny fixes for satbox
    Change-Id: I2f25097b202ccf4547eee0de2723ad0bcc5d6abf
    Reviewed-on: https://trac.frm2.tum.de/review/2046
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

--- custom/panda/lib/satbox.py
+++ custom/panda/lib/satbox.py
@@ -36,37 +36,45 @@ class SatBox(Moveable):
     }
 
     valuetype = int
-    _widths = [1, 2, 5, 10, 20]
+    _blades = [1, 2, 5, 10, 20]
 
     def doRead(self, maxage=0):
-        inx = self._adevs['bus'].ReadBitsOutput(0x1020, len(self._widths))
-        return sum([inx[i]*self._widths[i] for i in range(len(self._widths))])
-
+        inx = self._adevs['bus'].ReadBitsOutput(0x1020, len(self._blades))
+        width = sum( [inx[i] * blade for i, blade in enumerate(self._blades)])
         #~ # currently the input bits dont work, since the magnetic field of the monoburg switches them all on
-        #~ inx = self._adevs['bus'].ReadBitsInput(0x1000, 10)
+        #~ inx = self._adevs['bus'].ReadBitsInput(0x1000, 2*len(self._blades))
         #~ self.log.debug('position: %s' % inx)
         #~ width = 0
+        #~ for i, blade in enumerate( self._blades):
+            #~ if not inx[i*2]:
+                #~ if inx[i*2+1]:
+                    #~ width += blade
+                #~ else:
+                    #~ self.log.warning('%d mm blade in inconsistent state' % blade)
+        return width
+
     def doStatus(self, maxage=0):
-        return status.OK, ''
-        #~ in1 = self._adevs['bus'].ReadBitsInput(0x1000, 8)
-        #~ in2 = self._adevs['bus'].ReadBitsInput(0x1008, 2)
-        #~ inx = in1 + in2
-        #~ for i in range(5):
+        #~ # currently the input bits dont work, since the magnetic field of the monoburg switches them all on		
+        #~ inx = self._adevs['bus'].ReadBitsInput(0x1000, 2*len(self._blades))
+        #~ for i, blade in enumerate( self._blades):
             #~ if inx[i*2] and inx[i*2+1]:
-                #~ return status.BUSY, '%d mm blade moving' % self._widths[i]
-        #~ return status.OK, ''
+                #~ return status.BUSY, '%d mm blade moving' % blade
+        return status.OK, ''
 
     def doStart(self, rpos):
-        if rpos > sum(self._widths):
-            raise InvalidValueError(self, 'Value %d too big!, maximum is %d' % (rpos,sum(self._widths)))
-        which = [0] * len(self._widths)
+        if rpos > sum(self._blades):
+            raise InvalidValueError(self, 'Value %d too big!, maximum is %d' % (rpos,sum(self._blades)))
+        which = [0] * len(self._blades)
         pos = rpos
-        for i in range(len(self._widths)-1,-1,-1):
-            if pos >= self._widths[i]:
+        for i in xrange(len(self._blades)-1,-1,-1):
+            blade = self._blades[i]
+            if not blade:
+                continue        # skip disabled (0 or None) blades
+            if pos >= blade:
                 which[i] = 1
-                pos -= self._widths[i]
+                pos -= blade
         if pos != 0:
             self.log.warning('Value %d impossible, trying %d instead!'%(rpos,rpos+1))
             return self.doStart( rpos+1)
-        self.log.debug('setting blades: %s' % [which[i]*self._widths[i] for i in range(len(which))])
+        self.log.debug('setting blades: %s' % [ which[i] * blade for i,blade in enumerate(which)])
         self._adevs['bus'].WriteBitsOutput(0x1020, which)
