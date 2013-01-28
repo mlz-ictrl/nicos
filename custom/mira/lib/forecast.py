d0879e86428c872bab442026c91fa6b6d71bae66
Georg Brandl <georg.brandl@frm2.tum.de>
2013-01-28 19:44:43 +0100

    MIRA: add detector "forecast" device
    The device will give a forecast for all channels of the selected FRM counter card.
    Useful in the status monitor to display a forecast of the achieved final count.
    
    Change-Id: Id2cac669bac30f717375575c3b3bf1cf225f519f

---
+++ custom/mira/lib/forecast.py
@@ -0,0 +1,55 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2012 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Georg Brandl <georg.brandl@frm2.tum.de>
+#
+# *****************************************************************************
+
+"""Forecast final values of a TACO detector."""
+
+__version__ = "$Revision$"
+
+import time
+
+from nicos.core import status, Readable
+from nicos.devices.taco.detector import FRMDetector
+
+
+class Forecast(Readable):
+    attached_devices = {
+        'det': (FRMDetector, 'The detector to forecast values.'),
+    }
+
+    def doRead(self, maxage=0):
+        percent_complete = 0
+        for m in self._adevs['det']._masters:
+            v = m.read(maxage)[0]
+            p = m.preselection
+            percent_complete = max(percent_complete, v / p)
+        res = []
+        for c in self._adevs['det']._counters:
+            res.append(c.read(maxage)[0]/percent_complete)
+        return res
+
+    def doStatus(self, maxage=0):
+        return status.OK, ''
+
+    def valueInfo(self):
+        return self._adevs['det'].valueInfo()
