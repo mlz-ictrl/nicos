714c62107077d18f991ec397d3e038c12f4ecfc8
Georg Brandl <georg.brandl@frm2.tum.de>
2013-04-11 15:12:55 +0200

    devices: separate channel/detector interface from TACO implementation
    This way, the import of nicos.devices.generic.virtual does *not* import
    the nicos.devices.taco libraries and everything works without TACO clients
    installed if they are not used.
    
    Change-Id: I50c8655c70a4bc260959347ed678b5ae263faaa0
    Reviewed-on: https://trac.frm2.tum.de/review/2390
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <Jens Krüger <jens.krueger@frm2.tum.de>>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>

--- custom/demo/setups/detector.py
+++ custom/demo/setups/detector.py
@@ -14,12 +14,10 @@ devices = dict(
                       countrate = 1000,
                      ),
 
-    # use the showcase TAS counter that simulates Bragg peaks and phonons
-    ctr1     = device('devices.generic.virtual.VirtualTasCounter',
+    ctr1     = device('devices.generic.VirtualCounter',
                       lowlevel = True,
                       type = 'counter',
                       countrate = 5,
-                      tas = 'tas',
                      ),
 
     ctr2     = device('devices.generic.VirtualCounter',
@@ -28,7 +26,7 @@ devices = dict(
                       countrate = 1,
                      ),
 
-    det      = device('devices.taco.FRMDetector',
+    det      = device('devices.generic.MultiChannelDetector',
                       timer = 'timer',
                       monitors = ['mon1'],
                       counters = ['ctr1', 'ctr2'],
