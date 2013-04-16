6a53e58173c0f206a96d98a33bf6801fbb2cbfb1
Georg Brandl <georg.brandl@frm2.tum.de>
2013-04-16 21:29:49 +0200

    virtual tas detector: add an option to actual take time for counting
    Change-Id: I4df6382dff2fcfd745e74c5834f92572c0d8a91b
    Reviewed-on: https://trac.frm2.tum.de/review/2450
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <Jens Krüger <jens.krueger@frm2.tum.de>>
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>

--- lib/nicos/devices/tas/virtual.py
+++ lib/nicos/devices/tas/virtual.py
@@ -26,6 +26,7 @@
 
 __version__ = "$Revision$"
 
+from time import time as currenttime
 from numpy import random
 
 from nicos.core import Readable, Measurable, Param, Value
@@ -37,6 +38,8 @@ class VirtualTasDetector(Measurable):
     }
 
     parameters = {
+        'realtime':   Param('Whether to wait for the preset counting time',
+                            type=bool, default=False, settable=True),
         'background': Param('Instrumental background', unit='cts/min',
                             default=1.0, settable=True),
         'mcpoints':   Param('Number of Monte-Carlo points', type=int,
@@ -45,8 +48,9 @@ class VirtualTasDetector(Measurable):
 
     def doInit(self, mode):
         self._lastpreset = {'t': 1}
-        self._counting = False
         self._lastresult = [0, 0, 0]
+        self._counting_started = 0
+        self._pause_time = 0
 
     def presetInfo(self):
         return ['t', 'mon']
@@ -62,20 +66,31 @@ class VirtualTasDetector(Measurable):
     def doStart(self, **preset):
         if preset:
             self._lastpreset = preset
-        self._counting = True
+        self._counting_started = currenttime()
+
+    def doPause(self):
+        self._pause_time = currenttime()
+        return True
+
+    def doResume(self):
+        if self._pause_time:
+            self._counting_started += (currenttime() - self._pause_time)
+        return True
 
     def doStop(self):
-        pass
+        self._counting_started = 0
 
     def doIsCompleted(self):
+        if 't' in self._lastpreset and self.realtime:
+            return currenttime() - self._counting_started >= self._lastpreset['t']
         return True
 
     def doRead(self, maxage=0):
+        return self._lastresult
+
+    def doSave(self):
         from nicos.devices.tas.rescalc import resmat, calc_MC, demosqw
         from nicos.commands.tas import _resmat_args
-
-        if not self._counting:
-            return self._lastresult
         taspos = self._adevs['tas'].read(0)
         mat = resmat(*_resmat_args(taspos, {}))
         # monitor rate (assume constant flux distribution from source)
@@ -94,6 +109,5 @@ class VirtualTasDetector(Measurable):
         bg = random.poisson(int(self.background * time / 60))
         counts = int(calc_MC([taspos], [bg, time], demosqw, mat,
                              self.mcpoints)[0])
-        self._counting = False
+        self._counting_started = 0
         self._lastresult = [time, moni, counts]
-        return self._lastresult
