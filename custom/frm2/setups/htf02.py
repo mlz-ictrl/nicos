b9feb0db7b36b20b3a8d1eb3d7335f708d5168a3
Jens Krüger <jens.krueger@frm2.tum.de>
2013-04-18 13:25:46 +0200

    Add the setups for the FRM-II high temperature furnaces.
    Change-Id: I691eec37befcdf68758fb90ebe8630644cf0b922
    Reviewed-on: https://trac.frm2.tum.de/review/2467
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>
    Reviewed-by: Jens Krueger <Jens Krüger <jens.krueger@frm2.tum.de>>
    Tested-by: Jens Krueger <Jens Krüger <jens.krueger@frm2.tum.de>>

---
+++ custom/frm2/setups/htf02.py
@@ -0,0 +1,23 @@
+description = 'FRM-II high temperature furnace'
+
+group = 'optional'
+
+includes = ['alias_T']
+
+nethost = 'htf02'
+
+devices = {
+    'T_%s' % (nethost, ) : device('devices.taco.TemperatureController',
+                  tacodevice = '//%s/htf/eurotherm/control' % (nethost, ),
+                  abslimits = (0, 2000),
+                  ramp = 0.1,
+                  unit = 'C',
+                  fmtstr = '%.3f',
+                 ),
+}
+
+startupcode = """
+Ts.alias = T_%s
+T.alias = T_%s
+AddEnvironment(Ts, T)
+""" % (nethost, nethost, )
