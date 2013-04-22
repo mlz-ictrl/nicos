6af5d56e29ebccbd51c28d23018e62a041cc03f5
Jens Krüger <jens.krueger@frm2.tum.de>
2013-04-22 13:18:52 +0200

    TOFTOF: Change the module name for the pyrometer
    Change-Id: Ie468004656436b841b9d9fd8923841c8664c3139
    Reviewed-on: https://trac.frm2.tum.de/review/2495
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>

--- custom/toftof/setups/pyro.py
+++ custom/toftof/setups/pyro.py
@@ -7,7 +7,7 @@ includes = ['system', 'alias_T']
 nethost = 'toftofsrv.toftof.frm2'
 
 devices = dict(
-    pyro = device('devices.toftof.ImpacPyrometer',
+    pyro = device('toftof.impac.ImpacPyrometer',
                   tacodevice = '//%s/toftof/rs232/ifpyrometer' % (nethost, ),
                   unit = 'C',
                   fmtstr = '%.3f',
