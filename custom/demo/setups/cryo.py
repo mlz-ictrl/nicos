76fdc1fc749586655604127302d96a15bdd43f19
Georg Brandl <georg.brandl@frm2.tum.de>
2013-04-23 11:28:48 +0200

    custom: add T / Ts aliases
    Change-Id: I531f2058004642b7279f21ec241ae42e850151db

--- custom/demo/setups/cryo.py
+++ custom/demo/setups/cryo.py
@@ -1,13 +1,19 @@
 group = 'optional'
 description = 'virtual temperature device'
 
-includes = ['alias_t']
-
 devices = dict(
-    T        = device('devices.generic.VirtualTemperature',
+    T        = device('devices.generic.DeviceAlias'),
+    Ts       = device('devices.generic.DeviceAlias'),
+
+    T_demo   = device('devices.generic.VirtualTemperature',
                       abslimits = (2, 300),
                       warnlimits = (0, 300),
                       speed = 6,
                       unit = 'K',
                      ),
 )
+
+startupcode = '''
+T.alias = T_demo
+Ts.alias = T_demo
+'''
