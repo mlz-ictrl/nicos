bf61d567e33d50b2cd93c0abab9299d3da08c5bc
Alexander Lenz <alexander.lenz@frm2.tum.de>
2013-05-21 18:33:06 +0200

    Fix import of CanReference in TacoMotor.
    Change-Id: I955719a7d7b8a2309be20b12901e4f380fa9f1a1
    Reviewed-on: https://trac.frm2.tum.de/review/2727
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    Tested-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>

--- lib/nicos/devices/taco/motor.py
+++ lib/nicos/devices/taco/motor.py
@@ -28,9 +28,9 @@
 from Motor import Motor as TACOMotor
 import TACOStates
 
-from nicos.core import status, waitForStatus, Param, Override, CanReference, \
+from nicos.core import status, waitForStatus, Param, Override, \
      oneof
-from nicos.devices.abstract import Motor as BaseMotor
+from nicos.devices.abstract import CanReference, Motor as BaseMotor
 from nicos.devices.taco.core import TacoDevice
 
 
