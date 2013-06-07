d6d91ed7489ffccd0e92b9f15a087a7b1ba9b3d5
Enrico Faulhaber <efaulhab@frm2.tum.de>
2013-06-07 15:11:49 +0200

    PANDA: update setups+scanplot after install
    ported to master in preparation of coming fork
    
    Change-Id: I20574d76e87ee10a92d7b3acad9c74a62be4b0aa
    Reviewed-on: https://trac.frm2.tum.de/review/2832
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Tested-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

--- custom/panda/setups/special/daemon.py
+++ custom/panda/setups/special/daemon.py
@@ -1,12 +1,23 @@
 description = 'setup for the execution daemon'
-
 group = 'special'
 
+//import hashlib
+//hashlib.md5('plantextpassword').hexdigest() # gives the hashvalue to use
+
 devices = dict(
-    Auth   = device('services.daemon.auth.Authenticator'),
-    Daemon = device('services.daemon.NicosDaemon',
-                    server = 'localhost',
-                    loglevel = 'debug',
-                    authenticators = ['Auth'],
+    UserDBAuth = device('frm2.auth.Frm2Authenticator'),
+    Auth       = device('services.daemon.auth.ListAuthenticator',
+                         description = 'Authentication device',
+                         hashing = 'md5',
+                         # first entry is the user name, second the hashed password, third the user level
+                         passwd = [('guest', '', 'guest'),
+                                   ('user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user'),
+                                   ('admin', '21232f297a57a5a743894a0e4a801fc3', 'admin')],
                    ),
+    Daemon = device('services.daemon.NicosDaemon',
+                    description = 'Daemon, executing commands and scripts',
+                    server = 'panda4.panda.frm2',
+                    authenticators = ['UserDBAuth','Auth',],
+                    loglevel = 'debug',
+                    ),
 )
