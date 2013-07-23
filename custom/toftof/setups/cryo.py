3c5c786bd4b03b1e9688aeb3f58c09bdcdb4f9da
Jens Krüger <jens.krueger@frm2.tum.de>
2013-07-23 08:46:29 +0200

    TOFTOF: add missing descriptions
    Change-Id: If621315442426dac90a5e3f66992d6f9f0af4f17
    Reviewed-on: https://trac.frm2.tum.de/review/3105
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/toftof/setups/cryo.py
+++ custom/toftof/setups/cryo.py
@@ -8,6 +8,7 @@ nethost = 'toftofsrv.toftof.frm2'
 
 devices = dict(
     T_stick   = device('devices.taco.TemperatureController',
+                       description = 'Controller for the sample stick',
                        tacodevice = '//%s/toftof/ls2/control' % (nethost,),
                        abslimits = (0, 600),
                        unit = 'K',
@@ -17,6 +18,7 @@ devices = dict(
                       ),
 
     T_tube = device('devices.taco.TemperatureController',
+                    description = 'Controller for tube of the CCR',
                     tacodevice = '//%s/toftof/ls1/control' % (nethost,),
                     abslimits = (0, 600),
                     unit = 'K',
@@ -26,42 +28,51 @@ devices = dict(
                    ),
 
     T_A = device('devices.taco.TemperatureSensor',
+                 description = 'Temperature of the channel A',
                  tacodevice = '//%s/toftof/ls2/sensora' % (nethost,),
                  unit = 'K',
                  fmtstr = '%.3f',
                 ),
 
     T_B = device('devices.taco.TemperatureSensor',
+                 description = 'Temperature of the channel B',
                  tacodevice = '//%s/toftof/ls2/sensorb' % (nethost,),
                  unit = 'K',
                  fmtstr = '%.3f',
                 ),
 
     T_C = device('devices.taco.TemperatureSensor',
+                 description = 'Temperature of the channel C',
                  tacodevice = '//%s/toftof/ls1/sensora' % (nethost,),
                  unit = 'K',
                  fmtstr = '%.3f',
                 ),
 
     T_D = device('devices.taco.TemperatureSensor',
+                 description = 'Temperature of the channel D',
                  tacodevice = '//%s/toftof/ls1/sensorb' % (nethost,),
                  unit = 'K',
                  fmtstr = '%.3f',
                 ),
 
     compressor_switch = device('devices.taco.DigitalOutput',
-                          tacodevice = '//%s/toftof/ccr/compressor' % (nethost,),
-                         ),
+                               description = 'The switch for the compressor',
+                               tacodevice = '//%s/toftof/ccr/compressor' % \
+                                            (nethost, ),
+                              ),
 
     gas_switch = device('devices.taco.DigitalOutput',
+                        description = 'The gas valve of the CCR',
                         tacodevice = '//%s/toftof/ccr/gas' % (nethost,),
                        ),
 
     vacuum_switch = device('devices.taco.DigitalOutput',
+                           description = 'The vaccuum valve of the CCR',
                            tacodevice = '//%s/toftof/ccr/vacuum' % (nethost,),
                           ),
 
     _p2 = device('devices.taco.AnalogInput',
+                 description = 'Pressure P2 of the CCR',
                  tacodevice = '//%s/toftof/ccr/p2' % (nethost,),
                 ),
 )
--- custom/toftof/setups/cryo_ccr.py
+++ custom/toftof/setups/cryo_ccr.py
@@ -8,6 +8,7 @@ nethost = 'ccr17'
 
 devices = dict(
     cryo   = device('devices.taco.TemperatureController',
+                    description = 'The control device to the sample',
                     tacodevice = '//%s/ccr/ls336/control2' % (nethost, ),
                     userlimits = (0, 600),
                     abslimits = (0, 600),
@@ -20,6 +21,7 @@ devices = dict(
                    ),
 
     cryo_tube = device('devices.taco.TemperatureController',
+                       description = 'The control device of the tube',
                        tacodevice = '//%s/ccr/ls336/control1' % (nethost, ),
                        userlimits = (0, 400),
                        abslimits = (0, 400),
@@ -32,54 +34,76 @@ devices = dict(
                       ),
 
     cryo_a = device('devices.taco.TemperatureSensor',
+                    description = 'Temperature at the tube',
                     tacodevice = '//%s/ccr/ls336/sensora' % (nethost, ),
                     unit = 'K',
-                    fmtstr = '%g'),
+                    fmtstr = '%g',
+                   ),
 
     cryo_b = device('devices.taco.TemperatureSensor',
+                    description = 'Temperature at the tube',
                     tacodevice = '//%s/ccr/ls336/sensorb' % (nethost, ),
                     unit = 'K',
-                    fmtstr = '%g'),
+                    fmtstr = '%g',
+                   ),
 
     cryo_c = device('devices.taco.TemperatureSensor',
+                    description = 'Temperature at the sample stick',
                     tacodevice = '//%s/ccr/ls336/sensorc' % (nethost, ),
                     unit = 'K',
-                    fmtstr = '%g'),
+                    fmtstr = '%g',
+                   ),
 
     cryo_d = device('devices.taco.TemperatureSensor',
+                    description = 'Temperature at the sample stick',
                     tacodevice = '//%s/ccr/ls336/sensord' % (nethost, ),
                     unit = 'K',
-                    fmtstr = '%g'),
+                    fmtstr = '%g',
+                   ),
 
     cryo_machine = device('devices.taco.DigitalOutput',
-                    tacodevice = '//%s/ccr/plc/on' % (nethost, )),
+                          description = 'Switch for the compressor',
+                          tacodevice = '//%s/ccr/plc/on' % (nethost, ),
+                         ),
 
     cryo_gs = device('devices.taco.DigitalOutput',
-                    lowlevel = True,
-                    tacodevice = '//%s/ccr/plc/gas' % (nethost, )),
+                     lowlevel = True,
+                     tacodevice = '//%s/ccr/plc/gas' % (nethost, ),
+                    ),
 
     cryo_gr = device('devices.taco.DigitalInput',
-                    lowlevel = True,
-                    tacodevice = '//%s/ccr/plc/fbgas' % (nethost, )),
+                     lowlevel = True,
+                     tacodevice = '//%s/ccr/plc/fbgas' % (nethost, ),
+                    ),
 
     cryo_g = device('devices.vendor.frm2.CCRSwitch',
+                    description = 'Gas valve switch',
                     write = 'cryo_gs',
-                    feedback = 'cryo_gr',),
+                    feedback = 'cryo_gr',
+                   ),
 
     cryo_vs = device('devices.taco.DigitalOutput',
-                    lowlevel = True,
-                    tacodevice = '//%s/ccr/plc/vacuum' % (nethost, )),
+                     lowlevel = True,
+                     tacodevice = '//%s/ccr/plc/vacuum' % (nethost, ),
+                    ),
 
     cryo_vr= device('devices.taco.DigitalInput',
                     lowlevel = True,
-                    tacodevice = '//%s/ccr/plc/fbvacuum' % (nethost, )),
+                    tacodevice = '//%s/ccr/plc/fbvacuum' % (nethost, ),
+                   ),
 
     cryo_v = device('devices.vendor.frm2.CCRSwitch',
+                    description = 'Vacuum valve switch',
                     write = 'cryo_vs',
-                    feedback = 'cryo_vr',),
+                    feedback = 'cryo_vr',
+                   ),
 
     cryo_p = device('devices.taco.AnalogInput',
-                    tacodevice = '//%s/ccr/plc/p2' % (nethost, )),
+                    description = 'Pressure in the vacuum chamber',
+                    tacodevice = '//%s/ccr/plc/p2' % (nethost, ),
+                    fmtstr = '%.4g',
+                    unit = 'mbar',
+                   ),
 )
 
 startupcode = """
