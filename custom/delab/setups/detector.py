dc6a10ad82c67accae77377faacb7c180e41cfd9
Jens Krüger <jens.krueger@frm2.tum.de>
2013-10-02 10:38:26 +0200

    DEL: add the Acqiris detector device and setup
    The Acqiris detector device implements the access to the CARESS
    device server or the Acqiris hardware.
    
    BP:
     * use absolute imports
     * add .gitignore
     * make delab a package
    
    Signed-off-by: Jens Krüger <jens.krueger@frm2.tum.de>
    
    Change-Id: I59110851f308d9a3558be7e2010ceb1902107bf7
    Reviewed-on: https://trac.frm2.tum.de/review/3451
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/delab/setups/detector.py
@@ -0,0 +1,46 @@
+description = 'acqiris detector'
+
+group = 'lowlevel'
+
+includes = []
+
+acqiris_config = (
+    'TIMEBASE: Time_div: 100.0e-6  MaxSample: 10000 Delay: -500.0e-6',
+    'CHANNEL: 1  FullScale:  5.0  Offset: -2.0  Coupling: DC_50Ohm Bandwidth: No_Limit',
+    'CHANNEL: 2  FullScale:  5.0  Offset: -2.0  Coupling: DC_50Ohm Bandwidth: No_Limit',
+    'CHANNEL: 3  FullScale:  5.0  Offset: -2.0  Coupling: DC_50Ohm Bandwidth: No_Limit',
+    'CHANNEL: 4  FullScale:  5.0  Offset: -2.0  Coupling: DC_50Ohm Bandwidth: No_Limit',
+    'CHANNEL: 5  FullScale:  5.0  Offset: -2.0  Coupling: DC_50Ohm Bandwidth: No_Limit',
+    'CHANNEL: 6  FullScale:  0.2  Offset: -0.0  Coupling: DC_50Ohm Bandwidth: No_Limit',
+    'TRIGGER: Internal trigCoupling: DC slope: Positive level1: 0.042 level2: 0.0 trigger_class: EDGE',
+    'CHANNEL: 7  FullScale:  5.0  Offset: -2.0  Coupling: DC_50Ohm Bandwidth: No_Limit',
+    'CHANNEL: 8  FullScale:  5.0  Offset: -2.0  Coupling: DC_50Ohm Bandwidth: No_Limit',
+)
+
+devices = dict(
+    tim = device('delab.acqiris.Timer',
+                 description = 'The timer device',
+                 nameserver = 'deldaq50.del.frm2',
+                 counterfile = '/home/caress/acqiris/data/runid.txt',
+                 objname = 'acqirishzb',
+                 ismaster = False,
+                ),
+    det1 = device('delab.acqiris.Counter',
+                  description = 'The counter device',
+                  nameserver = 'deldaq50.del.frm2',
+                  counterfile = '/home/caress/acqiris/data/runid.txt',
+                  runnumber = 123456,
+                  objname = 'acqirishzb',
+                  ismaster = True,
+                  config = '\n'.join(acqiris_config),
+                 ),
+    det = device('devices.generic.MultiChannelDetector',
+                 description = 'Acqiris card',
+                 timer  = 'tim',
+                 monitors = [],
+                 counters = ['det1'],
+                 fmtstr = 'timer %s, cnt %s',
+                 maxage = 2,
+                 pollinterval = 0.5,
+                ),
+)
