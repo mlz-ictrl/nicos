ea62e765e75bbf5a12d1de1ce71eeb36fe73c87c
Alexander Lenz <alexander.lenz@frm2.tum.de>
2013-10-14 14:11:28 +0200

    Antares: PartialDI/DO for sps access added.
    Change-Id: I7536c167470a0799b2f5208c4a838f7e9ec41946
    fixes: #1089
    Reviewed-on: https://trac.frm2.tum.de/review/3545
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    Tested-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

---
+++ custom/antares/lib/partialdio.py
@@ -0,0 +1,75 @@
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2013 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Alexander Lenz <alexander.lenz@frm2.tum.de>
+#
+# *****************************************************************************
+
+from nicos.core import Param, intrange
+from nicos.devices.tango import DigitalInput, DigitalOutput
+
+
+class PartialDigitalInput(DigitalInput):
+    """Base class for a TANGO DigitalOutput with only a part of the full
+    bit width accessed.
+    """
+
+    parameters = {
+        'startbit': Param('Number of the first bit', type=int, default=0),
+        'bitwidth': Param('Number of bits', type=int, default=1),
+    }
+
+    def doInit(self, mode):
+        self._mask = (1 << self.bitwidth) - 1
+        self.valuetype = intrange(0, self._mask)
+
+    def doRead(self, maxage=0):
+        rawValue = DigitalInput.doRead(self, maxage)
+        return ((rawValue >> self.startbit) & self._mask)
+
+class PartialDigitalOutput(DigitalOutput):
+    """Base class for a TANGO DigitalOutput with only a part of the full
+    bit width accessed.
+    """
+
+    parameters = {
+        'startbit': Param('Number of the first bit', type=int, default=0),
+        'bitwidth': Param('Number of bits', type=int, default=1),
+    }
+
+    def doInit(self, mode):
+        self._mask = (1 << self.bitwidth) - 1
+        self.valuetype = intrange(0, self._mask)
+
+    def doRead(self, maxage=0):
+        value = DigitalOutput.doRead(self, maxage)
+        value = (value >> self.startbit) & self._mask
+        return value
+
+    def doStart(self, target):
+        curVal = DigitalOutput.doRead(self)
+        newVal = (curVal & ~(self._mask << self.startbit)) | \
+                   (target << self.startbit)
+        DigitalOutput.doStart(self, newVal)
+
+    def doIsAllowed(self, target):
+        if target < 0 or target > self._mask:
+            return False, '%d outside range [0, %d]' % (target, self._mask)
+        return True, ''
+
