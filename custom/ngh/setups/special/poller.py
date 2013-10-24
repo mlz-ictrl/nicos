3ffededa09759aa0172810e44b0787d72d6d0bbe
Alexander Lenz <alexander.lenz@frm2.tum.de>
2013-10-24 13:49:09 +0200

    [WIP] NGH: Adding experimental neutron guide hall custom.
    Change-Id: I7c0cee8801b74dde0ccb6884c564855b5df27408
    Reviewed-on: https://trac.frm2.tum.de/review/3754
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

---
+++ custom/ngh/setups/special/poller.py
@@ -0,0 +1,21 @@
+# This setup file configures the nicos poller service.
+
+description = 'setup for the poller'
+group = 'special'
+
+sysconfig = dict(
+    cache = 'localhost'
+)
+
+devices = dict(
+    Poller = device('services.poller.Poller',
+                    alwayspoll = ['memograph-kws123',
+                                  'memograph-maria',
+                                  'memograph-sans1',
+                                  'memograph-treff'],
+                    neverpoll = [],  # setups that should not be polled even if loaded
+                    blacklist = [],  # DEVICES that should never be polled
+                                     # (usually detectors or devices that have problems
+                                     # with concurrent access from processes)
+                   ),
+)
---
+++ custom/ngh/setups/special/watchdog.py
@@ -0,0 +1,49 @@
+# This setup file configures the nicos poller service.
+
+description = 'setup for the NICOS watchdog'
+group = 'special'
+
+# watchlist:
+# The entries in this list are dictionaries. Possible keys:
+#
+# 'setup' -- setup that must be loaded (default '' to mean all setups)
+# 'condition' -- condition for warning (a Python expression where cache keys
+#    can be used: t_value stands for t/value etc.
+# 'gracetime' -- time in sec allowed for the condition to be true without
+#    emitting a warning (default 5 sec)
+# 'message' -- warning message to display
+# 'priority' -- normal priorities are 1 or 2, where 2 is more severe (default 1)
+#     priority 0 does not emit warnings (useful together with 'pausecount'
+#     for conditions that should block counting but are not otherwise errors)
+# 'pausecount' -- if True, the count loop should be paused on the condition
+#     (default False)
+# 'action' -- code to execute if condition is true (default no code is executed)
+watchlist = [
+    dict(condition = 't_in_memograph_kws123_value > 3',
+         message = 'Temperatur zu hoch (T_in KWS-123)',
+         priority = 1,
+         gracetime = 5),
+]
+
+devices = dict(
+    # Configure source and copy addresses to an existing address.
+    email    = device('devices.notifiers.Mailer',
+                      sender = 'ictrl@frm2.tum.de',
+                      receivers = ['alexander.lenz@frm2.tum.de'],
+                      subject = 'NICOS Warning',
+                     ),
+
+    # Configure SMS receivers if wanted and registered with IT.
+    smser    = device('devices.notifiers.SMSer',
+                      server = 'triton.admin.frm2',
+                      receivers = [],
+                     ),
+
+    Watchdog = device('services.watchdog.Watchdog',
+                      cache = 'localhost:14869',
+                      notifiers_1 = ['email'],  # notifiers for prio 1
+                      notifiers_2 = ['email'],  # notifiers for prio 2
+                      mailreceiverkey = 'email/receivers',
+                      watch = watchlist,
+                     ),
+)
