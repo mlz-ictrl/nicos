b10f844cefd6bf025312966f9b99ef1f1539de3b
Georg Brandl <georg.brandl@frm2.tum.de>
2013-11-12 10:22:29 +0100

    FRM2: prepare setup for IRF furnace
    Change-Id: Ic79a0717bf1265e5e7842c1996884b00f01a5d22
    Reviewed-on: https://trac.frm2.tum.de/review/3868
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>
    Tested-by: Georg Brandl <georg.brandl@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/frm2/setups/irf01.py
@@ -0,0 +1,24 @@
+description = 'FRM-II infra-red furnace'
+
+group = 'optional'
+
+includes = ['alias_T']
+
+nethost = 'irf01'
+
+devices = {
+    'T_%s' % (nethost, ) : device('devices.taco.TemperatureController',
+                                  description = 'The sample temperature',
+                                  tacodevice = '//%s/irf/eurotherm/control' % \
+                                            (nethost, ),
+                                  abslimits = (0, 1200),
+                                  unit = 'C',
+                                  fmtstr = '%.3f',
+                                 ),
+}
+
+startupcode = """
+Ts.alias = T_%s
+T.alias = T_%s
+AddEnvironment(Ts, T)
+""" % (nethost, nethost, )
