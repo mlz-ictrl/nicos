3a9a8d2002ba1fd9d4af05c99fcc70281d12921d
Sans1 People <sans1@frm2.tum.de>
2013-11-28 13:29:01 +0100

    SANS1: support plotting temperatures for htf03
    tested on sans1.
    code by A. Wilhelm.
    
    Change-Id: Id395685da06a1c78d6ce9460bd69bfa2d6061cb8
    Reviewed-on: https://trac.frm2.tum.de/review/4037
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

---
+++ custom/sans1/setups/special/monitor-htf.py
@@ -0,0 +1,57 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2012 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Georg Brandl <georg.brandl@frm2.tum.de>
+#
+# *****************************************************************************
+
+description = 'setup for the status monitor'
+group = 'special'
+
+Row = Column = Block = BlockRow = lambda *args: args
+Field = lambda *args, **kwds: args or kwds
+
+_testblock = Block(
+    'HTF03',
+    [
+     BlockRow(Field(gui='/sans1control/lib/nicos/sans1/gui/htf03.ui')),
+    ],
+)
+
+_testcolumn = Column(_testblock)
+
+
+
+devices = dict(
+    Monitor = device('nicos.services.monitor.qt.Monitor',
+                     title = 'SANS-1 status monitor',
+#                     loglevel = 'debug',
+                     loglevel = 'info',
+                     cache = 'sans1ctrl.sans1.frm2',
+                     prefix = 'nicos/',
+                     font = 'Luxi Sans',
+                     valuefont = 'Consolas',
+                     fontsize = 12,
+                     padding = 3,
+                     layout = [
+                                 Row(_testcolumn),
+                               ],
+                    ),
+)
