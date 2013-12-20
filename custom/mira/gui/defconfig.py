2085d7c2243a54e90ddb56a4a9824acb57769470
Georg Brandl <georg.brandl@frm2.tum.de>
2013-12-20 15:14:29 +0100

    MIRA: change GUI layout to include the expinfo panel
    Change-Id: Iabe5d1278072bd95101213bdb1c0dbc0c4a5146d
    Reviewed-on: https://trac.frm2.tum.de/review/4183
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>

--- custom/mira/gui/defconfig.py
+++ custom/mira/gui/defconfig.py
@@ -2,6 +2,41 @@
 
 from nicos.clients.gui.config import vsplit, hsplit, panel, window, tool, tabbed
 
+main_window = vsplit(
+    hsplit(
+        panel('status.ScriptStatusPanel'),
+        tabbed(('Experiment info', panel('expinfo.ExpInfoPanel',
+                                         sample_panel='setup_panel.TasSamplePanel')),
+               ('Watch expressions', panel('watch.WatchPanel'))),
+    ),
+    panel('console.ConsolePanel'),
+)
+
+windows = [
+    # window('Setup', 'setup', True,
+    #        tabbed(('Experiment', panel('setup_panel.ExpPanel')),
+    #               ('Setups',     panel('setup_panel.SetupsPanel')),
+    #               ('Detectors/Environment', panel('setup_panel.DetEnvPanel')),
+    #        )),
+    window('Editor', 'editor', True,
+           panel('editor.EditorPanel',
+                 tools = [
+                     tool('Scan', 'nicos.clients.gui.tools.scan.ScanTool')
+                 ])),
+    window('Live data', 'live', True,
+           panel('nicos.mira.gui.live.LiveDataPanel')),
+    window('Scans', 'plotter', True,
+           panel('scans.ScansPanel')),
+    window('History', 'find', True,
+           panel('history.HistoryPanel')),
+    window('Devices', 'table', True,
+           panel('devices.DevicesPanel')),
+    window('Logbook', 'table', True,
+           panel('elog.ELogPanel')),
+    window('NICOS log files', 'table', True,
+           panel('logviewer.LogViewerPanel')),
+]
+
 maint_commands = [
     ('Restart NICOS poller',
      'ssh maint@mira1 sudo /etc/init.d/nicos-system restart poller'),
@@ -27,59 +62,29 @@ MIEZE_settings = [
     '280_420',
 ]
 
-config = ('Default', [
-    vsplit(
-        hsplit(
-            panel('status.ScriptStatusPanel'),
-            panel('watch.WatchPanel')),
-        panel('console.ConsolePanel'),
-        ),
-    window('Setup', 'setup', True,
-           tabbed(('Experiment', panel('setup_panel.ExpPanel')),
-                  ('Setups',     panel('setup_panel.SetupsPanel')),
-                  ('Detectors/Environment', panel('setup_panel.DetEnvPanel')),
-           )),
-    window('Editor', 'editor', True,
-           panel('editor.EditorPanel',
-                 tools = [
-                     tool('Scan', 'nicos.clients.gui.tools.scan.ScanTool')
-                 ])),
-    window('Live data', 'live', True,
-           panel('nicos.mira.gui.live.LiveDataPanel')),
-    window('Scans', 'plotter', True,
-           panel('scans.ScansPanel')),
-    window('History', 'find', True,
-           panel('history.HistoryPanel')),
-    window('Devices', 'table', True,
-           panel('devices.DevicesPanel')),
-    window('Logbook', 'table', True,
-           panel('elog.ELogPanel')),
-    window('NICOS log files', 'table', True,
-           panel('logviewer.LogViewerPanel')),
-    ], [
-        tool('Maintenance',
-             'nicos.clients.gui.tools.commands.CommandsTool',
-             commands=maint_commands),
-        tool('Calculator',
-             'nicos.clients.gui.tools.calculator.CalculatorTool',
-             mieze=MIEZE_settings),
-        tool('Neutron cross-sections',
-             'nicos.clients.gui.tools.website.WebsiteTool',
-             url='http://www.ncnr.nist.gov/resources/n-lengths/'),
-        tool('Neutron activation',
-             'nicos.clients.gui.tools.website.WebsiteTool',
-             url='http://www.frm2.tum.de/intranet/activation/'),
-        tool('Neutron calculations',
-             'nicos.clients.gui.tools.website.WebsiteTool',
-             url='http://www.frm2.tum.de/intranet/neutroncalc/'),
-        tool('MIRA Wiki',
-             'nicos.clients.gui.tools.website.WebsiteTool',
-             url='http://mira2.mira.frm2:8080/'),
-        tool('Phone database',
-             'nicos.clients.gui.tools.website.WebsiteTool',
-             url='http://www.frm2.tum.de/intern/funktionen/phonedb/index.html'),
-        tool('Report NICOS bug',
-             'nicos.clients.gui.tools.website.WebsiteTool',
-             url='http://trac.frm2.tum.de/redmine/projects/nicos/issues/new'),
-    ]
-)
+tools = [
+    tool('Maintenance',
+         'nicos.clients.gui.tools.commands.CommandsTool',
+         commands=maint_commands),
+    tool('Calculator',
+         'nicos.clients.gui.tools.calculator.CalculatorTool',
+         mieze=MIEZE_settings),
+    tool('Neutron cross-sections',
+         'nicos.clients.gui.tools.website.WebsiteTool',
+         url='http://www.ncnr.nist.gov/resources/n-lengths/'),
+    tool('Neutron activation',
+         'nicos.clients.gui.tools.website.WebsiteTool',
+         url='http://www.frm2.tum.de/intranet/activation/'),
+    tool('Neutron calculations',
+         'nicos.clients.gui.tools.website.WebsiteTool',
+         url='http://www.frm2.tum.de/intranet/neutroncalc/'),
+    tool('MIRA Wiki',
+         'nicos.clients.gui.tools.website.WebsiteTool',
+         url='http://mira2.mira.frm2:8080/'),
+    tool('Phone database',
+         'nicos.clients.gui.tools.website.WebsiteTool',
+         url='http://www.frm2.tum.de/intern/funktionen/phonedb/index.html'),
+    tool('Report NICOS bug',
+         'nicos.clients.gui.tools.website.WebsiteTool',
+         url='http://trac.frm2.tum.de/redmine/projects/nicos/issues/new'),
+]
