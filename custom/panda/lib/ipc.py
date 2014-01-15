c65c4a9f03e17eb7ca90d898bdd4d80c1db72e20
Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
2014-01-15 11:03:56 +0100

    PANDA: rework workaround for buggy IPC-cards
    to only be active for non-encoded axes as this made problems
    on the encoded ones. Also document the bugs a little.
    
    Should work as it is a mix of already working, or has been working before
    settings.
    
    Also inlcudes a so-far-untested maybe-workaround class.
    If in need this can easily be used be renaming at the instrument (PANDA).
    
    Change-Id: Ia07d187c20cea6abcf5a10e8bcbcedc2ba08d423
    Reviewed-on: https://trac.frm2.tum.de/review/4251
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

--- custom/panda/lib/ipc.py
+++ custom/panda/lib/ipc.py
@@ -25,10 +25,10 @@
 """IPC class to work around reset bug of triple cards from göttingen"""
 
 
-from nicos.core import usermethod
+from nicos.core import usermethod, status
 from nicos.devices.vendor.ipc import Motor as _Motor
 
-# create a workaround class
+# create a workaround class, not resetting
 class Motor(_Motor):
     def doReset(self):
         if self._hwtype == 'single':
@@ -41,3 +41,90 @@ class Motor(_Motor):
     @usermethod
     def reallyReset(self):
         _Motor.reset(self)
+
+
+
+#
+# second possible solution:
+#
+doc = """
+IPC Motor cards made by Göttingen have (at least) two major problems:
+a) triple cards reset all three channels upon a reset command.
+b) sometimes they drive the stepper motor with wrong stepping frequencies,
+   basically not moving it.
+
+a) poses a problem for NICOS, because issuing a reset command to one channel
+should not influence other channels. As we don't know which devices (if any)
+are connected to the other two channels, we try to store the parameters
+(which include the current position) into the internal eeprom before issuing
+a reset. This is a real problem, if not all channels are used for encoded axes!
+
+It is still inclear if the reset is needed at all, or what it really does.
+
+b) is difficult, as NICOS assumes that if Hardware (motordriver) says it moved
+by a certain number of steps, the corresponding motor actually did just that.
+If it doesn't, and the axis has an encoder, NICOS raises an internal Exception
+(Schleppfehler) or if the requested movement was small, a PositionError and
+then retries the positioning after calling a reset on the motor.
+Here a) and b) play together to make instrument scientists very unhappy.
+
+As far as we could figure out, once b) happens, the card appears to be 'locked'
+in that state, where it 'forgets' to use the accel-ramp for a movement and
+instead just ramps down, starting from twice the normal stepping frequency.
+No stepper motor can handle that, so no actual movement happens.
+So NICOS again gets an Exception, calls reset and retries until it finally
+gives up after maxtries tries.
+
+So far we found one reliably working sequence to go out of this 'locked state'
+by issuing a reset and then moving to a slightly different position.
+If one of those two steps is missing, it does NOT _reliably_ free the card from
+that state. (i.e. sometimes it still works.)
+
+The chance to enter that 'lock'state seems to depend on a combination of
+the accel, speed, microstep, position and target value.
+We suspect an internal overflow. Unfortunately, once that state is entered,
+changing only the target position does not seem to allow it to exit that state.
+
+We try to work around this by following the sequence of:
+- change current position a little bit
+  (This should avoid entering the 'locked' state again, but still maintain
+  (roughly) the same position. THIS IS NOT IDEAL! (anybody have a better idea?)
+- save all params to eeprom, so that the other channels of this card are not
+  reset to bogus values.
+- do a reset (which actually resets all channels!)
+
+    U N T E S T E D !
+
+Firmware-upgrade of the cards is impossible, as the developer is no longer
+available and we dont have access to the sourcecode....
+"""
+
+# create a workaround class
+# if using this, please rename the class!
+class MotorUntested(_Motor):
+    """UNTESTED!"""
+    def doReset(self):
+        if self._hwtype == 'triple':
+            self.log.warning('Resetting a triple Motor cards, please check '
+                             'the result carefully!')
+            bus = self._adevs['bus']
+            # make sure we are stopped.
+            if self.status(0)[0] != status.OK:  # busy or error
+                bus.send(self.addr, 33)  # stop
+                try:
+                    self.doWait()      # this might take a while, ignore errors
+                except Exception:
+                    pass
+            self._store()
+            _Motor.reset(self)
+            # move by one step (so far this always worked)
+            if self.doReadSteps() & 1L:
+                bus.send(self.addr, 35) # go in negative direction
+            else:
+                bus.send(self.addr, 34) # go in positive direction
+            # do ONE step (basically toggling the least-significant bit)
+            bus.send(self.addr, 46, 1, 6)
+            self.doWait()
+        else:
+            _Motor.reset(self)
+
