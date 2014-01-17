1e87cb2dffde59603f9d9a28422992798bccb5be
Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
2014-01-17 17:39:52 +0100

    PANDA: rewrite monitor setup and use new features
    tested and works.
    
    Change-Id: Ie4de60ebe93372b8d0edcad6b3a7460976c18527
    Reviewed-on: https://trac.frm2.tum.de/review/4285
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

--- custom/panda/setups/special/monitor.py
+++ custom/panda/setups/special/monitor.py
@@ -2,244 +2,353 @@ description = 'setup for the status monitor'
 
 group = 'special'
 
-expcolumn = [
-    ('Experiment', [
-        [{'key': 'exp/proposal', 'name': 'Proposal'},
-         {'key': 'exp/title', 'name': 'Title', 'istext': True, 'width': 70},
-         {'key': 'sample/samplename', 'name': 'Sample', 'istext': True, 'width': 30},],[
-         {'name': 'Current status', 'key': 'exp/action', 'width': 100,
-          'istext': True, 'default':'Idle' },
-         {'key': 'exp/lastscan', 'name': 'Last file'}]
-    ])]
+Row = Column = Block = BlockRow = lambda *args: args
+Field = lambda *args, **kwds: args or kwds
 
-filters = ('Primary Beam/Filters', [
-    ['Saph', 'Power', 'Shutter', 'ms1'],
-    [{'dev': 'befilter', 'name': 'Be'}, dict(dev='tbefilter', name= 'BeT'),
-     {'dev': 'beofilter', 'name': 'BeO'}, {'dev': 'pgfilter', 'name': 'PG'}],
-])
+expcolumn = Column(
+    Block('Experiment', [
+        BlockRow(
+            Field(key='exp/proposal', name='Proposal'),
+            Field(key='exp/title', name='Title', istext=True, width=70),
+            Field(key='sample/samplename', name='Sample', istext=True, width=30),
+        ),
+        BlockRow(
+            Field(key='exp/action', name='Current status', width=100,
+                  istext=True, default='Idle' ),
+            Field(key='exp/lastscan', name='Last file'),
+        ),
+    ], '!always!')
+)
 
-primary = ('Monochromator', [
-    [{'dev': 'mono', 'name': 'Mono'}, {'key': 'mono/focmode', 'name': 'Focus'},
-     {'dev': 'mth', 'name': 'mth (A1)'}, {'dev': 'mtt', 'name': 'mtt (A2)'}],
-])
+filters = Block('Primary Beam/Filters', [
+    BlockRow(
+        Field(dev='saph', name='Saphir'),
+        Field(dev='reactor', name='Power'),
+        Field(dev='shutter', name='Shutter'),
+        Field(dev='ms1', name='ms1'),
+    ),
+    BlockRow(
+        Field(dev='befilter', name='Be'),
+        Field(dev='tbefilter', name='BeT'),
+        Field(dev='beofilter', name='BeO'),
+        Field(dev='pgfilter', name='PG'),
+    ),
+], '!always!')
 
-sample = ('Sample stage', [
-    [{'dev':'stx','format':'%.1f'}, 'sty', dict(dev='stz',format='%.2f'), 'sat'],
-    [dict(dev='sgx',format='%.2f'), dict(dev='sgy',format='%.2f'), {'dev': 'sth', 'name': 'sth (A3)'},
-     {'dev': 'stt', 'name': 'stt (A4)'}],
-])
+primary = Block('Monochromator', [
+    BlockRow(
+        Field(dev='mono', name='Mono'),
+        Field(key='mono/focmode', name='Focus'),
+        Field(dev='mth', name='mth (A1)'),
+        Field(dev='mtt', name='mtt (A2)'),
+    ),
+], '!always!')
 
-analyzer = ('Analyzer', [
-    [{'dev': 'ana', 'name': 'Ana'}, {'key': 'ana/focmode', 'name': 'Focus'},
-     {'dev': 'ath', 'name': 'ath (A5)', 'unit': ''}, {'dev': 'att', 'name': 'att (A6)', 'unit': ''}],
-])
+sample = Block('Sample stage', [
+    BlockRow(
+        Field(dev='stx', format='%.1f'),
+        Field(dev='sty'),
+        Field(dev='stz',format='%.2f'),
+        Field(dev='sat'),
+    ),
+    BlockRow(
+        Field(dev='sgx',format='%.2f'),
+        Field(dev='sgy',format='%.2f'),
+        Field(dev='sth', name='sth (A3)'),
+        Field(dev='stt', name='stt (A4)'),
+    ),
+], '!always!')
 
-collimation = ('Collimation and Lengths', [
-    [dict(dev='ca1',default='None'), 'ca2', 'ca3', 'ca4'],
-    ['lsm','lms','lsa','lad'],
-#    [dict(dev='lsm', name='Src->Mono',width=8),
-#     dict(dev='lms', name='Mono->Samp',width=8),],
-#    [dict(dev='lsa', name='Samp->Ana',width=8),
-#     dict(dev='lad', name='Ana->Det',width=8)],
-])
+analyzer = Block('Analyzer', [
+    BlockRow(
+        Field(dev='ana', name='Ana'),
+        Field(key='ana/focmode', name='Focus'),
+        Field(dev='ath', name='ath (A5)', unit=''),
+        Field(dev='att', name='att (A6)', unit=''),
+    ),
+], '!always!')
 
-column1 = [filters, primary, sample, analyzer]
+collimation = Block('Collimation and Lengths', [
+    BlockRow(
+        Field(dev='ca1',default='None'),
+        Field(dev='ca2',default='None'),
+        Field(dev='ca3',default='None'),
+        Field(dev='ca4',default='None'),
+    ),
+    BlockRow(
+        Field(dev='lsm'),
+        Field(dev='lms'),
+        Field(dev='lsa'),
+        Field(dev='lad'),
+    ),
+], '!always!')
 
-detector = ('Detector', [
-    ['timer', {'dev':'mon1', 'format':'%d'}, {'dev':'mon2', 'format':'%d'}],
-    [{'dev':'det1', 'format': '%d'}, {'dev':'det2', 'format': '%d'}],
-])
+column1 = Column(filters, primary, sample, analyzer)
 
-detector_small = ('Detector', [
-    ['timer', {'dev':'mon1', 'format':'%d'}, {'dev':'mon2', 'format':'%d'},
-    {'dev':'det1', 'format': '%d'}, {'dev':'det2', 'format': '%d'}],
-])
+
+detector = Block('Detector', [
+    BlockRow(
+        Field(dev='timer'),
+        Field(dev='mon1', format='%d'),
+        Field(dev='mon2', format='%d'),
+    ),
+    BlockRow(
+        Field(dev='det1', format='%d'),
+        Field(dev='det2', format='%d'),
+    ),
+], '!always!')
+
+detector_small = Block('Detector', [
+    BlockRow(
+        Field(dev='timer'),
+        Field(dev='mon1', format='%d'),
+        Field(dev='mon2', format='%d'),
+        Field(dev='det1', format='%d'),
+        Field(dev='det2', format='%d'),
+    ),
+], '!always!')
 
 # for setup lakeshore
-lakeshore = ('LakeShore', [
-    ['T_ls340', 'T_ls340_A','T_ls340_B'],
-    [{'dev': 't_ls340/setpoint', 'name': 'Setpoint'}, {'dev': 't_ls340/p', 'name': 'P', 'width': 5},
-     {'dev': 't_ls340/i', 'name': 'I', 'width': 5}, {'dev': 't_ls340/d', 'name': 'D', 'width': 5}],
+lakeshore = Block('LakeShore', [
+    BlockRow(
+        Field(dev='t_ls340', name='Regulation'),
+        Field(dev='t_ls340_a', name='Sensor A'),
+        Field(dev='t_ls340_b', name='Sensor B'),
+    ),
+    BlockRow(
+        Field(key='t_ls340/setpoint', name='Setpoint'),
+        Field(key='t_ls340/p', name='P', width=5),
+        Field(key='t_ls340/i', name='I', width=5),
+        Field(key='t_ls340/d', name='D', width=5),
+    ),
 ],'lakeshore')
 
-# for setup cryo1
-cryo1 = ('Cryo1:3He-insert', [
-    [   dict( key='t_cryo1/value', name='Regulation', max=38),
-        dict( key='t_cryo1_a/value', name='Sensor A', max=38),
-        dict( key='t_cryo1_b/value', name='Sensor B',max=7),
-    ],[
-        dict( key='t_cryo1/setpoint', name='Setpoint'),
-        dict( key='t_cryo1/p', name='P', width=7),
-        dict( key='t_cryo1/i', name='I', width=7),
-        dict( key='t_cryo1/d', name='D', width=7),
-    ],
-],'cryo1')
-
-cryo1supp = ('Cryo1-misc',[
-    [   dict( key='cryo1_p1/value', name='Pump (mbar)', width=10),
-        dict( key='cryo1_p4/value', name='Cond. (bar)', width=10),
-    ],[
-        dict( key='cryo1_p5/value', name='Dump (bar)', width=10),
-        dict( key='cryo1_p6/value', name='IVC (mbar)', width=10),
-    ],[
-        dict( key='cryo1_flow/value', name='Flow', width=10),
-    ]
-],'cryo1')
-
-# for setup cryo3
-cryo3 = ('Cryo3:dilution-insert', [
-    [   dict( key='t_cryo3/value', name='Regulation', max=38),
-        dict( key='t_cryo3_a/value', name='Sensor A', max=38),
-        dict( key='t_cryo3_b/value', name='Sensor B',max=7),
-    ],[
-        dict( key='t_cryo3/setpoint', name='Setpoint'),
-        dict( key='t_cryo3/p', name='P', width=7),
-        dict( key='t_cryo3/i', name='I', width=7),
-        dict( key='t_cryo3/d', name='D', width=7),
-    ],
-],'cryo3')
-
-cryo3supp = ('Cryo3-misc',[
-    [   dict( key='cryo3_p1/value', name='Pump (mbar)', width=10),
-        dict( key='cryo3_p4/value', name='Cond. (bar)', width=10),
-    ],[
-        dict( key='cryo3_p5/value', name='Dump (bar)', width=10),
-        dict( key='cryo3_p6/value', name='IVC (mbar)', width=10),
-    ],[
-        dict( key='cryo3_flow/value', name='Flow', width=10),
-    ]
-],'cryo3')
-
-# for setup cryo4
-cryo4 = ('Cryo4:3He-insert', [
-    [   dict( key='t_cryo4/value', name='Regulation'),
-        dict( key='t_cryo4_a/value', name='Sensor A'),
-        dict( key='t_cryo4_b/value', name='Sensor B'),
-    ],
-],'cryo4')
-
-cryo4supp = ('Cryo4-misc',[
-    [   dict( key='cryo4_p1/value', name='Pump (mbar)', width=10),
-        dict( key='cryo4_p4/value', name='Cond. (bar)', width=10),
-    ],[
-        dict( key='cryo4_p5/value', name='Dump (bar)', width=10),
-        dict( key='cryo4_p6/value', name='IVC (mbar)', width=10),
-    ],[
-        dict( key='cryo4_flow/value', name='Flow', width=10),
-    ]
-],'cryo4')
-
-# for setup cryo5
-cryo5 = ('Cryo5:3He-insert', [
-    [   dict( key='t_cryo5/value', name='Regulation'),
-        dict( key='t_cryo5_a/value', name='Sensor A'),
-        dict( key='t_cryo5_b/value', name='Sensor B'),
-    ],
-],'cryo5')
-
-cryo5supp = ('Cryo5-misc',[
-    [   dict( key='cryo5_p1/value', name='Pump (mbar)', width=10),
-        dict( key='cryo5_p4/value', name='Cond. (bar)', width=10),
-    ],[
-        dict( key='cryo5_p5/value', name='Dump (bar)', width=10),
-        dict( key='cryo5_p6/value', name='IVC (mbar)', width=10),
-    ],[
-        dict( key='cryo5_flow/value', name='Flow', width=10),
-    ]
-],'cryo5')
+lakeshoreplot = Block('LakeShore', [
+    BlockRow(
+        Field(widget='nicos.guisupport.plots.TrendPlot',
+              width=25, height=25, plotinterval=300,
+              devices=['t_ls340/setpoint', 't_ls340_a', 't_ls340_b'],
+              names=['Setpoint', 'A', 'B'],
+        ),
+    ),
+], 'lakeshore')
 
 
-# for setup ccr11
-ccr11 = ('CCR11-Pulse tube', [
-    [dict(key='t_ccr11_a/value',name='Cooling'),
-    dict(key='t_ccr11/value',name='Regulation'),
-    dict(key='t_ccr11_b/value',name='Sample'),],
-    [{'key': 't_ccr11/setpoint', 'name': 'Setpoint'}, {'key': 't_ccr11/p', 'name': 'P', 'width': 7},
-     {'key': 't_ccr11/i', 'name': 'I', 'width': 7}, {'key': 't_ccr11/d', 'name': 'D', 'width': 6, }],
-],'ccr11')
+# generic Cryo-stuff
+cryos = []
+cryosupps = []
+cryoplots = []
+cryonames = ['3He-insert', 'Dilution-insert', 'Dilution-insert', '3He-insert',
+             '3He-insert'] # correct?
+for i in range(1, 5 + 1):
+    cryos.append(
+        Block('Cryo%d:%s' % (i, cryonames[i-1]), [
+            BlockRow(
+                Field(dev='t_cryo%d' % i, name='Regulation', max=38),
+                Field(dev='t_cryo%d_a' % i, name='Sensor A', max=38),
+                Field(dev='t_cryo%d_b' % i, name='Sensor B',max=7),
+            ),
+            BlockRow(
+                Field(key='t_cryo%d/setpoint' % i, name='Setpoint'),
+                Field(key='t_cryo%d/p' % i, name='P', width=7),
+                Field(key='t_cryo%d/i' % i, name='I', width=7),
+                Field(key='t_cryo%d/d' % i, name='D', width=7),
+            ),
+        ],'cryo%d' % i)
+    )
+    cryosupps.append(
+        Block('Cryo%d-misc' % i,[
+            BlockRow(
+                Field(dev='cryo%d_p1' % i, name='Pump', width=10),
+                Field(dev='cryo%d_p4' % i, name='Cond.', width=10),
+            ),
+            BlockRow(
+                Field(dev='cryo%d_p5' % i, name='Dump', width=10),
+                Field(dev='cryo%d_p6' % i, name='IVC', width=10),
+            ),
+            BlockRow(
+                Field(key='cryo%d_flow' % i, name='Flow', width=10),
+            ),
+        ],'cryo%d' % i)
+    )
+    cryoplots.append(
+        Block('cryo%d' % i, [
+            BlockRow(
+                Field(widget='nicos.guisupport.plots.TrendPlot',
+                      plotinterval=300, width=25, height=25,
+                      devices=['t_cryo%d/setpoint' % i, 't_cryo%d' % i],
+                      names=['Setpoint', 'Regulation'],
+                ),
+            ),
+        ], 'cryo%d' % i)
+    )
 
-ccr11supp = ( 'CCR11', [
-    ['T_ccr11_A','T_ccr11_B'],
-    ['T_ccr11_C','T_ccr11_D'],
-    ['ccr11_p1','ccr11_p2'],
-], 'ccr11')
+
+# generic CCR-stuff
+ccrs = []
+ccrsupps = []
+ccrplots = []
+for i in range(10, 22 + 1):
+    ccrs.append(
+        Block('CCR%d-Pulse tube' % i, [
+            BlockRow(
+                Field(dev='t_ccr%d_c' % i, name='Coldhead'),
+                Field(dev='t_ccr%d_d' % i, name='Regulation'),
+                Field(dev='t_ccr%d_b' % i, name='Sample'),
+            ),
+            BlockRow(
+                Field(key='t_ccr%d/setpoint' % i, name='Setpoint'),
+                Field(key='t_ccr%d/p' % i, name='P', width=7),
+                Field(key='t_ccr%d/i' % i, name='I', width=7),
+                Field(key='t_ccr%d/d' % i, name='D', width=6),
+            ),
+        ],['ccr%d' % i, '!cryo*'])
+    )
+    ccrsupps.append(
+        Block('CCR%d' % i, [
+            BlockRow(
+                Field(dev='T_ccr%d_A' % i, name='A'),
+                Field(dev='T_ccr%d_B' % i, name='B'),
+            ),
+            BlockRow(
+                Field(dev='T_ccr%d_C' % i, name='C'),
+                Field(dev='T_ccr%d_D' % i, name='D'),
+            ),
+            BlockRow(
+                Field(dev='ccr%d_p1' % i, name='P1'),
+                Field(dev='ccr%d_p2' % i, name='P2'),
+            ),
+            BlockRow(
+                Field(key='t_ccr%d/setpoint' % i, name='SetP.', width=6),
+                Field(key='t_ccr%d/p' % i, name='P', width=4),
+                Field(key='t_ccr%d/i' % i, name='I', width=4),
+                Field(key='t_ccr%d/d' % i, name='D', width=3),
+            ),
+        ], ['ccr%d' % i])
+    )
+    ccrplots.append(
+        Block('CCR%d' % i, [
+            BlockRow(
+                Field(widget='nicos.guisupport.plots.TrendPlot',
+                      plotinterval=300, width=25, height=25,
+                      devices=['t_ccr%d/setpoint' % i, 't_ccr%d_c' % i,
+                               't_ccr%d_d' % i, 't_ccr%d_b' % i],
+                      names=['Setpoint', 'Coldhead', 'Regulation', 'Sample'],
+                ),
+            ),
+        ], 'ccr%d' % i)
+    )
 
 # for setup magnet frm2-setup
-magnet75 = ('7T Magnet', [
-    [ 'B_m7T5', dict(key='b_m7t5/target', name='Target', fmtstr='%.2f'),
-    ],
+magnet75 = Block('7T Magnet', [
+    BlockRow(
+        Field(dev='B_m7T5'),
+        Field(key='b_m7t5/target', name='Target', fmtstr='%.2f'),
+    ),
 ],'magnet75')
 
-magnet75supp = ('Magnet', [
-    [dict(dev='sth_B7T5_Taco_motor',name='motor'),
-    dict(dev='sth_B7T5_Taco_coder',name='coder')],
-    [dict(dev='m7T5_T1'),
-    dict(dev='m7T5_T2')],
-    [dict(dev='m7T5_T3'),
-    dict(dev='m7T5_T4')],
-    [dict(dev='m7T5_T5'),
-    dict(dev='m7T5_T6')],
-    [dict(dev='m7T5_T7'),
-    dict(dev='m7T5_T8')],
+magnet75supp = Block('Magnet', [
+    BlockRow(
+        Field(dev='sth_B7T5_Taco_motor',name='motor'),
+        Field(dev='sth_B7T5_Taco_coder',name='coder'),
+    ),
+    BlockRow(
+        Field(dev='m7T5_T1'),
+        Field(dev='m7T5_T2'),
+    ),
+    BlockRow(
+        Field(dev='m7T5_T3'),
+        Field(dev='m7T5_T4'),
+    ),
+    BlockRow(
+        Field(dev='m7T5_T5'),
+        Field(dev='m7T5_T6'),
+    ),
+    BlockRow(
+        Field(dev='m7T5_T7'),
+        Field(dev='m7T5_T8'),
+    ),
 ],'magnet75')
 
 # for setup magnet PANDA-setup
-magnet7t5 = ('7T Magnet', [
-    [ 'B_m7T5', dict(key='b_m7t5/target', name='Target', fmtstr='%.2f'),
-    ],
+magnet7t5 = Block('7T Magnet', [
+    BlockRow(
+        Field(dev='B_m7T5'),
+        Field(key='b_m7t5/target', name='Target', fmtstr='%.2f'),
+    ),
 ],'7T5')
 
-magnet7t5supp = ('Magnet', [
-    [dict(dev='sth_B7T5_Taco_motor',name='motor'),
-    dict(dev='sth_B7T5_Taco_coder',name='coder')],
-    [dict(dev='m7T5_T1',max=4.3),       # Maximum temeratures for field operation above  80A (6.6T) taken from the manual
-    dict(dev='m7T5_T2',max=4.3)],
-    [dict(dev='m7T5_T3',max=5.1),
-    dict(dev='m7T5_T4',max=4.7)],
-    [dict(dev='m7T5_T5'),
-    dict(dev='m7T5_T6')],
-    [dict(dev='m7T5_T7'),
-    dict(dev='m7T5_T8',max=4.3)],
+magnet7t5supp = Block('Magnet', [
+    BlockRow(
+        Field(dev='sth_B7T5_Taco_motor',name='motor'),
+        Field(dev='sth_B7T5_Taco_coder',name='coder'),
+    ),
+    # Maximum temeratures for field operation above 80A (6.6T) taken from the manual
+    BlockRow(
+        Field(dev='m7T5_T1',max=4.3),
+        Field(dev='m7T5_T2',max=4.3),
+    ),
+    BlockRow(
+        Field(dev='m7T5_T3',max=5.1),
+        Field(dev='m7T5_T4',max=4.7),
+    ),
+    BlockRow(
+        Field(dev='m7T5_T5'),
+        Field(dev='m7T5_T6'),
+    ),
+    BlockRow(
+        Field(dev='m7T5_T7'),
+        Field(dev='m7T5_T8',max=4.3),
+    ),
 ],'7T5')
 
-vti = ('VTI', [
-    ['sTs','vti',dict(key='vti/setpoint',name='Setpoint',min=1,max=200),
-        dict(key='vti/heater',name='Heater (%)'),],
-    ['NV','LHe','LN2'],
-    ],'15T')
+vti = Block('VTI', [
+    BlockRow(
+        Field(dev='sTs'),
+        Field(dev='vti'),
+        Field(key='vti/setpoint',name='Setpoint',min=1,max=200),
+        Field(key='vti/heater',name='Heater (%)'),
+    ),
+    BlockRow(
+        Field(dev='NV'),
+        Field(dev='LHe'),
+        Field(dev='LN2'),
+    ),
+],['15T', 'variox'])
 
-variox = ('VTI', [
-    ['sTs','vti',dict(key='vti/setpoint',name='Setpoint',min=1,max=200),
-        dict(key='vti/heater',name='Heater (%)'),],
-    ['NV','LHe','LN2'],
-    ],'variox')
-
-magnet14t5 = ('14.5T Magnet', [
-    [   dict(key='b15t/value',name='b15t',unit='T'),
-        dict(key='b15t/target',name='Target',unit='T'),
-        dict(key='b15t/ramp',name='Ramp', unit='T/min')],
+magnet14t5 = Block('14.5T Magnet', [
+    BlockRow(
+        Field(dev='b15t', name='b15t', unit='T'),
+        Field(key='b15t/target', name='Target', unit='T'),
+        Field(key='b15t/ramp', name='Ramp', unit='T/min'),
+    ),
 ],'15T')
 
-kelvinox = ('Kelvinox', [
-    [   'mc' ],
-    [ dict(key='mc/setpoint',name='Setpoint',unit='K')],
-    [ 'sorb' ],
-    [ 'onekpot'],
-    [   'igh_p1' ],
-    [ 'igh_g1'],
-    ['igh_g2'],
+kelvinox = Block('Kelvinox', [
+    BlockRow(Field(dev='mc')),
+    BlockRow(Field(key='mc/setpoint',name='Setpoint',unit='K')),
+    BlockRow(Field(dev='sorb')),
+    BlockRow(Field(dev='onekpot')),
+    BlockRow(Field(dev='igh_p1')),
+    BlockRow(Field(dev='igh_g1')),
+    BlockRow(Field(dev='igh_g2')),
 ],'kelvinox')
 
-foki = ('Foki', [
-  ['mfh','mfv'],
-  ['afh'],
-])
+foki = Block('Foki', [
+    BlockRow(
+        Field(dev='mfh'),
+        Field(dev='mfv'),
+    ),
+    BlockRow(Field(dev='afh')),
+], '!always!')
 
-column2 = [collimation, detector, cryo1, cryo3, cryo4, cryo5, lakeshore, magnet75, magnet7t5, magnet14t5, vti, variox ]
+column2 = Column(collimation, detector) + Column(*cryos) + Column(*ccrs) + \
+          Column(lakeshore, magnet75, magnet7t5, magnet14t5, vti)
 
-column3 = [cryo1supp, cryo3supp, cryo4supp, cryo5supp, ccr11]
+column3 = Column(magnet75supp, magnet7t5supp, kelvinox, foki) + \
+          Column(*cryosupps) + Column(*ccrsupps)
 
-column4 = [magnet75supp, magnet7t5supp, kelvinox, foki]
+column4 = Column(lakeshoreplot) + Column(*cryoplots) + Column(*ccrplots)
 
 devices = dict(
     Monitor = device('services.monitor.qt.Monitor',
@@ -250,6 +359,6 @@ devices = dict(
                      font = 'Luxi Sans',
                      fontsize = 17,
                      valuefont = 'Luxi Sans',
-                     layout = [[expcolumn], [column1, column2, column3, column4]],
+                     layout = [Row(expcolumn), Row(column1, column2, column3, column4)],
                      )
 )
