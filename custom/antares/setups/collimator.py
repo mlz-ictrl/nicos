0d93a45b8759bc8bd3ac3be26cdaf397d0578168
Alexander Lenz <alexander.lenz@frm2.tum.de>
2014-02-11 16:10:19 +0100

    antares: commit local setups.
    Change-Id: If66e4747f9cc5a9fd54c0540e2cc42ab872a96c4
    Reviewed-on: https://trac.frm2.tum.de/review/4240
    Tested-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

--- custom/antares/setups/collimator.py
+++ custom/antares/setups/collimator.py
@@ -18,12 +18,19 @@ devices = dict(
     collimator = device('devices.generic.Switcher',
                         description = 'Collimator, value is L/D',
                         moveable = 'collimator_io',
-                        mapping = { 3200:1, 200:2, 800:3, 7100:4, 400:5, 1600:6, 'park':7, },
+                        mapping = { 200:2, 400:5, 800:3, 1600:6, 3200:1, 7100:4, 'park':7, },
                         fallback = '<undefined>',
                         unit = 'L/D',
                         precision = 0,
                        ),
-
+    pinhole = device('devices.generic.Switcher',
+                        description = 'Pinhole diameter',
+                        moveable = 'collimator_io',
+                        mapping = { 2:4, 4.5:1, 9:6, 18:3, 36:5, 71:2, 'park':7, },
+                        fallback = '<undefined>',
+                        unit = 'mm',
+                        precision = 0,
+                       ),
 )
 
 
