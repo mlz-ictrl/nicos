915965af359cc688527d74d661d4958620c162b6
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2014-02-14 09:01:22 +0100

    Improve build and add python3 as well
    This fixes some problems due to bugs in the old versions as well.
    
    Add commandline params
    
    Change-Id: I2798796a8a26c0fe8ce804c84c447a0cbb2d2fe8
    Reviewed-on: https://trac.frm2.tum.de/review/4504
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

--- tools/build_nicosstack.sh
+++ tools/build_nicosstack.sh
@@ -2,63 +2,132 @@
 BASEDIR=${BASEDIR:-"$PWD/nicos-stack"}
 MAKEOPTS="-j5"
 
+PYVERSION=${PYVERSION:-2}
+
+dofetch=1
+dobuild=1
+domkvenv=1
+
+function usage() {
+  echo "install a nicos stack"
+  echo "options:"
+  echo
+  echo "  -p | --pythonver (2|3)  install a python 2 or 3"
+  echo "  -b | --basedir basedir  basedir (default: $BASEDIR)"
+  echo "  -v | --verbose          more verbose output"
+  echo "  --nofetch               do not fetch files"
+  echo "  --nobuild               do not build"
+  echo "  --nomkvenv              do not create a venv"
+  echo "  -h | --help             this help"
+  echo
+  echo "enviroment vars : PYVERSION BASEDIR"
+ }
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --pythonver )        shift
+                                PYVERSION=$1
+                                ;;
+        -v | --verbose )        set -v
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        -b | --basedir )        shift
+                                BASEDIR=$1
+                                ;;
+        --nofetch )             dofetch=0
+                                ;;
+        --nobuild )             dobuild=0
+                                ;;
+        --nomkvenv )            domkvenv=0
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+
+if [ $PYVERSION -eq 3 ] ; then
+   PYVERSION="3.3.4"
+else
+   PYVERSION="2.7.6"
+fi
+
+
+
+PYEXEC=python${PYVERSION:0:1}
+PYBASEVER=`echo $PYVERSION|cut -d. -f1-2`
+
+
+function fetch
+{
 # fetch sources into a separate directory
 mkdir -p $BASEDIR/download
 cd $BASEDIR/download
 WGET="wget -c --no-check-certificate"
-$WGET http://www.python.org/ftp/python/2.7.6/Python-2.7.6.tar.xz
 $WGET https://trac.frm2.tum.de/externalpackages/Python-2.7.6.tar.xz
+$WGET https://trac.frm2.tum.de/externalpackages/Python-3.3.4.tar.xz
 $WGET https://trac.frm2.tum.de/externalpackages/PyQt-x11-gpl-4.10.1.tar.gz
 $WGET https://trac.frm2.tum.de/externalpackages/PyQwt-5.2.0.tar.gz
 $WGET https://trac.frm2.tum.de/externalpackages/sip-4.14.6.tar.gz
 $WGET https://trac.frm2.tum.de/externalpackages/QScintilla-gpl-2.7.1.tar.gz
 $WGET https://trac.frm2.tum.de/externalpackages/qt-everywhere-opensource-src-4.8.5.tar.gz
-$WGET https://trac.frm2.tum.de/externalpackages/virtualenv-1.11.1.tar.gz
-$WGET https://trac.frm2.tum.de/externalpackages/pip-1.5.1.tar.gz
+$WGET https://trac.frm2.tum.de/externalpackages/virtualenv-1.11.2.tar.gz
+$WGET https://trac.frm2.tum.de/externalpackages/pip-1.5.2.tar.gz
 $WGET https://trac.frm2.tum.de/externalpackages/setuptools-2.1.tar.gz
 
 # check downloaded files
 md5sum -c <<EOF
-4678c2ae5cce4e9234c3923d7dcb32f0  pip-1.5.1.tar.gz
+5da30919f732d68b1c666e484e7676f5  pip-1.5.2.tar.gz
 e5973c4ec0b0469f329bc00209d2ad9c  PyQt-x11-gpl-4.10.1.tar.gz
 fcd6c6029090d473dcc9df497516eae7  PyQwt-5.2.0.tar.gz
 bcf93efa8eaf383c98ed3ce40b763497  Python-2.7.6.tar.xz
+8fb961a20600aafafd249537af3ac637  Python-3.3.4.tar.xz
 da8939b5679a075e30c6632e54dc5abf  QScintilla-gpl-2.7.1.tar.gz
 1864987bdbb2f58f8ae8b350dfdbe133  qt-everywhere-opensource-src-4.8.5.tar.gz
 2044725530450d0517393882dc4b7508  setuptools-2.1.tar.gz
 d6493b9f0a7911566545f694327314c4  sip-4.14.6.tar.gz
-7875c2d8c2075571abe5e727449af4d8  virtualenv-1.11.1.tar.gz
+d3d915836c1ada1be731ccaa12412b98  virtualenv-1.11.2.tar.gz
 EOF
 if [ $? -ne 0 ]; then
     echo "some files failed md5check, please redownload"
     exit 1;
 fi;
 
+}
+
+function build
+{
 # create build dir and start building
 mkdir -p $BASEDIR/build
 
 #build python and a minimal set of required extra packages
 cd $BASEDIR/build
-tar xJf $BASEDIR/download/Python-2.7.6.tar.xz
-cd Python-2.7.6/
-./configure --prefix=$BASEDIR/python-2.7.6
+tar xJf $BASEDIR/download/Python-$PYVERSION.tar.xz
+cd Python-$PYVERSION/
+./configure --prefix=$BASEDIR/python-$PYVERSION
 make $MAKEOPTS && make install
+
 cd $BASEDIR/build
 tar xzf $BASEDIR/download/setuptools-2.1.tar.gz
 cd setuptools-2.1/
-../../python-2.7.6/bin/python setup.py install
+$BASEDIR/python-$PYVERSION/bin/$PYEXEC setup.py install
+
 cd $BASEDIR/build
-tar xzf $BASEDIR/download/pip-1.5.1.tar.gz
-cd pip-1.5.1/
-../../python-2.7.6/bin/python setup.py install
+tar xzf $BASEDIR/download/pip-1.5.2.tar.gz
+cd pip-1.5.2/
+$BASEDIR/python-$PYVERSION/bin/$PYEXEC setup.py install
+
 cd $BASEDIR/build
-tar xzf $BASEDIR/download/virtualenv-1.11.1.tar.gz
-cd virtualenv-1.11.1/
-../../python-2.7.6/bin/python setup.py install
+tar xzf $BASEDIR/download/virtualenv-1.11.2.tar.gz
+cd virtualenv-1.11.2/
+$BASEDIR/python-$PYVERSION/bin/$PYEXEC setup.py install
 
 cd $BASEDIR/build
 # get numpy as well
-$BASEDIR/python-2.7.6/bin/pip install numpy
+$BASEDIR/python-$PYVERSION/bin/pip install numpy
 
 
 #build Qt and Qt-related packages
@@ -74,22 +143,21 @@ cd $BASEDIR/build
 tar xzf $BASEDIR/download/QScintilla-gpl-2.7.1.tar.gz
 cd QScintilla-gpl-2.7.1/
 cd Qt4Qt5/
-../../../qt-4.8.5/bin/qmake
+$BASEDIR/qt-4.8.5/bin/qmake
 make $MAKEOPTS && make install
-cd ../../
-export PATH=$BASEDIR/python-2.7.6/bin/:$BASEDIR/qt-4.8.5/bin/:$PATH
+export PATH=$BASEDIR/python-$PYVERSION/bin/:$BASEDIR/qt-4.8.5/bin/:$PATH
 
 
 # PyQt and needed helpers
 cd $BASEDIR/build
 tar xzf $BASEDIR/download/sip-4.14.6.tar.gz
 cd sip-4.14.6/
-../../python-2.7.6/bin/python configure.py -b ../../python-2.7.6/bin/ -d ../../python-2.7.6/lib/python2.7/site-packages/ -e ../../python-2.7.6/include/ -v ../../python-2.7.6/share/sip
+$BASEDIR/python-$PYVERSION/bin/$PYEXEC configure.py -b $BASEDIR/python-$PYVERSION/bin/ -d $BASEDIR/python-$PYVERSION/lib/python$PYBASEVER/site-packages/ -e $BASEDIR/python-$PYVERSION/include/ -v $BASEDIR/python-$PYVERSION/share/sip
 make $MAKEOPTS && make install
 cd $BASEDIR/build
 tar xzf $BASEDIR/download/PyQt-x11-gpl-4.10.1.tar.gz
 cd PyQt-x11-gpl-4.10.1/
-../../python-2.7.6/bin/python configure.py -q ../../qt-4.8.5/bin/qmake --confirm-license
+$BASEDIR/python-$PYVERSION/bin/$PYEXEC configure.py -q $BASEDIR/qt-4.8.5/bin/qmake --confirm-license
 make $MAKEOPTS && make install
 
 
@@ -98,14 +166,30 @@ cd $BASEDIR/build
 tar xzf $BASEDIR/download/PyQwt-5.2.0.tar.gz
 cd PyQwt-5.2.0/
 make $MAKEOPTS 4 && make install-4
-cd ../
 
+}
 
+function mkvenv
+{
 #generate a virtualenv for nicos
 cd $BASEDIR
-virtualenv -p $BASEDIR/python-2.7.6/bin/python --system-site-packages --prompt=NICOS nicosvenv
+virtualenv -p $BASEDIR/python-$PYVERSION/bin/$PYEXEC --system-site-packages --prompt="NICOS\(py-$PYBASEVER\) " nicosvenv-py$PYBASEVER
 
+echo
+echo
+echo "  To use the virtualenv:"
+echo
+echo "    sh: . $BASEDIR/nicosvenv-py$PYBASEVER/bin/activate"
+echo "    tcsh/csh: source $BASEDIR/nicosvenv-py$PYBASEVER/bin/activate.csh"
+echo
+}
 
-#to use the virtualenv:
-# sh: . $BASEDIR/nicosvenv/bin/activate
-# (t)csh): source $BASEDIR/nicosvenv/bin/activate.csh
+if [ $dofetch -eq 1 ] ; then
+    fetch
+fi
+if [ $dobuild -eq 1 ] ; then
+    build
+fi
+if [ $domkvenv -eq 1 ] ; then
+    mkvenv
+fi
