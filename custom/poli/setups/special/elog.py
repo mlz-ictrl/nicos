ce984a97b2475e4a44605a588a783aa464e240f2
Jens Krüger <jens.krueger@frm2.tum.de>
2014-02-21 08:12:11 +0100

    [POLI] new instrument
    Change-Id: I4abcfb6b91ae2f91f3dc786ef18b1ea173d09783
    Reviewed-on: https://trac.frm2.tum.de/review/4610
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>

---
+++ custom/poli/setups/special/elog.py
@@ -0,0 +1,13 @@
+# This setup file configures the nicos elog.
+# If you want to use the electronic logbook, make sure the "system" setup has
+# also a cache configured.
+
+description = 'setup for the electronic logbook'
+group = 'special'
+
+devices = dict(
+    Logbook = device('services.elog.Logbook',
+                     prefix = 'logbook/',
+                     cache = 'localhost:14869',
+                    ),
+)
---
+++ custom/poli/setups/startup.py
@@ -0,0 +1,10 @@
+description = 'NICOS startup setup'
+group = 'lowlevel'
+
+# The startup setup is loaded first thing on NICOS session startup.  It should
+# usually stay empty.  In particular, it should not include any other setups
+# without good reason, so that on startup the previous master setups are loaded
+# automatically.
+
+startupcode = '''
+'''
