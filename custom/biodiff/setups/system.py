d81cce0d7b6e520d22162714b8f0e8022357031d
Christian Felder <c.felder@fz-juelich.de>
2014-03-03 16:25:59 +0100

    BIODIFF: create new instrument.
    This is just the minimal instrument configuration (skeleton). Further changes
    should be done in a seperate patch derived from this one.
    
    Change-Id: I1bb3253381f305ebfdf8ec7df73e106123a75edb
    Reviewed-on: https://forge.frm2.tum.de/review/4605
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

---
+++ custom/biodiff/setups/system.py
@@ -0,0 +1,119 @@
+# description: Description of the setup (detailed)
+description = 'system setup'
+
+# group: Group of the setup. The following groups are recognized:
+# - basic
+#       Basic setup for the instrument, of which only one should be
+#       loaded (e.g. "twoaxis" or "threeaxis"). These setups can be
+#       presented to the user.
+# - optional
+#       Optional setup, of which as many as needed can be loaded.
+#       These setups can be presented to the user for multiple
+#       selection. This is the default.
+# - lowlevel
+#       Low-level setup, which will be included by others, but should
+#       not be presented to users.
+# - special
+#        The setup is not a setup of instrument devices, but configures
+#        e.g. a NICOS service. For each service, there is one special
+#        setup (e.g. "cache", "poller", "daemon").
+group = 'lowlevel'
+
+# sysconfig: A dictionary with basic system configuration values.
+# Possible values:
+#   - cache
+#       A string giving the hostname of the cache server (or
+#       hostname:port, if the cache runs on a port other than 14869).
+#       If this is omitted, no caching will be available.
+#   - instrument
+#       The name of the instrument device, defined somewhere in a
+#       devices dictionary. The class for this device must be
+#       nicos.devices.instrument.Instrument or an instrument-specific
+#       subclass.
+#   - experiment
+#       The name of the experiment "device", defined somewhere in a
+#       devices dictionary. The class for this device must be
+#       nicos.devices.experiment.Experiment or an instrument-specific
+#       subclass.
+#   - datasinks
+#       A list of names of "data sinks", i.e. special devices that
+#       process measured data. These devices must be defined somewhere
+#       in a devices dictionary and be of class
+#       nicos.devices.datasinks.DataSink or a subclass.
+#   - notifiers
+#       A list of names of "notifiers", i.e. special devices that can
+#       notify the user or instrument responsibles via various channels
+#       (e.g. email). These devices must be defined somewhere in a
+#       devices dictionary and be of class
+#       nicos.devices.notifiers.Notifier or a subclass.
+
+sysconfig = dict(
+    cache = 'localhost',
+    instrument = 'biodiff',
+    experiment = 'Exp',
+    datasinks = ['conssink', 'filesink', 'daemonsink'],
+    notifiers = ['mailer', 'smser'],
+)
+
+modules = ['nicos.commands.standard']
+
+# devices: Contains all device definitions.
+# A device definition consists of a call like device(classname, parameters).
+# The class name is fully qualified (i.e., includes the package/module name).
+# The parameters are given as keyword arguments.
+devices = dict(
+    Sample   = device('devices.experiment.Sample',
+                      description = 'Sample under investigation'),
+
+    # Configure dataroot here (usually /data).
+    Exp      = device('devices.experiment.Experiment',
+                      description = 'Experiment device',
+                      dataroot = '/home/jcns/data',
+                      localcontact = "Tobias Schrader <t.schrader@fz-juelich.de>",
+                      sendmail = True,
+                      serviceexp = 'service',
+                      sample = 'Sample',
+                     ),
+
+    biodiff = device('devices.instrument.Instrument',
+                     description = 'Single crystal DIFFractometer ' +
+                                   'for BIOlogical macromolecules',
+                     instrument = "BIODIFF",
+                     responsible = "Tobias Schrader <t.schrader@fz-juelihc.de>, " +
+                                   "Andreas Ostermann <andreas.ostermann@frm2.tum.de>"
+                    ),
+
+    filesink = device('devices.datasinks.AsciiDatafileSink',
+                      description = 'Device storing scanfiles in Ascii output format.',
+                     ),
+
+    conssink = device('devices.datasinks.ConsoleSink',
+                      description = 'Device storing console output.',
+                     ),
+
+    daemonsink = device('devices.datasinks.DaemonSink',
+                        description = 'Device storing deamon output.',
+                       ),
+
+    Space    = device('devices.generic.FreeSpace',
+                      description = 'The amount of free space for storing data',
+                      path = None,
+                      minfree = 5,
+                     ),
+
+    # Configure source and copy addresses to an existing address.
+    mailer    = device('devices.notifiers.Mailer',
+                      description = 'E-Mail notifier',
+                      sender = 'noreply@fz-juelich.de',
+                      receivers = ['t.scharder@fz-juelich.de',
+                                   'andreas.ostermann@frm2.tum.de'],
+                      copies = ['c.felder@fz-juelich.de'],
+                      subject = '[NICOS] BIODIFF',
+                     ),
+
+    # Configure SMS receivers if wanted and registered with IT.
+    smser    = device('devices.notifiers.SMSer',
+                      description = 'SMS notifier',
+                      server = 'triton.admin.frm2',
+                     ),
+)
