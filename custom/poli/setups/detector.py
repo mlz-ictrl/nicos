58cc051e3846f04007b2aefc6f1f5ac11f6f6fcf
Jens Krüger <jens.krueger@frm2.tum.de>
2014-03-05 18:23:11 +0100

    [POLI] Add detector setup
    Change-Id: I8ab678092b2d770792d40d17ac83d91a6f376880
    Reviewed-on: https://trac.frm2.tum.de/review/4618
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/poli/setups/detector.py
@@ -0,0 +1,46 @@
+description = 'POLI detector and counter card'
+group = 'lowlevel'
+
+includes = []
+
+nethost = 'heidi22.heidi.frm2'
+
+devices = dict(
+    timer = device('devices.taco.FRMTimerChannel',
+                   tacodevice = '//%s/heidi2/frmctr/at' % nethost,
+                   fmtstr = '%.2f',
+                   lowlevel = True,
+                  ),
+    mon1  = device('devices.taco.FRMCounterChannel',
+                   tacodevice = '//%s/heidi2/frmctr/a1' % nethost,
+                   type = 'monitor',
+                   fmtstr = '%d',
+                   lowlevel = True,
+                  ),
+    mon2  = device('devices.taco.FRMCounterChannel',
+                   tacodevice = '//%s/heidi2/frmctr/a2' % nethost,
+                   type = 'monitor',
+                   fmtstr = '%d',
+                   lowlevel = True,
+                  ),
+    ctr1  = device('devices.taco.FRMCounterChannel',
+                   tacodevice = '//%s/heidi2/frmctr/a3' % nethost,
+                   type = 'counter',
+                   fmtstr = '%d',
+                   lowlevel = True,
+                  ),
+
+    det   = device('devices.generic.MultiChannelDetector',
+                   description = 'FRM-II multichannel counter card',
+                   timer  = 'timer',
+                   monitors = ['mon1', 'mon2'],
+                   counters = ['ctr1'],
+                   fmtstr = 'timer %s, mon1 %s, mon2 %s, ctr1 %s',
+                   maxage = 2,
+                   pollinterval = 1.0,
+                  ),
+)
+
+startupcode = """
+SetDetectors(det)
+"""
