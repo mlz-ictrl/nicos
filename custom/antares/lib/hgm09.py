31985a59566fa54b075ac387df89d64608200959
Alexander Lenz <alexander.lenz@frm2.tum.de>
2014-06-23 15:02:06 +0200

    [ANTARES] Implement a simple device for hgm09.
    (gaussmeter)
    
    Change-Id: Ic392f159c83d3e2fc0094db092a144e703163cf3
    Reviewed-on: https://forge.frm2.tum.de/review/5332
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    Tested-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

---
+++ custom/antares/lib/hgm09.py
@@ -0,0 +1,64 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2013 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+# *****************************************************************************
+
+"""
+Devices for the HGM09 gaussmeter.
+"""
+
+from IO import StringIO
+
+from nicos.core import Readable, Override, CommunicationError, NicosError
+from nicos.devices.taco import TacoDevice
+
+
+class HGM09(TacoDevice, Readable):
+    """Class for the HGM09 gaussmeter.
+
+    Uses a taco StringIO device to communicate directly with the hardware.
+    """
+
+    taco_class = StringIO
+    valuetype = float
+
+    parameter_overrides = {
+        'unit': Override(mandatory=False, volatile=True),
+    }
+
+    def doInit(self, mode):
+        if mode == 'simulation':
+            return
+        reply = self._communicate('*IDN?')
+        if not reply.startswith('MAGSYS-MAGNET-SYSTEME,HGM09'):
+            raise CommunicationError('wrong identification: %r' % reply)
+
+    def doRead(self, maxage=0):
+        try:
+            value = HGM09.valuetype(self._communicate('READ?'))
+        except (NicosError, ValueError):
+            # retry communication/handle error
+            value = HGM09.valuetype(self._communicate('READ?'))
+        return value
+
+    def doReadUnit(self):
+        return self._communicate('UNIT?')
+
+    def _communicate(self, msg):
+        return self._taco_guard(self._dev.communicate, msg).strip()
+
---
+++ custom/antares/setups/hgm09.py
@@ -0,0 +1,14 @@
+description = 'HGM09 Hand Gauss Meter'
+group = 'optional'
+
+includes = []
+
+devices = dict(
+    hgm09 = device('antares.hgm09.HGM09',
+                description = 'HGM09 Hand Gauss Meter',
+                tacodevice = 'antares/rs232/hgm09',
+               ),
+)
+
+startupcode = '''
+'''
