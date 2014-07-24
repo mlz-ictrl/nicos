7d5b5e726339a7c9806ed51e5d42b28cc66e40cd
Antares People <antares@frm2.tum.de>
2014-07-24 15:35:41 +0200

    Commit local changes.
    Change-Id: I5fb901b192b1a353aad0d319c2a820d93f2260e0
    Reviewed-on: https://forge.frm2.tum.de/review/5595
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    Tested-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

--- custom/antares/lib/hgm09.py
+++ custom/antares/lib/hgm09.py
@@ -16,6 +16,10 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Alexander Lenz <alexander.lenz@frm2.tum.de>
+#
 # *****************************************************************************
 
 """
@@ -25,6 +29,7 @@ Devices for the HGM09 gaussmeter.
 from IO import StringIO
 
 from nicos.core import Readable, Override, CommunicationError, NicosError
+
 from nicos.devices.taco import TacoDevice
 
 
