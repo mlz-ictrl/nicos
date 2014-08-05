75b2ed6d49cc75e5225bb2b163c58572472dc285
Antares People <antares@frm2.tum.de>
2014-08-05 10:25:13 +0200

    antares: new setup detbox_l for translation of camera added
    Change-Id: I4f61f00713e694c6e4713946a852167a2956f38f
    Reviewed-on: https://forge.frm2.tum.de/review/5672
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    Tested-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

---
+++ custom/antares/setups/detbox_l.py
@@ -0,0 +1,17 @@
+description = 'Motor for FOV camera translation in large detector box'
+
+group = 'optional'
+
+includes = []
+
+devices = dict(
+    ccdtx = device('devices.taco.Motor',
+                        description = 'Camera Translation X',
+                        tacodevice = 'antares/copley/m06',
+                        abslimits = (-9999, 9999),
+                        userlimits = (-0, 693),
+                      ),
+)
+
+startupcode = '''
+'''
