57ea2e7864475cac37080cefd1054341680785c0
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2014-08-28 08:28:37 +0200

    Guard against unset target
    if target has not been set, it is the string 'unknown',
    causing errors when used numerically.
    
    Change-Id: Iac6974c71effc4465fb65f3028abbada3e6ae83d
    Fixes: #1488
    Reviewed-on: https://forge.frm2.tum.de/review/5828
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Petr Cermak <p.cermak@fz-juelich.de>
    Tested-by: Petr Cermak <p.cermak@fz-juelich.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>

--- nicos/devices/tas/mono.py
+++ nicos/devices/tas/mono.py
@@ -301,7 +301,7 @@ class Monochromator(HasLimits, HasPrecision, Moveable):
             new_umin = max(new_umin, new_absmin)
             new_umax = min(new_umax, new_absmax)
             self.userlimits = (new_umin, new_umax)
-        if 'target' in self._params and self.target:
+        if 'target' in self._params and self.target and self.target !='unknown':
             # this should be still within the limits
             self._setROParam('target', self._fromlambda(
                 self._tolambda(self.target), value))
