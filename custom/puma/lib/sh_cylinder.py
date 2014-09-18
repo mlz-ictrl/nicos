3b0f1f1c5269bd6ed23b2a6410ded9076b5d5ec8
Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
2014-09-18 15:21:09 +0200

    [PUMA] add support for new shutter devices
    Change-Id: I8456f9b07d2e7a8b8a86202f36790f33dbb27fbe
    Reviewed-on: https://forge.frm2.tum.de/review/5999
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

---
+++ custom/puma/lib/sh_cylinder.py
@@ -0,0 +1,78 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2013 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Oleg Sobolev <oleg.sobolev@frm2.tum.de>
+#
+# *****************************************************************************
+
+"""Device class for the shutter cylinder device."""
+
+import time
+
+from nicos.core import Moveable, Readable, NicosError, Param
+
+
+class SH_Cylinder(Moveable):
+
+    attached_devices = {
+        'io_ref':    (Readable, 'limit switches'),
+        'io_air':    (Moveable, 'air in the open/close direction'),
+        'io_pos':    (Moveable, 'position stop, if in closed position, -1'),
+    }
+
+    parameters = {
+        'timedelay': Param('Waiting time for the movement',
+                        type=float, default=3, settable=True, mandatory=False, unit='s'),
+    }
+
+    def doStart(self, position):
+        if position == self.read(0):
+            return
+
+        if self._checkAir() != 1:
+            raise NicosError(self, 'No air! Please open the shutter with the bottom near the door.')
+
+
+        self._adevs['io_air'].move(0)
+        time.sleep(self.timedelay)
+
+        if self._adevs['io_ref'].read(0) != 1:
+            raise NicosError(self, 'Cannot close the shutter!')
+
+
+        if position != -1:
+            self._adevs['io_pos'].move(position)
+            time.sleep(self.timedelay)
+            self._adevs['io_air'].move(1)
+            time.sleep(self.timedelay)
+
+    def doRead(self, maxage=0):
+        if self._adevs['io_ref'].read(0) == 1:
+            return -1
+        if self._adevs['io_air'].read(0) == 1:
+            return self._adevs['io_pos'].read(0)
+
+    def _checkAir(self):
+        if self._adevs['io_ref'].read(0) == 1:
+            self._adevs['io_air'].move(1)
+            time.sleep(self.timedelay)
+            if self._adevs['io_ref'].read(0) == 1:
+                return 0
+        return 1
