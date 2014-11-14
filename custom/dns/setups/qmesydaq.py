d6c56bb60b0a3b9179039141a5831e4446ebb40e
L.Fleischhauer-Fuss <l.fleischhauer-fuss@fz-juelich.de>
2014-11-14 15:15:44 +0100

    DNS: qmesydaq setup
    Change-Id: Ica54ad5f5467376dc28e10b57a6a577660003c73
    Reviewed-on: https://forge.frm2.tum.de/review/6462
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>

---
+++ custom/dns/setups/qmesydaq.py
@@ -0,0 +1,77 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2014 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Lydia Fleischhauer-Fuss <l.fleischhauer-fuss@fz-juelich.de>
+#
+# **************************************************************************
+
+description = 'qmesydaq channel devices'
+
+group = 'optional'
+
+nethost = 'mesydaq.dns.frm2'
+qm = '//%s/test/qmesydaq/' % nethost
+
+devices = dict(
+    mon1 = device('nicos.devices.vendor.qmesydaq.QMesyDAQCounter',
+                  description = 'QMesyDAQ Counter0',
+                  tacodevice = qm + 'counter0',
+                  type = 'monitor',
+                 ),
+    mon2 = device('nicos.devices.vendor.qmesydaq.QMesyDAQCounter',
+                  description = 'QMesyDAQ Counter1',
+                  tacodevice = qm + 'counter1',
+                  type = 'monitor',
+                 ),
+    #~ mon3 = device('nicos.devices.vendor.qmesydaq.QMesyDAQCounter',
+                     #~ description = 'QMesyDAQ Counter2',
+                     #~ tacodevice = qm + 'counter2',
+                     #~ type = 'monitor',
+                     #~ ),
+    #~ mon4 = device('nicos.devices.vendor.qmesydaq.QMesyDAQCounter',
+                     #~ description = 'QMesyDAQ Counter3',
+                     #~ tacodevice = qm + 'counter3',
+                     #~ type = 'monitor',
+                     #~ ),
+    #~ events = device('nicos.devices.vendor.qmesydaq.QMesyDAQCounter',
+                     #~ description = 'QMesyDAQ Events channel',
+                     #~ tacodevice = qm + 'events',
+                     #~ type = 'counter',
+                     #~ ),
+    timer = device('nicos.devices.vendor.qmesydaq.QMesyDAQTimer',
+                   description = 'QMesyDAQ Timer',
+                   tacodevice = qm + 'timer',
+                  ),
+    qm_det = device('nicos.devices.vendor.qmesydaq.QMesyDAQImage',
+                    description = 'QMesyDAQ MultiChannel Detector',
+                    tacodevice = qm + 'det',
+                    events = 'events',
+                    timer = 'timer',
+                    counters = [],
+                    #~ monitors = ['mon1', 'mon2', 'mon3', 'mon4'],
+                    monitors = ['mon1', 'mon2'],
+                    fileformats = [],
+                    subdir = '',
+                    readout = 'none',
+                   ),
+)
+
+startupcode = """
+"""
