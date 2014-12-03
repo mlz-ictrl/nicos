28e178c356e1f81050f3199225a09a946ffda08d
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2014-12-03 16:58:24 +0100

    Add a custom dir for the Laue camera
    Copy of skeleton with instrument name changed and
    some not needed files removed.
    
    Set mail to noreply@frm2.tum.de (this  address exist ;))
    
    All other changes will be done in separate patches.
    
    Change-Id: Ie74a9199940c52cb81568491499989ae95505aac
    Reviewed-on: https://forge.frm2.tum.de/review/6602
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>

---
+++ custom/laue/setups/special/watchdog.py
@@ -0,0 +1,62 @@
+# This setup file configures the nicos poller service.
+
+description = 'setup for the NICOS watchdog'
+group = 'special'
+
+# watchlist:
+# The entries in this list are dictionaries. Possible keys:
+#
+# 'setup' -- setup that must be loaded (default '' to mean all setups)
+# 'condition' -- condition for warning (a Python expression where cache keys
+#    can be used: t_value stands for t/value etc.
+# 'gracetime' -- time in sec allowed for the condition to be true without
+#    emitting a warning (default 5 sec)
+# 'message' -- warning message to display
+# 'type' -- for defining different types of warnings; this corresponds to the
+#     configured notifiers (default 'default')
+#     type '' does not emit warnings (useful together with 'pausecount'
+#     for conditions that should block counting but are not otherwise errors)
+# 'pausecount' -- if True, the count loop should be paused on the condition
+#     (default False)
+# 'action' -- code to execute if condition is true (default no code is executed)
+# watchlist = [
+#     dict(condition = 't_value > 100',
+#          message = 'Temperature too high',
+#          type = 'critical',
+#          action = 'maw(T, 0)',
+#         ),
+#     dict(condition = 'phi_value > 100 and mono_value > 1.5',
+#          message = 'phi angle too high for current mono setting',
+#          gracetime = 5,
+#         ),
+# ]
+
+notifiers = {
+    'default':  ['mailer'],
+    'critical': ['mailer', 'smser'],
+}
+
+devices = dict(
+    # Configure source and copy addresses to an existing address.
+    mailer   = device('devices.notifiers.Mailer',
+                      sender = 'noreply@frm2.tum.de',
+                      receivers = ['bjoern.pedersen@frm2.tum.de'],
+                      subject = 'NICOS Warning',
+                     ),
+
+    # Configure SMS receivers if wanted and registered with IT.
+    smser    = device('devices.notifiers.SMSer',
+                      server = 'triton.admin.frm2',
+                      receivers = [],
+                     ),
+
+#     Watchdog = device('services.watchdog.Watchdog',
+#                       # use only 'localhost' if the cache is really running on
+#                       # the same machine, otherwise use the official computer
+#                       # name
+#                       cache = 'localhost',
+#                       notifiers = notifiers,
+#                       mailreceiverkey = 'email/receivers',
+#                       watch = watchlist,
+#                      ),
+)
