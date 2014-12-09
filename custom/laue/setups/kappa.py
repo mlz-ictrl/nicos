eb67b50400376ab5abb98f8edc7d398ce42fe7a6
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2014-12-09 14:34:00 +0100

    Add kappa axes
    Add all axes controlled by the Huber Controller for the
    Kappa goniometer
    
    Change-Id: If90d1655cae16ae3838e5242d9240d3b925f3887
    Reviewed-on: https://forge.frm2.tum.de/review/6656
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>

---
+++ custom/laue/setups/kappa.py
@@ -0,0 +1,53 @@
+description = 'kappa goniometer'
+group = 'lowlevel'
+
+devices = dict(
+   twotheta =  device('devices.taco.Motor',
+                      description = 'two-theta angle',
+                      tacodevice = '//lauectrl.laue.frm2/laue/hubermc9300/twotheta',
+                      abslimits = (-180, 180),
+                      fmtstr = '%.3f'),
+
+   omega =  device('devices.taco.Motor',
+                      description = 'omega angle',
+                      tacodevice = '//lauectrl.laue.frm2/laue/hubermc9300/omega',
+                      abslimits = (-180, 180),
+                      fmtstr = '%.3f'),
+
+   kappa =  device('devices.taco.Motor',
+                      description = 'kappa angle',
+                      tacodevice = '//lauectrl.laue.frm2/laue/hubermc9300/kappa',
+                      abslimits = (-180, 180),
+                      fmtstr = '%.3f'),
+
+   phi =  device('devices.taco.Motor',
+                      description = 'phi angle',
+                      tacodevice = '//lauectrl.laue.frm2/laue/hubermc9300/phi',
+                      abslimits = (-180, 180),
+                      fmtstr = '%.3f'),
+
+   stx =  device('devices.taco.Motor',
+                      description = 'sample x translation',
+                      tacodevice = '//lauectrl.laue.frm2/laue/hubermc9300/samplex',
+                      abslimits = (-25, 25),
+                      fmtstr = '%.3f'),
+
+   sty =  device('devices.taco.Motor',
+                      description = 'sample y translation',
+                      tacodevice = '//lauectrl.laue.frm2/laue/hubermc9300/sampley',
+                      abslimits = (-25, 25),
+                      fmtstr = '%.3f'),
+
+   stz =  device('devices.taco.Motor',
+                      description = 'sample z translation',
+                      tacodevice = '//lauectrl.laue.frm2/laue/hubermc9300/samplez',
+                      abslimits = (-1, 25),
+                      fmtstr = '%.3f'),
+
+   detz =  device('devices.taco.Motor',
+                      description = 'detector position',
+                      tacodevice = '//lauectrl.laue.frm2/laue/hubermc9300/detz',
+                      abslimits = (100, 120),
+                      fmtstr = '%.3f'),
+
+)
