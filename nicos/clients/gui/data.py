gui scans panel: fix wrong normalization after adding datasets
For subtract and divide, the new curve removes the normalization info,
so that there is no nonsense "normalized" mode.

Also now treats error bars correctly in the combined datasets.

Internal change: dataset.datady is now always the correct errors
(in the case of sqrt() errors, it is calculated on arrival of data
instead of on-the-fly).

Change-Id: Ic73e0836c6b4e4b7eb22fe5e62418bf707d5b177
Fixes: #1743
Reviewed-on: https://forge.frm2.tum.de/review/6761
Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
Reviewed-by: Georg Brandl <georg.brandl@frm2.tum.de>

nicos/clients/gui/data.py
@@ -206,10 +206,16 @@ class DataHandler(QObject):
             except IndexError:
                 curve.datax.append(len(curve.datax))
             curve.datay.append(yvalues[curve.yindex])
-            if curve.dyindex != -1:
+            if curve.dyindex >= 0:
                 curve.datady.append(yvalues[curve.dyindex])
+            elif curve.dyindex == -2:
+                curve.datady.append(np.sqrt(yvalues[curve.yindex]))
+            else:
+                curve.datady.append(0)
             if curve.timeindex != -1:
                 curve.datatime.append(yvalues[curve.timeindex])
+            else:
+                curve.datatime.append(0)
             for i in curve.monindices:
                 if yvalues[i] != 0:
                     curve.datamon.append(yvalues[i])

nicos/clients/gui/widgets/grplotting.py
@@ -438,9 +438,7 @@ class DataSetPlot(DataSetPlotMixin, NicosGrPlot):
         x, y = np.array(curve.datax), np.array(curve.datay, float)
         n = len(curve.datax)
         if n > 0:
-            if curve.dyindex == -2:
-                dy = np.sqrt(abs(y))
-            elif curve.dyindex > -1:
+            if curve.dyindex != -1:
                 dy = np.array(curve.datady)
             if self.normalized:
                 norm = None
@@ -470,9 +468,7 @@ class DataSetPlot(DataSetPlotMixin, NicosGrPlot):
         y = np.array(curve.datay, float)
         n = len(curve.datax)
         dy = None
-        if curve.dyindex == -2:
-            dy = np.sqrt(abs(y))
-        elif curve.dyindex > -1:
+        if curve.dyindex != -1:
             dy = np.array(curve.datady)
         if self.normalized:
             norm = None

nicos/clients/gui/widgets/qwtplotting.py
@@ -641,9 +641,7 @@ class DataSetPlot(DataSetPlotMixin, NicosQwtPlot):
         x = np.array(curve.datax)
         y = np.array(curve.datay, float)
         dy = None
-        if curve.dyindex == -2:
-            dy = np.sqrt(abs(y))
-        elif curve.dyindex > -1:
+        if curve.dyindex != -1:
             dy = np.array(curve.datady)
         if self.normalized:
             norm = None