0304481dda0977dec801d3dfe168798adf1eb9a9
Georg Brandl <georg.brandl@frm2.tum.de>
2015-01-24 13:04:47 +0100

    test: add a minimal test for custom/ to at least import all modules
    Change-Id: Ie652d6252a33ae92868d8d202e451bc5238632d8
    Reviewed-on: https://forge.frm2.tum.de/review/6958
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ test/test_basic/test_custom.py
@@ -0,0 +1,60 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2015 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Georg Brandl <georg.brandl@frm2.tum.de>
+#
+# *****************************************************************************
+
+"""NICOS custom lib tests: import all custom modules at least once."""
+
+import os
+from os import path
+
+from nicos.utils.tacostubs import generate_stubs
+
+from test.utils import rootdir, SkipTest
+
+
+def setup_module():
+    generate_stubs()
+
+
+def import_and_check(modname):
+    try:
+        __import__(modname)
+    except ImportError:  # we lack a precondition module, don't worry about that
+        raise SkipTest
+    except ValueError as err:
+        if 'has already been set to' in str(err):
+            # import order error with GUI widget modules
+            raise SkipTest
+        raise
+
+
+def test_import_all():
+    custom_dir = path.join(rootdir, '..', '..', 'custom')
+    for instr in sorted(os.listdir(custom_dir)):
+        if instr == 'delab':
+            continue
+        if not path.isdir(path.join(custom_dir, instr, 'lib')):
+            continue
+        for mod in os.listdir(path.join(custom_dir, instr, 'lib')):
+            if mod.endswith('.py'):
+                yield import_and_check, 'nicos.%s.%s' % (instr, mod[:-3])
