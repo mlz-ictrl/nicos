cce07a51e0d04f15bfb4848211c1fb15b145efb6
Georg Brandl <georg.brandl@frm2.tum.de>
2015-01-29 10:50:49 +0100

    FRM2: add setup for ccmhts01 magnet
    Currently works in terms of current.
    
    Change-Id: I7cc64d4e71b59cc5c0abe2d256a22bdd3c399e59
    Reviewed-on: https://forge.frm2.tum.de/review/7022
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ custom/frm2/setups/ccmhts01.py
@@ -0,0 +1,21 @@
+description = 'High-Tc superconducting magnet'
+
+group = 'optional'
+
+includes = ['alias_B']
+
+taco_host = 'ccmhts01'
+
+devices = dict(
+    # by convention this needs to be B_%(setupname)s
+    B_ccmhts01  = device('devices.taco.CurrentSupply',
+                         description = 'magnetic field device',
+                         tacodevice = '//%s/magnet/kepco/current' % taco_host,
+                         unit = 'A',
+                         abslimits = (-210, 210),
+                        ),
+)
+
+startupcode = '''
+B.alias = B_ccmhts01
+'''
