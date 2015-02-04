e0a0bd004bef595ed01bd45abd7ad63e892cb14b
Petr Cermak <p.cermak@fz-juelich.de>
2015-02-04 10:02:14 +0100

    Changes in PANDA template files
    added new slit script for better slit adjustment
    commented out Sample.psi0 as it is not used with setAlign command
    other small changes
    
    Change-Id: I71083757e74d57956047db4c8c5b8f097144be86
    Reviewed-on: https://forge.frm2.tum.de/review/7105
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: Georg Brandl <g.brandl@fz-juelich.de>

--- custom/panda/template/slits.simple.py
+++ custom/panda/template/slits.simple.py
@@ -9,13 +9,6 @@ move(ss2, (-20, 20, -40, 40))
 wait(ss1, ss2)
 
 
-scan(ss1.top, 40, -2, 25, countfor)
-maw(ss1.top, 40)
-
-scan(ss1.bottom, -40, 2, 25, countfor)
-maw(ss1.bottom, -40)
-
-
 scan(ss1.left, -20, 2, 15, countfor)
 maw(ss1.left, -20)
 
@@ -23,17 +16,12 @@ scan(ss1.right, 20, -2, 15, countfor)
 maw(ss1.right, 20)
 
 
+scan(ss1.bottom, -40, 2, 25, countfor)
+maw(ss1.bottom, -40)
 
+scan(ss1.top, 40, -2, 25, countfor)
+maw(ss1.top, 40)
 
-scan(ss2.top, 40, -2, 25, countfor)
-try:
-    maw(ss2.top, 40)
-except:
-    ss2.top.reset()
-    maw(ss2.top, 40)
-
-scan(ss2.bottom, -40, 2, 25, countfor)
-maw(ss2.bottom, -40)
 
 
 scan(ss2.left, -20, 2, 15, countfor)
@@ -42,3 +30,8 @@ maw(ss2.left, -20)
 scan(ss2.right, 20, -2, 15, countfor)
 maw(ss2.right, 20)
 
+scan(ss2.bottom, -40, 2, 25, countfor)
+maw(ss2.bottom, -40)
+
+scan(ss1.top, 40, -2, 25, countfor)
+maw(ss1.top, 40)
