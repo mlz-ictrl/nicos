20fd8c2f20af804017841d49b209667e8ab0d60f
Georg Brandl <georg.brandl@frm2.tum.de>
2015-02-12 14:44:21 +0100

    setalign: report new psi0 too
    Change-Id: Id4168963411ef0ac0ed06f9ee949490be3505148
    Reviewed-on: https://forge.frm2.tum.de/review/7185
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos/commands/tas.py
+++ nicos/commands/tas.py
@@ -659,7 +659,8 @@ def setalign(hkl, psival=None):
         psival = psi.read(0)
     diff = psicalc - psival
     sample = tas._adevs['cell']
-    printinfo('adjusting %s.psi0 by %s' % (sample, psi.format(diff, True)))
+    printinfo('adjusting %s.psi0 by %s to %s' % (
+        sample, psi.format(diff, True), psi.format(sample.psi0 + diff, True)))
     sample.psi0 += diff
     newpsi = tas._calpos(target + (None, None), printout=False)[3]
     printinfo('%s is now at %s' % (tuple(hkl), psi.format(newpsi, True)))
