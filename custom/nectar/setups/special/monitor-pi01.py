ecc270533daee54f5fdcecf5c2b86ebe1a70b3f4
Nectar People <nectar@frm2.tum.de>
2015-02-19 13:34:13 +0100

    nectar: add monitor setup for pi01.
    Change-Id: I560e2cf3a1540374ee0db027b25c77936b03be25
    Reviewed-on: https://forge.frm2.tum.de/review/7290
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    Tested-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

---
+++ custom/nectar/setups/special/monitor-pi01.py
@@ -0,0 +1,73 @@
+# This setup file configures the nicos status monitor.
+
+description = 'setup for the status monitor'
+group = 'special'
+
+Row = Column = Block = BlockRow = lambda *args: args
+Field = lambda *args, **kwds: args or kwds
+
+_expcolumn = Column(
+    Block('Experiment', [
+        BlockRow(
+            Field(name='Proposal', key='exp/proposal', width=10),
+            Field(name='Title', key='exp/title', width=50, istext=True),
+            Field(name='Current status', key='exp/action', width=30, istext=True),
+        ),
+        BlockRow(
+            Field(name='Sample', key='sample/samplename', width=40,
+                 istext=True, maxlen=40),
+            Field(name='Remark',   key='exp/remark',   width=40,
+                 istext=True, maxlen=40),
+        ),
+        ],
+    ),
+)
+
+_translationColumn = Column(Block('Sample translation', [
+    BlockRow(Field(dev='stx'),),
+    BlockRow(Field(dev='sty'),),
+    BlockRow(Field(dev='sry'),),
+    ],
+    'servostar',
+))
+
+_detectorblock = Column(Block('Detector', [
+    BlockRow(
+        Field(name='Last Image', key='ccd.lastfilename'),
+        ),
+    BlockRow(
+        Field(dev='ccdTemp'), Field(name='CCD status', key='ccd/status', width=15, item=1),
+        ),
+    BlockRow(
+        Field(name='bin', key='ccd.bin'),
+        Field(name='flip (H,V)', key='ccd.flip'),
+        Field(name='rotation', key='ccd.rotation'),
+        ),
+    BlockRow(
+        Field(name='roi', key='ccd.roi'),
+        Field(name='hsspeed', key='ccd.hsspeed', width=4),
+        Field(name='vsspeed', key='ccd.vsspeed', width=4),
+        Field(name='pgain', key='ccd.pgain', width=4),
+        ),
+    ],
+    'detector',
+))
+
+_ubahnColumn = Column(Block('U-Bahn', [
+    BlockRow(Field(dev='UBahn'),),
+    ],
+    'ubahn',
+))
+
+
+devices = dict(
+    Monitor = device('services.monitor.qt.Monitor',
+                     title = 'NICOS status monitor',
+                     loglevel = 'info',
+                     cache = 'nectarhw.nectar.frm2',
+                     font = 'Luxi Sans',
+                     valuefont = 'Consolas',
+                     padding = 0,
+                     layout = [[_expcolumn],[_translationColumn, _detectorblock, _ubahnColumn]],
+                    ),
+)
