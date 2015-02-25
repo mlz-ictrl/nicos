d9f368232823af04c4fc7c02885aa066d1e70f38
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2015-02-25 15:16:53 +0100

    Add LAUE hbin writer
    Write esmeralda compatible hbin files
    
    Still work in progress, the format is tricky...
    
    Change-Id: I912045a5c17c630f152a1601c706f5147e92a53e
    Reviewed-on: https://forge.frm2.tum.de/review/7468
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>

--- custom/laue/setups/detector.py
+++ custom/laue/setups/detector.py
@@ -8,10 +8,12 @@ devices = dict(
                  address = 'lauedet.laue.frm2',
                  port = 50000,
                  subdir='',
-                 fileformats = ['raw','fits','tiff'],
+                 fileformats=['raw', 'fits', 'tiff', 'hbin'],
                  lowlevel = False),
     raw = device('devices.fileformats.raw.RAWFileFormat'),
     fits = device('devices.fileformats.fits.FITSFileFormat'),
     tiff = device('laue.lauetiff.TIFFLaueFileFormat',
                   filenametemplate=['%08d.tif']),
+    hbin=device('laue.lauehbin.HBINLaueFileFormat',
+                  filenametemplate=['%08d.hbin']),
     )
