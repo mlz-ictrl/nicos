80a81a9436c72e51f556184e8ea8fa27ebc7b81b
Georg Brandl <georg.brandl@frm2.tum.de>
2015-02-27 12:00:42 +0100

    taco axis: fix HoveringAxis
    Change-Id: Id330eeec5d30a22464ec2a2496830b531cc21b8c
    Reviewed-on: https://forge.frm2.tum.de/review/7497
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos/devices/taco/axis.py
+++ nicos/devices/taco/axis.py
@@ -212,10 +212,14 @@ class HoveringAxis(SequencerMixin, Axis):
             SeqDev(self._adevs['switch'], self.switchvalues[1]),
             SeqSleep(self.startdelay),
             SeqCall(Axis.doStart, self, target),
-            SeqCall(Axis.doWait, self),
+            SeqCall(self._wait),
             SeqSleep(self.stopdelay),
             SeqDev(self._adevs['switch'], self.switchvalues[0]),
         ]
+
+    def _wait(self):
+        while Axis.doStatus(self)[0] == status.BUSY:
+            sleep(self._base_loop_delay)
 
     def doStart(self, target):
         if self._seq_thread and self._seq_thread.isAlive():
