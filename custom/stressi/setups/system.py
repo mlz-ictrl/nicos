744f63c7284a008d5a8ddf16ddba716753590b32
Jens Krüger <jens.krueger@frm2.tum.de>
2015-03-04 06:52:35 +0100

    Add the STRESSI instrument
    The instrument is not fully configured at the moment but
    preconfigured.
    
    Change-Id: I0bf2481e6f5c98b4cb56d24d7d945d37247f65bf
    Reviewed-on: https://forge.frm2.tum.de/review/8379
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/stressi/setups/system.py
@@ -0,0 +1,117 @@
+# description: Description of the setup (detailed)
+description = 'system setup'
+
+# group: Group of the setup. The following groups are recognized:
+# - basic
+#       Basic setup for the instrument, of which only one should be
+#       loaded (e.g. "twoaxis" or "threeaxis"). These setups can be
+#       presented to the user.
+# - optional
+#       Optional setup, of which as many as needed can be loaded.
+#       These setups can be presented to the user for multiple
+#       selection. This is the default.
+# - lowlevel
+#       Low-level setup, which will be included by others, but should
+#       not be presented to users.
+# - special
+#       The setup is not a setup of instrument devices, but configures
+#       e.g. a NICOS service. For each service, there is one special
+#       setup (e.g. "cache", "poller", "daemon").
+group = 'lowlevel'
+
+# sysconfig: A dictionary with basic system configuration values.
+# Possible values:
+#   - cache
+#       A string giving the hostname[:port] of the cache server,
+#       the default port is 14869.
+#       If this value is omitted, no caching will be available.
+#   - instrument
+#       The name of the instrument device, defined somewhere in a
+#       devices dictionary. The class for this device must be
+#       nicos.devices.instrument.Instrument or an instrument-specific
+#       subclass.
+#   - experiment
+#       The name of the experiment "device", defined somewhere in a
+#       devices dictionary. The class for this device must be
+#       nicos.devices.experiment.Experiment or an instrument-specific
+#       subclass.
+#   - datasinks
+#       A list of names of "data sinks", i.e. special devices that
+#       process measured data. These devices must be defined somewhere
+#       in a devices dictionary and be of class
+#       nicos.devices.datasinks.DataSink or a subclass.
+#   - notifiers
+#       A list of names of "notifiers", i.e. special devices that can
+#       notify the user or instrument responsibles via various channels
+#       (e.g. email). These devices must be defined somewhere in a
+#       devices dictionary and be of class
+#       nicos.devices.notifiers.Notifier or a subclass.
+
+sysconfig = dict(
+    cache = 'localhost',
+    instrument = None,
+    experiment = 'Exp',
+    datasinks = ['conssink', 'filesink', 'daemonsink'],
+    notifiers = ['email', 'smser'],
+)
+
+modules = ['nicos.commands.standard']
+
+# devices: Contains all device definitions.
+# A device definition consists of a call like device(classname, parameters).
+# The class name is fully qualified (i.e., includes the package/module name).
+# The parameters are given as keyword arguments.
+devices = dict(
+    Stressi = device('devices.instrument.Instrument',
+                     description = 'instrument object',
+                     instrument = 'STRESSI',
+                     responsible = 'Michael Hofmann <michael.hofmann@frm2.tum.de>',
+                    ),
+
+    Sample   = device('devices.sample.Sample',
+                      description = 'The current used sample',
+                     ),
+
+    # Configure dataroot here (usually /data).
+    Exp      = device('devices.experiment.Experiment',
+                      description = 'experiment object',
+                      dataroot = 'data',
+                      sendmail = True,
+                      serviceexp = '0',
+                      sample = 'Sample',
+                     ),
+
+    filesink = device('devices.datasinks.AsciiDatafileSink',
+                      lowlevel = True,
+                     ),
+
+    conssink = device('devices.datasinks.ConsoleSink',
+                      lowlevel = True,
+                     ),
+
+    daemonsink = device('devices.datasinks.DaemonSink',
+                        lowlevel = True,
+                       ),
+
+    Space    = device('devices.generic.FreeSpace',
+                      description = 'The amount of free space for storing data',
+                      path = None,
+                      minfree = 5,
+                     ),
+
+    # Configure source and copy addresses to an existing address.
+    email    = device('devices.notifiers.Mailer',
+                      sender = 'stressi@frm2.tum.de',
+                      copies = [('michael.hofmann@frm2.tum.de', 'all'),   # gets all messages
+                                ('michael.hofmann@frm2.tum.de', 'important')], # gets only important messages
+                      subject = 'NICOS',
+                      lowlevel = True,
+                     ),
+
+    # Configure SMS receivers if wanted and registered with IT.
+    smser    = device('devices.notifiers.SMSer',
+                      server = 'triton.admin.frm2',
+                      receivers = [],
+                      lowlevel = True,
+                     ),
+)
