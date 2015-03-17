876b27ce00529918310515c559669b94c63fd4ea
Georg Brandl <georg.brandl@frm2.tum.de>
2015-03-17 17:37:54 +0100

    BIODIFF: add reactor setup.
    Change-Id: If0bb86f84646d4f2efa45bcc5361397612566000
    Reviewed-on: https://forge.frm2.tum.de/review/7838
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    (cherry picked from commit 17f2428af26b4045ddd3a936c4eb19b08f09e5f3)
    Reviewed-on: https://forge.frm2.tum.de/review/7840
    Tested-by: Georg Brandl <g.brandl@fz-juelich.de>

--- custom/biodiff/setups/ccd.py
+++ custom/biodiff/setups/ccd.py
@@ -6,7 +6,7 @@ __author__  = "Christian Felder <c.felder@fz-juelich.de>"
 description = "Andor DV936 CCD camera setup"
 group = "basic"
 
-includes = ["counter", "shutter", "microstep"]
+includes = ["counter", "shutter", "microstep", "reactor"]
 
 tango_host = "tango://phys.biodiff.frm2:10000"
 _TANGO_BASE_URL = "%s/biodiff/detector" % tango_host
