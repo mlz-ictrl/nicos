7a1387216a4fb9791375debbcd9d053edc839973
Georg Brandl <g.brandl@fz-juelich.de>
2015-03-24 16:53:09 +0100

    POLI: add lakeshore setup
    Change-Id: I2c6ebdd63a22d03779ae076c7435bb9e6f5e4ae2
    Reviewed-on: https://forge.frm2.tum.de/review/7921
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ custom/poli/setups/lakeshore.py
@@ -0,0 +1,31 @@
+description = 'LakeShore 340 cryo controller'
+group = 'optional'
+
+includes = ['alias_T']
+
+devices = dict(
+    T_ls340   = device('devices.taco.TemperatureController',
+                       description = 'Lakeshore temperature regulation',
+                       tacodevice = '//heidi22/heidi2/ls340/control',
+                       pollinterval = 0.7,
+                       maxage = 2,
+                       abslimits = (0, 300),
+                      ),
+    T_ls340_A = device('devices.taco.TemperatureSensor',
+                       description = 'Lakeshore sensor A',
+                       tacodevice = '//heidi22/heidi2/ls340/sensa',
+                       pollinterval = 0.7,
+                       maxage = 2,
+                      ),
+    T_ls340_B = device('devices.taco.TemperatureSensor',
+                       description = 'Lakeshore sensor B',
+                       tacodevice = '//heidi22/heidi2/ls340/sensb',
+                       pollinterval = 0.7,
+                       maxage = 2,
+                      ),
+)
+
+startupcode = '''
+T.alias = T_ls340
+Ts.alias = T_ls340_A
+'''
