169d907364cd99fae5545c5510197c829d74304e
Georg Brandl <g.brandl@fz-juelich.de>
2015-03-31 09:39:38 +0200

    POLI: add multimeter setup
    Change-Id: I16a92b78190450b994ffffa17c176c0d6bb3c12c
    Reviewed-on: https://forge.frm2.tum.de/review/8399
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

---
+++ custom/poli/setups/multimeter.py
@@ -0,0 +1,9 @@
+description = 'HP34420 multimeter readout'
+group = 'optional'
+
+devices = dict(
+    R = device('devices.taco.AnalogInput',
+               description = 'multimeter readout',
+               tacodevice = '//heidi22/heidi2/fluke/resistance',
+              ),
+)
