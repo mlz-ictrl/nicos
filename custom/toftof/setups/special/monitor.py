cb1c7092ec1e47cf99255008968db2221e56c377
Jens Krüger <jens.krueger@frm2.tum.de>
2015-04-30 17:44:21 +0200

    TOFTOF: Modify the status monitor setup
    Change-Id: I9b137bb92cd194d5e77fd32a0054567801134b20
    Reviewed-on: https://forge.frm2.tum.de/review/8242
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/toftof/setups/special/monitor.py
+++ custom/toftof/setups/special/monitor.py
@@ -2,101 +2,182 @@ description = 'setup for the status monitor'
 
 group = 'special'
 
-_expcolumn = [
-    ('Experiment', [
-        [{'name': 'Proposal', 'key': 'exp/proposal', 'width': 7},
-         {'name': 'Title', 'key': 'exp/title', 'width': 30,
-          'istext': True, 'maxlen': 40},
-         {'name': 'Sample', 'key': 'sample/samplename', 'width': 30,
-          'istext': True},
-         {'name': 'Remark', 'key': 'exp/remark', 'width': 30,
-          'istext': True}
-        ],
+_reactorblock = Block('Reactor', [
+    BlockRow(Field(name='Power', dev='ReactorPower'),
+            ),
+    ],
+)
+
+_expcolumn = Row(
+    Block('Experiment', [
+        BlockRow(Field(name='Proposal', key='exp/proposal', width=7),
+                 Field(name='Title', key='exp/title', width=30, istext=True,
+                       maxlen=40),
+                 Field(name='Sample', key='sample/samplename', width=30,
+                       istext=True, maxlen=40),
+                 Field(name='Last Image file', key='exp/lastimage'),
+                ),
         ],
     ),
-]
-
-_reactorblock = ('Reactor', [
-    [{'name': 'Power', 'dev': 'ReactorPower'}]
-    ],
 )
 
-_chopperblock = ('Chopper system', [
-    [{'name': 'Wavelength', 'dev': 'chWL', 'format': '%.1f'},
-     {'name': 'Nom. Speed', 'dev': 'chSpeed', 'format': '%.0f'},
-     {'name': 'Act. Speed', 'dev': 'ch', 'format': '%.0f'}],
-    [{'name': 'Ratio', 'dev': 'chRatio'},
-     {'name': 'CRC', 'dev': 'chCRC'},
-     {'name': 'Slit type', 'dev': 'chST'}],
+_chopperblock = Block('Chopper system', [
+    BlockRow(Field(name='Wavelength', dev='chWL', format='%.1f'),
+             Field(name='Nom. Speed', dev='chSpeed', format='%.0f'),
+             Field(name='Act. Speed', dev='ch', format='%.0f'),
+            ),
+    BlockRow(Field(name='Ratio', dev='chRatio'),
+             Field(name='CRC', dev='chCRC'),
+             Field(name='Slit type', dev='chST'),
+            ),
     '---',
-    [{'name': 'Disk 1', 'dev': 'chDS', 'item': 0, 'format': '%.1f'},
-     {'name': 'Disk 2', 'dev': 'chDS', 'item': 1, 'format': '%.1f'},
-     {'name': 'Disk 3', 'dev': 'chDS', 'item': 2, 'format': '%.1f'},
-     {'name': 'Disk 4', 'dev': 'chDS', 'item': 3, 'format': '%.1f'}],
-    [{'name': 'Disk 5', 'dev': 'chDS', 'item': 4, 'format': '%.1f'},
-     {'name': 'Disk 6', 'dev': 'chDS', 'item': 5, 'format': '%.1f'},
-     {'name': 'Disk 7', 'dev': 'chDS', 'item': 6, 'format': '%.1f'}],
+    BlockRow(Field(name='Disk 1', dev='chDS', item=0, format='%.1f'),
+             Field(name='Disk 2', dev='chDS', item=1, format='%.1f'),
+             Field(name='Disk 3', dev='chDS', item=2, format='%.1f'),
+             Field(name='Disk 4', dev='chDS', item=3, format='%.1f'),
+            ),
+    BlockRow(Field(name='Disk 5', dev='chDS', item=4, format='%.1f'),
+             Field(name='Disk 6', dev='chDS', item=5, format='%.1f'),
+             Field(name='Disk 7', dev='chDS', item=6, format='%.1f'),
+            ),
+    '---',
+    BlockRow(Field(name='Flow in', dev='flow_in_ch_cooling',),),
+    '---',
+    BlockRow(Field(name='Gauge 1', dev='vac0', format='%.2g'),
+             Field(name='Gauge 2', dev='vac1', format='%.2g'),
+             Field(name='Gauge 3', dev='vac2', format='%.2g'),
+             Field(name='Gauge 4', dev='vac3', format='%.2g'),
+            ),
+    ],
+    setups='measurement',
+)
+
+_choppercoolingblock = Block('Cooling system (Chopper)', [
+    BlockRow(Field(name='T in', dev='t_in_ch_cooling',),
+             Field(name='T out', dev='t_out_ch_cooling',),
+             Field(name='Flow in', dev='flow_in_ch_cooling',),
+             Field(name='P in', dev='p_in_ch_cooling',),
+             Field(name='Leakage', dev='leak_ch_cooling',),
+             Field(name='Cool power', dev='power_ch_cooling',),
+            )
+    ],
+    setups='measurement',
+)
+
+_collimationblock = Block('Radial Collimator/Collimation', [
+    BlockRow(Field(name='Radial', dev='rc',),
+             Field(name='Collimator', dev='ngc',),
+            ),
+    ],
+    setups='measurement',
+)
+
+_samplecoolingblock = Block('Cooling system (Sample)', [
+    BlockRow(Field(name='Flow in', dev='flow_in_sa_cooling',),
+            ),
+    ],
+    setups='biofurnace or ccr* or htf*',
+)
+
+_ccrblock = Block('Sample environment', [
+    BlockRow(Field(name='Stick', dev='T_ccr17_stick',),
+             Field(name='Tube', dev='T_ccr17_tube',),
+             Field(name='Vacuum/Gas', dev='ccr17_p2',)
+            ),
+    ],
+    setups='ccr17',
+)
+
+_htfblock = Block('Sample environment', [
+    BlockRow(Field(name='Sample', dev='T_htf01'),
+             Field(name='Vacuum', dev='htf01_p'),
+            ),
+    ],
+    setups='htf01',
+)
+
+_biofurnaceblock = Block('Sample environment', [
+    BlockRow(Field(name='Sample', dev='T_bio',),)
+    ],
+    setups='biofurnace',
+)
+
+_pressureblock = Block('Sample environment', [
+    BlockRow(Field(name='Pressure', dev='pressure',)),
+    ],
+    setups='pressure',
+)
+
+_ngblock = Block('Neutron Guide', [
+    BlockRow(Field(name='Focus', dev='ng_focus')),
+    ],
+    setups='ng',
+)
+
+_vacuumblock = Block('Vacuum', [
+    BlockRow(Field(name='Gauge 1', dev='vac0', format='%.2g'),
+             Field(name='Gauge 2', dev='vac1', format='%.2g'),
+             Field(name='Gauge 3', dev='vac2', format='%.2g'),
+             Field(name='Gauge 4', dev='vac3', format='%.2g'),
+            ),
     ],
 )
 
-_vacuumblock = ('Vacuum', [
-    [{'name': 'Gauge 1', 'dev': 'vac0', 'format': '%.2g'},
-     {'name': 'Gauge 2', 'dev': 'vac1', 'format': '%.2g'},
-     {'name': 'Gauge 3', 'dev': 'vac2', 'format': '%.2g'},
-     {'name': 'Gauge 4', 'dev': 'vac3', 'format': '%.2g'},
-     ]
+_tableblock = Block('Sample table', [
+    BlockRow(Field(dev='gx'),
+             Field(dev='gy'),
+             Field(dev='gz'),
+            ),
+    BlockRow(Field(dev='gcx'),
+             Field(dev='gcy'),
+             Field(dev='gphi'),
+            ),
+    ]
+)
+
+_slitblock = Block('Sample slit', [
+    BlockRow(Field(dev='slit', istext=True, width=24, name='Slit'))
     ],
 )
 
-_tableblock = ('Sample table', [
-    [{'dev': 'gx'}, {'dev': 'gy'}, {'dev': 'gz'}],
-    [{'dev': 'gcx'}, {'dev': 'gcy'}, {'dev': 'gphi'}],
+_measblock = Block('Measurement', [
+    BlockRow(Field(key='m/timechannels', name='Time channels'),),
+#   BlockRow(Field(name='Last file', key='m/lastfilename'),),
+    BlockRow(Field(key='m/laststats', item=0, name='Time', format='%.1f'),
+             Field(key='m/laststats', item=1, name='Monitor', format='%d'),
+             Field(key='m/laststats', item=2, name='Counts', format='%d'),
+            ),
+    BlockRow(Field(key='m/laststats', item=0, name='Monitor rate', format='%.1f'),
+             Field(key='m/laststats', item=0, name='Count rate', format='%.1f'),
+            ),
     ],
 )
 
-_slitblock = ('Sample slit', [
-    [{'dev': 'slit', 'istext': True, 'width': 24, 'name': 'Slit'}]
+_rateblock = Block('Detector rates', [
+    BlockRow(Field(gui='custom/toftof/lib/gui/ratespanel.ui')),
     ],
+    setups='measurement',
 )
 
-_measblock = ('Measurement', [
-    [{'key': 'm/timechannels', 'name': 'Time channels'},
-     {'name': 'Last file', 'key': 'm/lastfilename'}],
-    [{'key': 'm/laststats', 'item': 0, 'name': 'Time', 'format': '%.1f'},
-     {'key': 'm/laststats', 'item': 1, 'name': 'Monitor', 'format': '%d'},
-     {'key': 'm/laststats', 'item': 2, 'name': 'Counts', 'format': '%d'}],
-    [{'key': 'm/laststats', 'item': 0, 'name': 'Monitor rate', 'format': '%.1f'},
-     {'key': 'm/laststats', 'item': 0, 'name': 'Count rate', 'format': '%.1f'}],
-    ],
-)
-
-_cryoblock = ('Cryo', [
-    [{'key': 'cryo/setpoint', 'name': 'Setpoint'},
-     {'dev': 'cryo_b', 'name': 'Sample'},
-     {'dev': 'cryo_a', 'name': 'Sensor A'}],
-    [{'key': 'cryo/p', 'name': 'P'}, {'key': 'cryo/i', 'name': 'I'},
-     {'key': 'cryo/d', 'name': 'D'}, {'dev': 'cryo_p', 'name': 'pressure'}]
-    ],
-)
-
-_col1 = [
-    _reactorblock,
-    _chopperblock,
-    _vacuumblock,
-]
-
-_col2 = [
-    _tableblock,
-    _slitblock,
+_col1 = Column(
     _measblock,
-]
+    _chopperblock,
+)
 
-_col3 = [
-    _cryoblock,
-#    _he3block,
-#    _htfblock,
-#    _stickblock,
-]
+_col2 = Column(
+    _collimationblock,
+    _reactorblock,
+    _rateblock,
+)
+
+_col3 = Column(
+    _ngblock,
+    _samplecoolingblock,
+    _ccrblock,
+    _htfblock,
+    _biofurnaceblock,
+    _pressureblock,
+)
 
 devices = dict(
     Monitor = device('services.monitor.qt.Monitor',
@@ -106,7 +187,7 @@ devices = dict(
                      prefix = 'nicos/',
                      font = 'Ubuntu',
                      valuefont = 'DejaVu Sans Mono',
-                     padding = 5,
+                     padding = 2,
                      layout = [[_expcolumn], [_col1, _col2, _col3]],
                     ),
 )
