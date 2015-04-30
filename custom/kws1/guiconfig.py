94ea4ba8955c27a1fcd00331d1d472f481ea8a73
Georg Brandl <georg.brandl@frm2.tum.de>
2015-04-30 13:53:18 +0200

    Initial custom directory for KWS-1.
    Change-Id: I1e24ecc94ffff87498f5f99c05c252f0fa0e6953
    Reviewed-on: https://forge.frm2.tum.de/review/8326
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ custom/kws1/guiconfig.py
@@ -0,0 +1,76 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2015 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Georg Brandl <georg.brandl@frm2.tum.de>
+#
+# *****************************************************************************
+
+"""NICOS GUI default configuration."""
+
+from nicos.clients.gui.config import vsplit, window, panel, tool, docked
+# additional may be used also : tabbed, setups
+
+main_window = docked(
+    vsplit(
+        panel('status.ScriptStatusPanel'),
+        # panel('watch.WatchPanel'),
+        panel('console.ConsolePanel'),
+    ),
+    ('NICOS devices',
+     panel('nicos.clients.gui.panels.devices.DevicesPanel',
+           icons=True, dockpos='right',
+          )
+    ),
+    ('Experiment Information and Setup',
+     panel('nicos.clients.gui.panels.expinfo.ExpInfoPanel',
+       )),
+)
+
+windows = [
+    window('Editor', 'editor', panel('editor.EditorPanel')),
+    window('Scans', 'plotter', panel('scans.ScansPanel')),
+    window('History', 'find', panel('history.HistoryPanel')),
+    window('Logbook', 'table', panel('elog.ELogPanel')),
+    window('Log files', 'table', panel('logviewer.LogViewerPanel')),
+    window('Errors', 'errors', panel('errors.ErrorPanel')),
+    #window('Live data', 'live', panel('live.LiveDataPanel')),
+]
+
+tools = [
+    tool('Downtime report', 'downtime.DownTimeTool',
+# If not at the FRM-II facility you have to change this reporting address
+         receiver='f.carsughi@fz-juelich.de',
+# If you are not at the FRM-II facility you have to change your mail server
+         mailserver='smtp.frm2.tum.de',
+# Please change the sender address to a valid, instrument specific address
+         sender='demo@frm2.tum.de',
+        ),
+    tool('Calculator', 'calculator.CalculatorTool'),
+    tool('Neutron cross-sections', 'website.WebsiteTool',
+         url='http://www.ncnr.nist.gov/resources/n-lengths/'),
+    tool('Neutron activation', 'website.WebsiteTool',
+         url='https://webapps.frm2.tum.de/intranet/activation/'),
+    tool('Neutron calculations',
+         'nicos.clients.gui.tools.website.WebsiteTool',
+         url='https://webapps.frm2.tum.de/intranet/neutroncalc/'),
+    tool('Report NICOS bug or request enhancement', 'bugreport.BugreportTool'),
+    tool('Emergency stop button', 'estop.EmergencyStopTool',
+         runatstartup=False),
+]
---
+++ custom/kws1/setups/system.py
@@ -0,0 +1,67 @@
+description = 'system setup'
+
+group = 'lowlevel'
+
+sysconfig = dict(
+    cache = 'localhost',
+    instrument = None,
+    experiment = 'Exp',
+    datasinks = ['conssink', 'filesink', 'daemonsink'],
+    notifiers = ['email'],
+)
+
+modules = ['nicos.commands.standard']
+
+devices = dict(
+    KWS1     = device('devices.instrument.Instrument',
+                      description = 'KWS-1 instrument',
+                      instrument = 'KWS-1',
+                      responsible = 'H. Frielinghaus <h.frielinghaus@fz-juelich.de>',
+                     ),
+
+    Sample   = device('devices.sample.Sample',
+                      description = 'Sample object',
+                     ),
+
+    Exp      = device('devices.experiment.Experiment',
+                      description = 'experiment object',
+                      dataroot = 'data',
+                      sendmail = True,
+                      serviceexp = '0',
+                      sample = 'Sample',
+                     ),
+
+    filesink = device('devices.datasinks.AsciiDatafileSink',
+                      lowlevel = True,
+                     ),
+
+    conssink = device('devices.datasinks.ConsoleSink',
+                      lowlevel = True,
+                     ),
+
+    daemonsink = device('devices.datasinks.DaemonSink',
+                        lowlevel = True,
+                       ),
+
+    Space    = device('devices.generic.FreeSpace',
+                      description = 'The amount of free space for storing data',
+                      path = None,
+                      minfree = 5,
+                     ),
+
+    # Configure source and copy addresses to an existing address.
+    email    = device('devices.notifiers.Mailer',
+                      sender = 'kws1@frm2.tum.de',
+                      copies = [('g.brandl@fz-juelich.de', 'all'),   # for now
+                               ],
+                      subject = '[KWS-1]',
+                      lowlevel = True,
+                     ),
+
+    # Configure SMS receivers if wanted and registered with IT.
+    smser    = device('devices.notifiers.SMSer',
+                      server = 'triton.admin.frm2',
+                      receivers = [],
+                      lowlevel = True,
+                     ),
+)
