361f5c44abd70a8b5ad00c2fd23d81cf10ab1ef9
Sans1 People <sans1@frm2.tum.de>
2015-05-05 15:50:52 +0200

    SANS-1: add wut box
    Change-Id: I789a8dd28746bf2b55ce9576c79ab2e976911726
    Reviewed-on: https://forge.frm2.tum.de/review/8357
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Tested-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

---
+++ custom/sans1/lib/wut.py
@@ -0,0 +1,117 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2015 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Andreas Wilhelm <andreas.wilhelm@frm2.tum.de>
+#
+# *****************************************************************************
+
+"""Show values read out from a w&t web UI.
+
+w&t box shows pressore before and after the filter at the instrument.
+"""
+
+from nicos.core import Param, Readable, Override, CommunicationError, \
+     ConfigurationError, NicosError, status
+
+try:
+    from urllib2 import urlopen
+except ImportError:
+    urlopen = None
+
+
+class WutValue(Readable):
+
+    parameters = {
+        'hostname':     Param('Host name of the wut site',
+                              type=str, mandatory=True),
+        'port':         Param('Port of the sensor',
+                              type=str, mandatory=True),
+    }
+
+    parameter_overrides = {
+        'unit':         Override(mandatory=False),
+        'pollinterval': Override(default=60),
+        'maxage':       Override(default=125),
+    }
+
+    def _getRaw(self):
+        if urlopen is None:
+            raise NicosError(self, 'cannot parse web page, urllib2 is not '
+                             'installed on this system')
+        url = 'http://%s/Single%s' % (self.hostname, self.port)
+        try:
+            response = urlopen(url)
+            html = response.read()
+            return html
+        except ConfigurationError:  # pass through error raised above
+            raise
+        except Exception as err:
+            raise CommunicationError(self, 'wut-box not responding or '
+                                     'changed format: %s' % err)
+
+    def doReadUnit(self):
+        return self._getRaw().split(';')[-1].split(' ')[-1]
+
+    def doRead(self, maxage=0):
+        return float(self._getRaw().split(';')[-1].split(' ')[0].replace(',', '.'))
+
+    def doStatus(self, maxage=0):
+        return status.OK, ''
+
+
+class WutDiff(Readable):
+
+    parameters = {
+        'hostname':     Param('Host name of the wut site',
+                              type=str, mandatory=True),
+    }
+
+    parameter_overrides = {
+        'unit':         Override(mandatory=False),
+        'pollinterval': Override(default=60),
+        'maxage':       Override(default=125),
+    }
+
+    def _getRaw(self):
+        if urlopen is None:
+            raise NicosError(self, 'cannot parse web page, urllib2 is not '
+                             'installed on this system')
+        url1 = 'http://%s/Single1' % (self.hostname)
+        url2 = 'http://%s/Single2' % (self.hostname)
+        try:
+            response1 = urlopen(url1)
+            response2 = urlopen(url2)
+            html = [response1.read(), response2.read()]
+            return html
+        except ConfigurationError:  # pass through error raised above
+            raise
+        except Exception as err:
+            raise CommunicationError(self, 'wut-box not responding or '
+                                     'changed format: %s' % err)
+
+    def doReadUnit(self):
+        return self._getRaw()[0].split(';')[-1].split(' ')[-1]
+
+    def doRead(self, maxage=0):
+        return (float(self._getRaw()[0].split(';')[-1].split(' ')[0].replace(',', '.'))-
+              float(self._getRaw()[1].split(';')[-1].split(' ')[0].replace(',', '.')))
+
+    def doStatus(self, maxage=0):
+        return status.OK, ''
---
+++ custom/sans1/setups/wut.py
@@ -0,0 +1,56 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2014 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Andreas Wilhelm <andreas.wilhelm@frm2.tum.de>
+#
+# *****************************************************************************
+
+description = 'wut readout'
+
+includes = []
+
+#group = 'lowlevel'
+
+devices = dict(
+    p_in_wut = device('sans1.wut.WutValue',
+                            hostname = 'sans1wut.office.frm2',
+                            port = '1',
+                            description = 'pressure in front of filter',
+                            fmtstr = '%.2F',
+                            lowlevel = False,
+                            loglevel = 'info',
+    ),
+    p_out_wut = device('sans1.wut.WutValue',
+                            hostname = 'sans1wut.office.frm2',
+                            port = '2',
+                            description = 'pressure behind of filter',
+                            fmtstr = '%.2F',
+                            lowlevel = False,
+                            loglevel = 'info',
+    ),
+    p_diff_wut = device('sans1.wut.WutDiff',
+                            hostname = 'sans1wut.office.frm2',
+                            description = 'pressure in front of filter minus pressure behind filter',
+                            fmtstr = '%.2F',
+                            lowlevel = False,
+                            loglevel = 'info',
+    ),
+)
+
