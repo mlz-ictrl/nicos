e94cbd8a90307fcaaa765e131544db82fb56aee6
Jens Krüger <jens.krueger@frm2.tum.de>
2015-05-05 14:11:03 +0200

    TOFTOF: rename the temperature device in biofurnace setup
    Change-Id: Ia9809934dc4a7761d4cc4e54896f09e545b94666
    Reviewed-on: https://forge.frm2.tum.de/review/8265
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/toftof/setups/biofurnace.py
+++ custom/toftof/setups/biofurnace.py
@@ -7,19 +7,19 @@ includes = ['alias_T']
 nethost = 'toftofsrv.toftof.frm2'
 
 devices = dict(
-    bio = device('toftof.julabo.Controller',
-                 description = 'Julabo temperature controller',
-                 tacodevice = '//%s/toftof/rs232/ifbiofurnace' % (nethost,),
-                 intern_extern = 0,
-                 userlimits = (-40, 160),
-                 abslimits = (-50, 200),
-                 unit = 'degC',
-                 fmtstr = '%g',
-                ),
+    T_bio = device('toftof.julabo.Controller',
+                   description = 'Julabo temperature controller',
+                   tacodevice = '//%s/toftof/rs232/ifbiofurnace' % (nethost,),
+                   intern_extern = 0,
+                   userlimits = (-40, 160),
+                   abslimits = (-50, 200),
+                   unit = 'degC',
+                   fmtstr = '%g',
+                  ),
 )
 
 startupcode = """
-Ts.alias = bio
-T.alias = bio
+Ts.alias = T_bio
+T.alias = T_bio
 SetEnvironment(Ts, T)
 """
