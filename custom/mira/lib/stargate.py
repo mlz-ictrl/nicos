c46e46a6e63ffe3e831216b0a7f9d0a9246d7c09
Georg Brandl <georg.brandl@frm2.tum.de>
2015-05-05 14:21:19 +0200

    MIRA: implement stargate
    Change-Id: I86c32ba58a4c38e874c6cf93e4c25554d3b174c3
    Reviewed-on: https://forge.frm2.tum.de/review/8266
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ custom/mira/lib/stargate.py
@@ -0,0 +1,157 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2015 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#    Tobias Weber <tweber@frm2.tum.de>
+#
+# *****************************************************************************
+
+"""Mira-Stargate.
+
+This is the shielding of the analyzer with 11 blocks.  The att axis does not
+move any elements under the blocks, so we can move to a new block state at
+any time (in this implementation, before starting the axis).
+
+Only 0, 1 or 2 blocks may be opened at a time.  The first and last block should
+not be opened since they are stationary.
+
+The blocks are controlled via a Festo valve arrangement of 11 stable valves
+represented by two bits that can be moved into open (01) or closed (10)
+positions.
+
+Festo uses Modbus, and the 22 needed output bits are distributed in the lower 8
+bits of three consecutive 16-bit holding registers (offset_out).  Readback is
+done in three different holding registers with addresses n, n+2, n+4.
+"""
+
+from time import time as currenttime
+
+from nicos.core import Param, listof, status, InvalidValueError
+from nicos.devices.taco.axis import HoveringAxis
+from nicos.devices import tango
+
+
+class Stargate(tango.DigitalOutput):
+    """Device for controlling the MIRA-Stargate blocks."""
+
+    valuetype = listof(int)
+
+    parameters = {
+        'offset_in':  Param('Offset of digital input values',
+                            type=int, mandatory=True),
+        'offset_out': Param('Offset of digital output values',
+                            type=int, mandatory=True),
+        'chevron_att_angles': Param('att angle for shielding elements',
+                             type=listof(listof(int)), mandatory=True),
+    }
+
+    _started = 0
+
+    def doRead(self, maxage=0):
+        words = self._dev.ReadOutputWords([self.offset_in, 5])
+        bitvals = [words[0], words[2], words[4]]
+        chevrons = []
+
+        for bitval in bitvals:
+            for _ in range(4):
+                chevrons.append(int(bitval & 0b11 == 0b01))
+                bitval >>= 2
+
+        return chevrons[:11]
+
+    def doStatus(self, maxage=0):  # pylint: disable=W0221
+        return status.OK, ''
+
+    def doIsCompleted(self):
+        return not self._started or self._started + 3 < currenttime()
+
+    def doStart(self, value):
+        bitvals = [0, 0, 0]
+        for curidx in range(len(value)):
+            curval = value[curidx]
+
+            byteidx = curidx / 4
+            bitidx = (curidx % 4) * 2
+
+            if curval:
+                bitvals[byteidx] |= (1<<bitidx)
+            else:
+                bitvals[byteidx] |= (1<<(bitidx+1))
+
+        self._dev.WriteOutputWords([self.offset_out] + bitvals)
+        self._started = currenttime()
+
+    def doIsAllowed(self, value):
+        if len(value) != 11:
+            raise InvalidValueError(self, 'list must have 11 entries')
+        # map everything to 0 or 1
+        value = map(bool, value)
+        # check allowed positions
+        if value == [True] * 11:
+            # open everything is allowed
+            return True, ''
+        if sum(value) > 2:
+            return False, 'cannot open more than 2 chevrons'
+        if value[0] or value[10]:
+            return False, 'cannot open first or last chevron'
+        return True, ''
+
+    def doReadFmtstr(self):
+        return '[' + ', '.join(['%d'] * 11) + ']'
+
+    def get_chevrons_for_att(self, att):
+        chevrons = []
+
+        for curidx in range(len(self.chevron_att_angles)):
+            maxmin = self.chevron_att_angles[curidx]
+
+            if(len(maxmin) < 2):
+                chevrons.append(0)
+                continue
+
+            if(att < maxmin[0] and att > maxmin[1]):
+                chevrons.append(1)
+            else:
+                chevrons.append(0)
+
+        return chevrons
+
+
+class ATT(HoveringAxis):
+    attached_devices = {
+        'stargate': (Stargate, 'stargate switch device'),
+    }
+
+    @property
+    def stargate(self):
+        return self._adevs['stargate']
+
+    def doStart(self, value):
+        self.stargate.start(self.stargate.get_chevrons_for_att(value))
+        HoveringAxis.doStart(self, value)
+
+    def doStatus(self, maxage=0):
+        retval = HoveringAxis.doRead(self, maxage)
+        chevrons = list(self.stargate.read(maxage))
+        if chevrons != self.stargate.get_chevrons_for_att(retval):
+            return status.ERROR, 'invalid stargate position for att angle'
+        return HoveringAxis.doStatus(self, maxage)
+
+    def doIsCompleted(self):
+        return HoveringAxis.doIsCompleted(self) and self.stargate.isCompleted()
--- custom/mira/setups/analyzer.py
+++ custom/mira/setups/analyzer.py
@@ -26,8 +26,9 @@ devices = dict(
                       precision = 0.005,
                      ),
 
-    att      = device('devices.taco.HoveringAxis',
+    att      = device('mira.stargate.ATT',
                       description = 'analyzer two-theta angle',
+                      stargate = 'stargate',
                       tacodevice = '//mirasrv/mira/axis/att',
                       abslimits = (-90 - 135, -90 + 135),
                       startdelay = 1,
@@ -47,5 +48,26 @@ devices = dict(
                       abslimits = (0.1, 10),
                       dvalue = 3.355,
                       scatteringsense = -1,
+                     ),
+
+    stargate = device('mira.stargate.Stargate',
+                      description = 'Mira-Stargate (i.e. analyser shielding blocks)',
+                      tangodevice = 'tango://mira1.mira.frm2:10000/mira/anablocks/mb1',
+                      offset_out = 40003,
+                      offset_in = 45395,
+                      chevron_att_angles =
+                      [
+                          [],               # 1
+                          [150, 78.5],      # 2,  mid: 120.5
+                          [120.5, 62.5],    # 3,  mid: 78.5
+                          [78.5, 32.5],     # 4,  mid: 62.5
+                          [62.5, 2.],       # 5,  mid: 32.5
+                          [32.5, -28],      # 6,  mid: 2
+                          [2., -59.5],      # 7,  mid: -28
+                          [-28, -90],       # 8,  mid: -59.5
+                          [-59.5, -120.5],  # 9,  mid: -90
+                          [-90., -150.],    # 10, mid: -120.5
+                          [],
+                      ],
                      ),
 )
