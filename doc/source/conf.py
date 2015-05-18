582c8b63d7486bf078e6dbb9b84d94f989e09351
Alexander Lenz <alexander.lenz@frm2.tum.de>
2015-05-18 14:17:52 +0200

    doc: add default theme specification.
    Change-Id: I8f321677b882a2441a268d181edd07f55d180e82
    Reviewed-on: https://forge.frm2.tum.de/review/8375
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -27,6 +27,8 @@ html_last_updated_fmt = '%F %H:%M'
 _dark  = '#0b2c47'
 _light = '#175791'
 
+html_theme = 'default'
+
 html_theme_options = {'sidebarbgcolor': '#EDF1F3',
                       'relbarbgcolor': '#DBDEDE',
                       'relbartextcolor': 'black',
