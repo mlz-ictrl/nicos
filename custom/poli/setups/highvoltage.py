ebd133479a8a367bcdc05585c06c6a4465baf6f7
Christian Felder <c.felder@fz-juelich.de>
2015-05-28 09:36:59 +0200

    POLI: highvoltage: Add fug device.
    Change-Id: Id08a1d5c6cd836329b4b541cf3e60cf9e35676b4
    Reviewed-on: https://forge.frm2.tum.de/review/8452
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

---
+++ custom/poli/setups/highvoltage.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+__author__  = "Christian Felder <c.felder@fz-juelich.de>"
+
+
+description = "High voltage setup"
+group = "optional"
+
+nethost = "heidi22.poli.frm2"
+
+devices = dict(
+    fug = device("devices.taco.VoltageSupply",
+                 description = "High voltage",
+                 tacodevice = "//%s/heidi2/fug/ctrl" % nethost,
+                 abslimits = (-3.1, 3.1),
+                 unit = "kV",
+                 pollinterval = 5,
+                 maxage = 6,
+                ),
+)
+
