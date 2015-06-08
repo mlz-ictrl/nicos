c4c1e8125ac19fb64e86b0325bd8cf5df8033f00
Stefan Rainow <s.rainow@fz-juelich.de>
2015-06-08 11:34:56 +0200

    SPHERES: Added mesydaq detector setup
    Added a setup to read the mesydaq detector.
    
    There will be a monitor at some point int he future.
    Currently there is none.
    
    Change-Id: Ia5b9be60ce8acae9400d872f63a50284f806ab1a
    Reviewed-on: https://forge.frm2.tum.de/review/8553
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Stefan Rainow <s.rainow@fz-juelich.de>

---
+++ custom/spheres/setups/qmesydaq.py
@@ -0,0 +1,36 @@
+description = 'qmesydaq channel devices'
+
+group = 'optional'
+
+nethost = 'mesydaq.spheres.frm2'
+qm = '//%s/spheres/qmesydaq/' % nethost
+
+devices = dict(
+#    mon1 = device('nicos.devices.vendor.qmesydaq.taco.Counter',
+#                  description = 'QMesyDAQ Counter',
+#                  tacodevice = qm + 'counter0',
+#                  type = 'monitor',
+#                 ),
+    events = device('nicos.devices.vendor.qmesydaq.taco.Counter',
+                    description = 'QMesyDAQ Events channel',
+                    tacodevice = qm + 'events',
+                    type = 'counter',
+                    ),
+    timer = device('nicos.devices.vendor.qmesydaq.taco.Timer',
+                   description = 'QMesyDAQ Timer',
+                   tacodevice = qm + 'timer',
+                  ),
+    image = device('nicos.devices.vendor.qmesydaq.taco.Image',
+                   description = 'QMesyDAQ MultiChannel Detector',
+                   tacodevice = qm + 'image',
+                  ),
+    det = device('devices.generic.Detector',
+                 description = 'QMesyDAQ Image type Detector',
+                 timers = ['timer'],
+                 counters = ['events'],
+                 monitors = [],
+                 images = ['image'],
+                 fileformats = [],
+                 subdir = '',
+                ),
+)
