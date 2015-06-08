a5bc3b0cfece7260e15609545cbc42e4518d2720
Sans1 People <sans1@frm2.tum.de>
2015-06-08 11:27:26 +0200

    SANS-1: change dia10 position of att switcher
    A. Wilhelm
    
    Change-Id: I5c4e7f143e950bb5b77e5286b942c9f7ddc0b7cb
    Reviewed-on: https://forge.frm2.tum.de/review/8559
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/sans1/setups/collimation.py
+++ custom/sans1/setups/collimation.py
@@ -78,7 +78,8 @@ devices = dict(
     att        = device('sans1.collimotor.Sans1ColliSwitcher',
                         description = 'Attenuator',
                         #mapping = dict(OPEN=0, x1000=117, x100=234, x10=351), old 4 position att
-                        mapping = dict(open=0, x1000=95, x100=190, x10=285, dia10=380), #new 5 position att
+                        #mapping = dict(open=0, x1000=95, x100=190, x10=285, dia10=380), #new 5 position att
+                        mapping = dict(open=0, x1000=95, x100=190, x10=285, dia10=379.85), #new att_m position!!!
                         moveable = 'att_m',
                         blockingmove = False,
                         pollinterval = 15,
