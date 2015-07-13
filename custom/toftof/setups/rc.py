866672a7ba86e744c7912191c0f726e77190aa9d
Alexander Lenz <alexander.lenz@frm2.tum.de>
2015-07-13 11:26:14 +0200

    toftof: switch to new rc control.
    Change-Id: Id5dc53ce8da07915488081febf55069e755c95f9
    Reviewed-on: https://forge.frm2.tum.de/review/8848
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    Tested-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    (cherry picked from commit c4afef2e587e398d8717e6af8894a10c02763005)
    Reviewed-on: https://forge.frm2.tum.de/review/8893
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/toftof/setups/rc.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+description = 'TOFTOF radial collimator'
+
+group = 'optional'
+
+includes = []
+
+tango_host = 'tango://tofhw.toftof.frm2:10000'
+
+devices = dict(
+    rc_onoff = device('devices.tango.NamedDigitalOutput',
+                        description = 'Activates radial collimator',
+                        tangodevice = '%s/toftof/rc/_rc_onoff' % tango_host,
+                        mapping = {
+                            'on': 1,
+                            'off': 0,
+                        },
+                      ),
+
+    rc_motor = device('devices.tango.AnalogOutput',
+                        description = 'Radial collimator motor',
+                        tangodevice = '%s/toftof/rc/_rc_motor' % tango_host,
+                        lowlevel = True,
+                     ),
+)
+
+
+startupcode = '''
+'''
