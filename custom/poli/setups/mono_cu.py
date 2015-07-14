fa54e07db8b392ea9a2cd36151105bd79c616354
Christian Felder <c.felder@fz-juelich.de>
2015-07-14 16:25:39 +0200

    POLI: Use abslimits from tango server.
    Change-Id: I4777a2ba481090cfa724a1c56026374c0d4a030f
    Reviewed-on: https://forge.frm2.tum.de/review/8866
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

--- custom/poli/setups/mono_cu.py
+++ custom/poli/setups/mono_cu.py
@@ -12,16 +12,12 @@ devices = dict(
                  description = 'Copper monochromator horizontal focus',
                  tangodevice = '%s/poli/fzjs7/cuh' % (tango_host, ),
                  fmtstr = '%.2f',
-                 abslimits = (-75, 75),
                 ),
     cuv = device('devices.tango.Motor',
                  lowlevel = False,
                  description = 'Copper monochromator vertical focus',
                  tangodevice = '%s/poli/fzjs7/cuv' % (tango_host, ),
                  fmtstr = '%.2f',
-                 # abslimits = (0, 171.2),
-                 # until limit switches are fixed...
-                 abslimits = (45.0, 171.2),
                 ),
 )
 
