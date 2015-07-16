2c94a4c2cb64d35bc330c5dbf5b994d6e9cf3aae
Jens Krüger <jens.krueger@frm2.tum.de>
2015-07-16 12:56:41 +0200

    TOFTOF: Reformat setup
    Change-Id: I826df1ee4c4557c8ff2256367935b6a9fb9af277
    Reviewed-on: https://forge.frm2.tum.de/review/8888
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/toftof/setups/rc.py
+++ custom/toftof/setups/rc.py
@@ -6,22 +6,22 @@ group = 'optional'
 
 includes = []
 
-tango_host = 'tango://tofhw.toftof.frm2:10000'
+tango_host = 'tofhw.toftof.frm2:10000'
 
 devices = dict(
     rc_onoff = device('devices.tango.NamedDigitalOutput',
-                        description = 'Activates radial collimator',
-                        tangodevice = '%s/toftof/rc/_rc_onoff' % tango_host,
-                        mapping = {
-                            'on': 1,
-                            'off': 0,
-                        },
-                      ),
+                      description = 'Activates radial collimator',
+                      tangodevice = 'tango://%s/toftof/rc/_rc_onoff' % tango_host,
+                      mapping = {
+                          'on': 1,
+                          'off': 0,
+                      },
+                     ),
 
     rc_motor = device('devices.tango.AnalogOutput',
-                        description = 'Radial collimator motor',
-                        tangodevice = '%s/toftof/rc/_rc_motor' % tango_host,
-                        lowlevel = True,
+                      description = 'Radial collimator motor',
+                      tangodevice = 'tango://%s/toftof/rc/_rc_motor' % tango_host,
+                      lowlevel = True,
                      ),
 )
 
