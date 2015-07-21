e97f2f36eae6a1cc6bacbcca278e2548859c47f8
Jens Krueger <jens.krueger@tum.de>
2015-07-21 09:46:49 +0200

    PGAA: Clean up setup files
    Remove the copyright header from setup files
    
    Change-Id: I0d105c913a7e1400520350c49690c170ea6c8cee
    Reviewed-on: https://forge.frm2.tum.de/review/8947
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/pgaa/setups/sample.py
+++ custom/pgaa/setups/sample.py
@@ -1,27 +1,3 @@
-#  -*- coding: utf-8 -*-
-# *****************************************************************************
-# NICOS, the Networked Instrument Control System of the FRM-II
-# Copyright (c) 2009-2015 by the NICOS contributors (see AUTHORS)
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
-# details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-# Module authors:
-#   Georg Brandl <georg.brandl@frm2.tum.de>
-#
-# *****************************************************************************
-
 description = 'sample table devices'
 
 # includes = ['system']
@@ -33,8 +9,10 @@ devices = dict(
                            tacodevice = '//%s/pgaa/phytronixe/sample' % (nethost,),
                            fmtstr = '%7.3f',
                            abslimits = (-5, 356),
+                           lowlevel = True,
                           ),
     sample = device('devices.generic.Switcher',
+                    description = 'Sample switcher',
                     moveable = 'sample_motor',
                     mapping  = {'1' : 4.00,
                                 '2' : 74.00,
@@ -47,20 +25,25 @@ devices = dict(
                     blockingmove = False,
                     unit = '',
                    ),
-    e1    = device('devices.taco.Coder',
-                   tacodevice = '//%s/pgaa/phytronixe/e1' % (nethost,),
-                   fmtstr = '%7.3f',
-                  ),
-    ellip = device('devices.taco.DigitalInput',
-                   tacodevice = '//%s/pgaa/phytronixe/ellip' % (nethost,),
-                  ),
-    ftube = device('devices.taco.DigitalInput',
-                   tacodevice = '//%s/pgaa/phytronixe/ftube' % (nethost,),
-                  ),
-    press1 = device('devices.taco.DigitalInput',
-                    tacodevice = '//%s/pgaa/phytronixe/press1' % (nethost,),
-                   ),
-    press2 = device('devices.taco.DigitalInput',
-                    tacodevice = '//%s/pgaa/phytronixe/press2' % (nethost,),
-                   ),
+#   e1    = device('devices.taco.Coder',
+#                  description = '',
+#                  tacodevice = '//%s/pgaa/phytronixe/e1' % (nethost,),
+#                  fmtstr = '%7.3f',
+#                 ),
+#   ellip = device('devices.taco.DigitalInput',
+#                  description = '',
+#                  tacodevice = '//%s/pgaa/pgai/ellip' % (nethost,),
+#                 ),
+#   ftube = device('devices.taco.DigitalInput',
+#                  description = '',
+#                  tacodevice = '//%s/pgaa/pgai/ftube' % (nethost,),
+#                 ),
+#   press1 = device('devices.taco.DigitalInput',
+#                   description = '',
+#                   tacodevice = '//%s/pgaa/pgai/press1' % (nethost,),
+#                  ),
+#   press2 = device('devices.taco.DigitalInput',
+#                   description = '',
+#                   tacodevice = '//%s/pgaa/pgai/press2' % (nethost,),
+#                  ),
 )
