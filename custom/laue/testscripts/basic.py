014e15f1ca04255a97771cb8d52f5170e99e40cf
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2015-09-14 11:38:50 +0200

    Add basic laue test scripts
    Change-Id: I471868cee2094aa719f8dcd2e5456cf034ea3588
    Fixes: #2319
    Reviewed-on: https://forge.frm2.tum.de/review/9209
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>

---
+++ custom/laue/testscripts/basic.py
@@ -0,0 +1,8 @@
+# pylint: skip-file
+
+# read all devices
+read()
+
+# count: Manually check if image looks OK (liveView, tif with external app)
+count(10)
+
---
+++ custom/laue/testscripts/scan.py
@@ -0,0 +1,9 @@
+# pylint: skip-file
+
+# basic scan:
+
+for x in range(-10,10,1):
+    maw(stx,x)
+    count(10)
+
+
