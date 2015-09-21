1a768ac8d8f3baad22779b7ea5a5ebdf9b43a215
Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
2015-09-21 18:08:07 +0200

    CCMHTS: set calibration data
    Change-Id: Ic8eb78b17ada80989968421d769a7f1553a15a8b
    fixes: #2383
    Reviewed-on: https://forge.frm2.tum.de/review/9572
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/frm2/setups/ccmhts01.py
+++ custom/frm2/setups/ccmhts01.py
@@ -8,13 +8,26 @@ taco_host = setupname
 
 devices = dict(
     # by convention this needs to be B_%(setupname)s
-    B_ccmhts01  = device('devices.taco.CurrentSupply',
-                         description = 'magnetic field device',
+    I_ccmhts01  = device('devices.taco.CurrentSupply',
+                         description = 'current device',
                          tacodevice = '//%s/magnet/kepco/current' % taco_host,
                          unit = 'A',
                          abslimits = (-210, 210),
                         ),
+    B_ccmhts01  = device('devices.generic.magnet.CalibratedMagnet',
+                         description = 'magnetic field device',
+                         currentsource = 'I_ccmhts01',
+                         unit = 'mT',
+                         abslimits = (-2250, 2250),
+                         # curve from Juergen Peters
+                         calibration = [9.52412, 1.9862, 31.6875, 195.56, 0.01228],
+                         # calibration = [9.49484, 0, 0, 201.5, 0.011742],
+                         # curve from Vladimir Hutanu
+                         # calibration = [7.67752, 239.074, 0.00822055, 6.14579, 0.913737],
+                        ),
+
 )
 alias_config = {
-    'B': {'B_ccmhts01': 100},
+    # I is included for the rare case you want to control the current directly instead of field.
+    'B': {'B_ccmhts01': 100, 'I_ccmhts01': 90},
 }
