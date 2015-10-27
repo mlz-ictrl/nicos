7f0f2f43005ebdea0a25056d94a329c190ee80ca
Antares People <antares@frm2.tum.de>
2015-10-27 10:35:29 +0100

    antares: added He pressure sensor
    Added newly installed He pressure sensor to center3 setup
    
    Change-Id: Id1f8bf56bc4ae6e37d9eb8dcafaea6316cb35795
    Reviewed-on: https://forge.frm2.tum.de/review/9577
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>

--- custom/antares/setups/center3.py
+++ custom/antares/setups/center3.py
@@ -1,6 +1,8 @@
-description = 'Center 3'
+description = 'Helium pressures'
 
 group = 'optional'
+
+tango_host = 'tango://cpci01.antares.frm2:10000'
 
 devices = dict(
     center3_sens1 = device('devices.taco.AnalogInput',
@@ -13,4 +15,9 @@ devices = dict(
                            tacodevice = 'antares/center3/sens2',
                            unit = 'mbar',
                           ),
+    He_pressure = device('devices.tango.AnalogInput',
+                         description = 'Pressure of He bottle',
+                         tangodevice = '%s/antares/FZJDP_Analog/Druckgeber' % tango_host,
+                         unit = 'bar',
+                        ),
 )
