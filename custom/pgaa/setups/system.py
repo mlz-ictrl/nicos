d2cd0ee37ca0c8d6d9dcc4e6db822cf09ab7db6c
Jens Krüger <jens.krueger@frm2.tum.de>
2015-10-28 13:03:52 +0100

    PGAA: Clean up 'system' setup file
    - add missed email addresses
    - update email addresses
    - add missing description
    
    Change-Id: I0409d2a36dfcda90823db8cc7bacc416aaaf58ae
    Reviewed-on: https://forge.frm2.tum.de/review/10795
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/pgaa/setups/system.py
+++ custom/pgaa/setups/system.py
@@ -2,25 +2,27 @@ description = 'system setup'
 
 sysconfig = dict(
     cache = 'tequila.pgaa.frm2',
-    instrument = 'Instrument',
+    instrument = 'PGAA',
     experiment = 'Exp',
     datasinks = ['conssink', 'filesink', 'daemonsink'],
-    notifiers = [],
+    notifiers = ['email', 'smser', ],
 )
 
 modules = ['commands.standard']
+
+includes = ['notifiers', ]
 
 devices = dict(
     Sample   = device('devices.sample.Sample',
                       description = 'The sample',
                      ),
 
-    Instrument = device('devices.instrument.Instrument',
-                        description = 'Prompt gamma and in-beam neutron '
-                                      'activation analysis facility',
-                        doi = 'http://dx.doi.org/10.17815/jlsrf-1-46',
-                        responsible = 'Dr. Zsolt Revay <zsolt.revay@frm2.tum.de>',
-                       ),
+    PGAA = device('devices.instrument.Instrument',
+                  description = 'Prompt gamma and in-beam neutron activation '
+                                'analysis facility',
+                  doi = 'http://dx.doi.org/10.17815/jlsrf-1-46',
+                  responsible = 'Dr. Zsolt Revay <zsolt.revay@frm2.tum.de>',
+                 ),
 
     Exp      = device('devices.experiment.Experiment',
                       description = 'The currently running experiment',
@@ -42,5 +44,6 @@ devices = dict(
     Space    = device('devices.generic.FreeSpace',
                       description = 'The amount of free space for storing data',
                       minfree = 0.5,
+                      path = None,
                      ),
 )
