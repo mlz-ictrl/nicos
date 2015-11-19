54b6e751a33d45c5feb948bc024e317703c40dcd
Alexander Lenz <alexander.lenz@frm2.tum.de>
2015-11-19 15:27:56 +0100

    Add ictrl custom.
    Setups etc for ictrlfs/ictrlsrv.
    
    Change-Id: Ib7a9b48660b317dba1d171e3cc29740da8796cd0
    Reviewed-on: https://forge.frm2.tum.de/review/9719
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

---
+++ custom/ictrl/setups/ictrl.py
@@ -0,0 +1,5 @@
+description = 'Basic ictrl setup'
+group = 'basic'
+
+includes = ['ubahn', 'fak40', 'guidehall', 'reactor']
+
---
+++ custom/ictrl/setups/special/daemon.py
@@ -0,0 +1,55 @@
+# This setup file configures the nicos daemon service.
+
+description = 'setup for the execution daemon'
+group = 'special'
+
+devices = dict(
+    # to authenticate against the UserOffice, needs the "propdb" parameter
+    # set on the Experiment object
+    UserDB = device('frm2.proposaldb.Authenticator'),
+
+    # fixed list of users:
+    # first entry is the user name, second the hashed password, third the user
+    # level
+    # The user level are 'guest, 'user', and 'admin', ascending ordered in
+    # respect to the rights
+    # The entries for the password hashes are generated from randomized
+    # passwords and not reproduceable, please don't forget to create new ones:
+    # start python
+    # >>> import hashlib
+    # >>> hashlib.md5('password').hexdigest()
+    # or
+    # >>> hashlib.sha1('password').hexdigest()
+    # depending on the hashing algorithm
+    #AuthLDAP   = device('services.daemon.auth.LDAPAuthenticator',
+    #                server = 'phaidra.admin.frm2',
+    #                userbasedn = 'ou=People,dc=frm2,dc=de',
+    #                groupbasedn = 'ou=Group,dc=frm2,dc=de',
+    #                grouproles = {
+    #                    'ictrl' : 'admin',
+    #                },
+    #               ),
+
+    Auth   = device('services.daemon.auth.ListAuthenticator',
+                    # the hashing maybe 'md5' or 'sha1'
+                    hashing = 'md5',
+                    passwd = [('guest', '', 'guest'),
+                              ('user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user'),
+                              ('admin', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
+                             ],
+                   ),
+    Daemon = device('services.daemon.NicosDaemon',
+                    # 'localhost' will normally bind the daemon to the loopback
+                    # device, therefore just clients on the same machine will be
+                    # able to connect !
+                    # '' will bind the daemon to all network interfaces in the
+                    # machine
+                    # If server is a hostname (official computer name) or an IP
+                    # address the daemon service will be bound the the
+                    # corresponding network interface.
+                    server = '0.0.0.0',
+                    #authenticators = ['AuthLDAP',], # and/or 'UserDB'
+                    authenticators = ['Auth',], # and/or 'UserDB'
+                    loglevel = 'info',
+                   ),
+)
