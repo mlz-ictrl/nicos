098db0549de681e98c99eb5c5ecb24c33d5c07a9
Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
2015-11-19 18:21:49 +0100

    REFSANS: add setup for refsans_flipper
    using https://forge.frm2.tum.de/review/#/c/9714
    
    Change-Id: I29160d0457766dfac883a65a9aa89d4ff194505f
    Reviewed-on: https://forge.frm2.tum.de/review/9720
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

---
+++ custom/refsans/setups/flipper.py
@@ -0,0 +1,58 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2015 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
+#
+# **************************************************************************
+
+description = 'Refsans_flipper special HW'
+
+# not included by others
+group = 'optional'
+
+uribase = 'tango://refsans10.refsans.frm2:10000/refsans/flipper/'
+
+devices = dict(
+    guide = device('devices.tango.AnalogInput',
+                   description = 'Temperature of Flipping Guide',
+                   tangodevice = uribase + 'guide_temp',
+                  ),
+    coil = device('devices.tango.AnalogInput',
+                  description = 'Temperature of Flipping Coil',
+                  tangodevice = uribase + 'coil_temp',
+                 ),
+    current = device('devices.tango.AnalogInput',
+                     description = 'Current of Flipping Coil',
+                     tangodevice = uribase + 'current',
+                    ),
+    frequency = device('devices.tango.AnalogInput',
+                       description = 'Frequency of Flipping Field',
+                       tangodevice = uribase + 'frequency',
+                      ),
+    flipper = device('devices.tango.NamedDigitalOutput',
+                     description = 'Flipper',
+                     tangodevice = uribase + 'flipper',
+                     mapping = dict(ON=1, OFF=0),
+                    ),
+)
+
+startupcode = """
+"""
+
