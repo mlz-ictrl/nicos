25a4b1871ae2b38838507900b740466049771d90
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2015-11-30 11:25:59 +0100

    fix special method signature
     [E0302(unexpected-special-method-signature), lc_dict.__delitem__]
      The special method '__delitem__' expects 1 param(s), 2 were given
    
    Change-Id: Iac57958867a3b58a0bd3750bf26f68f562a2254f
    Reviewed-on: https://forge.frm2.tum.de/review/9809
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>

--- nicos/utils/__init__.py
+++ nicos/utils/__init__.py
@@ -82,7 +82,7 @@ class lc_dict(dict):
     def __setitem__(self, key, value):
         return dict.__setitem__(self, key.lower(), value)
 
-    def __delitem__(self, key, value):
+    def __delitem__(self, key):
         return dict.__delitem__(self, key.lower())
 
 
