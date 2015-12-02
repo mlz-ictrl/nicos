a964cb9259c71ec45d524845ce38fbe9415cc539
Jens Krüger <jens.krueger@frm2.tum.de>
2015-12-02 09:02:22 +0100

    SPODI: Move notifiers into a separate config file
    Change-Id: I926e1a54663e1ab4ea46a6bb7ce11630da34a793
    Reviewed-on: https://forge.frm2.tum.de/review/9829
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/spodi/setups/notifiers.py
@@ -0,0 +1,22 @@
+description = 'Email and SMS notifiers'
+
+group = 'lowlevel'
+
+devices = dict(
+    # Configure source and copy addresses to an existing address.
+    email    = device('devices.notifiers.Mailer',
+                      sender = 'spodi@frm2.tum.de',
+                      copies = [('markus.hoelzel@frm2.tum.de', 'all'),   # gets all messages
+                                ('anatoliy.senishyn@frm2.tum.de', 'all'),   # gets all messages
+                                ('markus.hoelzel@frm2.tum.de', 'important')], # gets only important messages
+                      subject = 'SPODI',
+                      lowlevel = True,
+                     ),
+
+    # Configure SMS receivers if wanted and registered with IT.
+    smser    = device('devices.notifiers.SMSer',
+                      server = 'triton.admin.frm2',
+                      receivers = [],
+                      lowlevel = True,
+                     ),
+)
