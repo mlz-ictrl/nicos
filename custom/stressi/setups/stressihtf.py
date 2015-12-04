b4b83239e545d9780e1ac583a491538ddc454995
Jens Krüger <jens.krueger@frm2.tum.de>
2015-12-04 14:35:28 +0100

    STRESSI: Add the setup for the stressi HTF
    Change-Id: I709b48b345c5db1c0dff861051d1084a5f9046eb
    Reviewed-on: https://forge.frm2.tum.de/review/9892
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/stressi/setups/stressihtf.py
@@ -0,0 +1,23 @@
+description = 'Stressi specific high temperature furnace'
+
+group = 'plugplay'
+
+includes = ['alias_T']
+
+nethost = setupname
+
+devices = {
+    'T_%s' % setupname : device('devices.taco.TemperatureController',
+                                description = 'The sample temperature',
+                                tacodevice = '//%s/htf/et/control' % \
+                                             nethost,
+                                abslimits = (0, 2000),
+                                unit = 'C',
+                                fmtstr = '%.1f',
+                               ),
+}
+
+alias_config = {
+    'T':  {'T_%s' % setupname: 100},
+    'Ts': {'T_%s' % setupname: 100},
+}
