da463720d1505e6044913cdbd0dc5dc9be5f6fc9
Jens Krüger <jens.krueger@frm2.tum.de>
2015-12-09 12:21:34 +0100

    ICTRL: add CCR test box devices
    Change-Id: I672f6f4416b2503d97e4553e0b60e3eacf7a1a69
    Reviewed-on: https://forge.frm2.tum.de/review/9919
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/ictrl/setups/ccrtestbox.py
@@ -0,0 +1,138 @@
+description = 'Devices for the CCR test box'
+
+group = 'optional'
+
+tangohost = 'ictrlsrv.ictrl.frm2:10000'
+
+dev_prefix = 'tango://%s/ccrtest/plc' % tangohost
+
+devices = dict(
+    tb_v1 = device('devices.tango.AnalogOutput',
+                   description = 'Simulated pressure sensor 1 signal (0-10V)',
+                   tangodevice = '%s/_v1' % dev_prefix,
+                   abslimits = (0, 10),
+                   unit = 'V',
+                   pollinterval = 5,
+                   maxage = 6,
+                  ),
+    tb_v2 = device('devices.tango.AnalogOutput',
+                   description = 'Simulated pressure sensor 2 signal (0-10V)',
+                   tangodevice = '%s/_v2' % dev_prefix,
+                   abslimits = (0, 10),
+                   unit = 'V',
+                   pollinterval = 5,
+                   maxage = 6,
+                  ),
+    tb_err_gaspressure = device('devices.tango.NamedDigitalOutput',
+                                description = 'gas pressure error input to the ccr',
+                                tangodevice = '%s/_gaspressure_err' % dev_prefix,
+                                pollinterval = 5,
+                                maxage = 6,
+                                mapping = {'ok': 0, 'signal_error': 1,}
+                               ),
+    tb_err_gastemp = device('devices.tango.NamedDigitalOutput',
+                            description = 'gas temperature error input to the ccr',
+                            tangodevice = '%s/_gastemp_err' % dev_prefix,
+                            pollinterval = 5,
+                            maxage = 6,
+                            mapping = {'ok': 0, 'signal_error': 1,}
+                           ),
+    tb_err_motortemp = device('devices.tango.NamedDigitalOutput',
+                              description = 'motor temperature error input to the ccr',
+                              tangodevice = '%s/_gastemp_err' % dev_prefix,
+                              pollinterval = 5,
+                              maxage = 6,
+                              mapping = {'ok': 0, 'signal_error': 1,}
+                             ),
+    tb_err_oil = device('devices.tango.NamedDigitalOutput',
+                        description = 'oil temperature error input to the ccr',
+                        tangodevice = '%s/_oil_err' % dev_prefix,
+                        pollinterval = 5,
+                        maxage = 6,
+                        mapping = {'ok': 0, 'signal_error': 1,}
+                       ),
+    tb_err_power = device('devices.tango.NamedDigitalOutput',
+                          description = 'power fail error input to the ccr',
+                          tangodevice = '%s/_oil_err' % dev_prefix,
+                          pollinterval = 5,
+                          maxage = 6,
+                          mapping = {'ok': 0, 'signal_error': 1,}
+                         ),
+    tb_err_waterin = device('devices.tango.NamedDigitalOutput',
+                            description = 'water inlet error input to the ccr',
+                            tangodevice = '%s/_oil_err' % dev_prefix,
+                            pollinterval = 5,
+                            maxage = 6,
+                            mapping = {'ok': 0, 'signal_error': 1,}
+                           ),
+    tb_err_waterout = device('devices.tango.NamedDigitalOutput',
+                             description = 'water outlet error input to the ccr',
+                             tangodevice = '%s/_oil_err' % dev_prefix,
+                             pollinterval = 5,
+                             maxage = 6,
+                             mapping = {'ok': 0, 'signal_error': 1,}
+                            ),
+    tb_state_run = device('devices.tango.NamedDigitalOutput',
+                          description = 'running state of the compressor input to the ccr',
+                          tangodevice = '%s/_runstat_state' % dev_prefix,
+                          pollinterval = 5,
+                          maxage = 6,
+                          mapping = {'on': 1, 'off': 0,}
+                         ),
+    tb_warn_solenoid = device('devices.tango.NamedDigitalOutput',
+                              description = 'bypass solenoid input to the ccr',
+                              tangodevice = '%s/_solenoid' % dev_prefix,
+                              pollinterval = 5,
+                              maxage = 6,
+                              mapping = {'on': 1, 'off': 0,}
+                             ),
+    tb_compressor = device('devices.tango.NamedDigitalInput',
+                           description = 'Feedback from CCR box: Compressor state',
+                           tangodevice = '%s/_compressor_on' % dev_prefix,
+                           pollinterval = 1,
+                           maxage = 2,
+                           mapping = {'on': 1, 'off': 0,}
+                          ),
+    tb_p1power = device('devices.tango.NamedDigitalInput',
+                        description = 'Feedback from test box: P1 power state',
+                        tangodevice = '%s/_powerp1_ok' % dev_prefix,
+                        pollinterval = 1,
+                        maxage = 2,
+                        mapping = {'ok': 1, 'failure': 0,}
+                       ),
+    tb_gasvalve = device('devices.tango.NamedDigitalInput',
+                         description = 'Feedback from CCR box: gas valve state',
+                         tangodevice = '%s/_gasvalve_on' % dev_prefix,
+                         pollinterval = 1,
+                         maxage = 2,
+                         mapping = {'on': 1, 'off': 0,}
+                        ),
+    tb_remote = device('devices.tango.NamedDigitalInput',
+                       description = 'Feedback from CCR box: remote control state',
+                       tangodevice = '%s/_remote_on' % dev_prefix,
+                       pollinterval = 1,
+                       maxage = 2,
+                       mapping = {'on': 1, 'off': 0,}
+                      ),
+    tb_reset = device('devices.tango.NamedDigitalInput',
+                      description = 'Feedback from CCR box: reset state',
+                      tangodevice = '%s/_reset_on' % dev_prefix,
+                      pollinterval = 1,
+                      maxage = 2,
+                      mapping = {'on': 1, 'off': 0,}
+                     ),
+    tb_p2power = device('devices.tango.NamedDigitalInput',
+                        description = 'Feedback from test box: P2  power state',
+                        tangodevice = '%s/_powerp2_ok' % dev_prefix,
+                        pollinterval = 1,
+                        maxage = 2,
+                        mapping = {'ok': 1, 'failure': 0,}
+                       ),
+    tb_vacuumvalve = device('devices.tango.NamedDigitalInput',
+                            description = 'Feedback from CCR box: vacuum valve state',
+                            tangodevice = '%s/_vacuumvalve_on' % dev_prefix,
+                            pollinterval = 1,
+                            maxage = 2,
+                            mapping = {'on': 1, 'off': 0,}
+                           ),
+)
