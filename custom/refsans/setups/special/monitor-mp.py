50e9c75735ccc347cc1fd7d685bdd11d7deb202b
Matthias Pomm <matthias.pomm@hzg.de>
2016-02-10 09:44:11 +0100

    Diary monitor setup
    This is a special version for MP to work for a diary monitor.
    
    Change-Id: Ic56ad883b9e01ebc097f90643df2bf846e1104ed
    Reviewed-on: https://forge.frm2.tum.de/review/10146
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/refsans/setups/special/monitor-mp.py
@@ -0,0 +1,71 @@
+#pylint: skip-file
+description = 'setup for the status monitor'
+group = 'special'
+
+_expcolumn = Column(
+    Block('Experiment', [
+        BlockRow(Field(name='Proposal', key='exp/proposal', width=7),
+                 Field(name='Title',    key='exp/title',    width=20,
+                       istext=True, maxlen=20),
+                 Field(name='Current status', key='exp/action', width=50,
+                       istext=True, maxlen=40),
+                 Field(name='Last file', key='det/lastfilenumber'),
+            )
+        ],
+        # setups='experiment'
+    ),
+)
+
+# NOK's
+_noklist = 'nok1 nok2 nok3 nok4 nok5a zb0 nok5b zb1 nok6 zb2 nok7 zb3 nok8 bs1 nok9'.split()
+_nok_array = []
+_r = []
+_blockwidth = 4
+for i, nok in enumerate(_noklist):
+    if (i % _blockwidth == 0) and len(_r):
+        _nok_array.append(BlockRow(*_r))
+        _r = []
+    _r.append(Field(dev=nok, width=16))
+else:
+    _nok_array.append(BlockRow(*_r))
+
+_nokcolumn = Column(Block('NOK-System', _nok_array))
+
+_flippercolumn = Column(
+    Block('Flipper', [
+          BlockRow( Field(dev='frequency'),
+                    Field(dev='current'),
+                    Field(dev='flipper', name='Flipping_State'),
+                  )
+          ]),
+)
+
+_refcolumn = Column(
+    Block('References', [
+        BlockRow( Field(dev='nok_refa1', name='ref_A1'),
+                  Field(dev='nok_refb1', name='ref_B1'),
+                  Field(dev='nok_refc1', name='ref_C1'),),
+        BlockRow( Field(dev='nok_refa2', name='ref_A2'),
+                  Field(dev='nok_refb2', name='ref_B2'),
+                  Field(dev='nok_refc2', name='ref_C2'),),
+        ],
+    ),
+)
+
+devices = dict(
+    Monitor = device('services.monitor.qt.Monitor',
+                     title = 'NICOS status monitor',
+                     loglevel = 'info',
+                     cache = 'refsans10.refsans.frm2',
+                     prefix = 'nicos/',
+                     font = 'Luxi Sans',
+                     valuefont = 'Consolas',
+                     fontsize = 12,
+                     padding = 5,
+                     layout = [
+                               Row(_expcolumn),
+                               Row(_nokcolumn,), #  _refcolumn),
+                               Row(_flippercolumn),
+                              ],
+                    ),
+)
