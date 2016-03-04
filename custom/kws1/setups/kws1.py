0b4687549b2a5223d659a296f60d2e82f859e21c
Georg Brandl <g.brandl@fz-juelich.de>
2016-03-04 15:46:27 +0100

    KWS1: add full KWS1 setup
    Change-Id: Id582eaf3d4e3b97e632ae5c83fc4cc715e1f81ac
    Reviewed-on: https://forge.frm2.tum.de/review/10264
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ custom/kws1/setups/kws1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+description = 'KWS-1 setup'
+group = 'basic'
+
+modules = ['kws1.commands']
+
+includes = [
+    'sample',
+    'selector',
+    'detector',
+    'chopper',
+    'collimation',
+    'polarizer',
+    'lenses',
+    'daq',
+]
