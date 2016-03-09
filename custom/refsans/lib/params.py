dfbf47a9b4c6e91959067ce107084360f08af306
Jens Krüger <jens.krueger@frm2.tum.de>
2016-03-09 11:32:47 +0100

    REFSANS: Introduce instrument specific parameter
    The motor address on the Beckhoff PLC must have well defined
    addresses. To check this easily the motoradress parameter was
    introduced. The test cases are added.
    
    Change-Id: Ifc970d88ce80e5f5aa0f782b1cd4cf6fe057e290
    Reviewed-on: https://forge.frm2.tum.de/review/10310
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/refsans/lib/params.py
@@ -0,0 +1,38 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2009-2016 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Jens Krüger <jens.krueger@frm2.tum.de>
+#
+# *****************************************************************************
+
+"""REFSANS specific parameter definition helpers."""
+
+
+def motoraddress(address=0x3020):
+    """The motor address should start either at addresses 0x3020 or 0x4020
+    and must have an offset of the multiple of 10 words (each motor control
+    block is 20 bytes = 10 words). There are only 200 allowed addresse for one
+    address offset.
+    """
+    if not (address & 0xF000) in (0x3000, 0x4000) or \
+       not (0x20 <= (address & 0xFFF) <= 0x7f0) or \
+       ((address & 0xFFF) - 0x20) % 10 != 0:
+        raise ValueError('Invalid motor address: %0x04x!' % address)
+    return address
--- doc/source/custom/refsans/index.rst
+++ doc/source/custom/refsans/index.rst
@@ -30,3 +30,16 @@ Custom classes
     classes
 
 
+Parameter definition
+====================
+
+.. module:: custom.refsans.lib.params
+
+.. function:: motoraddress
+
+   Converter that accepts only valid motor block addresses for the Beckhoff
+   PLC.
+
+   Valid address must start with an offset 0x3020 or 0x4020 and must add a
+   multiple of 10. The multiple is limited to 200 so the valid range
+   goes from 0x20 to 0x7f0.
