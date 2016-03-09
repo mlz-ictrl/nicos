2e6d7354b3e9f0073a2dfb1ca513fe4e889e0ef4
Georg Brandl <g.brandl@fz-juelich.de>
2016-03-09 12:24:21 +0100

    DEMO: fix setup excludes
    Change-Id: Idf34a1a4beb6b179c99dcb86b99773668808ceb7
    Reviewed-on: https://forge.frm2.tum.de/review/10312
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- custom/demo/setups/detector.py
+++ custom/demo/setups/detector.py
@@ -2,7 +2,7 @@ description = 'virtual detector'
 group = 'lowlevel'
 
 includes = ['system']
-excludes = ['refsans', 'sans', 'qmchannel']
+excludes = ['refsans', 'sans', 'qmchannel', 'pgaa']
 
 devices = dict(
     timer    = device('devices.generic.VirtualTimer',
