9e59ea36e188ea75afbf55a0305c140dcbe64ab1
Christian Felder <c.felder@fz-juelich.de>
2016-03-18 14:35:06 +0100

    delab: update filenametemplate in qmesydaq setup
    Change-Id: I3811cc93e8d52f2851156e0562193a0aa3af2e66
    Reviewed-on: https://forge.frm2.tum.de/review/10493
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- custom/delab/setups/qmesydaq.py
+++ custom/delab/setups/qmesydaq.py
@@ -13,9 +13,10 @@ nethost = 'localhost'
 devices = dict(
     # RAWFileSaver      = device('devices.datasinks.RawImageSink',
     #                          description = 'Saves image data in RAW format',
-    #                          filenametemplate = ['%(proposal)s_%(counter)06d.raw',
-    #                          '%(proposal)s_%(session.experiment.lastscan)s_'
-    #                          '%(counter)s_%(scanpoint)s.raw'],
+    #                          filenametemplate = [
+    #                              '%(proposal)s_%(pointcounter)06d.raw',
+    #                              '%(proposal)s_%(scancounter)s_'
+    #                              '%(pointcounter)s_%(pointnumber)s.raw'],
     #                          subdir = 'QMesyDAQ2',
     #                          lowlevel = True,
     #                         ),
