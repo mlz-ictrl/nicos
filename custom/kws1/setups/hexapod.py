7f2630ce27b929a0c9b15e2ab394c5975c6bcc4c
Georg Brandl <g.brandl@fz-juelich.de>
2016-04-08 09:55:26 +0200

    KWS1: add hexapod setup
    Change-Id: I62dce36d32812a9a86b8ede7f2b1e6decfbf6dc7
    Reviewed-on: https://forge.frm2.tum.de/review/10666
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ custom/kws1/setups/hexapod.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+description = "Hexapod control"
+group = "optional"
+
+excludes = []
+
+tango_base = "tango://phys.kws1.frm2:10000/kws1/"
+
+devices = dict(
+    hexapod_dt    = device("devices.tango.Motor",
+                           description = "Hexapod base rotation table",
+                           tangodevice = tango_base + "hexapodbase/dt",
+                           unit = "deg",
+                           precision = 0.01,
+                           fmtstr = "%.2f",
+                          ),
+    hexapod_rx    = device("devices.tango.Motor",
+                           description = "Hexapod rotation around X",
+                           tangodevice = tango_base + "hexapodbase/rx",
+                           unit = "deg",
+                           precision = 0.01,
+                           fmtstr = "%.2f",
+                          ),
+    hexapod_ry    = device("devices.tango.Motor",
+                           description = "Hexapod rotation around Y",
+                           tangodevice = tango_base + "hexapodbase/ry",
+                           unit = "deg",
+                           precision = 0.01,
+                           fmtstr = "%.2f",
+                          ),
+    hexapod_rz    = device("devices.tango.Motor",
+                           description = "Hexapod rotation around Z",
+                           tangodevice = tango_base + "hexapodbase/rz",
+                           unit = "deg",
+                           precision = 0.01,
+                           fmtstr = "%.2f",
+                          ),
+    hexapod_tx    = device("devices.tango.Motor",
+                           description = "Hexapod translation in X",
+                           tangodevice = tango_base + "hexapodbase/tx",
+                           unit = "mm",
+                           precision = 0.1,
+                           fmtstr = "%.1f",
+                          ),
+    hexapod_ty    = device("devices.tango.Motor",
+                           description = "Hexapod translation in Y",
+                           tangodevice = tango_base + "hexapodbase/ty",
+                           unit = "mm",
+                           precision = 0.1,
+                           fmtstr = "%.1f",
+                          ),
+    hexapod_tz    = device("devices.tango.Motor",
+                           description = "Hexapod translation in Z",
+                           tangodevice = tango_base + "hexapodbase/tz",
+                           unit = "mm",
+                           precision = 0.1,
+                           fmtstr = "%.1f",
+                          ),
+)
