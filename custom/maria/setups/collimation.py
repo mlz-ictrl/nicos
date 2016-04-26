c6868f7f2435c87604328f2917f5994debb33bff
Christian Felder <c.felder@fz-juelich.de>
2016-04-26 09:56:02 +0200

    maria: Add collimation setup.
    Change-Id: I8a253dd8dffb45796dab5228360e927b4688502b
    Reviewed-on: https://forge.frm2.tum.de/review/10872
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>

---
+++ custom/maria/setups/collimation.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+description = "Collimation setup"
+group = "optional"
+
+tango_base = "tango://phys.maria.frm2:10000/maria"
+tango_s7 = tango_base + "/FZJS7"
+tango_dio = tango_base + "/FZJDP_Digital"
+
+atten_mapping = {"out": 0, "in": 1}
+
+devices = dict(
+    giref = device("devices.tango.Motor",
+                   description = "Collimation/GISANS",
+                   tangodevice = tango_s7 + "/giref",
+                   precision = 0.01,
+                  ),
+    # TODO: add info about attenuation factors, or at least about the relative
+    #       attenuation of A, B, C
+    atten_A = device("devices.tango.NamedDigitalOutput",
+                     description = "Attenuator A",
+                     tangodevice = tango_dio + "/attenA",
+                     mapping = atten_mapping,
+                    ),
+    atten_B = device("devices.tango.NamedDigitalOutput",
+                     description = "Attenuator B",
+                     tangodevice = tango_dio + "/attenB",
+                     mapping = atten_mapping,
+                    ),
+    atten_C = device("devices.tango.NamedDigitalOutput",
+                     description = "Attenuator C",
+                     tangodevice = tango_dio + "/attenC",
+                     mapping = atten_mapping,
+                    ),
+)
