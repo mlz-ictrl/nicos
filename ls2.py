d6056cb2da93e4a99cd06348fe9c08da3707a06f
Alexander Lenz <alexander.lenz@frm2.tum.de>
2016-05-02 14:37:46 +0200

    se: add ls1&ls2.
    Change-Id: Ie1cb549e82b262ad3405da8a2bc7778cb8f8cd28
    Reviewed-on: https://forge.frm2.tum.de/review/10901
    Tested-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

---
+++ ls2.py
@@ -0,0 +1,49 @@
+description = 'LakeShore LS336 controller N2'
+
+tango_base = 'tango://seplot.se.frm2:10000/se/%s' % setupname
+
+devices = {
+    'T_%s_control1' % setupname : device('devices.tango.TemperatureController',
+                                  description = 'Loop 1',
+                                  tangodevice = '%s/t_control1' % tango_base,
+                                  unit = 'K',
+                                  fmtstr = '%.3f',
+                                  maxage = 7,
+                                  abslimits = (0, 300),
+                                 ),
+    'T_%s_control2' % setupname : device('devices.tango.TemperatureController',
+                                  description = 'Loop 2',
+                                  tangodevice = '%s/t_control2' % tango_base,
+                                  unit = 'K',
+                                  fmtstr = '%.3f',
+                                  maxage = 7,
+                                  abslimits = (0, 300),
+                                 ),
+    'T_%s_1' % setupname : device('devices.tango.Sensor',
+                                  description = 'Sensor 1',
+                                  tangodevice = '%s/t_sensor1' % tango_base,
+                                  unit = 'K',
+                                  fmtstr = '%.3f',
+                                 ),
+    'T_%s_2' % setupname : device('devices.tango.Sensor',
+                                  description = 'Sensor 2',
+                                  tangodevice = '%s/t_sensor2' % tango_base,
+                                  unit = 'K',
+                                  fmtstr = '%.3f',
+                                 ),
+    'T_%s_3' % setupname : device('devices.tango.Sensor',
+                                  description = 'Sensor 3',
+                                  tangodevice = '%s/t_sensor3' % tango_base,
+                                  unit = 'K',
+                                  fmtstr = '%.3f',
+                                 ),
+    'T_%s_4' % setupname : device('devices.tango.Sensor',
+                                  description = 'Sensor 4',
+                                  tangodevice = '%s/t_sensor4' % tango_base,
+                                  unit = 'K',
+                                  fmtstr = '%.3f',
+                                 ),
+}
+
+startupcode = '''
+'''
