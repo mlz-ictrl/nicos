ed9dd7b973bef0950d26be84e321acd4216ce7bf
Jens Krüger <jens.krueger@frm2.tum.de>
2016-06-27 17:04:57 +0200

    STRESSI: Integrate the tensile machine
    Change-Id: Ide06057847e5d48a0e68652462db40a5771d4ce2
    Reviewed-on: https://forge.frm2.tum.de/review/11291
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/stressi/setups/tensile.py
@@ -0,0 +1,35 @@
+description = 'Tensile machine'
+
+group = 'optional'
+
+servername = 'STRESSICTRL'
+
+nameservice = 'stressictrl'
+
+devices = dict(
+
+    teload = device('devices.vendor.caress.Motor',
+                    description = 'load value of the tensile machine',
+                    nameserver = '%s' % (nameservice,),
+                    config = 'TELOAD 500 TensileLoad.ControllableDevice',
+                    abslimits = (-50000, 50000),
+                    unit = 'N',
+                    fmtstr = '%.2f',
+                   ),
+    tepos = device('devices.vendor.caress.Motor',
+                   description = 'position value of the tensile machine',
+                   nameserver = '%s' % (nameservice,),
+                   config = 'TEPOS 500 TensilePos.ControllableDevice',
+                   abslimits = (0, 70),
+                   unit = 'mm',
+                   fmtstr = '%.3f',
+                  ),
+    teext = device('devices.vendor.caress.Motor',
+                   description = 'extension value of the tensile machine',
+                   nameserver = '%s' % (nameservice,),
+                   config = 'TEEXT 500 TensileExt.ControllableDevice',
+                   abslimits = (-3000, 3000),
+                   unit = 'um',
+                   fmtstr = '%.3f',
+                  ),
+)
