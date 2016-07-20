9cf01965ac90706e299e19947ab6423b8e343d79
Andreas Wilhelm <andreas.wilhelm@frm2.tum.de>
2016-07-20 08:21:22 +0200

    SANS-1: remove sample_table_2 setup from sc1 and sc2 setup
    do not include sample_table_2 anymore within setup sc1 and sc2
    
    Change-Id: I0e6c881f12a5ac79eadfc4a2db1f510707c326d0
    Reviewed-on: https://forge.frm2.tum.de/review/11556
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- custom/sans1/setups/sc2.py
+++ custom/sans1/setups/sc2.py
@@ -2,7 +2,7 @@ description = 'sample changer 2 devices'
 
 group = 'optional'
 
-includes = ['sample_changer', 'sample_table_1']
+includes = ['sample_changer']#, 'sample_table_1']
 
 nethost = 'sans1srv.sans1.frm2'
 
