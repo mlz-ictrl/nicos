36c8fcfbd79763539059ce045d8447867144f53f
Christian Felder <c.felder@fz-juelich.de>
2016-07-20 13:27:42 +0200

    BIODIFF: rename minikappa axes.
    Change-Id: I761cf3b6bcb677a63a8978895d3e3f6833bc6528
    Fixes: #2834
    Reviewed-on: https://forge.frm2.tum.de/review/11571
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

--- custom/biodiff/setups/special/monitor.py
+++ custom/biodiff/setups/special/monitor.py
@@ -43,9 +43,9 @@ _sampleTableBlock = Block('Sample table', [
 )
 
 _kappaBlock = Block('Mini-kappa', [
-    BlockRow(Field(name='kappa', dev='minikappa_kappa',)),
-    BlockRow(Field(name='phi', dev='minikappa_phi',)),
-    BlockRow(Field(name='omega', dev='minikappa_omega',)),
+    BlockRow(Field(name='kappa', dev='kappa_minikappa',)),
+    BlockRow(Field(name='phi', dev='phi_minikappa',)),
+    BlockRow(Field(name='omega', dev='omega_minikappa',)),
     ],
     setups='kappa',
 )
