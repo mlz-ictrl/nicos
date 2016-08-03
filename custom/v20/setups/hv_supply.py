a83b7263ab457cfb2638e5c18111669813085f3f
Michael Wedel <michael.wedel@esss.se>
2016-08-03 14:26:15 +0200

    V20: Add high voltage supply
    This change makes a CARESS-based power supply available at V20.
    
    Fixes: #2786
    
    Change-Id: I24d43a486bbeb6e70786ea2744c59a22b04ff73d
    Reviewed-on: https://forge.frm2.tum.de/review/12040
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/v20/setups/hv_supply.py
@@ -0,0 +1,15 @@
+description = 'High voltage via moxa box'
+servername = 'EXV20'
+nameservice = '192.168.1.254'
+
+devices = dict(
+    hv_mon = device('devices.vendor.caress.Driveable',
+                    description = 'High voltage for beam monitor',
+                    fmtstr = '%.2f',
+                    unit = 'V',
+                    abslimits = (0, 1400),
+                    nameserver = '%s' % (nameservice,),
+                    objname = '%s' % (servername),
+                    config = 'HVMON 76 2 usemoxa 1 1 0 20 -1 0 0 0',
+                   ),
+)
