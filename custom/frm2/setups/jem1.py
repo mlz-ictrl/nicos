f72b662dac50c8f51ce36e7b68b0efecdae6c320
Georg Brandl <g.brandl@fz-juelich.de>
2016-09-07 15:19:58 +0200

    FRM2: add setup for JCNS electromagnet (jem)
    Change-Id: I2098e19491ccba034a40dddc4ef24bb627c22703
    Reviewed-on: https://forge.frm2.tum.de/review/12324
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ custom/frm2/setups/jem1.py
@@ -0,0 +1,37 @@
+description = 'JCNS 2.2T electromagnet'
+group = 'plugplay'
+
+includes = ['alias_B']
+
+tango_base = 'tango://%s:10000/box/' % setupname
+
+devices = {
+    'I_%s' % setupname: device('devices.tango.PowerSupply',
+                               description = 'Magnet current',
+                               tangodevice = tango_base + 'bruker/supply',
+                               fmtstr = '%.1f',
+                               unit = 'A',
+                               timeout = 60.0,
+                               precision = 0.1,
+                              ),
+    'B_%s' % setupname: device('devices.generic.CalibratedMagnet',
+                               currentsource = 'I_%s' % setupname,
+                               description = 'Magnet field',
+                               # abslimits are automatically determined from I
+                               unit = 'T',
+                               fmtstr = '%.4f',
+                               # calibration from measurement of A. Feoktystov
+                               # during cycle 39, fitted 2016-09-19
+                               calibration = (
+                                   1.2833,
+                                   65.032,
+                                   0.035235,
+                                   -127.25,
+                                   0.029687
+                               )
+                              ),
+}
+
+alias_config = {
+    'B': {'B_%s' % setupname: 100, 'I_%s' % setupname: 80},
+}
