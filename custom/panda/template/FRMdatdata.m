fba125d5413b1bc88614470cad4066c4a973ea93
Petr Cermak <p.cermak@fz-juelich.de>
2016-09-21 11:20:18 +0200

    PANDA: update data loading routine for Matlab
    Change-Id: I8f38be30e6481272615b16e4f60ee5071b4a49fd
    Reviewed-on: https://forge.frm2.tum.de/review/12372
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: Georg Brandl <g.brandl@fz-juelich.de>

--- custom/panda/template/FRMdatdata.m
+++ custom/panda/template/FRMdatdata.m
@@ -1,134 +1,98 @@
-function [x, y, err, xname, yname, mname, optpars] = FRMdatdata(filespec)
+function [x, y, err, xname, yname,mname,optpars] = FRMdatdata(filespec)
 % Load .dat data from FRM2 files
-% Works with PANDA data in .dat format
-% Simon Ward, March 2012, simon.ward@psi.ch
-% Sandor Toth, August 2014, sandor.toth@psi.ch
-
-x=[]; y= []; err=[]; yname=''; xname=''; mname = ''; normval=1;
+% Works with PANDA data in .dat format 
+% V 1.1, March 2012 
+% V 1.2, Sept  2015
+% Simon Ward, simon.ward@psi.ch
+initial
+x=[]; y= []; err=[]; yname=''; xname=''; mname = '';
 
 %----- Parse filespec --------------------------------------
 
-[fspec, filespec]=strtok(filespec,',');
+[fspec, filespec] = strtok(filespec,',');
 while ~isempty(filespec)
-    [s, filespec]=strtok(filespec,',');
-    fspec=str2mat(fspec,s);
+    [s, filespec] = strtok(filespec,',');
+    fspec = str2mat(fspec,s);
 end
-
-[~,nchars]=size(fspec);
+[nargs,nchars] = size(fspec);
 
 %----- Update scan parameters from filespec---------------------------
 
-i=strmatch('X=',fspec);
+i = strmatch('X=',fspec);
 if ~isempty(i)
-    xname=deblank(fspec(i(end),3:nchars)); end
-i=strmatch('Y=',fspec);
+    xname = deblank(fspec(i(end),3:nchars)); end
+i = strmatch('Y=',fspec);
 if ~isempty(i)
-    yname=deblank(fspec(i(end),3:nchars)); end
-i=strmatch('M=',fspec);
+    yname = deblank(fspec(i(end),3:nchars)); end
+i = strmatch('M=',fspec);
 if ~isempty(i)
-    mname=deblank(fspec(i(end),3:nchars)); end
-i=strmatch('N=',fspec);
-if ~isempty(i)
-    normval=str2double(deblank(fspec(i(end),3:nchars))); end
+    mname = deblank(fspec(i(end),3:nchars)); end
 
-%----- Do we check for malformed input files? ---------------------------
-global EOF
-if isempty(EOF)
-    scheck=0;       % No we don't
-else
-    if EOF==1
-        scheck=1;   % Yes we do
-        warning('Malformed end of file statement checking is disabled!')
-    else
-        scheck=0;
-    end
-end
-
-filename=deblank(fspec(1,:));
+filename = deblank(fspec(1,:));
 
 
 % ---- Get Data from file ----------------------------------
-fid=fopen(filename,'r');
+fid = fopen(filename,'r');
+
+
 if fid ==-1
-    warning('MATLAB:unableToFindFile','Unable to find/open file %s', filename)
-    return
+    error('MATLAB:unableToFindFile','Unable to find/open file %s', filename)
 end
-clo=1;
-i=1;
-b=true;
+
+c = onCleanup(@()fclose(fid));
+
+i = 1;
+b = true;
 while(b)
-    linDat=fgetl(fid);
-    % Find start of data file
-    if strmatch('### Scan data',linDat)
-        linDat=fgetl(fid);
-        linDat=linDat(3:end);
+    linDat = fgetl(fid);
+    % Find start of data file 
+    if strcmp('### Scan data',linDat)
+        linDat = fgetl(fid);
+        linDat = linDat(3:end);
         % Generate list of vairables
-        [lspec, linspec]=strtok(linDat,char(9));
+        [lspec, linspec] = strtok(linDat,char(9));
         while ~isempty(linspec)
             [s, linspec] = strtok(linspec,char(9));
             lspec = str2mat(lspec,s);
         end
         % Find the location of the needed vairables in the variable list
-        xpos=[];    ypos=[];    mpos=[];
-        for j=1:length(lspec(:,1))
+        xpos = [];    ypos = [];    mpos = [];
+        for j = 1:length(lspec(:,1))
             if strcmp(deblank(lspec(j,:)),xname)
-                xpos(length(xpos)+1)=j;
+                xpos(length(xpos)+1) = j;
             end
             if strcmp(deblank(lspec(j,:)),yname)
-                ypos(length(ypos)+1)=j;
+                ypos(length(ypos)+1) = j;
             end
             if strcmp(deblank(lspec(j,:)),mname)
-                mpos(length(mpos)+1)=j;
+                mpos(length(mpos)+1) = j;
             end
         end
-        noOfEnt=length(lspec);
-        linDat=fgetl(fid);      % We do not want the units line
-        j=1;
-        endOfFile=false;
-        while ~endOfFile
-            linDat=fgetl(fid);
+        noOfEnt = length(lspec);
+        linDat = fgetl(fid);      % We do not want the units line
+        j = 1;
+        endOfFile = false;
+        while ~endOfFile || ~feof(fid)
+            linDat = fgetl(fid);
             % Are we at the end of the file?
             if strfind(linDat,'### End of NICOS data file')
-                endOfFile=true;
+                endOfFile = true;
             end
             % Parse the data line
-            [lspec linspec]=strtok(linDat,char(9));
-            data(j,1)=str2double(lspec);
-            for k=2:noOfEnt
-                [lspec linspec]=strtok(linspec,char(9));
-                data(j,k)=str2double(lspec);
+            [lspec, linspec] = strtok(linDat,char(9));
+            data(j,1) = str2double(lspec);
+            for k = 2:noOfEnt
+                [lspec, linspec] = strtok(linspec,char(9));
+                data(j,k) = str2double(lspec);
             end
-            % Check for malformed EOF line
-            if (sum(isnan(data(j,:)))==noOfEnt && ~scheck) && ~endOfFile
-                warning(sprintf(['I think your scan file is improperly formatted (Possible scan ineruption?).\n'...
-                    'Reading data is stopped. If this is in error, disable this feature by creating a global vairable EOF with value 1']))
-                endOfFile=true;
-            end
-            j=j+1;
+            j = j+1;
         end
         % Set return data
-        %x=mean(data(1:end-1,xpos),2);
-        x=data(1:end-1,xpos(end));
-        y=data(1:end-1,ypos)./data(1:end-1,mpos);
-        err=sqrt(data(1:end-1,ypos))./data(1:end-1,mpos);
-        y=y*normval;
-        err=err*normval;
-        clo=fclose(fid);
-        b=false;
+        x = mean(data(1:end-1,xpos),2);
+        y = data(1:end-1,ypos)./data(1:end-1,mpos);
+        err = sqrt(data(1:end-1,ypos))./data(1:end-1,mpos);
+        clear c;
+        b = false;
     end
-    i=i+1;
-end
-% If something goes wrong, close the data file
-if clo~=0;
-    fclose(fid);
-end
-
-% dat.x = x;
-% dat.y = y;
-% dat.e = err;
-% dat.xname = xname;
-% dat.yname = yname;
-% dat.mname = mname;
-% dat.optpars = [];
-
+    i = i+1;
 end
