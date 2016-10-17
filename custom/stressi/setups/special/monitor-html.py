d742b6e25558aa3245d33ec3f5d3d7858d181178
Jens Krüger <jens.krueger@frm2.tum.de>
2016-10-17 15:55:03 +0200

    STRESSI: Add HTML status monitor
    Change-Id: Idc3d10baae8d24362c5c34c4abe5c32e3db1e384
    Reviewed-on: https://forge.frm2.tum.de/review/12656
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/stressi/setups/special/monitor-html.py
@@ -0,0 +1,126 @@
+# This setup file configures the nicos status monitor.
+
+description = 'setup for the status monitor'
+group = 'special'
+
+_expcolumn = Column(
+    Block('Experiment', [
+        BlockRow(Field(name='Proposal', key='exp/proposal', width=7),
+                 Field(name='Title',    key='exp/title',    width=20,
+                       istext=True, maxlen=20),
+                 Field(name='Last file', key='exp/lastscan')),
+        BlockRow(Field(name='Current status', key='exp/action', width=40,
+                       istext=True, maxlen=40)),
+    ],
+    )
+)
+
+_sampletable = Column(
+    Block('Sample table', [
+        BlockRow(Field(dev='xt', format='%.1f')),
+        BlockRow(Field(dev='yt')),
+        BlockRow(Field(dev='zt', format='%.2f')),
+        BlockRow(Field(dev='omgs')),
+        BlockRow(Field(dev='tths')),
+    ],
+    setups = 'sampletable or vstressi',
+    ),
+    Block('Eulerian', [
+        BlockRow(Field(dev='chis')),
+        BlockRow(Field(dev='phis')),
+    ],
+    setups = 'eulerian*',
+    ),
+    Block('Robot', [
+        BlockRow(Field(dev='robx')),
+        BlockRow(Field(dev='roby')),
+        BlockRow(Field(dev='robz')),
+        BlockRow(Field(dev='chir')),
+        BlockRow(Field(dev='phir')),
+        BlockRow(Field(dev='omgr')),
+        BlockRow(Field(dev='tthr')),
+        BlockRow(Field(dev='robt')),
+        BlockRow(Field(dev='robs')),
+        BlockRow(Field(dev='robsl')),
+        BlockRow(Field(dev='robsj')),
+        BlockRow(Field(dev='robsr')),
+    ],
+    setups = 'robot*',
+    ),
+)
+
+_measurement = Column(
+    Block('Measurement', [
+        BlockRow(Field(name='timer', key='tim1/value'),
+                 Field(name='monitor', key='mon/value'),
+                 Field(name='counts', key='image/value'),)
+    ],
+    ),
+    Block('Primary slit', [
+        BlockRow(Field(dev='pst'),
+                 Field(dev='psz'),
+                 Field(dev='psw'),
+                 Field(dev='psh'),),
+    ],
+    setups = 'primaryslit*',
+    ),
+    Block('Secondary optics', [
+        BlockRow(Field(dev='sst'),
+                 Field(dev='ssw'),),
+    ],
+    setups = 'secondaryslit',
+    ),
+    Block('Radial collimator', [
+        BlockRow(Field(dev='mot1'),
+                 Field(dev='rad_fwhm'),),
+    ],
+    setups = 'radial',
+    ),
+    Block('Tesile machine', [
+        BlockRow(Field(dev='teload',)),
+        BlockRow(Field(dev='tepos',)),
+        BlockRow(Field(dev='teext',)),
+    ],
+    setups = 'tensile',
+    ),
+)
+
+_eulerian = Column(
+    Block('', [
+        BlockRow(Field(dev='ReactorPower',)),
+        BlockRow(Field(dev='hv1',),
+                 Field(dev='hv2',)),
+    ],
+    ),
+    Block('Monochromator', [
+        BlockRow(Field(name='Crystal', dev='transm', istext=True)),
+        BlockRow(Field(name='Wave length', dev='wav',)),
+    ]
+    ),
+    Block('Sample Temperature', [
+         BlockRow(Field(dev='T')),
+    ],
+    setups = 'stressihtf or htf* or ccr*',
+    ),
+)
+
+devices = dict(
+    Monitor = device('services.monitor.html.Monitor',
+                     title = 'STRESS-SPEC status monitor',
+                     loglevel = 'info',
+                     interval = 10,
+                     # Use only 'localhost' if the cache is really running on
+                     # the same machine, otherwise use the hostname (official
+                     # computer name) or an IP address.
+                     filename = '/stressicontrol/status.html',
+                     cache = 'stressictrl.stressi.frm2',
+                     font = 'Luxi Sans',
+                     valuefont = 'Consolas',
+                     padding = 0,
+                     prefix = 'nicos/',
+                     # padding = 2,
+                     fontsize = 17,
+                     layout = [Row(_expcolumn),
+                               Row(_sampletable, _measurement, _eulerian)],
+                    ),
+)
