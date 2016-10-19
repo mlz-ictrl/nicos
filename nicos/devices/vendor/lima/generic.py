a61f5d1c427053127dc934897847d3b5964a2c7b
Alexander Lenz <alexander.lenz@frm2.tum.de>
2016-10-19 10:03:47 +0200

    devices/lima/generic: fix simulation.
    Change-Id: I0bcfce20a87b265d3f0411e24f633a3668d322ad
    Reviewed-on: https://forge.frm2.tum.de/review/12688
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

--- nicos/devices/vendor/lima/generic.py
+++ nicos/devices/vendor/lima/generic.py
@@ -171,20 +171,23 @@ class GenericLimaCCD(PyTangoDevice, ImageChannelMixin, PassiveChannel):
         if mode != SIMULATION:
             self._initOptionalComponents()
 
-        if self._dev.camera_model.startswith('SIMCAM'):
-            self.log.warn('Using lima simulation camera! If that\'s not '
-                          'intended, please check the cables and '
-                          'restart the camera and the lima server')
+            if self._dev.camera_model.startswith('SIMCAM'):
+                self.log.warn('Using lima simulation camera! If that\'s not '
+                              'intended, please check the cables and '
+                              'restart the camera and the lima server')
 
-        # set some dummy roi to avoid strange lima rotation behaviour
-        # (not at 0, 0 to avoid possible problems with strides)
-        self._writeRawRoi((5, 5, 5, 5))
-        # ensure NO rotation
-        self._dev.image_rotation = 'NONE'
-        # set full detector size as roi
-        self._writeRawRoi((0, 0, 0, 0))
-        # cache full detector size
-        self._full_shape = (self.imagewidth, self.imageheight)
+            # set some dummy roi to avoid strange lima rotation behaviour
+            # (not at 0, 0 to avoid possible problems with strides)
+            self._writeRawRoi((5, 5, 5, 5))
+            # ensure NO rotation
+            self._dev.image_rotation = 'NONE'
+            # set full detector size as roi
+            self._writeRawRoi((0, 0, 0, 0))
+            # cache full detector size
+            self._full_shape = (self.imagewidth, self.imageheight)
+        else:
+            # some dummy shape for simulation
+            self._full_shape = (2048, 2048)
 
     def doInit(self, mode):
         # Determine image type
