5b301b557fa27fa2822e5147795f37f98793d4e8
Christian Felder <c.felder@fz-juelich.de>
2016-10-26 16:17:08 +0200

    MARIA: add instrument setup.
    Change-Id: I0fdae71c451492bddd818e555400b9cf3087a52f
    Reviewed-on: https://forge.frm2.tum.de/review/12826
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

---
+++ custom/maria/setups/maria.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+description = "MARIA setup"
+group = "basic"
+
+modules = ["maria.scan"]
+
+includes = [
+    "beamstops",
+    "chopper",
+    "collimation",
+    "counter",
+    "det",
+    "hexapod",
+    "ls332",
+    "ls455",
+    "polarizer",
+    "shutter",
+    "slits",
+]
