eff4e29823fdcc5def7bd7240938190d99e31f38
Michael Wedel <michael.wedel@esss.se>
2016-12-14 10:32:28 +0100

    Update motion control setup for ESSIIP lab
    There's now an actual motion axis in the lab, so the setup file is
    renamed and the base PV is set properly.
    
    Change-Id: Id97e7fbb8b5c3e5ff37b7136bd758af7e731f9fc
    Reviewed-on: https://forge.frm2.tum.de/review/13239
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/essiip/setups/motion_stages.py
@@ -0,0 +1,9 @@
+description = 'All motion stages in the ESSIIP-lab.'
+
+devices = dict(
+    m1=device('essiip.epics_motor.EpicsMotor',
+                  epicstimeout=3.0,
+                  description='Single axis positioner',
+                  motorpv='LabS-ESSIIP:MC-MCU-01:m1',
+                  ),
+)
