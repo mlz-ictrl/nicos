2e123810812fe5a620a1aa67766b27c91228f733
L.Fleischhauer-Fuss <l.fleischhauer-fuss@fz-juelich.de>
2017-01-09 16:05:20 +0100

    TREFF: cryo ls332 and gauss meter ls455 setups
    Change-Id: Iae516b2391f3ccc1fad0291644347d76e0085657
    Reviewed-on: https://forge.frm2.tum.de/review/13409
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

---
+++ custom/treff/setups/ls332.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+description = "Setup for the LakeShore 332 cryo temp. controller"
+group = "optional"
+
+tango_base = "tango://phys.treff.frm2:10000/treff/"
+
+devices = dict(
+    T_ls332      = device("devices.tango.TemperatureController",
+                          description = "Temperature regulation",
+                          tangodevice = tango_base + "ls332/t_control1",
+                          pollinterval = 2,
+                          maxage = 5,
+                          abslimits = (0, 300),
+                          precision = 0.01,
+                         ),
+    T_ls332_A    = device("devices.tango.Sensor",
+                          description = "Sensor A",
+                          tangodevice = tango_base + "ls332/t_sensor1",
+                          pollinterval = 2,
+                          maxage = 5,
+                         ),
+    T_ls332_B    = device("devices.tango.Sensor",
+                          description = "Sensor B",
+                          tangodevice = tango_base + "ls332/t_sensor2",
+                          pollinterval = 2,
+                          maxage = 5,
+                         ),
+)
+alias_config = {
+    'T':  {'T_%s' % setupname: 100},
+    'Ts': {'T_%s_A' % setupname: 110,
+           'T_%s_B' % setupname: 100,
+           'T_%s' % setupname: 120,
+          },
+}
---
+++ custom/treff/setups/ls455.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+description = "Gauss meter setup"
+group = "optional"
+
+tango_base = "tango://phys.treff.frm2:10000/treff/"
+
+devices = dict(
+    field = device("devices.tango.AnalogInput",
+                   description = "Lakeshore LS455, DSP Gauss Meter",
+                   tangodevice = tango_base + "ls455/field",
+                  ),
+)
