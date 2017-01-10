d789d022f944567d31013f7abdee1b4f2b695ec2
Alexander Lenz <alexander.lenz@frm2.tum.de>
2017-01-10 10:22:26 +0100

    sans1: add wmirfgen.
    Change-Id: I0c7efd7df31ff328ffbbea43ced948457dd9d0e8
    Reviewed-on: https://forge.frm2.tum.de/review/13417
    Reviewed-by: Stefan Rainow <s.rainow@fz-juelich.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

---
+++ custom/sans1/lib/wmirfgen.py
@@ -0,0 +1,61 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2016 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Alexander Lenz <alexander.lenz@frm2.tum.de>
+#
+# *****************************************************************************
+
+"""Special devices for the WMI microwave generator."""
+
+
+from nicos.core import Param, oneof
+from nicos.devices.tango import AnalogOutput
+
+
+class Frequency(AnalogOutput):
+    """Device for RF generator frequency and frequency modulation settings."""
+    parameters = {
+        'deviation': Param('FM signal deviation', type=float, settable=True),
+        'modulationsource': Param('Modulation source', type=oneof('internal_1',
+                                                                  'internal_2',
+                                                                  'external_1',
+                                                                  'noise'),
+                                  settable=True),
+        'enablemodulation': Param('Enable frequency modulation', type=bool,
+                                  settable=True),
+    }
+
+    def doReadDeviation(self):
+        return self._dev.moddeviation
+
+    def doWriteDeviation(self, value):
+        self._dev.moddeviation = value
+
+    def doReadModulationsource(self):
+        return self._dev.modsource
+
+    def doWriteModulationsource(self, value):
+        self._dev.modsource = value
+
+    def doReadEnablemodulation(self):
+        return self._dev.modulation
+
+    def doWriteEnablemodulation(self, value):
+        self._dev.modulation = value
---
+++ custom/sans1/setups/wmirfgen.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+description = 'WMI microwave generator'
+
+group = 'optional'
+
+includes = []
+
+tango_base = 'tango://%s:10000/box/' % setupname
+
+devices = dict(
+    modulation_io = device('devices.tango.DigitalOutput',
+        description = 'Tango device for the modulation switch',
+        tangodevice = tango_base + 'rfgen/modulation',
+        lowlevel = True,
+    ),
+    swmodulation = device('devices.generic.Switcher',
+        description = 'Modulation switch',
+        moveable = 'modulation_io',
+        mapping = {'on': 1,
+                   'off': 0},
+        fallback = '<undefined>',
+        precision = 0,
+    ),
+    output_io = device('devices.tango.DigitalOutput',
+        description = 'Tango device for the output switch',
+        tangodevice = tango_base + 'rfgen/output',
+        lowlevel = True,
+    ),
+    swoutput = device('devices.generic.Switcher',
+        description = 'Output switch',
+        moveable = 'output_io',
+        mapping = {'on': 1,
+                   'off': 0},
+        fallback = '<undefined>',
+        precision = 0,
+    ),
+    frequency = device('sans1.wmirfgen.Frequency',
+        description = 'Device the frequency and frequency modulation',
+        tangodevice = tango_base + 'rfgen/frequency',
+    ),
+    frequency_rf1 = device('devices.tango.AnalogOutput',
+        description = 'Tango device for the first '
+        'internal frequency generator '
+        '(for modulation)',
+        tangodevice = tango_base + 'rfgen/mod_rf1',
+    ),
+    power = device('devices.tango.AnalogOutput',
+        description = 'Tango device for the power level',
+        tangodevice = tango_base + 'rfgen/power',
+    ),
+)
