b6da3afaf1adc24843aea84d759b694c082c44b1
Christian Felder <c.felder@fz-juelich.de>
2017-01-12 16:21:23 +0100

    TREFF: Slit setups
    Change-Id: I4cd06f80815da39ac5a23278e769baaa2e5f89a2
    Reviewed-on: https://forge.frm2.tum.de/review/13303
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

--- custom/treff/setups/treff.py
+++ custom/treff/setups/treff.py
@@ -3,5 +3,5 @@ description = 'Main Treff setup'
 group = 'basic'
 
 includes = ['analyzer', 'beamstop', 'detector', 'filter', 'flipper', 'mc2',
-            'polarizer', 'sampletable', 'shutter', 'slit1', 'slit2',
+            'polarizer', 'sampletable', 'shutter', 'slits',
            ]
