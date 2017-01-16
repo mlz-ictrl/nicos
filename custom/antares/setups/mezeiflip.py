cbcce9f30274b94310b7271ef7facc92f36c211d
Antares People <antares@frm2.tum.de>
2017-01-16 14:11:38 +0100

    antares: mezeiflip: lambda via entangle.
    Change-Id: I112757fc106ed90f9e99ea1cb130bfed6678c1e8
    Reviewed-on: https://forge.frm2.tum.de/review/13511
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    Tested-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

--- custom/antares/setups/mezeiflip.py
+++ custom/antares/setups/mezeiflip.py
@@ -1,17 +1,19 @@
 description = 'Mezei spin flipper using Lambda Genesys power supply'
 group = 'optional'
 
+tango_base = 'tango://slow.antares.frm2:10000/antares/'
+
 devices = dict(
-    dct1 = device('devices.taco.CurrentSupply',
+    dct1 = device('devices.tango.PowerSupply',
                   description = 'Current 1',
-                  tacodevice = '//antaressrv/antares/lambda/out1',
+                  tangodevice = tango_base + 'lambda1/current',
                   abslimits = (0, 5),
                   lowlevel = False,
                  ),
 
-    dct2 = device('devices.taco.CurrentSupply',
+    dct2 = device('devices.tango.PowerSupply',
                   description = 'Current 2',
-                  tacodevice = '//antaressrv/antares/lambda/out2',
+                  tangodevice = tango_base + 'lambda2/current',
                   abslimits = (0, 5),
                   lowlevel = False,
                  ),
