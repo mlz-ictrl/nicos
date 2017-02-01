d259d0cfc51bb67c7053d532147b08891573cc2b
Christian Felder <c.felder@fz-juelich.de>
2017-02-01 14:20:14 +0100

    TREFF: Add aflipper with values 'up', 'down'
    Change-Id: I023ab933f17cc73208d342f3d51469f0cf3f79ce
    Reviewed-on: https://forge.frm2.tum.de/review/13774
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>

--- custom/treff/setups/pow2cc.py
+++ custom/treff/setups/pow2cc.py
@@ -8,12 +8,18 @@ tango_base = "tango://phys.treff.frm2:10000/treff/"
 excludes = ["pow2vc","virtual_polarizer"]
 
 devices = dict(
-    pow2comp  = device('devices.tango.PowerSupply',
+    pow2comp  = device("devices.tango.PowerSupply",
                       description = "Power supply 2 current control ch 1",
                       tangodevice = tango_base + "toellner/pow2comp",
                      ),
-    pow2flip  = device('devices.tango.PowerSupply',
+    pow2flip  = device("devices.tango.PowerSupply",
                       description = "Power supply 2 current control ch 2",
                       tangodevice = tango_base + "toellner/pow2flip",
                      ),
+    aflipper  = device("treff.flipper.Flipper",
+                       description = "Analyzer flip",
+                       flip = "pow2flip",
+                       corr = "pow2comp",
+                       currents = (1., 0.),
+                      ),
  )
