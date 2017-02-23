82cef585ed3788968d2de41ccb9d13dbb9a575dc
Michael Hofmann <michael.hofmann@frm2.tum.de>
2017-02-23 11:50:47 +0100

    STRESSI: Combined setup Huber Eulerian and XYZ table
    Change-Id: Ic5ec127641490c0f46a7681bd96fef64f0eeb6d4
    Reviewed-on: https://forge.frm2.tum.de/review/14000
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/stressi/setups/eulerian_huber_xyz.py
@@ -0,0 +1,52 @@
+description = 'STRESS-SPEC setup with Huber Eulerian cradle plus small xyz table'
+
+group = 'basic'
+
+includes = ['system','eulerian_huber',  'mux', 'monochromator', 'detector', 'sampletable',
+            'primaryslit', 'slits', 'reactor']
+
+excludes = ['eulerian_newport', 'eulerian_tensile', 'robot', 'primaryslit_huber']
+
+sysconfig = dict(
+    datasinks = ['caresssink'],
+)
+
+servername = 'VME'
+
+nameservice = 'stressictrl.stressi.frm2'
+
+devices = dict(
+    xe =      device('devices.vendor.caress.Motor',
+                     description = 'HWB XE',
+                     fmtstr = '%.2f',
+                     unit = 'mm',
+                     coderoffset = 0,
+                     abslimits = (-80, 80),
+                     nameserver = '%s' % (nameservice,),
+                     objname = '%s' % (servername),
+                     config = 'XE 115 11 0x00f1c000 4 50 1000 100 1 0 0 0 0 1'
+                              ' 5000 1 10 0 0 0',
+                    ),
+    ye =      device('devices.vendor.caress.Motor',
+                     description = 'HWB YE',
+                     fmtstr = '%.2f',
+                     unit = 'mm',
+                     coderoffset = 0,
+                     abslimits = (-80, 80),
+                     nameserver = '%s' % (nameservice,),
+                     objname = '%s' % (servername),
+                     config = 'YE 115 11 0x00f1d000 2 50 1000 100 1 0 0 0 0 1 '
+                              '5000 1 10 0 0 0',
+                    ),
+    ze =      device('devices.vendor.caress.Motor',
+                     description = 'HWB ZE',
+                     fmtstr = '%.2f',
+                     unit = 'mm',
+                     coderoffset = 0,
+                     abslimits = (0, 20),
+                     nameserver = '%s' % (nameservice,),
+                     objname = '%s' % (servername),
+                     config = 'ZE 115 11 0x00f1f000 4 200 1000 100 1 0 0 0 0 1 '
+                              '5000 1 10 0 0 0',
+                    ),
+)
