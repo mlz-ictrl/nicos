0ac98814d384f789dcc6fa4938a507f48ae80728
Jens Krüger <jens.krueger@tum.de>
2017-02-28 19:59:49 +0100

    DEMO: Reformat daemon setup
    Change-Id: Ic141fd8d8773692fe0eebf45a8ea194829fedbc9
    Reviewed-on: https://forge.frm2.tum.de/review/14061
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/demo/setups/special/daemon.py
+++ custom/demo/setups/special/daemon.py
@@ -1,19 +1,20 @@
 description = 'setup for the execution daemon'
+
 group = 'special'
 
 import hashlib
 
 devices = dict(
-    Auth   = device('services.daemon.auth.ListAuthenticator',
-                    hashing = 'sha1',
-                    # first entry is the user name, second the hashed password, third the user level
-                    passwd = [('guest', '', 'guest'),
-                              ('user', hashlib.sha1(b'user').hexdigest(), 'user'),
-                              ('admin', hashlib.sha1(b'admin').hexdigest(), 'admin')],
-                   ),
+    Auth = device('services.daemon.auth.ListAuthenticator',
+                  hashing = 'sha1',
+                  # first entry is the user name, second the hashed password, third the user level
+                  passwd = [('guest', '', 'guest'),
+                            ('user', hashlib.sha1(b'user').hexdigest(), 'user'),
+                            ('admin', hashlib.sha1(b'admin').hexdigest(), 'admin')],
+                 ),
     Daemon = device('services.daemon.NicosDaemon',
-                    server = 'localhost',
                     authenticators = ['Auth'],
                     loglevel = 'debug',
+                    server = 'localhost',
                    ),
 )
