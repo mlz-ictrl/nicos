23e777c2e3dad0d7b90255ba3797b212cc02213d
Georg Brandl <g.brandl@fz-juelich.de>
2017-03-17 12:19:12 +0100

    FRM2: add setups for Kelvinox and Variox
    Change-Id: I1b32e359370e4b3a2a28f88f44449cc191f8d187
    Reviewed-on: https://forge.frm2.tum.de/review/12032
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ custom/frm2/setups/kelvinox1.py
@@ -0,0 +1,39 @@
+description = 'Kelvinox low temperature insert'
+
+group = 'plugplay'
+
+includes = ['alias_T']
+
+tango_base = 'tango://%s:10000/box/' % setupname
+
+devices = {
+    'T_%s_sorb' % setupname: device('devices.tango.Sensor',
+                                    description = 'Sorb temperature',
+                                    tangodevice = tango_base + 'igh/sorb',
+                                    unit = 'K',
+                                    fmtstr = '%.3f',
+                                    pollinterval = 1,
+                                    maxage = 2,
+                                   ),
+    'T_%s_pot' % setupname:  device('devices.tango.Sensor',
+                                    description = '1K pot temperature',
+                                    tangodevice = tango_base + 'igh/pot',
+                                    unit = 'K',
+                                    fmtstr = '%.3f',
+                                    pollinterval = 1,
+                                    maxage = 2,
+                                   ),
+    'T_%s_mix' % setupname:  device('devices.tango.Sensor',
+                                    description = 'Mix chamber temperature',
+                                    tangodevice = tango_base + 'igh/mix',
+                                    unit = 'K',
+                                    fmtstr = '%.3f',
+                                    pollinterval = 1,
+                                    maxage = 2,
+                                   ),
+
+}
+
+alias_config = {
+    'Ts':  {'T_%s_mix' % setupname: 200},
+}
