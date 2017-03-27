f46544bb345fbcacf23c352f714cb4f835dd015c
Georg Brandl <g.brandl@fz-juelich.de>
2017-03-27 07:42:20 +0200

    FRM2: add setup for cce2 (CC with extension) cryostat
    Change-Id: I54702fc90d6498d78b9f9aa4740ef338caec2bf0
    Reviewed-on: https://forge.frm2.tum.de/review/14351
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ custom/frm2/setups/cce2.py
@@ -0,0 +1,41 @@
+description = 'Closed-cycle cryostat with extension finger'
+
+group = 'plugplay'
+
+includes = ['alias_T']
+
+tango_base = 'tango://%s:10000/box/' % setupname
+
+devices = {
+    'T_%s' % setupname: device('devices.tango.TemperatureController',
+                               description = 'The main temperature control',
+                               tangodevice = tango_base + 'ls/control',
+                               unit = 'K',
+                               fmtstr = '%.3f',
+                              ),
+
+    'T_%s_A' % setupname: device('devices.tango.Sensor',
+                               description = 'Sensor A',
+                               tangodevice = tango_base + 'ls/sensora',
+                               unit = 'K',
+                               fmtstr = '%.3f',
+                              ),
+
+    'T_%s_B' % setupname: device('devices.tango.Sensor',
+                               description = 'Sensor B',
+                               tangodevice = tango_base + 'ls/sensorb',
+                               unit = 'K',
+                               fmtstr = '%.3f',
+                              ),
+
+    'T_%s_range' % setupname: device('devices.tango.AnalogOutput',
+                               description = 'The heater range',
+                               tangodevice = tango_base + 'ls/range',
+                               fmtstr = '%d',
+                              ),
+}
+
+alias_config = {
+    'T':  {'T_%s' % setupname: 200},
+    'Ts': {'T_%s_A' % setupname: 100, 'T_%s_B' % setupname: 90},
+}
