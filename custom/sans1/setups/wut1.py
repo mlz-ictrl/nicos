4d5707ff12e8c9c68e7190a5a3a5e34ddf32e231
Sans1 People <sans1@frm2.tum.de>
2017-03-29 10:30:57 +0200

    SANS-1: change hostname do back to dns name
    after ip correction the hostname ist now again the dns name
    
    Change-Id: I86200e5d20a2033ca3ffb008cd1975c3b6a44cc2
    Reviewed-on: https://forge.frm2.tum.de/review/14394
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

--- custom/sans1/setups/wut1.py
+++ custom/sans1/setups/wut1.py
@@ -6,8 +6,7 @@ includes = []
 
 devices = dict(
     s1_wut1 = device('sans1.wut.WutValue',
-                            #hostname = 'sans1wut1.office.frm2',
-                            hostname = '172.25.49.24',
+                            hostname = 'sans1wut1.sans1.frm2',
                             port = '1',
                             description = 'sensor 1 of wut box 1',
                             fmtstr = '%.2F',
@@ -16,7 +15,7 @@ devices = dict(
                             unit = 'V',
     ),
     s2_wut2 = device('sans1.wut.WutValue',
-                            hostname = '172.25.49.24',
+                            hostname = 'sans1wut1.sans1.frm2',
                             port = '2',
                             description = 'sensor 2 of wut box 1',
                             fmtstr = '%.2F',
