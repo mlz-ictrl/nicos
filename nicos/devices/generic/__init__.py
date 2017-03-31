4ba8967d3e981c0d5d1916ff8e77e8a3344804ca
Jens Krüger <jens.krueger@frm2.tum.de>
2017-03-31 09:25:44 +0200

    GENERIC: add some classes and fix some PEP8 issues
    Change-Id: If560f640ff7c73d54c41400386af4bd433c814af
    Reviewed-on: https://forge.frm2.tum.de/review/14425
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos/devices/generic/__init__.py
+++ nicos/devices/generic/__init__.py
@@ -28,19 +28,20 @@
 from nicos.core.device import DeviceAlias, NoDevice
 
 from nicos.devices.generic.axis import Axis
-from nicos.devices.generic.detector import Detector, \
-    ImageChannelMixin, TimerChannelMixin, CounterChannelMixin, \
-    PassiveChannel, ActiveChannel, DetectorForecast, GatedDetector
+from nicos.devices.generic.cache import CacheReader, CacheWriter
+from nicos.devices.generic.detector import ActiveChannel, CounterChannelMixin,\
+    Detector, DetectorForecast, GatedDetector, ImageChannelMixin, \
+    PassiveChannel, RectROIChannel, TimerChannelMixin
+from nicos.devices.generic.magnet import BipolarSwitchingMagnet, \
+    CalibratedMagnet
 from nicos.devices.generic.manual import ManualMove, ManualSwitch
 from nicos.devices.generic.oscillator import Oscillator
 from nicos.devices.generic.paramdev import ParamDevice, ReadonlyParamDevice
+from nicos.devices.generic.sequence import BaseSequencer, LockedDevice
 from nicos.devices.generic.slit import Slit, TwoAxisSlit
-from nicos.devices.generic.switcher import Switcher, ReadonlySwitcher, \
-    MultiSwitcher
-from nicos.devices.generic.cache import CacheReader, CacheWriter
+from nicos.devices.generic.switcher import MultiSwitcher, ReadonlySwitcher, \
+    Switcher
 from nicos.devices.generic.system import FreeSpace
-from nicos.devices.generic.virtual import VirtualMotor, VirtualCoder, \
-    VirtualTimer, VirtualCounter, VirtualTemperature, VirtualImage
-from nicos.devices.generic.sequence import LockedDevice, BaseSequencer
-from nicos.devices.generic.magnet import CalibratedMagnet, \
-    BipolarSwitchingMagnet
+from nicos.devices.generic.virtual import VirtualCoder, VirtualCounter, \
+    VirtualImage, VirtualMotor, VirtualRealTemperature, \
+    VirtualScanningDetector, VirtualTemperature, VirtualTimer
