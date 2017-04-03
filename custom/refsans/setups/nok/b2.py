38729a5facbe9b7a35b61f995789f5497bfefb76
Matthias Pomm <matthias.pomm@hzg.de>
2017-04-03 17:54:35 +0200

    REFSANS: Add b2 axes for reactor and sample side
    Change-Id: Iacae6cdbbfa320d2a70d20cb2cf14aa796f6f354
    Reviewed-on: https://forge.frm2.tum.de/review/14444
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/refsans/setups/nok/b2.py
+++ custom/refsans/setups/nok/b2.py
@@ -16,26 +16,40 @@ devices = dict(
 #
 ## smccorvusserver b2 exports
 #
+    b2r    = device('devices.generic.Axis',
+                    description = 'b2, reactorside',
+                    motor = 'smccorvus_b2mr',
+                    precision = 0.0,
+                   ),
+    b2s    = device('devices.generic.Axis',
+                    description = 'b2, sampleside',
+                    motor = 'smccorvus_b2ms',
+                    precision = 0.0,
+                   ),
     smccorvus_b2er = device('devices.taco.Coder',
                             description = 'Device test/smccorvus/b2er of Server smccorvusserver b2',
                             tacodevice = '//%s/test/smccorvus/b2er' % nethost,
+                            lowlevel = True,
                            ),
 
     smccorvus_b2mr = device('devices.taco.Motor',
                             description = 'Device test/smccorvus/b2mr of Server smccorvusserver b2',
                             tacodevice = '//%s/test/smccorvus/b2mr' % nethost,
                             abslimits = (-294, 222),
+                            lowlevel = True,
                            ),
 
     smccorvus_b2es = device('devices.taco.Coder',
                             description = 'Device test/smccorvus/b2es of Server smccorvusserver b2',
                             tacodevice = '//%s/test/smccorvus/b2es' % nethost,
+                            lowlevel = True,
                            ),
 
     smccorvus_b2ms = device('devices.taco.Motor',
                             description = 'Device test/smccorvus/b2ms of Server smccorvusserver b2',
                             tacodevice = '//%s/test/smccorvus/b2ms' % nethost,
                             abslimits = (-296, 213),
+                            lowlevel = True,
                            ),
 
 )
