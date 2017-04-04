f56441c0168b7cc563d42cca94e9829ebd46f132
Christian Felder <c.felder@fz-juelich.de>
2017-04-04 18:02:06 +0200

    TREFF: Add filter temperatures.
    Change-Id: I0fc1d43b21bb9cd57906df42549aadaa846ddfa1
    Reviewed-on: https://forge.frm2.tum.de/review/14463
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

--- custom/treff/setups/filter.py
+++ custom/treff/setups/filter.py
@@ -7,20 +7,36 @@ tango_base = 'tango://phys.treff.frm2:10000/treff/'
 excludes = ['virtual_filter']
 
 devices = dict(
-    be_filter = device('devices.tango.NamedDigitalOutput',
-                       description = 'Beryllium filter',
-                       tangodevice = tango_base + 'FZJDP_Digital/BeFilter',
-                       mapping = {
-                          "in" : 1,
-                          "out": 0,
-                       }
-                      ),
-    be_heater = device('devices.tango.NamedDigitalOutput',
-                       description = 'Beryllium heater',
-                       tangodevice = tango_base + 'FZJDP_Digital/BeHeater',
-                       mapping = {
-                          "off" : 0,
-                          "on"  : 1,
-                       }
-                      ),
+    be_filter   = device('devices.tango.NamedDigitalOutput',
+                         description = 'Beryllium filter',
+                         tangodevice = tango_base + 'FZJDP_Digital/BeFilter',
+                         mapping = {
+                            "in" : 1,
+                            "out": 0,
+                         }
+                        ),
+    be_heater   = device('devices.tango.NamedDigitalOutput',
+                         description = 'Beryllium heater',
+                         tangodevice = tango_base + 'FZJDP_Digital/BeHeater',
+                         mapping = {
+                            "off" : 0,
+                            "on"  : 1,
+                         }
+                        ),
+    T_be_filter = device('devices.tango.AnalogInput',
+                         description = 'Beryllium filter/crystal temperature',
+                         tangodevice = tango_base + 'FZJDP_Analog/TBeFilter',
+                         fmtstr = '%.0f',
+                         unit = 'K',
+                         pollinterval = 5,
+                         maxage = 6,
+                        ),
+    T_be_heater = device('devices.tango.AnalogInput',
+                         description = 'Beryllium heater temperature',
+                         tangodevice = tango_base + 'FZJDP_Analog/TBeHeater',
+                         fmtstr = '%.0f',
+                         unit = 'K',
+                         pollinterval = 5,
+                         maxage = 6,
+                        ),
 )
