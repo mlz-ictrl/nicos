5b13744761099a99afa0c08635718360ed304833
Georg Brandl <g.brandl@fz-juelich.de>
2017-04-12 08:48:01 +0200

    KWS3: implement selector lambda control
    Change-Id: I90c31711b51da5434421833b2dfa58e3aa62baf6
    Reviewed-on: https://forge.frm2.tum.de/review/14564
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ custom/kws3/lib/selector.py
@@ -0,0 +1,64 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2017 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Georg Brandl <g.brandl@fz-juelich.de>
+#
+# *****************************************************************************
+
+"""Class for KWS-3 selector (custom solution using Pfeiffer controller)."""
+
+import time
+
+from nicos.core import Moveable, Readable, HasLimits, HasPrecision, \
+    Attach, MoveError, status
+
+
+class SelectorSpeed(HasLimits, HasPrecision, Moveable):
+    """Control selector speed via SPS I/O devices."""
+
+    attached_devices = {
+        'valid':     Attach('Output for the "valid" bit', Moveable),
+        'speedset':  Attach('Output for speed setpoint', Moveable),
+        'speedread': Attach('Input for reading the speed', Readable),
+        'status':    Attach('Input for reading the status', Readable),
+    }
+
+    def doRead(self, maxage=0):
+        return self._attached_speedread.read(maxage)
+
+    def doStatus(self, maxage=0):
+        stbits = self._attached_status.read(maxage)
+        if not stbits & 1:
+            return status.WARN, 'local mode'
+        if not stbits & 2:
+            if self._attached_speedread.read(maxage) == 0:
+                return status.WARN, 'off'
+            return status.BUSY, 'moving'
+        return status.OK, ''
+
+    def doStart(self, pos):
+        stbits = self._attached_status.read(0)
+        if not stbits & 1:
+            raise MoveError(self, 'selector is in local mode')
+        # valid bit needs a rising edge
+        self._attached_valid.maw(0)
+        self._attached_speedset.maw(pos)
+        self._attached_valid.maw(1)
+        time.sleep(0.2)
---
+++ custom/kws3/setups/config_selector.py
@@ -0,0 +1,7 @@
+description = 'preset values for the velocity selector'
+group = 'configdata'
+
+SELECTOR_PRESETS = {
+    '12A':   dict(lam=12,   spread=0.2),
+    '15A':   dict(lam=15,   spread=0.2),
+}
