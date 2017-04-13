c83e6f4df9a7c59b84406da06650be295c7013fd
Jens Krüger <jens.krueger@frm2.tum.de>
2017-04-13 18:11:16 +0200

    PGAA: add the XYZ sample table setup
    Change-Id: I8cecafa7a3e1a68531a665c1f7fd9d9e4ecd183a
    Reviewed-on: https://forge.frm2.tum.de/review/14599
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/pgaa/setups/pgai.py
@@ -0,0 +1,23 @@
+description = 'PGAA setup with Huber sample table'
+
+group = 'basic'
+
+includes = ['system',
+            'reactor',
+            'nl4b',
+            'pressure',
+            'sampletable',
+            'pilz',
+            'detector',
+            'collimation',
+           ]
+
+devices = dict(
+    sink = device('pgaa.datasinks.PGAASink',
+                  settypes = set(['scan']),
+                  det1 = '_60p',
+                  det2 = 'LEGe',
+                  vac = 'chamber_pressure',
+                  lowlevel = True,
+                 )
+)
