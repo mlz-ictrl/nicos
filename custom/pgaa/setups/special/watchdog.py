87f9e3dcde1abc8bf53d359ddcbf787b95b23b1a
Jens Krüger <jens.krueger@frm2.tum.de>
2017-04-13 17:31:09 +0200

    PGAA: add watchdog setup
    Change-Id: Ib0ed60dd5cc3fd2c649a7d8baceb1878bdae180a
    Reviewed-on: https://forge.frm2.tum.de/review/14591
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ custom/pgaa/setups/special/watchdog.py
@@ -0,0 +1,48 @@
+# This setup file configures the nicos poller service.
+
+description = 'setup for the NICOS watchdog'
+group = 'special'
+
+# watchlist:
+# The entries in this list are dictionaries. Possible keys:
+#
+# 'setup' -- setup that must be loaded (default '' to mean all setups)
+# 'condition' -- condition for warning (a Python expression where cache keys
+#    can be used: t_value stands for t/value etc.
+# 'gracetime' -- time in sec allowed for the condition to be true without
+#    emitting a warning (default 5 sec)
+# 'message' -- warning message to display
+# 'type' -- for defining different types of warnings; this corresponds to the
+#     configured notifiers (default 'default')
+#     type '' does not emit warnings (useful together with scriptaction)
+# 'scriptaction' -- 'pausecount' to pause the count loop on the condition
+#     or 'stop' or 'immediatestop' to cancel script execution
+#     (default '')
+# 'action' -- code to execute if condition is true (default no code is executed)
+watchlist = [
+#    dict(condition = 't_value > 100',
+#         message = 'Temperature too high',
+#         type = 'critical',
+#         action = 'maw(T, 0)',
+#        ),
+#    dict(condition = 'phi_value > 100 and mono_value > 1.5',
+#         message = 'phi angle too high for current mono setting',
+#         gracetime = 5,
+#        ),
+]
+
+includes = ['notifiers']
+
+notifiers = {
+    'default':  ['mailer'],
+    'critical': ['mailer', 'smser'],
+}
+
+devices = dict(
+    Watchdog = device('services.watchdog.Watchdog',
+                      cache = 'tequila.pgaa.frm2:14869',
+                      notifiers = notifiers,
+                      mailreceiverkey = 'email/receivers',
+                      watch = watchlist,
+                     ),
+)
