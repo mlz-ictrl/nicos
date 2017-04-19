6d17d21aecf5202b1ed2f2b72c2caa7713b6fa5f
Alexander Lenz <alexander.lenz@frm2.tum.de>
2017-04-19 16:07:04 +0200

    magnet75 renamed to ccm55v.
    Change-Id: If5a89b2997ea265d9ac79589396a8f62b881447a
    Reviewed-on: https://forge.frm2.tum.de/review/14618
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>
    Tested-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

--- custom/frm2/setups/ccm55v.py
+++ custom/frm2/setups/ccm55v.py
@@ -1,17 +1,19 @@
-description = 'FRM II 7.5 T superconducting magnet'
+description = 'FRM II 5.5 T superconducting magnet'
 
-group = 'optional'
+group = 'plugplay'
 
 includes = ['alias_B', 'alias_sth']
 
-# Important: nethost != setupname
-nethost = 'magnet'
+# setupname is set by nicos before loading this file
+# setupname = filename - '.py' extension
+nethost = setupname
+
 
 devices = {
     'B_%s' % setupname: device('devices.taco.CurrentSupply',
                                description = 'The magnetic field',
                                tacodevice = '//%s/magnet/smc120/t' % nethost,
-                               abslimits = (-7.5, 7.5),
+                               abslimits = (-5.555, 5.555),
                               ),
 
     'sth_%s' % setupname: device('devices.taco.Axis',
@@ -26,7 +28,7 @@ maxtemps = [None, 4.3, 4.3, 5.1, 4.7, None, None, None, 4.3]
 
 for i in range(1, 9):
     dev = device('devices.taco.TemperatureSensor',
-                 description = '7.5T magnet temperature sensor %d' % i,
+                 description = '5.5T magnet temperature sensor %d' % i,
                  tacodevice = '//%s/magnet/ls218/sens%d' % (nethost, i),
                  warnlimits = (0, maxtemps[i]),
                  pollinterval = 30,
