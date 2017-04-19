38b402b956774ab9cade67ed9b1f0f471142239a
Jens Krüger <jens.krueger@frm2.tum.de>
2017-04-19 17:06:10 +0200

    REFSANS: Fix some PEP8 issues
    Change-Id: I4f40114bb562b9ae276492efaaff4c8b93d9df1a
    Reviewed-on: https://forge.frm2.tum.de/review/14621
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/refsans/lib/beckhoff/nok.py
+++ custom/refsans/lib/beckhoff/nok.py
@@ -44,7 +44,6 @@ from nicos.refsans.params import motoraddress
 # according to docu: '_2013-04-05 Anhang A V0.6.pdf'
 
 
-
 class SingleMotorOfADoubleMotorNOK(DeviceMixinBase):
     """
     empty class marking a Motor as beeing useable by a (DoubleMotorNok)
@@ -55,7 +54,8 @@ class SingleMotorOfADoubleMotorNOK(DeviceMixinBase):
 class BeckhoffCoderBase(TacoDevice, Coder):
     """
     Device object for a digital Input device via a Beckhoff modbus interface.
-    For a stripped down motor control block which only provides the current position.
+    For a stripped down motor control block which only provides the current
+    position.
     """
     taco_class = Modbus
 
@@ -64,71 +64,71 @@ class BeckhoffCoderBase(TacoDevice, Coder):
     parameters = {
         # provided by parent class: speed, unit, fmtstr, warnlimits,
         #                           abslimits, userlimits, precision, ...
-        'address'    : Param('Starting offset (words) of Motor control Block',
-                             type=motoraddress, mandatory=True,
-                             settable=False, userparam=False),
-        'slope'      : Param('Slope of the Motor in FULL steps per physical '
-                             'unit', type=float, default=10000.,
-                             unit='steps/main', userparam=False,
-                             settable=False),
-        'firmware'   : Param('Firmware Version of motor control block',
-                             type=str, default='?', unit='', settable=False),
+        'address': Param('Starting offset (words) of Motor control Block',
+                         type=motoraddress, mandatory=True,
+                         settable=False, userparam=False),
+        'slope': Param('Slope of the Motor in FULL steps per physical '
+                       'unit', type=float, default=10000.,
+                       unit='steps/main', userparam=False,
+                       settable=False),
+        'firmware': Param('Firmware Version of motor control block',
+                          type=str, default='?', unit='', settable=False),
     }
     parameters['address'].ext_desc = '''
     ModBus word-register address of the motor control block to use.
     It consists of a coupler specific offset (0x3000 or 0x4000),
     the base location of the first motor control block (byte offset 64
-    = word offset 0x20) and an integer multiple of the size of the control
-    blocks (20 Bytes = 10 words). So the lowest legal value is 0x3020
+    = word offset 0x20) and an integer multiple of the size of the
+    control blocks (20 Bytes = 10 words). So the lowest legal value is 0x3020
     (control block 0) and the higest would be 0x47fa (control block 201).
     Normaly, only the first N motor control blocks are used (without gaps).
     If this scheme changes, the check needs to be adopted.
     '''
 
     parameter_overrides = {
-        'unit':  Override(default='mm'),
+        'unit': Override(default='mm'),
     }
 
     # Not all classes deriving from here use all the errors /stati.
     # this is a unification of all so far checked motors
     # Note: docu is in german
     HW_Errors = {
-        1   : 'Antrieb nicht bereit',
-        3   : 'Auftrag abgebrochen (Safety key?)',
-        90  : 'Achse nicht aktivierbar',
-        99  : 'NC-Error',
-        100 : 'Motorfehler',
-        101 : 'Motorfehler',
-        102 : 'Motorfehler',
-        103 : 'Motorfehler',
-        104 : 'Motorfehler',
-        105 : 'Motorfehler',
-        106 : 'Motorfehler',
-        107 : 'Motorfehler',
-        108 : 'Motorfehler',
-        109 : 'Motorfehler',
-        110 : 'Encodersignal invalid',
-        111 : 'Zusatzendschalter aktiv/Kollisionswarnung: Technikum anrufen!',
-        112 : 'Encoderfehler',
-        113 : 'Encoderfehler',
-        114 : 'Encoderfehler',
-        115 : 'Encoderfehler',
-        116 : 'Encoderfehler',
-        117 : 'Encoderfehler',
-        118 : 'Encoderfehler',
-        119 : 'Encoderfehler',
-        128 : 'Kommunikation Leistungsendstufe fehlgeschlagen (reset?)',
-        129 : 'Kommunikation Leistungsendstufe final fehlgeschlagen (powercycle?)',
-        130 : 'Bremsenfehler',
-        131 : 'Bremsenfehler',
-        132 : 'Bremsenfehler',
-        133 : 'Bremsenfehler',
-        134 : 'Bremsenfehler',
-        135 : 'Bremsenfehler',
-        136 : 'Bremsenfehler',
-        137 : 'Bremsenfehler',
-        138 : 'Bremsenfehler',
-        139 : 'Bremsenfehler',
+        1  : 'Antrieb nicht bereit',
+        3  : 'Auftrag abgebrochen (Safety key?)',
+        90 : 'Achse nicht aktivierbar',
+        99 : 'NC-Error',
+        100: 'Motorfehler',
+        101: 'Motorfehler',
+        102: 'Motorfehler',
+        103: 'Motorfehler',
+        104: 'Motorfehler',
+        105: 'Motorfehler',
+        106: 'Motorfehler',
+        107: 'Motorfehler',
+        108: 'Motorfehler',
+        109: 'Motorfehler',
+        110: 'Encodersignal invalid',
+        111: 'Zusatzendschalter aktiv/Kollisionswarnung: Technikum anrufen!',
+        112: 'Encoderfehler',
+        113: 'Encoderfehler',
+        114: 'Encoderfehler',
+        115: 'Encoderfehler',
+        116: 'Encoderfehler',
+        117: 'Encoderfehler',
+        118: 'Encoderfehler',
+        119: 'Encoderfehler',
+        128: 'Kommunikation Leistungsendstufe fehlgeschlagen (reset?)',
+        129: 'Kommunikation Leistungsendstufe final fehlgeschlagen (powercycle?)',
+        130: 'Bremsenfehler',
+        131: 'Bremsenfehler',
+        132: 'Bremsenfehler',
+        133: 'Bremsenfehler',
+        134: 'Bremsenfehler',
+        135: 'Bremsenfehler',
+        136: 'Bremsenfehler',
+        137: 'Bremsenfehler',
+        138: 'Bremsenfehler',
+        139: 'Bremsenfehler',
     }
     HW_Errorbits = (
         (0, 'Unterspannung Logic'),
@@ -164,6 +164,7 @@ class BeckhoffCoderBase(TacoDevice, Coder):
         # switch off watchdog, important before doing any write access
         if mode != SIMULATION:
             self._taco_guard(self._dev.writeSingleRegister, (0, 0x1120, 0))
+
     #
     # access-helpers for accessing the fields inside the MotorControlBlock
     #
@@ -183,7 +184,7 @@ class BeckhoffCoderBase(TacoDevice, Coder):
         self.log.debug('write control bit: %x', tmpval)
         self._taco_guard(self._dev.writeSingleRegister,
                          (0, self.address, tmpval))
-        session.delay(0.1) # work around race conditions....
+        session.delay(0.1)  # work around race conditions....
 
     def _writeDestination(self, value):
         self.log.debug('_writeDestination %r', value)
@@ -254,7 +255,7 @@ class BeckhoffCoderBase(TacoDevice, Coder):
     def _speed2phys(self, speed):
         self.log.debug('_speed2phys: %r speed -> %s/s',
                        speed, self.format(speed, unit=True))
-        return speed  / float(self.slope)
+        return speed / float(self.slope)
 
     def _phys2speed(self, value):
         speed = int(value * float(self.slope))
@@ -265,11 +266,8 @@ class BeckhoffCoderBase(TacoDevice, Coder):
     #
     # Hardware abstraction: which actions do we want to do...
     #
-
     def _HW_status(self):
-        """ used Status bits:
-
-        """
+        """Used status bits."""
         # read HW values
         errval = self._readErrorWord()
         statval = self._readStatusWord() ^ self.HW_Status_Inv
@@ -277,11 +275,12 @@ class BeckhoffCoderBase(TacoDevice, Coder):
         msg = bitDescription(statval, *self.HW_Statusbits)
         # check for errors first, then warnings, busy and Ok
         if errval & 0xff:
-            msg = '%s, %s' % (bitDescription(errval & 0xff, *self.HW_Errorbits), msg)
+            msg = '%s, %s' % (bitDescription(errval & 0xff,
+                                             *self.HW_Errorbits), msg)
         if errval:
             errnum = errval >> 8
-            return status.ERROR, 'ERROR %d: %s, %s' % (errnum,
-                self.HW_Errors.get(errnum, 'Unknown Error'), msg)
+            return status.ERROR, 'ERROR %d: %s, %s' % (
+                errnum, self.HW_Errors.get(errnum, 'Unknown Error'), msg)
 
         for mask, stat in self.HW_Status_map:
             if statval & mask:
@@ -292,7 +291,6 @@ class BeckhoffCoderBase(TacoDevice, Coder):
     #
     # Nicos methods
     #
-
     def doRead(self, maxage=0):
         return self._steps2phys(self._readPosition())
 
@@ -319,7 +317,7 @@ class BeckhoffMotorBase(CanReference, HasTimeout, BeckhoffCoderBase, Motor):
     # invert bit 13 (referenced) to NOT REFERENCED
     # invert bit 8 (ready) to NOT READY
     # invert bit 0 (enabled) to NOT ENABLED
-    HW_Status_Inv = (1<<13) | (1<<8) | (1<<0)
+    HW_Status_Inv = (1 << 13) | (1 << 8) | (1 << 0)
     # map mask of bits to status to return if any bits within mask is set
     HW_Status_map = (
         #  FEDCBA9876543210
@@ -340,7 +338,7 @@ class BeckhoffMotorBase(CanReference, HasTimeout, BeckhoffCoderBase, Motor):
 
     def _HW_reference(self):
         """Do the referencing and update position to refpos"""
-        self._writeControlBit(4, 1)     # docu: bit4 = reference, autoresets ???
+        self._writeControlBit(4, 1)     # docu: bit4 = reference, autoresets???
         session.delay(0.5)
         self._writeControlBit(4, 0)
 
@@ -361,17 +359,18 @@ class BeckhoffMotorBase(CanReference, HasTimeout, BeckhoffCoderBase, Motor):
     @requires(level='user')
     def _HW_readParameter(self, index):
         if index not in self.HW_readable_Params:
-            raise UsageError("Reading not possible for parameter index %d" % index)
+            raise UsageError('Reading not possible for parameter index %d' %
+                             index)
 
         index = self.HW_readable_Params.get(index, index)
         self.log.debug('readParameter %d', index)
-        if self._readStatusWord() & (1<<7):
+        if self._readStatusWord() & (1 << 7):
             raise UsageError(self, 'Can not access Parameters while Motor is '
                              'moving, please stop it first!')
         # wait for inactive ACK/NACK
         self.log.debug('Wait for idle ACK/NACK bits')
         for _ in range(1000):
-            if self._readStatusWord() & (3<<14) == 0:
+            if self._readStatusWord() & (3 << 14) == 0:
                 break
         else:
             raise CommunicationError(self, 'HW still busy, can not read '
@@ -382,13 +381,13 @@ class BeckhoffMotorBase(CanReference, HasTimeout, BeckhoffCoderBase, Motor):
 
         self.log.debug('Wait for ACK/NACK bits')
         for _ in range(1000):
-            if self._readStatusWord() & (3<<14) != 0:
+            if self._readStatusWord() & (3 << 14) != 0:
                 break
         else:
             raise CommunicationError(self, 'ReadPar command not recognized by '
                                      'HW, please retry later....')
 
-        if self._readStatusWord() & (1<<14):
+        if self._readStatusWord() & (1 << 14):
             raise CommunicationError(self, 'Reading of Parameter %r failed, '
                                      'got a NACK' % index)
         return self._readReturn()
@@ -396,25 +395,27 @@ class BeckhoffMotorBase(CanReference, HasTimeout, BeckhoffCoderBase, Motor):
     @requires(level='admin')
     def _HW_writeParameter(self, index, value, store2eeprom=False):
         if index not in self.HW_writeable_Params:
-            raise UsageError("Writing not possible for parameter index %d" % index)
+            raise UsageError('Writing not possible for parameter index %d' %
+                             index)
 
         index = self.HW_writeable_Params.get(index, index)
         self.log.debug('writeParameter %d:0x%04x', index, value)
 
         if store2eeprom:
-            if self._HW_ReadStatusWord() & (1<<6) == 0:
+            if self._HW_ReadStatusWord() & (1 << 6) == 0:
                 # target reached not set -> problem
-                raise UsageError(self, 'Param acces no possible until target reached')
+                raise UsageError(self, 'Param acces no possible until target '
+                                 'reached')
             self.log.warning('writeParameter stores to eeprom !')
 
-        if self._readStatusWord() & (1<<7):
+        if self._readStatusWord() & (1 << 7):
             raise UsageError(self, 'Can not access Parameters while Motor is '
                              'moving, please stop it first!')
 
         # wait for inactive ACK/NACK
         self.log.debug('Wait for idle ACK/NACK bits')
         for _ in range(1000):
-            if self._readStatusWord() & (3<<14) == 0:
+            if self._readStatusWord() & (3 << 14) == 0:
                 break
         else:
             raise CommunicationError(self, 'HW still busy, can not write '
@@ -423,24 +424,25 @@ class BeckhoffMotorBase(CanReference, HasTimeout, BeckhoffCoderBase, Motor):
         self._writeDestination(value)
         if store2eeprom:
             # store to eeprom
-            # index goes to bits 8..15, also set bit1 (2) = eeprom and bit0 (1) = set_parameter
-            self._writeUpperControlWord((index<< 8) | 3)
+            # index goes to bits 8..15, also set bit1 (2) = eeprom and
+            # bit0 (1) = set_parameter
+            self._writeUpperControlWord((index << 8) | 3)
         else:
             # store to volatile memory
             # index goes to bits 8..15, also set bit0 (1) = set_parameter
-            self._writeUpperControlWord((index<< 8) | 1)
+            self._writeUpperControlWord((index << 8) | 1)
 
         self.log.debug('Wait for ACK/NACK bits')
         for _ in range(1000):
-            if self._readStatusWord() & (3<<14) != 0:
+            if self._readStatusWord() & (3 << 14) != 0:
                 break
         else:
             raise CommunicationError(self, 'WritePar command not recognized '
-                                            'by HW, please retry later....')
+                                     'by HW, please retry later....')
 
-        if self._readStatusWord() & (1<<14):
+        if self._readStatusWord() & (1 << 14):
             raise CommunicationError(self, 'Writing of Parameter %r failed, '
-                                            'got a NACK' % index)
+                                     'got a NACK' % index)
         return self._readReturn()
 
     def _HW_wait_while_BUSY(self):
@@ -449,15 +451,16 @@ class BeckhoffMotorBase(CanReference, HasTimeout, BeckhoffCoderBase, Motor):
         for _ in range(1000):
             session.delay(0.1)
             statval = self._readStatusWord()
-            # if motor moving==0 and target reached==1 -> Ok
-            if (statval & 0b10100000 == 0) and (statval & 0b01000000):
+            # if motor moving==0 and target ready==1 -> Ok
+            if (statval & 0b10100000 == 0) and \
+               (statval & 0b100000000) and \
+               (statval & 0b1):
                 return
             # limit switch triggered or stop issued
-            if statval & (7<<10):
+            if statval & (7 << 10):
                 session.delay(0.5)
                 return
-        raise TimeoutError("HW still BUSY after 100s")
-
+        raise TimeoutError('HW still BUSY after 100s')
 
     #
     # Nicos methods
@@ -492,7 +495,9 @@ class BeckhoffMotorBase(CanReference, HasTimeout, BeckhoffCoderBase, Motor):
 
 # Box Cab1: B1, NOK5a, NOK5b
 class BeckhoffMotorCab1(BeckhoffMotorBase):
-    HW_readable_Params = dict(refPos=2, vMax=3, motorTemp=41, encoderRawValue=60, maxValue=120, minValue=121, firmwareVersion=253)
+    HW_readable_Params = dict(refPos=2, vMax=3, motorTemp=41,
+                              encoderRawValue=60, maxValue=120,
+                              minValue=121, firmwareVersion=253)
     HW_writeable_Params = dict(vMax=3)
 
     @usermethod
@@ -505,7 +510,7 @@ class BeckhoffMotorCab1(BeckhoffMotorBase):
 
     @usermethod
     def HW_readMotorTemp(self):
-        return self._HW_readParameter('motorTemp') # in degC
+        return self._HW_readParameter('motorTemp')  # in degC
 
     @usermethod
     def HW_readEncoderRawValue(self):
@@ -542,8 +547,10 @@ class BeckhoffMotorCab1M13(BeckhoffMotorCab1):
         self._HW_writeParameter('vMax', self._phys2speed(value))
 
 
-# NOK5a: Motor M11 (reactorside, 0x3034) & M12 (sample side, 0x303e), should be moved together!
-# NOK5b: Motor M11 (reactorside, 0x3052) & M12 (sample side, 0x305c), should be moved together!
+# NOK5a: Motor M11 (reactorside, 0x3034) & M12 (sample side, 0x303e),
+# should be moved together!
+# NOK5b: Motor M11 (reactorside, 0x3052) & M12 (sample side, 0x305c),
+# should be moved together!
 class BeckhoffMotorCab1M1x(SingleMotorOfADoubleMotorNOK, BeckhoffMotorCab1M13):
     def _HW_setSync(self, syncvalue):
         # from docu:
@@ -559,7 +566,8 @@ class BeckhoffMotorCab1M1x(SingleMotorOfADoubleMotorNOK, BeckhoffMotorCab1M13):
         self._writeControlBit(8, syncvalue, numbits=2)
 
     # There must be a device combining both motors to a 2-tuple valued device.
-    # this super-device can trigger referencing or moves (it needs to call _HW_setSync(3) last)
+    # this super-device can trigger referencing or moves (it needs to call
+    # _HW_setSync(3) last)
     def doReference(self):
         raise UsageError('This device can not be referenced like this! '
                          'see docu!, try referencing one of %s' % self._sdevs)
@@ -582,10 +590,11 @@ class BeckhoffMotorCab1M12(BeckhoffMotorCab1M1x):
 # Box detectorantrieb: det_z: Achse 0x3020, Coder_readout 0x302a
 class BeckhoffMotorDetector(BeckhoffMotorBase):
     parameter_overrides = {
-        'slope' : Override(default=100),
+        'slope': Override(default=100),
     }
     HW_readable_Params = dict(refPos=2, vMax=3, motorTemp=41, maxValue=120,
-        minValue=121, disableCoder=135, dragError=136, firmwareVersion=253)
+                              minValue=121, disableCoder=135, dragError=136,
+                              firmwareVersion=253)
     HW_writeable_Params = dict(vMax=3, disableCoder=135, dragError=136)
 
     @usermethod
@@ -598,7 +607,7 @@ class BeckhoffMotorDetector(BeckhoffMotorBase):
 
     @usermethod
     def HW_readMotorTemp(self):
-        return self._HW_readParameter('motorTemp') # in degC
+        return self._HW_readParameter('motorTemp')  # in degC
 
     @usermethod
     def HW_readMaxValue(self):
@@ -628,7 +637,7 @@ class BeckhoffMotorDetector(BeckhoffMotorBase):
     @requires(level='admin')
     def HW_writeDisableCoder(self, value):
         if value:
-            self.log.warning("disabling Coder !!!")
+            self.log.warning('disabling Coder !!!')
             self._HW_writeParameter('disableCoder', 1)
         else:
             self._HW_writeParameter('disableCoder', 0)
@@ -642,14 +651,15 @@ class BeckhoffMotorDetector(BeckhoffMotorBase):
 class BeckhoffCoderDetector(BeckhoffCoderBase):
     """stripped down control block which only provides the current position"""
     parameter_overrides = {
-        'slope' : Override(default=100),
+        'slope': Override(default=100),
     }
     HW_Status_Inv = 0
+
 
 # Box horizontalblende: _2013-04-05\ Anhang\ A\ V0.6.pdf
 class BeckhoffMotorHSlit(BeckhoffMotorBase):
     parameter_overrides = {
-        'slope' : Override(default=1000),
+        'slope': Override(default=1000),
     }
     HW_readable_Params = dict(vMax=3, offset=50, firmwareVersion=253)
     HW_writeable_Params = dict(vMax=3, offset=50, firmwareReset=255)
