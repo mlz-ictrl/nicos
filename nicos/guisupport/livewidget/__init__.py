livewidget: Fix regression in un-/masking zeros in logscale plots.
In commit 04060cde85215c56c4bc21648034464507ef45bf the calculation of
the sums along x, y have changed. With each _rescale the curve is
recalculated. Thus we also need to apply un-/masking in that step.

Change-Id: I8c838c3a0f81dbce40a7f4a9fa2d43e64fc8e6ec
Reviewed-on: https://forge.frm2.tum.de/review/15126
Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

nicos/guisupport/livewidget/__init__.py
@@ -446,6 +446,29 @@ class IntegralLiveWidget(LiveWidget):
         gr.setcharheight(self._charheight)
         gr.text(x, y, svalue)
 
+    def _applymask(self):
+        """Mask or unmask values in plotcurves if necessary and rescale
+        plotaxes in respect to logscale."""
+        win = self.axes.getWindow()
+        if not win:
+            return
+        xmin, xmax, ymin, ymax = win
+        # set minimum for sum axis in respect to logscale
+        # and un-/mask values if necessary
+        if self._logscale:
+            x0, y0 = .1, .1
+            self.curvex.x = numpy.ma.masked_equal(self.curvex.x, 0)
+            self.curvey.y = numpy.ma.masked_equal(self.curvey.y, 0)
+        else:  # unmask if masked if not logscale
+            x0, y0 = 0, 0
+            if numpy.ma.is_masked(self.curvex._x):
+                self.curvex.x = self.curvex._x.filled(0)
+            if numpy.ma.is_masked(self.curvey._y):
+                self.curvey.y = self.curvey._y.filled(0)
+        # rescale axes
+        self.axesyint.setWindow(xmin - .5, xmax - .5, y0, self.curvey.y.max())
+        self.axesxint.setWindow(x0, self.curvex.x.max(), ymin - .5, ymax - .5)
+
     def _rescale(self):
         """Rescales integral plots in respect to the main/cellarray plot."""
         xmin, xmax, ymin, ymax = self.axes.getWindow()
@@ -463,12 +486,8 @@ class IntegralLiveWidget(LiveWidget):
                                                             dtype=numpy.float)
         self.curvey.y = self._array[int(y0):int(y1), :].sum(axis=0,
                                                             dtype=numpy.float)
-        # restore min for sum axis in respect to logscale
-        # @see updateZData method sets the minimum accordingly
-        x0 = self.axesxint.getWindow()[0]  # xmin sum along x
-        y0 = self.axesyint.getWindow()[2]  # ymin sum along y
-        self.axesyint.setWindow(xmin - .5, xmax - .5, y0, self.curvey.y.max())
-        self.axesxint.setWindow(x0, self.curvex.x.max(), ymin - .5, ymax - .5)
+        self._applymask()
+
 
     def _setData(self, array, nx, ny, nz, newrange):
         LiveWidget._setData(self, array, nx, ny, nz, newrange)
@@ -480,23 +499,6 @@ class IntegralLiveWidget(LiveWidget):
         self.curvey.x = numpy.arange(0, nx)
         self.curvex.y = numpy.arange(0, ny)
 
-    def updateZData(self):
-        LiveWidget.updateZData(self)
-        if self._logscale:
-            x0, y0 = .1, .1
-            self.curvex.x = numpy.ma.masked_equal(self.curvex.x, 0)
-            self.curvey.y = numpy.ma.masked_equal(self.curvey.y, 0)
-        else:  # unmask if masked if not logscale
-            x0, y0 = 0, 0
-            if numpy.ma.is_masked(self.curvex._x):
-                self.curvex.x = self.curvex._x.filled(0)
-            if numpy.ma.is_masked(self.curvey._y):
-                self.curvey.y = self.curvey._y.filled(0)
-        xmin, xmax, _ymin, ymax = self.axesyint.getWindow()
-        self.axesyint.setWindow(xmin, xmax, y0, ymax)
-        _xmin, xmax, ymin, ymax = self.axesxint.getWindow()
-        self.axesxint.setWindow(x0, xmax, ymin, ymax)
-
     def unzoom(self):
         nx, ny = len(self.curvey.x), len(self.curvex.y)
         self.axesyint.setWindow(0, nx, 1, self.curvey.y.max())
@@ -507,9 +509,11 @@ class IntegralLiveWidget(LiveWidget):
         self.gr.printDialog()
 
     def logscale(self, on):
+        LiveWidget.logscale(self, on)
         self.axesxint.setLogX(on)
         self.axesyint.setLogY(on)
-        LiveWidget.logscale(self, on)
+        self._applymask()
+        self.gr.update()
 
     def setCenterMark(self, flag):
         self.axesxint.drawxylines = flag