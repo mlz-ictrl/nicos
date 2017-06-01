7af635c835e24f92faf946eb017b20489ffdaf00
Christian Felder <c.felder@fz-juelich.de>
2017-06-01 16:53:04 +0200

    TREFF: ndp setup.
    Change-Id: I36224ac4f967beeef385db0aa7be203d88b20b53
    Reviewed-on: https://forge.frm2.tum.de/review/15164
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

---
+++ custom/treff/setups/ndp.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+description = "Neutron depth profiling detector setup"
+group = "basic"
+
+includes = ["filter", "counter",
+            "polarizer", "sampletable", "shutter", "slits",
+            "guidehall", "nl5", "reactor",
+           ]
+excludes = ["det"]
+
+sysconfig = dict(
+    datasinks = ["LiveViewSink", "NPGZFileSink", "YAMLSaver"],
+)
+
+tango_base = "tango://phys.treff.frm2:10000/ndp"
+
+basename = "%(proposal)s_%(session.experiment.sample.filename)s_"
+scanbasename = basename + "%(scancounter)08d_%(pointnumber)08d"
+countbasename = basename + "%(pointpropcounter)010d"
+
+devices = dict(
+    LiveViewSink  = device("devices.datasinks.LiveViewSink",
+                           description = "Sends image data to LiveViewWidget",
+                          ),
+    NPGZFileSink  = device("maria.npsaver.NPGZFileSink",
+                           description = "Saves image data in numpy text "
+                                         "format",
+                           filenametemplate = [scanbasename + ".gz",
+                                               countbasename + ".gz",
+                                              ],
+                          ),
+    YAMLSaver     = device("maria.yamlformat.YAMLFileSink",
+                           filenametemplate = [scanbasename + ".yaml",
+                                               countbasename + ".yaml",
+                                              ],
+                           lowlevel = True,
+                          ),
+    ndpimg        = device("devices.tango.ImageChannel",
+                           description = "NDP detector image",
+                           tangodevice = tango_base + "/fastcomtec/detector",
+                          ),
+    ndpdet        = device("devices.generic.Detector",
+                           description = "NDP detector",
+                           timers = ["timer"],
+                           monitors = ["mon0", "mon1"],
+                           images = ["ndpimg"],
+                           liveinterval = 0,
+                          ),
+)
+
+startupcode = "SetDetectors(ndpdet)"
