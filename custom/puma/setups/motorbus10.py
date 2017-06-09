8c8c140ff8ad3b834f8250a6521e6ca542bb38f5
Jens Krüger <jens.krueger@frm2.tum.de>
2017-06-09 22:57:59 +0200

    PUMA: Fix motorbus10 setup
    Change-Id: I614c14c3e58835aebcc037a044413c02c3aaef52
    Reviewed-on: https://forge.frm2.tum.de/review/15154
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/puma/setups/motorbus10.py
+++ custom/puma/setups/motorbus10.py
@@ -1,10 +1,11 @@
 description = 'Motor bus 10'
+
 group = 'lowlevel'
 
 devices = dict(
     motorbus10 = device('devices.vendor.ipc.IPCModBusTaco',
-                        tacodevice = 'puma/rs485/1',     # WARNING: There is no such tacodevice!
-                        bustimeout = 0.1,
-                        lowlevel = True,
-                       ),
+        tacodevice = 'puma/rs485/moxa2_5',
+        bustimeout = 0.1,
+        lowlevel = True,
+    ),
 )
