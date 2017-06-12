afe366b2b76f20f38aa2589fd8dddaa3f027ddcd
Christian Felder <c.felder@fz-juelich.de>
2017-06-12 17:15:33 +0200

    TREFF: Add iviumstat cell trigger device.
    Change-Id: I34a6113d511a2c80c7da4e8d5849a81e9e8e4b62
    Reviewed-on: https://forge.frm2.tum.de/review/15167
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

---
+++ custom/treff/setups/cell.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+description = "IviumStat Electrochemical Interface"
+group = "optional"
+
+tango_base = "tango://phys.treff.frm2:10000/treff/"
+
+devices = dict(
+    cell = device("jcns.iviumstat.StopNamedDigitalOutput",
+                  description = "Cell charger",
+                  tangodevice = tango_base + "ivium/trigger",
+                  mapping = {
+                      "off": 0,
+                      "charge": 1,
+                      "discharge": 2,
+                      "measure": 3,
+                  }),
+)
