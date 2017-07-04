01488b14951184cefb269b8cb30881c06ea4b930
Georg Brandl <g.brandl@fz-juelich.de>
2017-07-04 09:36:16 +0200

    ANTARES: integrate tilt motor for selector
    ... and add a sequencer that prevents from moving the tilt
    unless the selector is below 3000rpm.
    
    (This should be a hardware interlock, but it is better than
    the current situation, which has no satefy except fix().)
    
    Change-Id: I9c4d3d495a646139a65caa91611bbbc25cbeb3e8
    Reviewed-on: https://forge.frm2.tum.de/review/15377
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

---
+++ nicos_mlz/antares/devices/selector.py
@@ -0,0 +1,68 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2017 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Georg Brandl <g.brandl@fz-juelich.de>
+#
+# *****************************************************************************
+
+"""Selector tilt motor for ANTARES with interlock."""
+
+from nicos.core import Moveable, HasLimits, Attach, Param, MoveError
+from nicos.devices.generic.sequence import BaseSequencer, SeqDev, SeqMethod
+
+
+class SelectorTilt(HasLimits, BaseSequencer):
+    """Before the selector is allowed to tilt, the speed has to be reduced
+    to a certain value.
+    """
+
+    attached_devices = {
+        'selector': Attach('The selector speed', Moveable),
+        'motor': Attach('The tilt motor', Moveable),
+    }
+
+    parameters = {
+        'maxtiltspeed': Param('Maximum safe speed for tilting the selector',
+                              mandatory=True, type=int, unit='rpm'),
+    }
+
+    def _generateSequence(self, target, *args, **kwargs):
+        seq = []
+        if self._attached_selector.read(0) > self.maxtiltspeed:
+            seq.insert(0, SeqDev(self._attached_selector, self.maxtiltspeed))
+        seq.append(SeqMethod(self, '_check_speed'))
+        seq.append(SeqMethod(self._attached_motor, 'release'))
+        seq.append(SeqDev(self._attached_motor, target))
+        seq.append(SeqMethod(self._attached_motor, 'fix',
+                             'only move this using %s' % self))
+        return seq
+
+    def _check_speed(self):
+        if self._attached_selector.read(0) > self.maxtiltspeed + 50:
+            raise MoveError(self, 'selector not in safe speed range')
+
+    def doRead(self, maxage=0):
+        return self._attached_motor.read(maxage)
+
+    def doIsAllowed(self, target):
+        return self._attached_motor.isAllowed(target)
+
+    def _getWaiters(self):
+        return [self._attached_motor]
