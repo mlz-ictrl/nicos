73e485f183cc189428d8f1dd3defe862c702d629
Georg Brandl <g.brandl@fz-juelich.de>
2017-07-19 14:22:30 +0200

    J-NSE: add setup for power suplies
    Change-Id: Ibbf38a58892ca29faaf919a6092e5fc78e4f64d4
    Reviewed-on: https://forge.frm2.tum.de/review/15626
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ custom/j_nse/setups/power.py
@@ -0,0 +1,27 @@
+description = 'power supplies'
+
+group = 'optional'
+
+tango_base = 'tango://phys.j-nse.frm2:10000/j-nse/'
+
+devices = dict(
+    temp_rack1 = device('devices.tango.Sensor',
+                        description = 'PSU Temperature in Rack 1',
+                        tangodevice = tango_base + 'JCNSPowerSupply/pow2_temp',
+                        unit = 'degC',
+                       ),
+    temp_rack2 = device('devices.tango.Sensor',
+                        description = 'PSU Temperature in Rack 2',
+                        tangodevice = tango_base + 'JCNSPowerSupply/pow23_temp',
+                        unit = 'degC',
+                       ),
+)
+
+for i in range(1, 39):
+    name = 'pow%d' % i
+    devices[name] = \
+        device('devices.tango.PowerSupply',
+               description = 'Power Supply Port %d' % i,
+               tangodevice = tango_base + 'JCNSPowerSupply/' + name,
+               unit = 'A',
+              )
