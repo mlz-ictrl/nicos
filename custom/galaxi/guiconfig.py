0d28c2612b960a926049fc0164b5899b3d916c7d
Georg Brandl <g.brandl@fz-juelich.de>
2017-07-22 17:36:38 +0200

    GALAXI: update outdated guiconfig format
    Change-Id: I73546379db1c717e6e7a6e12f3c54ca27692e77f
    Reviewed-on: https://forge.frm2.tum.de/review/15637
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- custom/galaxi/guiconfig.py
+++ custom/galaxi/guiconfig.py
@@ -1,43 +1,42 @@
-# Default GALAXI GUI config
 """NICOS GUI configuration for GALAXI."""
 
-from nicos.clients.gui.config import hsplit, vsplit, window, panel, tool, tabbed
-
-config = ('Default', [
-        hsplit(
-            vsplit(
-                panel('generic.GenericPanel',
-                      uifile = 'custom/galaxi/lib/gui/jcnslogo.ui'),
-                panel('expinfo.ExpInfoPanel'),
-            ),
-            vsplit(
-                panel('status.ScriptStatusPanel'),
-                panel('console.ConsolePanel'),
-            ),
-            vsplit(
-                panel('devices.DevicesPanel'),
-            ),
-        ),
-        window('Setup', 'setup',
-            tabbed(('Experiment', panel('setup_panel.ExpPanel')),
-                   ('Setups',     panel('setup_panel.SetupsPanel')),
-            )),
-        window('Editor', 'editor',
-            vsplit(
-                panel('scriptbuilder.CommandsPanel'),
-                panel('editor.EditorPanel'))),
-        window('Scans', 'plotter',
-            panel('scans.ScansPanel')),
-        window('History', 'find',
-            panel('history.HistoryPanel')),
-        window('Logbook', 'table',
-            panel('elog.ELogPanel')),
-        window('Errors', 'errors',
-            panel('errors.ErrorPanel')),
-    ], [
-        tool('Calculator', 'calculator.CalculatorTool'),
-        tool('Report NICOS bug or request enhancement', 'bugreport.BugreportTool'),
-        tool('Emergency stop button', 'estop.EmergencyStopTool',
-             runatstartup=True),
-    ]
+main_window = hsplit(
+    vsplit(
+        panel('generic.GenericPanel',
+              uifile = 'custom/galaxi/lib/gui/jcnslogo.ui'),
+        panel('expinfo.ExpInfoPanel'),
+    ),
+    vsplit(
+        panel('status.ScriptStatusPanel'),
+        panel('console.ConsolePanel'),
+    ),
+    vsplit(
+        panel('devices.DevicesPanel'),
+    ),
 )
+
+windows = [
+    window('Setup', 'setup',
+           tabbed(('Experiment', panel('setup_panel.ExpPanel')),
+                  ('Setups',     panel('setup_panel.SetupsPanel'))),
+           ),
+    window('Editor', 'editor',
+           vsplit(
+               panel('scriptbuilder.CommandsPanel'),
+               panel('editor.EditorPanel'))),
+    window('Scans', 'plotter',
+           panel('scans.ScansPanel')),
+    window('History', 'find',
+           panel('history.HistoryPanel')),
+    window('Logbook', 'table',
+           panel('elog.ELogPanel')),
+    window('Errors', 'errors',
+           panel('errors.ErrorPanel')),
+]
+
+tools = [
+    tool('Calculator', 'calculator.CalculatorTool'),
+    tool('Report NICOS bug or request enhancement', 'bugreport.BugreportTool'),
+    tool('Emergency stop button', 'estop.EmergencyStopTool',
+         runatstartup=True),
+]
