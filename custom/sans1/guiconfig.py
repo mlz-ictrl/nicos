0cda30725c392561fc0a82912ba6810bd1339af8
Georg Brandl <g.brandl@fz-juelich.de>
2017-07-22 17:36:50 +0200

    SANS1: update outdated guiconfig format
    Change-Id: I7f86091bda07872ed7ddefe01987a781678a1d6e
    Reviewed-on: https://forge.frm2.tum.de/review/15638
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- custom/sans1/guiconfig.py
+++ custom/sans1/guiconfig.py
@@ -43,99 +43,99 @@
 # 'nicos.clients.gui.panels.watch.WatchPanel'
 
 
-
-# config = (ignored, [Mainwindow + N*window(...)], [tools])
-config = ('Default', [
-        docked(
-            vsplit(
-                panel('nicos.clients.gui.panels.status.ScriptStatusPanel'),
-#               panel('watch.WatchPanel'),
-                panel('nicos.clients.gui.panels.console.ConsolePanel',
-                      watermark='/sans1control/custom/sans1/watermark.png'),
-            ),
-            # ('Watch Expressions',panel('watch.WatchPanel')),
-            ('NICOS devices',
-             panel('nicos.clients.gui.panels.devices.DevicesPanel', icons=True,
-                   dockpos='right',)
-            ),
-            ('Experiment info',
-             panel('nicos.clients.gui.panels.expinfo.ExpInfoPanel')),
-        ),
-        window('Setup', 'setup',
-            tabbed(
-                ('Experiment',
-                 panel('nicos.clients.gui.panels.setup_panel.ExpPanel')),
-                ('Setups',
-                 panel('nicos.clients.gui.panels.setup_panel.SetupsPanel')),
-                ('Detectors/Environment',
-                 panel('nicos.clients.gui.panels.setup_panel.DetEnvPanel')),
-                ('Samples',
-                 panel('sans1.gui.samplechanger.SamplechangerSetupPanel',
-                       # image='custom/sans1/lib/gui/sampleChanger11.png',
-                       image='custom/sans1/lib/gui/sampleChanger22.png',
-                       # positions = 11, setups='!setup22',)),
-                       positions = 22, setups=['sc1', 'sc2', 'ccmsanssc'],),
-                ),
-            )
-        ),
-        window('Editor', 'editor',
-            vsplit(
-                panel('nicos.clients.gui.panels.scriptbuilder.CommandsPanel'),
-                panel('nicos.clients.gui.panels.editor.EditorPanel',
-                    tools = [
-                            tool('Scan',
-                                 'nicos.clients.gui.tools.scan.ScanTool')
-                            ]
-                    )
-                )
-        ),
-        window('Watches', 'leds/blue_on',
-               panel('nicos.clients.gui.panels.watch.WatchPanel')),
-        window('Scans', 'plotter',
-               panel('nicos.clients.gui.panels.scans.ScansPanel')),
-        window('History', 'find',
-               panel('nicos.clients.gui.panels.history.HistoryPanel')),
-        window('Logbook', 'table',
-               panel('nicos.clients.gui.panels.elog.ELogPanel')),
-        window('Log files', 'table',
-               panel('nicos.clients.gui.panels.logviewer.LogViewerPanel')),
-        window('Errors', 'errors',
-               panel('nicos.clients.gui.panels.errors.ErrorPanel')),
-        window('Camera', 'live',
-               panel('nicos.clients.gui.panels.liveqwt.LiveDataPanel',
-                     instrument='poli')),
-    ], [
-        tool('Downtime report',
-             'nicos.clients.gui.tools.downtime.DownTimeTool',
-             receiver='f.carsughi@fz-juelich.de',
-             mailserver='smtp.frm2.tum.de',
-             sender='sans1@frm2.tum.de',
-            ),
-        tool('Calculator',
-             'nicos.clients.gui.tools.calculator.CalculatorTool'),
-        tool('Neutron cross-sections',
-             'nicos.clients.gui.tools.website.WebsiteTool',
-             url='http://www.ncnr.nist.gov/resources/n-lengths/'),
-        tool('Neutron activation',
-             'nicos.clients.gui.tools.website.WebsiteTool',
-             url='https://webapps.frm2.tum.de/intranet/activation/'),
-        tool('Neutron calculations',
-             'nicos.clients.gui.tools.website.WebsiteTool',
-             url='https://webapps.frm2.tum.de/intranet/neutroncalc/'),
-        tool('Report NICOS bug or request enhancement',
-             'nicos.clients.gui.tools.bugreport.BugreportTool'),
-        tool('Emergency stop button',
-             'nicos.clients.gui.tools.estop.EmergencyStopTool',
-             runatstartup=False),
-        tool('Maintenance commands',
-             'nicos.clients.gui.tools.commands.CommandsTool',
-             commands=[
-                 ('TACO server control panel (beta)',
-                  'SSH_ASKPASS=/usr/bin/ssh-askpass setsid /usr/bin/ssh -XY '
-                  'maint@sans1hw.sans1.frm2 "source /etc/tacoenv.sh && '
-                  'sudo /usr/bin/python /opt/tacocp/tacocp.py '
-                  'sans1srv.sans1.frm2" && exit',
-                 ),
-             ]),
-    ]
+main_window = docked(
+    vsplit(
+        panel('nicos.clients.gui.panels.status.ScriptStatusPanel'),
+        # panel('watch.WatchPanel'),
+        panel('nicos.clients.gui.panels.console.ConsolePanel',
+              watermark='/sans1control/custom/sans1/watermark.png'),
+    ),
+    # ('Watch Expressions',panel('watch.WatchPanel')),
+    ('NICOS devices',
+     panel('nicos.clients.gui.panels.devices.DevicesPanel', icons=True,
+           dockpos='right',)
+    ),
+    ('Experiment info',
+     panel('nicos.clients.gui.panels.expinfo.ExpInfoPanel')),
 )
+
+windows = [
+    window('Setup', 'setup',
+           tabbed(
+               ('Experiment',
+                panel('nicos.clients.gui.panels.setup_panel.ExpPanel')),
+               ('Setups',
+                panel('nicos.clients.gui.panels.setup_panel.SetupsPanel')),
+               ('Detectors/Environment',
+                panel('nicos.clients.gui.panels.setup_panel.DetEnvPanel')),
+               ('Samples',
+                panel('sans1.gui.samplechanger.SamplechangerSetupPanel',
+                      # image='custom/sans1/lib/gui/sampleChanger11.png',
+                      image='custom/sans1/lib/gui/sampleChanger22.png',
+                      # positions = 11, setups='!setup22',)),
+                      positions = 22, setups=['sc1', 'sc2', 'ccmsanssc'],),
+               ),
+           )
+    ),
+    window('Editor', 'editor',
+           vsplit(
+               panel('nicos.clients.gui.panels.scriptbuilder.CommandsPanel'),
+               panel('nicos.clients.gui.panels.editor.EditorPanel',
+                     tools = [
+                         tool('Scan',
+                              'nicos.clients.gui.tools.scan.ScanTool')
+                     ]
+               )
+           )
+    ),
+    window('Watches', 'leds/blue_on',
+           panel('nicos.clients.gui.panels.watch.WatchPanel')),
+    window('Scans', 'plotter',
+           panel('nicos.clients.gui.panels.scans.ScansPanel')),
+    window('History', 'find',
+           panel('nicos.clients.gui.panels.history.HistoryPanel')),
+    window('Logbook', 'table',
+           panel('nicos.clients.gui.panels.elog.ELogPanel')),
+    window('Log files', 'table',
+           panel('nicos.clients.gui.panels.logviewer.LogViewerPanel')),
+    window('Errors', 'errors',
+           panel('nicos.clients.gui.panels.errors.ErrorPanel')),
+    window('Camera', 'live',
+           panel('nicos.clients.gui.panels.liveqwt.LiveDataPanel',
+                 instrument='poli')),
+]
+
+tools = [
+    tool('Downtime report',
+         'nicos.clients.gui.tools.downtime.DownTimeTool',
+         receiver='f.carsughi@fz-juelich.de',
+         mailserver='smtp.frm2.tum.de',
+         sender='sans1@frm2.tum.de',
+    ),
+    tool('Calculator',
+         'nicos.clients.gui.tools.calculator.CalculatorTool'),
+    tool('Neutron cross-sections',
+         'nicos.clients.gui.tools.website.WebsiteTool',
+         url='http://www.ncnr.nist.gov/resources/n-lengths/'),
+    tool('Neutron activation',
+         'nicos.clients.gui.tools.website.WebsiteTool',
+         url='https://webapps.frm2.tum.de/intranet/activation/'),
+    tool('Neutron calculations',
+         'nicos.clients.gui.tools.website.WebsiteTool',
+         url='https://webapps.frm2.tum.de/intranet/neutroncalc/'),
+    tool('Report NICOS bug or request enhancement',
+         'nicos.clients.gui.tools.bugreport.BugreportTool'),
+    tool('Emergency stop button',
+         'nicos.clients.gui.tools.estop.EmergencyStopTool',
+         runatstartup=False),
+    tool('Maintenance commands',
+         'nicos.clients.gui.tools.commands.CommandsTool',
+         commands=[
+             ('TACO server control panel (beta)',
+              'SSH_ASKPASS=/usr/bin/ssh-askpass setsid /usr/bin/ssh -XY '
+              'maint@sans1hw.sans1.frm2 "source /etc/tacoenv.sh && '
+              'sudo /usr/bin/python /opt/tacocp/tacocp.py '
+              'sans1srv.sans1.frm2" && exit',
+             ),
+         ]),
+]
