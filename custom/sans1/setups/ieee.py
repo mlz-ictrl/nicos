c951fb1e68668da41b15d8ab6609777dcd7d35b1
Jens Krüger <jens.krueger@frm2.tum.de>
2017-07-26 18:50:10 +0200

    SANS1: Fix wrong device path
    Change-Id: Ibab3c3bab9c449922ea1648c20bbaf3f6a32fa87
    Reviewed-on: https://forge.frm2.tum.de/review/15738
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- custom/sans1/setups/ieee.py
+++ custom/sans1/setups/ieee.py
@@ -5,5 +5,5 @@ group = 'optional'
 devices = dict()
 
 for i in range(1, 11):
-    devices['ieee_%d' % i] = device('sans1.bersans.IEEEDevice',
+    devices['ieee_%d' % i] = device('nicos_mlz.sans1.devices.bersans.IEEEDevice',
                                     description = 'IEEE device No. %d' % i)
