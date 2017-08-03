65e70ecc5330a65ba7f02e6fe0b638bea0130e25
Reseda People <reseda@frm2.tum.de>
2017-08-03 15:18:38 +0200

    reseda: configure ldap auth.
    Change-Id: Id3092f8052a551e60b3ecf6e68a00463cff7418c
    Reviewed-on: https://forge.frm2.tum.de/review/15833
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

--- nicos_mlz/reseda/setups/special/daemon.py
+++ nicos_mlz/reseda/setups/special/daemon.py
@@ -2,13 +2,20 @@ description = 'setup for the execution daemon'
 group = 'special'
 
 devices = dict(
+    LDAPAuth = device('nicos.services.daemon.auth.ldap.Authenticator',
+        uri = 'ldap://phaidra.admin.frm2',
+        userbasedn = 'ou=People,dc=frm2,dc=de',
+        groupbasedn = 'ou=Group,dc=frm2,dc=de',
+        grouproles = {
+            'reseda': 'admin',
+            'ictrl': 'admin',
+        }
+    ),
     UserDBAuth = device('nicos_mlz.frm2.devices.proposaldb.Authenticator',
         description = 'FRM II user office authentication',
     ),
     Auth = device('nicos.services.daemon.auth.list.Authenticator',
         hashing = 'md5',
-        # first entry is the user name,
-        # second the hashed password, third the user level
         passwd = [
             ('guest', '', 'guest'),
             ('user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user'),
@@ -18,9 +25,9 @@ devices = dict(
     Daemon = device('nicos.services.daemon.NicosDaemon',
         server = '0.0.0.0',
         authenticators = [
+            'LDAPAuth',
             'UserDBAuth',
             'Auth',
         ],
-        loglevel = 'debug',
     ),
 )
