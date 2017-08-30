3b91ba990e23e972707f6d2552b4b0fd4eeff11b
Jens Krüger <jens.krueger@frm2.tum.de>
2017-08-30 11:34:48 +0200

    Fix module author entry
    Change-Id: I6206548b5759c729decdbde6e949c5afb02e54d1
    Reviewed-on: https://forge.frm2.tum.de/review/16068
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- test/test_basic/test_sxtalpositions.py
+++ test/test_basic/test_sxtalpositions.py
@@ -18,7 +18,7 @@
 # 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 # Module authors:
-#   pedersen
+#   Björn Pedersen <bjoern.pedersen@frm2.tum.de>
 #
 # *****************************************************************************
 """
