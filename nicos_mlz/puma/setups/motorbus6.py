2db3677c20ea58426da24fb3510447373723c2f5
Jens Krüger <jens.krueger@frm2.tum.de>
2017-09-15 14:37:59 +0200

    PUMA: Change the motorbus6 and motorbus12 devices
    Change-Id: I9af0bdeffc0110eb4c8b791ef5fe1afed0de1ce8
    Reviewed-on: https://forge.frm2.tum.de/review/16243
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/puma/setups/motorbus6.py
+++ nicos_mlz/puma/setups/motorbus6.py
@@ -6,7 +6,8 @@ nethost = 'pumasrv.puma.frm2'
 
 devices = dict(
     motorbus6 = device('nicos.devices.vendor.ipc.IPCModBusTaco',
-        tacodevice = '//%s/puma/rs485/moxa3_1' % nethost,
+        # tacodevice = '//%s/puma/rs485/moxa3_1' % nethost,
+        tacodevice = '//%s/puma/rs485/motorbus6' % nethost,
         bustimeout = 0.1,
         lowlevel = True,
     ),
