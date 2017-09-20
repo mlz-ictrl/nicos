9f16a7b2d2476c62e628c5f83a66d13b6b000841
Nikhil Biyani <nikhil.biyani@psi.ch>
2017-09-20 15:08:05 +0200

    Cache: Added cache entry serializers
    Added a base class for serializing cache entries and a JSON
    serializer implementing this ia also added
    
    Change-Id: I7fbc05a0c6c68c05975ce2082c95f24b12a19793
    Reviewed-on: https://forge.frm2.tum.de/review/16294
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Nikhil Biyani <nikhil.biyani@psi.ch>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos/services/cache/database/entry.py
+++ nicos/services/cache/database/entry.py
@@ -22,6 +22,11 @@
 #
 # *****************************************************************************
 
+from json import JSONDecoder, JSONEncoder
+
+from nicos.core import Device
+from nicos.core.errors import ProgrammingError
+
 
 class CacheEntry(object):
     __slots__ = ('time', 'ttl', 'value', 'expired')
@@ -36,3 +41,54 @@ class CacheEntry(object):
         if self.expired:
             return '(%s+%s@%s)' % (self.time, self.ttl, self.value)
         return '%s+%s@%s' % (self.time, self.ttl, self.value)
+
+    def asDict(self):
+        return {x: getattr(self, x) for x in self.__slots__}
+
+
+class CacheEntrySerializer(Device):
+    """Base class to serialize and de-serialize the entries for cache
+
+    Following methods must be implemented in the derived classes:
+    * encode(entry, key)
+    where `entry` represents the instance to be serialized and the `key`
+    is the NICOS key for which this entry is written. Writing `key` is
+    optional but a desired feature.
+
+    * decode(entry) : returns tuple of (key, entry)
+    The decode method should return a key and entry tuple decoded from
+    the message.
+    """
+
+    def encode(self, entry, key=''):
+        raise ProgrammingError('Encoder not implemented')
+
+    def decode(self, entry):
+        raise ProgrammingError('Decoder not implemented')
+
+
+class JsonCacheEntrySerializer(CacheEntrySerializer):
+    """Encodes the entry object to JSON format
+
+    For encoding, first converts the entry object to a python dict and then
+    uses the inbuilt JSON encoder to serialize to JSON object.
+    For decoding, converts the JSON object to python dict and then to the
+    entry class
+    """
+
+    def doInit(self, mode):
+        self._encoder = JSONEncoder()
+        self._decoder = JSONDecoder()
+
+    def encode(self, entry, key=''):
+        entry_dict = entry.asDict()
+        if key:
+            entry_dict['key'] = key
+        return self._encoder.encode(entry_dict)
+
+    def decode(self, coded):
+        edict = self._decoder.decode(coded)
+        entry = CacheEntry(edict['time'], edict['ttl'], edict['value'])
+        entry.expired = edict['expired']
+        key = edict['key'] if 'key' in edict else None
+        return key, entry
