9c5e0910c0b8b850bc0e963baec29babdef439fb
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2017-10-02 11:11:29 +0200

    Add utils for finding (guiconf) files
    Later we can move some of the setup file finding code here as
    well.
    
    Change-Id: If10019616908143c0d8d43e97bd40f0d42f20c5a
    Reviewed-on: https://forge.frm2.tum.de/review/16366
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ nicos/utils/files.py
@@ -0,0 +1,58 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the FRM-II
+# Copyright (c) 2017 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Björn Pedersen <bjoern.pedersen@frm2.tum.de>
+#
+# *****************************************************************************
+"""
+Utility routines for nicos file finding
+
+
+This modules contains helper functions to find e.g. setupfiles etc.
+"""
+
+import os
+from os import path
+
+
+def findNicosFacilityDirs(where):
+    for facility in sorted(os.listdir(where)):
+        full = path.join(where, facility)
+        if facility.startswith('nicos_') and path.isdir(full):
+            yield facility, full
+
+
+def findNicosInstrumentSubDirs(parent, facility=''):
+    for instr in sorted(os.listdir(parent)):
+        fullinstr = path.join(parent, instr)
+        if path.isfile(path.join(fullinstr, 'nicos.conf')):
+            yield instr, facility, fullinstr
+
+
+def findAllNicosInstrumentDirs(root):
+    for facility, full in findNicosFacilityDirs(root):
+        for res in findNicosInstrumentSubDirs(full, facility):
+            yield res
+
+
+def findInstrGuiConfigs(where):
+    for guiconf in sorted(os.listdir(where)):
+        if 'guiconf' in guiconf and guiconf.endswith('.py'):
+            yield guiconf, path.join(where, guiconf)
