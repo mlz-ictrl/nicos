99853d9eae3fa36189881f59d32841f3dfc46f31
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2017-10-06 13:42:54 +0200

    Fix shutter device
    Change-Id: Ia02d4d5b91d62a16a788dffe6bc4238aac87d3c5
    Reviewed-on: https://forge.frm2.tum.de/review/16415
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>

--- nicos_mlz/laue/setups/shutter.py
+++ nicos_mlz/laue/setups/shutter.py
@@ -4,10 +4,10 @@ group = 'optional'
 devices = dict(
     primarydown = device('nicos.devices.taco.DigitalInput',
         description = 'RESI primary shutter state:closed',
-        tacodevice = '//resi1.resi.frm2/resi/cpci15x/shutt_pd'
+        tacodevice = '//resictrl.resi.frm2/resi/cpci15x/shutt_pd'
     ),
     primaryup = device('nicos.devices.taco.DigitalInput',
         description = 'RESI primary shutter state:open',
-        tacodevice = '//resi1.resi.frm2/resi/cpci15x/shutt_pu'
+        tacodevice = '//resictrl.resi.frm2/resi/cpci15x/shutt_pu'
     )
 )
