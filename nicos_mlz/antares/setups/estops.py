96ec06da066850b5c70b9c8bd6376d66ecdecba3
Antares People <antares@frm2.tum.de>
2017-10-10 14:43:11 +0200

    Add setup for emergency stop inputs from SPS.
    Change-Id: I86a4687a5e19035daac5076f7ad363531c0a621f
    Reviewed-on: https://forge.frm2.tum.de/review/17096
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

---
+++ nicos_mlz/antares/setups/estops.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+description = 'ANTARES cabinet emergency stops'
+
+group = 'optional'
+
+tango_base = 'tango://antareshw.antares.frm2:10000/antares/'
+
+devices = dict(
+    EstopMain = device('nicos.devices.tango.NamedDigitalInput',
+        description = 'Emergency stop of main cabinet',
+        tangodevice = tango_base + 'fzjdp_digital/EstopMain',
+        mapping = {'on': 1,
+                   'off': 0},
+        warnlimits = ('off', 'off'),
+    ),
+    EstopHuber = device('nicos.devices.tango.NamedDigitalInput',
+        description = 'Emergency stop of Huber cabinet',
+        tangodevice = tango_base + 'fzjdp_digital/EstopHuber',
+        mapping = {'on': 1,
+                   'off': 0},
+        warnlimits = ('off', 'off'),
+    ),
+    EstopSlitSmall = device('nicos.devices.tango.NamedDigitalInput',
+        description = 'Emergency stop of small slit cabinet',
+        tangodevice = tango_base + 'fzjdp_digital/EstopSlitSmall',
+        mapping = {'on': 1,
+                   'off': 0},
+        warnlimits = ('off', 'off'),
+    ),
+    EstopSlitLarge = device('nicos.devices.tango.NamedDigitalInput',
+        description = 'Emergency stop of large slit cabinet',
+        tangodevice = tango_base + 'fzjdp_digital/EstopSlitLarge',
+        mapping = {'on': 1,
+                   'off': 0},
+        warnlimits = ('off', 'off'),
+    ),
+    EstopDetector = device('nicos.devices.tango.NamedDigitalInput',
+        description = 'Emergency stop of detector cabinet',
+        tangodevice = tango_base + 'fzjdp_digital/EstopDetector',
+        mapping = {'on': 1,
+                   'off': 0},
+        warnlimits = ('off', 'off'),
+    ),
+)
