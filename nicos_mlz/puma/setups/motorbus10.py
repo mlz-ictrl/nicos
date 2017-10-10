85b600ea87deed29370cebdc3e1a5c0c31766716
Jens Krüger <jens.krueger@frm2.tum.de>
2017-10-10 10:41:23 +0200

    PUMA: Switch motorbus10 to MOXA box interface
    Change-Id: I3c9a585a5395b607fef282a25c4de2d0cfcb2715
    Reviewed-on: https://forge.frm2.tum.de/review/16446
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/puma/setups/motorbus10.py
+++ nicos_mlz/puma/setups/motorbus10.py
@@ -6,7 +6,8 @@ nethost = 'pumasrv.puma.frm2'
 
 devices = dict(
     motorbus10 = device('nicos.devices.vendor.ipc.IPCModBusTaco',
-        tacodevice = '//%s/puma/rs485/moxa2_5' % nethost,
+        # tacodevice = '//%s/puma/rs485/moxa2_5' % nethost,
+        tacodevice = '//%s/puma/rs485/motorbus10' % nethost,
         bustimeout = 0.1,
         lowlevel = True,
     ),
