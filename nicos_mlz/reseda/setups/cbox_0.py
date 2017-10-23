eef61467678a44a96cbc8a2dcf89b1024adef05d
Reseda People <reseda@frm2.tum.de>
2017-10-23 10:32:23 +0200

    reseda: rename cboxes.
    Change-Id: I0bd896baa316f6e87550d285d3523499be498d21
    Reviewed-on: https://forge.frm2.tum.de/review/16556
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Alexander Lenz <alexander.lenz@frm2.tum.de>

---
+++ nicos_mlz/reseda/setups/cbox_0.py
@@ -0,0 +1,125 @@
+#  -*- coding: utf-8 -*-
+
+description = 'Capacity box 0'
+group = 'optional'
+
+tango_base = 'tango://resedahw2.reseda.frm2:10000/reseda'
+
+devices = dict(
+    osci_pk2pk_1 = device('nicos.devices.tango.AnalogInput',
+        description = 'Osci at coil, peak to peak measurement',
+        tangodevice = '%s/cbox_0/osci_pk2pk_1' % tango_base,
+        tangotimeout = 5,
+        pollinterval = 5,
+        maxage = 12,
+    ),
+    osci_pk2pk_2 = device('nicos.devices.tango.AnalogInput',
+        description = 'Osci at coil, peak to peak measurement',
+        tangodevice = '%s/cbox_0/osci_pk2pk_1' % tango_base,
+        tangotimeout = 5,
+        pollinterval = 5,
+        maxage = 12,
+    ),
+    cbox_0_freq = device('nicos.devices.tango.AnalogOutput',
+        description = 'Frequency generator amplitude',
+        tangodevice = '%s/cbox_0/fg_freq' % tango_base,
+        pollinterval = 3,
+        fmtstr = '%.3g'
+    ),
+    cbox_0_amp = device('nicos.devices.tango.AnalogOutput',
+        description = 'Frequency generator amplitude',
+        tangodevice = '%s/cbox_0/fg_amplitude' % tango_base,
+        pollinterval = 3,
+    ),
+    cbox_0_fwdp = device('nicos.devices.tango.AnalogInput',
+        description = 'Power amplifier forward power',
+        tangodevice = '%s/cbox_0/pa_fwdp' % tango_base,
+        pollinterval = 3,
+    ),
+    cbox_0_revp = device('nicos.devices.tango.AnalogInput',
+        description = 'Power amplifier reverse power',
+        tangodevice = '%s/cbox_0/pa_revp' % tango_base,
+        pollinterval = 3,
+    ),
+    cbox_0_gain = device('nicos.devices.tango.AnalogOutput',
+        description = 'Power amplifier gain',
+        tangodevice = '%s/cbox_0/pa_gain' % tango_base,
+        pollinterval = 3,
+    ),
+    cbox_0 = device('nicos_mlz.reseda.devices.cbox.CBoxResonanceFrequency',
+        pollinterval = 3,
+        description = 'CBox',
+        unit = 'Hz',
+        power_divider = device('nicos.devices.tango.DigitalOutput',
+            description = 'Power divider to split the power for both coils',
+            tangodevice = '%s/cbox_0/plc_power_divider' % tango_base,
+            lowlevel = False,  # temporary due to inaccurate auto tune
+        ),
+        highpass = device('nicos.devices.tango.DigitalOutput',
+            description = 'Highpass filter to smooth the signal',
+            tangodevice = '%s/cbox_0/plc_highpass' % tango_base,
+            lowlevel = False,  # temporary due to inaccurate auto tune
+        ),
+        pa_fwdp = 'cbox_0_fwdp',
+        pa_revp = 'cbox_0_revp',
+        fg = 'cbox_0_freq',
+        diplexer = device('nicos.devices.tango.DigitalOutput',
+            description =
+            'Lowpass filter to smooth the signal (enable for low frequency, disable for high frequency)',
+            tangodevice = '%s/cbox_0/plc_diplexer' % tango_base,
+            lowlevel = False,  # temporary due to inaccurate auto tune
+        ),
+        coil1_c1 = device('nicos.devices.tango.DigitalOutput',
+            description = 'Coil 1: Capacitor bank 1',
+            tangodevice = '%s/cbox_0/plc_a_c1' % tango_base,
+            lowlevel = False,  # temporary due to inaccurate auto tune
+        ),
+        coil1_c2 = device('nicos.devices.tango.DigitalOutput',
+            description = 'Coil 1: Capacitor bank 2',
+            tangodevice = '%s/cbox_0/plc_a_c2' % tango_base,
+            lowlevel = False,  # temporary due to inaccurate auto tune
+        ),
+        coil1_c3 = device('nicos.devices.tango.DigitalOutput',
+            description = 'Coil 1: Capacitor bank 3',
+            tangodevice = '%s/cbox_0/plc_a_c3' % tango_base,
+            lowlevel = False,  # temporary due to inaccurate auto tune
+        ),
+        coil1_c1c2serial = device('nicos.devices.tango.DigitalOutput',
+            description = 'Coil 1: Use c1 and c2 in serial instead of parallel',
+            tangodevice = '%s/cbox_0/plc_a_c1c2serial' % tango_base,
+            lowlevel = False,  # temporary due to inaccurate auto tune
+        ),
+        coil1_transformer = device('nicos.devices.tango.DigitalOutput',
+            description =
+            'Coil 1: Used to manipulate the coil resistance to match the power amplifier resistance',
+            tangodevice = '%s/cbox_0/plc_a_transformer' % tango_base,
+            lowlevel = False,  # temporary due to inaccurate auto tune
+        ),
+        coil2_c1 = device('nicos.devices.tango.DigitalOutput',
+            description = 'Coil 2: Capacitor bank 1',
+            tangodevice = '%s/cbox_0/plc_b_c1' % tango_base,
+            lowlevel = False,  # temporary due to inaccurate auto tune
+        ),
+        coil2_c2 = device('nicos.devices.tango.DigitalOutput',
+            description = 'Coil 2: Capacitor bank 2',
+            tangodevice = '%s/cbox_0/plc_b_c2' % tango_base,
+            lowlevel = False,  # temporary due to inaccurate auto tune
+        ),
+        coil2_c3 = device('nicos.devices.tango.DigitalOutput',
+            description = 'Coil 2: Capacitor bank 3',
+            tangodevice = '%s/cbox_0/plc_b_c3' % tango_base,
+            lowlevel = False,  # temporary due to inaccurate auto tune
+        ),
+        coil2_c1c2serial = device('nicos.devices.tango.DigitalOutput',
+            description = 'Coil 2: Use c1 and c2 in serial instead of parallel',
+            tangodevice = '%s/cbox_0/plc_b_c1c2serial' % tango_base,
+            lowlevel = False,  # temporary due to inaccurate auto tune
+        ),
+        coil2_transformer = device('nicos.devices.tango.DigitalOutput',
+            description =
+            'Coil 2: Used to manipulate the coil resistance to match the power amplifier resistance',
+            tangodevice = '%s/cbox_0/plc_b_transformer' % tango_base,
+            lowlevel = False,  # temporary due to inaccurate auto tune
+        ),
+    ),
+)
--- nicos_mlz/reseda/setups/tuning.py
+++ nicos_mlz/reseda/setups/tuning.py
@@ -1,19 +1,27 @@
 group = 'optional'
 description = 'Reseda tunewave table support'
 
-includes = ['selector', 'coils']
+includes = ['selector', 'coils', 'cboxes']
 
 packs = ['0a', '0b', '1']
+cbox_components = [
+    'freq', 'amp', 'coil1_c1', 'coil1_c2', 'coil1_c1c2serial',
+    'coil1_transformer', 'coil2_c1', 'coil2_c2', 'coil2_c1c2serial',
+    'coil2_transformer', 'diplexer', 'power_divider'
+]
 
 devices = dict(
     echotime = device('nicos_mlz.reseda.devices.tuning.EchoTime',
         description = 'Echo time and tunewave table device',
         wavelength = 'selector_lambda',
         dependencies = ['gf%i' % i for i in range(5)]
-            + ['hsf_%s' % entry for entry in packs]
-            + ['sf_%s' % entry for entry in packs]
-            + ['hrf0', 'hrf1']
-            + ['nse0', 'nse1'],
+        + ['hsf_%s' % entry for entry in packs]
+        + ['sf_%s' % entry for entry in packs]
+        + ['hrf0', 'hrf1']
+        + ['nse0', 'nse1']
+        + ['cbox_%s_%s' % (pack, component)
+            for pack in packs
+            for component in cbox_components],
         unit = 'ns',
         fmtstr = '%g'
     ),
