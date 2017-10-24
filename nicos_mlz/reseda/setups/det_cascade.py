72b48184b13ee981fdabbc64390e72c3e72f1bec
Reseda People <reseda@frm2.tum.de>
2017-10-24 13:47:01 +0200

    reseda: scanning detectors.
    Change-Id: I55ce38a0f50552db19e828a7b76f9cd439781ec0
    Reviewed-on: https://forge.frm2.tum.de/review/15462
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Tested-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

--- nicos_mlz/reseda/setups/det_cascade.py
+++ nicos_mlz/reseda/setups/det_cascade.py
@@ -1,12 +1,20 @@
 description = 'CASCADE detector (Windows server version)'
 group = 'lowlevel'
 includes = ['det_base']
+excludes = ['det_3he']
 
 sysconfig = dict(
-    datasinks = ['psd_padformat', 'psd_liveview'],
+    datasinks = ['psd_padformat', 'psd_liveview', 'HDF5FileSaver'],
 )
 
 devices = dict(
+    scandet = device('nicos.devices.generic.ScanningDetector',
+        description = 'Scanning detector for scans per echotime',
+        scandev = 'subcoil_ps2',
+        detector = 'psd',
+        maxage = 2,
+        pollinterval = 0.5,
+    ),
     psd_padformat = device('nicos_mlz.mira.devices.cascade.CascadePadSink',
         subdir = 'cascade',
         lowlevel = True,
@@ -25,3 +33,7 @@ devices = dict(
         images = ['psd_channel'],
     ),
 )
+
+startupcode = '''
+SetDetectors(scandet)
+'''
