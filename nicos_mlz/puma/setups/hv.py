479d04e2f7b237c0ae6ac0849bd2f42752c49630
Jens Krüger <jens.krueger@frm2.tum.de>
2017-11-09 15:30:10 +0100

    PUMA: add HV power supply setups
    Change-Id: Ib2f48876dd21a8e64f0e6dd8af9de1427bb40c91
    Reviewed-on: https://forge.frm2.tum.de/review/16649
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/puma/setups/hv.py
@@ -0,0 +1,19 @@
+description = 'High voltage power supplies'
+
+group = 'lowlevel'
+
+tango_base = 'tango://puma5.puma.frm2:10000/puma/'
+
+devices = dict(
+    hvmonitor = device('nicos.devices.tango.PowerSupply',
+        description = 'HV for the monitor',
+        tangodevice = tango_base + 'monitor/hv',
+        fmtstr = '%.0f',
+    ),
+
+    hvdetector = device('nicos.devices.tango.PowerSupply',
+        description = 'HV for the detectors',
+        tangodevice = tango_base + 'detector/hv',
+        fmtstr = '%.0f',
+    )
+)
