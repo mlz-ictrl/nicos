e8d7dfded6f83b84b266a7477e5624ac2c534da3
Nikhil Biyani <nikhil.biyani@psi.ch>
2017-11-27 11:37:15 +0100

    SINQ: Improvements to AMOR GUI
    Change-Id: I31fce7ab1929f3f88cd32b4a4c102a4c73b438cc
    Reviewed-on: https://forge.frm2.tum.de/review/16767
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Nikhil Biyani <nikhil.biyani@psi.ch>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos_sinq/amor/guiconfig.py
+++ nicos_sinq/amor/guiconfig.py
@@ -24,20 +24,43 @@
 
 """NICOS GUI default configuration."""
 
-main_window = docked(
-    vsplit(
-        panel('nicos.clients.gui.panels.status.ScriptStatusPanel'),
-        # panel('nicos.clients.gui.panels.watch.WatchPanel'),
-        panel('nicos.clients.gui.panels.console.ConsolePanel'),
-    ),
-    ('NICOS devices',
-     panel('nicos.clients.gui.panels.devices.DevicesPanel',
-           icons=True, dockpos='right',
-          )
-    ),
-    ('Experiment Information and Setup',
-     panel('nicos.clients.gui.panels.expinfo.ExpInfoPanel',
-       )),
+main_window = tabbed(
+    ('Instrument', docked(
+        vsplit(
+            hsplit(
+                vsplit(
+                    panel(
+                        'nicos.clients.gui.panels.commandline.CommandLinePanel'),
+                    panel('nicos.clients.gui.panels.status.ScriptStatusPanel'),
+                ),
+            ),
+            tabbed(
+                ('All output',
+                 panel('nicos.clients.gui.panels.console.ConsolePanel',
+                       hasinput=False, hasmenu=False,
+                       watermark='nicos_sinq/amor/watermark.png')),
+                ('Errors/Warnings',
+                 panel('nicos.clients.gui.panels.errors.ErrorPanel')),
+            ),
+        ),
+        ('Experiment Setup',
+         panel('nicos.clients.gui.panels.expinfo.ExpInfoPanel',
+               dockpos='left')),
+        ('Control devices',
+         panel('nicos.clients.gui.panels.devices.DevicesPanel', icons=True,
+               dockpos='right')),
+    )),
+    ('Script Builder',
+     vsplit(
+         panel('nicos.clients.gui.panels.scriptbuilder.CommandsPanel'),
+         panel('nicos.clients.gui.panels.editor.EditorPanel',
+               tools=[
+                   tool('Scan Generator',
+                        'nicos.clients.gui.tools.scan.ScanTool')
+               ]),
+     )),
+    ('Device History', panel('nicos.clients.gui.panels.history.HistoryPanel')),
+    ('Log files', panel('nicos.clients.gui.panels.logviewer.LogViewerPanel')),
 )
 
 windows = [
@@ -45,17 +68,14 @@ windows = [
            panel('nicos.clients.gui.panels.editor.EditorPanel')),
     window('Scans', 'plotter',
            panel('nicos.clients.gui.panels.scans.ScansPanel')),
-    window('History', 'find',
-           panel('nicos.clients.gui.panels.history.HistoryPanel')),
     window('Logbook', 'table',
            panel('nicos.clients.gui.panels.elog.ELogPanel')),
-    window('Log files', 'table',
-           panel('nicos.clients.gui.panels.logviewer.LogViewerPanel')),
-    window('Errors', 'errors',
-           panel('nicos.clients.gui.panels.errors.ErrorPanel')),
 ]
 
 tools = [
+    tool('Emergency stop button',
+         'nicos.clients.gui.tools.estop.EmergencyStopTool',
+         runatstartup=False),
     tool('Calculator', 'nicos.clients.gui.tools.calculator.CalculatorTool'),
     tool('Neutron cross-sections',
          'nicos.clients.gui.tools.website.WebsiteTool',
@@ -66,7 +86,4 @@ tools = [
          url='https://webapps.frm2.tum.de/intranet/neutroncalc/'),
     tool('Report NICOS bug or request enhancement',
          'nicos.clients.gui.tools.bugreport.BugreportTool'),
-    tool('Emergency stop button',
-         'nicos.clients.gui.tools.estop.EmergencyStopTool',
-         runatstartup=False),
 ]
