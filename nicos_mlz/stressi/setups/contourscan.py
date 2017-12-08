6cc63d02dee53dd295177baf0f8487c10995ebb4
Jens Krüger <jens.krueger@tum.de>
2017-12-08 09:49:30 +0100

    STRESSI: Add a 'contourscan' setup
    This setup will be used to scan the shape of a sample.
    
    Change-Id: If69f63a1fb7a43f250136979eb82abb61ece88cb
    Reviewed-on: https://forge.frm2.tum.de/review/16872
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/stressi/setups/contourscan.py
@@ -0,0 +1,16 @@
+description = 'Perform shape of sample scanning'
+
+group = 'basic'
+
+includes = ['sampletable', 'sick', 'system']
+
+sysconfig = dict(
+    datasinks = ['contoursink'],
+)
+
+devices = dict(
+    contoursink = device('nicos.devices.datasinks.AsciiScanfileSink',
+       lowlevel = True,
+       filenametemplate = ['contour%(scancounter)08d.txt'],
+    ),
+)
