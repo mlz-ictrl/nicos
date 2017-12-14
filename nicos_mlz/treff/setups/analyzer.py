2af58df09d9eada2bcf742125f4ff6db3cbf8f1f
Christian Felder <c.felder@fz-juelich.de>
2017-12-14 18:49:14 +0100

    TREFF: changed motor setups
    Change-Id: Ia3c3bfb4507d1c86d6c6cb77f81a8f165a028a71
    Reviewed-on: https://forge.frm2.tum.de/review/16881
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

--- nicos_mlz/treff/setups/analyzer.py
+++ nicos_mlz/treff/setups/analyzer.py
@@ -2,31 +2,14 @@ description = 'Analyzer device'
 
 group = 'optional'
 
-taco_base = '//phys.treff.frm2/treff/'
+tango_base = 'tango://phys.treff.frm2:10000/treff/FZJS7/'
 
 devices = dict(
-    anabus          = device('nicos_mlz.treff.devices.ipc.IPCModBusTacoJPB',
-                             tacodevice = taco_base + 'goett/480',
-                             lowlevel = True,
-                            ),
-    analyz_tilt_mot = device('nicos.devices.vendor.ipc.Motor',
-                             description = 'Analyzer tilt motor',
-                             bus = 'anabus',
-                             addr = 0,
-                             abslimits = (-0.81, 1.9433),
-                             unit = 'deg',
-                             slope = 30000,
-                             speed = 30,
-                             zerosteps = 526300,
-                             precision = 0.01,
-                             lowlevel = True,
-                            ),
-    analyzer_tilt   = device('nicos.devices.generic.Axis',
-                             description = 'Analyzer tilt',
-                             motor = 'analyz_tilt_mot',
-                             coder = 'analyz_tilt_mot',
-                             backlash = 0.007,
-                             precision = 0.01,
-                             fmtstr = '%.3f',
-                            ),
+    analyzer_tilt = device('nicos.devices.tango.Motor',
+                           description = 'Analyzer tilt',
+                           tangodevice = tango_base + 'analyzer_tilt',
+                           precision = 0.01,
+                           fmtstr = '%.3f',
+                           unit = 'deg',
+                          ),
 )
--- nicos_mlz/treff/setups/beamstop.py
+++ nicos_mlz/treff/setups/beamstop.py
@@ -2,33 +2,14 @@ description = 'Beam stop device'
 
 group = 'optional'
 
-taco_base = '//phys.treff.frm2/treff/'
+tango_base = 'tango://phys.treff.frm2:10000/treff/FZJS7/'
 
 devices = dict(
-    beambus      = device('nicos_mlz.treff.devices.ipc.IPCModBusTacoJPB',
-                          tacodevice = taco_base + 'goett/490',
-                          lowlevel = True,
-                         ),
-    beamstop_mot = device('nicos_mlz.treff.devices.ipc.Motor',
-                          description = 'Beam stop motor',
-                          bus = 'beambus',
-                          addr = 0,
-                          abslimits = (-0.1, 42),
-                          slope = 1821.41,
-                          zerosteps = 420496,
-                          speed = 130,
-                          refsteps = 495478,
-                          limitdist = 900,
-                          refspeed = 110,
-                          unit = 'mm',
-                          lowlevel = True,
-                         ),
-    beamstop     = device('nicos.devices.generic.Axis',
-                          description = 'Beamstop position',
-                          motor = 'beamstop_mot',
-                          coder = 'beamstop_mot',
-                          backlash = 0.11,
-                          precision = 0.01,
-                          fmtstr = '%.3f',
-                         ),
+    beamstop = device('nicos.devices.tango.Motor',
+                      description = 'Beamstop position',
+                      tangodevice = tango_base + 'beamstop',
+                      precision = 0.01,
+                      unit = 'mm',
+                      fmtstr = '%.3f',
+                     ),
 )
