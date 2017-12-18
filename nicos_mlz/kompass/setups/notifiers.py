a6d9de4645ea8f5c1c4b067a21a8a17633594748
Jens Krüger <jens.krueger@frm2.tum.de>
2017-12-18 09:25:03 +0100

    KOMPASS: Reformat setup files
    Change-Id: Idfe7e68b9063cfe2dd2ed9df54f02e1c9658e76a
    Reviewed-on: https://forge.frm2.tum.de/review/16926
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/kompass/setups/notifiers.py
+++ nicos_mlz/kompass/setups/notifiers.py
@@ -4,18 +4,20 @@ group = 'lowlevel'
 
 devices = dict(
     # Configure source and copy addresses to an existing address.
-    email    = device('nicos.devices.notifiers.Mailer',
-                      sender = 'kompass@frm2.tum.de',
-                      copies = [('dmitry.gorkov@frm2.tum.de', 'all'),   # gets all messages
-                                ('georg.waldherr@frm2.tum.de', 'important')], # gets only important messages
-                      subject = 'NICOS',
-                      lowlevel = True,
-                     ),
+    email = device('nicos.devices.notifiers.Mailer',
+        sender = 'kompass@frm2.tum.de',
+        copies = [
+            ('dmitry.gorkov@frm2.tum.de', 'all'),
+            ('georg.waldherr@frm2.tum.de', 'important')
+        ],
+        subject = 'NICOS',
+        lowlevel = True,
+    ),
 
     # Configure SMS receivers if wanted and registered with IT.
-    smser    = device('nicos.devices.notifiers.SMSer',
-                      server = 'triton.admin.frm2',
-                      receivers = [],
-                      lowlevel = True,
-                     ),
+    smser = device('nicos.devices.notifiers.SMSer',
+        server = 'triton.admin.frm2',
+        receivers = [],
+        lowlevel = True,
+    ),
 )
--- nicos_mlz/kompass/setups/special/watchdog.py
+++ nicos_mlz/kompass/setups/special/watchdog.py
@@ -25,32 +25,32 @@ group = 'special'
 #     (default '')
 # 'action' -- code to execute if condition is true (default no code is executed)
 watchlist = [
-#   dict(condition = 't_value > 100',
-#        message = 'Temperature too high',
-#        type = 'critical',
-#        action = 'maw(T, 0)',
-#       ),
-#   dict(condition = 'phi_value > 100 and mono_value > 1.5',
-#        message = 'phi angle too high for current mono setting',
-#        gracetime = 5,
-#       ),
+    # dict(condition = 't_value > 100',
+    #     message = 'Temperature too high',
+    #     type = 'critical',
+    #     action = 'maw(T, 0)',
+    # ),
+    # dict(condition = 'phi_value > 100 and mono_value > 1.5',
+    #     message = 'phi angle too high for current mono setting',
+    #     gracetime = 5,
+    # ),
 ]
 
 includes = ['notifiers']
 
 notifiers = {
-    'default':  ['email'],
+    'default': ['email'],
     'critical': ['email', 'smser'],
 }
 
 devices = dict(
     Watchdog = device('nicos.services.watchdog.Watchdog',
-                      # use only 'localhost' if the cache is really running on
-                      # the same machine, otherwise use the official computer
-                      # name
-                      cache = 'kompassctrl.kompass.frm2',
-                      notifiers = notifiers,
-                      mailreceiverkey = 'email/receivers',
-                      watch = watchlist,
-                     ),
+        # use only 'localhost' if the cache is really running on
+        # the same machine, otherwise use the official computer
+        # name
+        cache = 'kompassctrl.kompass.frm2',
+        notifiers = notifiers,
+        mailreceiverkey = 'email/receivers',
+        watch = watchlist,
+    ),
 )
