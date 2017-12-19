ceb6a32f969cef80742a03ff52a52dc240beb7eb
Lydia Fleischhauer-Fuss <l.fleischhauer-fuss@fz-juelich.de>
2017-12-19 11:32:06 +0100

    ANTARES: flexio expanded, simplify setup
    Change-Id: Ic2af2a856a49bb7c707c2671e4cef5d4aed53c3e
    Reviewed-on: https://forge.frm2.tum.de/review/16944
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/antares/setups/flexio.py
+++ nicos_mlz/antares/setups/flexio.py
@@ -1,67 +1,23 @@
 # -*- coding: utf-8 -*-
 
-description = 'ANTARES flexible I/Os'
+description = 'Flexible I/Os'
 
 group = 'optional'
 
 tango_base = 'tango://antareshw.antares.frm2:10000/antares/'
 
-devices = dict(
+devices = dict()
 
-    # FlexOutput = device('nicos_mlz.antares.devices.DigitalOutput',
-    # description = '16bit Flex Output',
-    # tangodevice = tango_base + 'fzjdp_digital/FlexOutput',
-    # ),
-    # FlexInput = device('nicos_mlz.antares.devices.DigitalInput',
-    # description = '16bit Flex Input',
-    # tangodevice = tango_base + 'fzjdp_digital/FlexInput',
-    # ),
-    FlexIn01 = device('nicos_mlz.antares.devices.partialdio.PartialDigitalInput',
+for bit in range(10):
+    indev = device('nicos_mlz.antares.devices.partialdio.PartialDigitalInput',
         description = '1bit Flex Input',
         tangodevice = tango_base + 'fzjdp_digital/FlexInput',
-        startbit = 0,
-        bitwidth = 1,
-    ),
-    FlexIn02 = device('nicos_mlz.antares.devices.partialdio.PartialDigitalInput',
-        description = '1bit Flex Input',
-        tangodevice = tango_base + 'fzjdp_digital/FlexInput',
-        startbit = 1,
-        bitwidth = 1,
-    ),
-    FlexIn03 = device('nicos_mlz.antares.devices.partialdio.PartialDigitalInput',
-        description = '1bit Flex Input',
-        tangodevice = tango_base + 'fzjdp_digital/FlexInput',
-        startbit = 2,
-        bitwidth = 1,
-    ),
-    FlexIn04 = device('nicos_mlz.antares.devices.partialdio.PartialDigitalInput',
-        description = '1bit Flex Input',
-        tangodevice = tango_base + 'fzjdp_digital/FlexInput',
-        startbit = 3,
-        bitwidth = 1,
-    ),
-    FlexOut01 = device('nicos_mlz.antares.devices.partialdio.PartialDigitalOutput',
+        startbit = bit,
+    )
+    outdev = device('nicos_mlz.antares.devices.partialdio.PartialDigitalOutput',
         description = '1bit Flex Output',
         tangodevice = tango_base + 'fzjdp_digital/FlexOutput',
-        startbit = 0,
-        bitwidth = 1,
-    ),
-    FlexOut02 = device('nicos_mlz.antares.devices.partialdio.PartialDigitalOutput',
-        description = '1bit Flex Output',
-        tangodevice = tango_base + 'fzjdp_digital/FlexOutput',
-        startbit = 1,
-        bitwidth = 1,
-    ),
-    FlexOut03 = device('nicos_mlz.antares.devices.partialdio.PartialDigitalOutput',
-        description = '1bit Flex Output',
-        tangodevice = tango_base + 'fzjdp_digital/FlexOutput',
-        startbit = 2,
-        bitwidth = 1,
-    ),
-    FlexOut04 = device('nicos_mlz.antares.devices.partialdio.PartialDigitalOutput',
-        description = '1bit Flex Output',
-        tangodevice = tango_base + 'fzjdp_digital/FlexOutput',
-        startbit = 3,
-        bitwidth = 1,
-    ),
-)
+        startbit = bit,
+    )
+    devices['FlexIn%02d' % (bit + 1)] = indev
+    devices['FlexOut%02d' % (bit + 1)] = outdev
