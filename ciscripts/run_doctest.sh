d426c4dc8ee6f40139138909b06a63569c0904fa
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2017-12-20 14:35:58 +0100

    Fix ciscript
    Change-Id: I307d4bee7f7ca00ca9b93951c551b9517d452c53
    Reviewed-on: https://forge.frm2.tum.de/review/16959
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
    Tested-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>

--- ciscripts/run_doctest.sh
+++ ciscripts/run_doctest.sh
@@ -3,7 +3,7 @@ set +x
 . $NICOSVENV/bin/activate
 echo $PATH
 set -x
-export doc_changed=`git diff --name-status \\`git merge-base HEAD HEAD^\\` | sed -e '/^D/d' | sed -e 's/.\t//' | grep doc`
+export doc_changed=$(git diff --name-status `git merge-base HEAD HEAD^` | sed -e '/^D/d' | sed -e 's/.\t//' | grep doc)
 if [[ -n "$doc_changed" ]]; then
 
     cd doc
