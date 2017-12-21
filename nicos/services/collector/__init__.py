8ca3658ae72a2040764be166abefc74bb4abb7fa
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2017-12-21 15:06:19 +0100

    Extend cache collector as a general cache forwarder
    Allow the collector to handle multiple backends simultaneously.
    
    Add a webhook backend.
    
    Filters can be configured both on backend and collector level.
    
    Advantage of a webhook in this case: Easier to get port access
    than with a cache depending on where the web frontend is running.
    
    Change-Id: Ib156734a435e840a5197d1fd9f0a5f4f6485796b
    Related: #3555
    Reviewed-on: https://forge.frm2.tum.de/review/16976
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos/services/collector/__init__.py
+++ nicos/services/collector/__init__.py
@@ -21,47 +21,173 @@
 #   Georg Brandl <georg.brandl@frm2.tum.de>
 #
 # *****************************************************************************
-
 """The NICOS cache collector daemon."""
 
-from nicos.core import Override, Attach
-from nicos.protocols.cache import OP_TELL, OP_TELLOLD
+import re
+
+try:
+    import requests
+except ImportError:
+    requests = None
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+from nicos.core import Attach, Override, Param
+from nicos.core.device import Device
+from nicos.core.errors import ConfigurationError
+from nicos.core.mixins import DeviceMixinBase
+from nicos.core.params import listof, none_or, oneof
 from nicos.devices.cacheclient import BaseCacheClient
+from nicos.protocols.cache import OP_TELL, OP_TELLOLD
+
+PREFIX_RE = re.compile(r'[a-zA-Z0-9_/]+\.\*$')
 
 
-class GlobalCache(BaseCacheClient):
+class CacheKeyFilter(DeviceMixinBase):
+    """Mixin for filtering cache keys that might be forwarded.
+
+    This is used on the source side (Collector device), as well as on
+    individual sinks (ForwarderBase devices) for maximum flexibility.
+    """
+
+    parameters = {
+        'keyfilters': Param('Filter keys to send (regexps); if empty, all '
+                            'keys are accepted', type=none_or(listof(str))),
+    }
+
+    def _initFilters(self):
+        self._prefixfilters = ()
+        self._regexfilters = []
+        for regex in self.keyfilters:
+            # special case: prefixes
+            if PREFIX_RE.match(regex):
+                self._prefixfilters += (regex,)
+            else:
+                self._regexfilters.append(re.compile(regex))
+
+    def _checkKey(self, key):
+        if not self._prefixfilters and not self._regexfilters:
+            return True
+        if key.startswith(self._prefixfilters):
+            return True
+        for keyfilter in self._regexfilters:
+            if keyfilter.match(key):
+                return True
+        return False
+
+
+class ForwarderBase(CacheKeyFilter, DeviceMixinBase):
+    """Defines the interface for a sink that forwards cache updates to an
+    external service.
+    """
+
+    def _startWorker(self):
+        pass
+
+    def _putChange(self, time, key, value):
+        pass
+
+
+class CacheForwarder(ForwarderBase, BaseCacheClient):
+    """Forwards cache updates to another cache.
+
+    The `prefix` parameter of this device can be set to something other than
+    `nicos/` to put keys from various sources into unique namespaces.
+    """
+
+    def doInit(self, mode):
+        BaseCacheClient.doInit(self, mode)
+        self._initFilters()
 
     def _connect_action(self):
         # send no requests for keys or updates
         pass
 
-    def put_change(self, time, key, value):
+    def _startWorker(self):
+        self._worker.start()
+
+    def _putChange(self, time, key, value):
+        if not self._checkKey(key):
+            return
         if value is None:
             msg = '%s@%s%s%s\n' % (time, self._prefix, key, OP_TELLOLD)
         else:
             msg = '%s@%s%s%s%s\n' % (time, self._prefix, key, OP_TELL, value)
         self._queue.put(msg)
 
+    def _handle_msg(self, _time, _ttlop, _ttl, _tsop, _key, _op, _value):
+        pass
 
-class Collector(BaseCacheClient):
+
+class WebhookForwarder(ForwarderBase, Device):
+    """Forwards cache updates to a web service."""
+
+    parameters = {
+        'hook_url':      Param('Hook URL endpoint', type=str, mandatory=True),
+        'prefix':        Param('Cache key prefix', type=str, mandatory=True),
+        'http_mode':     Param('HTTP request mode', type=oneof('GET', 'POST'),
+                               mandatory=True),
+        'paramencoding': Param('Parameter encoding',
+                               type=oneof('plain', 'json'), mandatory=True),
+        'jsonname':      Param('JSON parameter name (used for GET requests)',
+                               type=str, default='json'),
+    }
+
+    def doInit(self, mode):
+        if requests is None:
+            raise ConfigurationError(self, 'requests package is missing')
+        self._prefix = self.prefix.strip('/')
+        if self._prefix:
+            self._prefix += '/'
+        self._initFilters()
+
+    def _putChange(self, time, key, value):
+        if not self._checkKey(key):
+            return
+        pdict = dict(time=time, key=self._prefix + key, value=value)
+        if self.paramencoding == 'json':
+            if self.http_mode == 'GET':
+                pdict = {self.jsonname: json.dumps(pdict)}
+                requests.get(self.hook_url, params=pdict)
+            elif self.http_mode == 'POST':
+                requests.post(self.hook_url, json=pdict)
+        else:
+            if self.http_mode == 'GET':
+                requests.get(self.hook_url, params=pdict)
+            elif self.http_mode == 'POST':
+                requests.post(self.hook_url, data=pdict)
+
+
+class Collector(CacheKeyFilter, BaseCacheClient):
+    """The main service that enables cache update forwarding."""
 
     attached_devices = {
-        'globalcache':  Attach('The cache to submit keys to', GlobalCache),
+        'forwarders': Attach('The services to submit keys to',
+                             ForwarderBase, multiple=True),
     }
 
     parameter_overrides = {
-        'prefix':  Override(mandatory=False, default='nicos/'),
+        'prefix': Override(mandatory=False, default='nicos/'),
     }
 
     def doInit(self, mode):
         BaseCacheClient.doInit(self, mode)
-        self._attached_globalcache._worker.start()
+        self._initFilters()
+        for service in self._attached_forwarders:
+            service._startWorker()
 
     def _handle_msg(self, time, ttlop, ttl, tsop, key, op, value):
         if not key.startswith(self._prefix):
+            return False
+        if not self._checkKey(key):
             return
         key = key[len(self._prefix):]
         if op == OP_TELL:
-            self._attached_globalcache.put_change(time, key, value or None)
+            value = value or None
         elif op == OP_TELLOLD:
-            self._attached_globalcache.put_change(time, key, None)
+            value = None
+        for service in self._attached_forwarders:
+            service._putChange(time, key, value)
--- nicos_demo/demo/setups/special/collector.py
+++ nicos_demo/demo/setups/special/collector.py
@@ -2,12 +2,19 @@ description = 'setup for the NICOS collector'
 group = 'special'
 
 devices = dict(
-    Global = device('nicos.services.collector.GlobalCache',
+    GlobalCache = device('nicos.services.collector.CacheForwarder',
         cache = 'localhost:14716',
         prefix = 'nicos/demosys/',
     ),
+    Webhook = device('nicos.services.collector.WebhookForwarder',
+        hook_url = 'http://localhost:14444/receive',
+        prefix = 'demo',
+        http_mode = 'POST',
+        paramencoding = 'json',
+        keyfilters = ['.*/value'],
+    ),
     Collector = device('nicos.services.collector.Collector',
         cache = 'localhost:14869',
-        globalcache = 'Global',
+        forwarders = ['GlobalCache', 'Webhook'],
     ),
 )
