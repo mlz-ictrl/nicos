fe0793202d7dd89830e90c6cb6aa38f5449b8ba2
Jens Krüger <jens.krueger@tum.de>
2017-12-21 14:06:40 +0100

    RESEDA: add missing guide field coil 0 device
    Change-Id: I1f02d37dc1c6b2dc62b504c2d318f79e94d154d5
    Reviewed-on: https://forge.frm2.tum.de/review/16967
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/reseda/setups/guide_fields.py
+++ nicos_mlz/reseda/setups/guide_fields.py
@@ -13,7 +13,7 @@ devices = {
         tangotimeout = 5.0,
         pollinterval = 5,
         maxage = 15,
-    ) for i in ([1, 2] + list(range(4, 11)))
+    ) for i in ([0, 1, 2] + list(range(4, 11)))
 }
 devices.update({
     'gf4': device('nicos.devices.tango.PowerSupply',
