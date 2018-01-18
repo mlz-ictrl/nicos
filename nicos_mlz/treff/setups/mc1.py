1f05d42d85fa49b9bb09592978b3211048a04948
Christian Felder <c.felder@fz-juelich.de>
2018-01-18 11:39:23 +0100

    TREFF: Remove non-existend mc1_x axis.
    Change-Id: I469cb02739c7fe249946670783677ec7b3c0ef7d
    Reviewed-on: https://forge.frm2.tum.de/review/17087
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

--- nicos_mlz/treff/setups/mc1.py
+++ nicos_mlz/treff/setups/mc1.py
@@ -12,10 +12,4 @@ devices = dict(
                      precision = 0.01,
                      unit = 'deg',
                     ),
-    mc1_x   = device('nicos.devices.tango.Motor',
-                     description = 'MC1 x motor',
-                     tangodevice = tango_s7 + 'mc1_x',
-                     precision = 0.01,
-                     unit = 'mm',
-                    ),
 )
