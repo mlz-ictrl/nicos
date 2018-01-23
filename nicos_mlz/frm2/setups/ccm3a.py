584ed0cfc54f0795a9acbe3c3896ab649d21a4d4
Georg Brandl <g.brandl@fz-juelich.de>
2018-01-23 17:36:45 +0100

    FRM2: add setup for ccm3a magnet
    Change-Id: I4f6190e1b43ace3c21a4c3f3391ed26849413d48
    Reviewed-on: https://forge.frm2.tum.de/review/17182
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

---
+++ nicos_mlz/frm2/setups/ccm3a.py
@@ -0,0 +1,55 @@
+description = '3T High-Tc superconducting magnet'
+
+group = 'plugplay'
+
+includes = ['alias_B']
+
+tango_base = 'tango://%s:10000/box/' % setupname
+
+devices = {
+    'B_%s' % setupname: device('nicos.devices.tango.RampActuator',
+        description = 'magnetic field device',
+        tangodevice = tango_base + 'plc/magneticfield_cal',
+        unit = 'T',
+        fmtstr = "%.4f",
+        abslimits = (-3, 3),
+        precision = 0.0005,
+    ),
+    'B_%s_readback' % setupname: device('nicos.devices.tango.AnalogInput',
+        description = 'magnetic field device',
+        tangodevice = tango_base + 'plc/currentmonitor_cal',
+        unit = 'T',
+        fmtstr = "%.4f",
+        pollinterval = 1,
+    ),
+    '%s_T1' % setupname: device('nicos.devices.tango.AnalogInput',
+        description = 'Temperature of the first stage of the '
+        'cryo-cooler',
+        tangodevice = tango_base + 'hts_mss/t1',
+        unit = 'K',
+        warnlimits = (0, 44),
+    ),
+    '%s_T2' % setupname: device('nicos.devices.tango.AnalogInput',
+        description = 'Temperature of the second stage of the '
+        'cryo-cooler',
+        tangodevice = tango_base + 'hts_mss/t2',
+        unit = 'K',
+        warnlimits = (0, 12),
+    ),
+    '%s_TA' % setupname: device('nicos.devices.tango.AnalogInput',
+        description = 'Temperature of coil pack A',
+        tangodevice = tango_base + 'hts_mss/t3',
+        unit = 'K',
+        warnlimits = (0, 18),
+    ),
+    '%s_TB' % setupname: device('nicos.devices.tango.AnalogInput',
+        description = 'Temperature of coil pack B',
+        tangodevice = tango_base + 'hts_mss/t4',
+        unit = 'K',
+        warnlimits = (0, 18),
+    ),
+}
+
+alias_config = {
+    'B': {'B_%s' % setupname: 100},
+}
