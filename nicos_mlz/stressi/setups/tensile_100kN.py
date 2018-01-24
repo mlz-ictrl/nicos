2752e6a7eba36ea2bdf8cc720fc3c10fe30f8ab9
Michael Hofmann <Michael.Hofmann@frm2.tum.de>
2018-01-24 10:19:40 +0100

    STRESSI: add setup for the 100kN tensile machine
    Change-Id: I438b8d99230e0e3b1a9a0bbb6fec179894f6f603
    Reviewed-on: https://forge.frm2.tum.de/review/17164
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/stressi/setups/tensile_100kN.py
@@ -0,0 +1,39 @@
+description = 'Tensile machine 100kN'
+
+group = 'optional'
+
+excludes = ['tensile']
+
+nameservice = 'stressictrl.stressi.frm2'
+
+devices = dict(
+    teload = device('nicos.devices.vendor.caress.Motor',
+        description = 'load value of the tensile machine',
+        nameserver = '%s' % nameservice,
+        config = 'TELOAD 500 TensileLoad.ControllableDevice',
+        absdev = False,
+        abslimits = (-100000, 100000),
+        unit = 'N',
+        fmtstr = '%.2f',
+    ),
+    tepos = device('nicos.devices.vendor.caress.Motor',
+        description = 'position value of the tensile machine',
+        nameserver = '%s' % nameservice,
+        config = 'TEPOS 500 TensilePos.ControllableDevice',
+        absdev = False,
+        abslimits = (-50, 50),
+        unit = 'mm',
+        fmtstr = '%.3f',
+    ),
+    teext = device('nicos.devices.vendor.caress.Motor',
+        description = 'extension value of the tensile machine',
+        nameserver = '%s' % nameservice,
+        config = 'TEEXT 500 TensileExt.ControllableDevice',
+        absdev = False,
+        abslimits = (-3000, 3000),
+        unit = 'um',
+        fmtstr = '%.3f',
+    ),
+)
+
+display_order = 40
