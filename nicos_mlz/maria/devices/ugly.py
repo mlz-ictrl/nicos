cbb05659f4891f01f8abc8906e1d562ff5b19475
Christian Felder <c.felder@fz-juelich.de>
2018-02-02 10:34:19 +0100

    MARIA ugly module.
    In order to conduct some tests during this cycle the ugly module provides
    a tango StringIO class which can be used in dry run mode as requested by
    the instrument scientists. Devices should be implemented using our
    standardized interfaces in tango after this cycle. This module is considered
    temporary though.
    
    Change-Id: I0adf39e5ab64e8098c62110626e2d71bf1f94c65
    Reviewed-on: https://forge.frm2.tum.de/review/17246
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

---
+++ nicos_mlz/maria/devices/ugly.py
@@ -0,0 +1,69 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2018 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Christian Felder <c.felder@fz-juelich.de>
+#
+# *****************************************************************************
+
+from nicos.devices.tango import StringIO as BaseStringIO
+
+
+class StringIO(BaseStringIO):
+    """StringIO which can be used in dry-run/simulation mode"""
+
+    def communicate(self, value):
+        if self._dev:
+            return BaseStringIO.communicate(self, value)
+
+    def flush(self):
+        if self._dev:
+            self._dev.Flush()
+
+    def read(self, value):
+        if self._dev:
+            return BaseStringIO.read(self, value)
+
+    def write(self, value):
+        if self._dev:
+            return BaseStringIO.write(self, value)
+
+    def readLine(self):
+        if self._dev:
+            return BaseStringIO.readLine(self)
+
+    def writeLine(self, value):
+        if self._dev:
+            return BaseStringIO.writeLine(self, value)
+
+    def multiCommunicate(self, value):
+        if self._dev:
+            return BaseStringIO.multiCommunicate(self, value)
+
+    @property
+    def availablechars(self):
+        if self._dev:
+            # pylint: disable=too-many-function-args
+            return BaseStringIO.availablechars.__get__(self)
+
+    @property
+    def availablelines(self):
+        if self._dev:
+            # pylint: disable=too-many-function-args
+            return BaseStringIO.availablelines.__get__(self)
