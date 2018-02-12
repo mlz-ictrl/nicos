501cd4488dd1b7755495ac4dd5125eec391fa534
Georg Brandl <g.brandl@fz-juelich.de>
2018-02-12 12:11:49 +0100

    KWS: fix sample edit dialog on reopen
    Change-Id: I6b1c899ad3aeaf3dfd131928dac7d152e51d109f
    Reviewed-on: https://forge.frm2.tum.de/review/17296
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos_mlz/kws1/gui/sampleconf.py
+++ nicos_mlz/kws1/gui/sampleconf.py
@@ -85,8 +85,9 @@ def configFromFrame(frame):
 
 class ConfigEditDialog(QDialog):
 
-    def __init__(self, parent, client, configs, config=None):
+    def __init__(self, parent, client, instrument, configs, config=None):
         QDialog.__init__(self, parent)
+        self.instrument = instrument
         self.configs = configs
         self.client = client
         self.setWindowTitle('Sample configuration')
@@ -456,8 +457,8 @@ class KWSSamplePanel(Panel):
 
     @pyqtSlot()
     def on_newBtn_clicked(self):
-        dlg = ConfigEditDialog(self, self.client, self.configs)
-        dlg.instrument = self.instrument
+        dlg = ConfigEditDialog(self, self.client, self.instrument,
+                               self.configs)
         if not dlg.exec_():
             return
         self.dirty = True
@@ -473,7 +474,7 @@ class KWSSamplePanel(Panel):
         index = self.list.currentRow()
         if index < 0:
             return
-        dlg = ConfigEditDialog(self, self.client,
+        dlg = ConfigEditDialog(self, self.client, self.instrument,
                                [config for (i, config) in
                                 enumerate(self.configs) if i != index],
                                self.configs[index])
