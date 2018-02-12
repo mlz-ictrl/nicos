d65e810eba52725eacada3de6ff3e57665454cfa
Kompass User <kompassuser@kompassctrl.kompass.frm2>
2018-02-12 09:09:45 +0100

    KOMPASS: current status (incl. setups update)
    Change-Id: I8cafaa91bd9915343c6a17d622427bc353a4e416
    Reviewed-on: https://forge.frm2.tum.de/review/17927
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

--- nicos_mlz/kompass/setups/selector.py
+++ nicos_mlz/kompass/setups/selector.py
@@ -2,21 +2,34 @@ description = 'Selector related devices'
 
 group = 'lowlevel'
 
-nethost = 'kompasshw.kompass.frm2'
+tango_base = 'tango://kompasshw.kompass.frm2:10000/kompass/'
 
 devices = dict(
-    nvslift_m = device('nicos.devices.taco.Motor',
+    nvslift_m = device('nicos.devices.tango.Motor',
         description = 'Neutron selector lift motor',
-        tacodevice = '//%s/kompass/lift/motor' % nethost,
-        abslimits = (0, 406.5),
+        tangodevice = tango_base + 'lift/motor',
         unit = 'mm',
         lowlevel = True,
     ),
+    nvslift_c = device('nicos.devices.tango.Sensor',
+        description = 'Selector lift coder',
+        tangodevice = tango_base + 'lift/coder',
+        fmtstr = '%.2f',
+        lowlevel = True,
+    ),
+    nvslift_ax = device('nicos.devices.generic.Axis',
+        description = 'Selector lift position',
+        motor = 'nvslift_m',
+#        coder = 'nvslift_c',  # temporarily, as the coder seems broken
+        coder = 'nvslift_m',
+        fmtstr = '%.2f',
+        precision = 0.1,
+    ),
     nvslift = device('nicos.devices.generic.Switcher',
         description = 'Neutron selector lift',
-        moveable = 'nvslift_m',
+        moveable = 'nvslift_ax',
         mapping = {'out': 0.,
-                   'in': 406.01},
+                   'in': 405.377},
         fallback = '',
         fmtstr = '%s',
         precision = 0.1,
