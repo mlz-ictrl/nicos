07a23b9b5e4d187fd1262d161cabf61d8699d4ab
Georg Brandl <g.brandl@fz-juelich.de>
2018-03-08 10:35:56 +0100

    demo: update setups after collector change
    Change-Id: I17ffbd016b821184d59ca013e49a41e1d63bab3a
    Reviewed-on: https://forge.frm2.tum.de/review/17445
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos_demo/vspodi/setups/special/collector.py
+++ nicos_demo/vspodi/setups/special/collector.py
@@ -2,12 +2,12 @@ description = 'setup for the NICOS collector'
 group = 'special'
 
 devices = dict(
-    Global = device('nicos.services.collector.GlobalCache',
+    Global = device('nicos.services.collector.CacheForwarder',
         cache = 'localhost:14716',
         prefix = 'nicos/demosys/',
     ),
     Collector = device('nicos.services.collector.Collector',
         cache = 'localhost:14869',
-        globalcache = 'Global',
+        forwarders = ['Global'],
     ),
 )
