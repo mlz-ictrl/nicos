b8acad673dd9df7dabe4e09e41a403de0b9b9afe
Matthias Pomm <matthias.pomm@hzg.de>
2018-03-23 19:20:35 +0100

    REFSANS: add triangle device
    Change-Id: I9ed815fbd3c3f77fa4809f071a3d7cf5b8da8c21
    Reviewed-on: https://forge.frm2.tum.de/review/17521
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/refsans/devices/triangle.py
@@ -0,0 +1,94 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2017 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Jens Krüger <jens.krueger@frm2.tum.de>
+#   Matthias Pomm <matthias.pomm@hzg.de>
+#
+# *****************************************************************************
+"""Devices for the Trioptic autocollimator."""
+
+from IO import StringIO
+
+from nicos.core import Override, Param, Readable, floatrange, intrange, status
+from nicos.core.errors import CommunicationError
+from nicos.devices.taco.core import TacoDevice
+
+
+class TriangleBase(TacoDevice, Readable):
+
+    taco_class = StringIO
+
+    parameter_overrides = {
+        'comtries': Override(default=1),
+    }
+
+    def _read_controller(self, index):
+        index = int(index)
+        self.log.debug('_read_controller %s %d' % (type(index), index))
+        res = self._taco_guard(self._dev.communicate, 'P 1\r')
+        self.log.debug('_read_controller res for %s' % res)
+        res = res.split(';')
+        self.log.debug('_read_controller res %s' % res)
+        res = [float(e) for e in res]
+        return res[index]
+
+
+class TriangleMaster(TriangleBase):
+
+    def doRead(self, maxage=0):
+        try:
+            res = self._read_controller(3)
+            self.log.debug('pos: %f', res)
+        except IndexError:
+            res = 0
+        return res
+
+    def doStatus(self, maxage=0):
+        try:
+            sig = self._read_controller(3)
+            if sig <= .01:
+                return status.WARN, 'beta_W'
+            return status.OK, 'beta'
+        except CommunicationError:
+            return status.ERROR, 'timeout check PC! SW running and COM-Port'
+
+
+class TriangleAngle(TriangleMaster):
+
+    parameters = {
+        'index': Param('index of return',
+                       type=intrange(0, 1), settable=False,
+                       volatile=False, userparam=False),
+        'water': Param('angle with water',
+                       type=floatrange(-.2, .2),
+                       settable=True,
+                       userparam=True,
+                       default=0),
+    }
+
+    def doRead(self, maxage=0):
+        try:
+            self.log.debug('index: %d' % self.index)
+            res = self._read_controller(self.index)
+            res -= self.water
+            self.log.debug('pos: %f' % res)
+        except IndexError:
+            res = 0
+        return res
--- nicos_mlz/refsans/setups/elements/triangle.py
+++ nicos_mlz/refsans/setups/elements/triangle.py
@@ -1,34 +1,28 @@
-description = 'Autokollimator von Trioptic'
+description = 'Trioptic autocollimator'
 
 # not included by others
 group = 'optional'
 
-uribase = 'tango://refsansctrl01.refsans.frm2:10000/refsans/flipper/'
+all_lowlevel = False  # or True
+
+nethost = 'refsanssrv.refsans.frm2'
 
 devices = dict(
-    _guide = device('nicos.devices.tango.AnalogInput',
-        description = 'Temperature of Flipping Guide',
-        tangodevice = uribase + 'guide_temp',
+    triangle = device('nicos_mlz.refsans.devices.triangle.TriangleMaster',
+        description = description,
+        tacodevice = '//%s/test/network/triangle' % nethost,
+        lowlevel = all_lowlevel,
     ),
-    _coil = device('nicos.devices.tango.AnalogInput',
-        description = 'Temperature of Flipping Coil',
-        tangodevice = uribase + 'coil_temp',
+    triangle_theta = device('nicos_mlz.refsans.devices.triangle.TriangleAngle',
+        description = 'triangle_Y',
+        lowlevel = all_lowlevel,
+        index = 0,
+        tacodevice = '//%s/test/network/triangle' % nethost,
     ),
-    _current = device('nicos.devices.tango.WindowTimeoutAO',
-        description = 'Current of Flipping Coil',
-        tangodevice = uribase + 'current',
-        precision = 0.1,
-    ),
-    _frequency = device('nicos.devices.tango.AnalogInput',
-        description = 'Frequency of Flipping Field',
-        tangodevice = uribase + 'frequency',
-    ),
-    _flipper = device('nicos.devices.tango.NamedDigitalOutput',
-        description = 'Flipper',
-        tangodevice = uribase + 'flipper',
-        mapping = dict(ON = 1, OFF = 0),
+    triangle_phi = device('nicos_mlz.refsans.devices.triangle.TriangleAngle',
+        description = 'triangle_X',
+        lowlevel = all_lowlevel,
+        index = 1,
+        tacodevice = '//%s/test/network/triangle' % nethost,
     ),
 )
-
-startupcode = """
-"""
