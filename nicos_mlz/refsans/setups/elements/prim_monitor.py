e185523d57f099b14feae1e6974aadf41e761c4f
Matthias Pomm <matthias.pomm@hzg.de>
2018-03-31 20:41:23 +0200

    REFSANS: Add setup for the primary monitor
    Change-Id: I78dddca1319feebeb3d56b010f60b3e614a6dcfb
    Reviewed-on: https://forge.frm2.tum.de/review/17575
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/refsans/setups/elements/prim_monitor.py
@@ -0,0 +1,27 @@
+description = 'postion of Monitor: X in beam; Z may be motor'
+
+group = 'optional'
+
+nethost = 'refsanssrv.refsans.frm2'
+tacodev = '//%s/test' % nethost
+
+
+devices = dict(
+    prim_monitor_z = device('nicos.devices.taco.Motor',
+        description = 'Monitor axis motor',
+        tacodevice = '%s/phytron/kanal_09' % tacodev,
+        abslimits = (10, 300),
+    ),
+    prim_monitor_x = device('nicos.devices.generic.ManualMove',
+        description = 'pos of monitor in beam',
+        abslimits = (0, 500),
+        fmtstr = '%.1f',
+        unit = 'mm',
+    ),
+    prim_monitor_typ = device('nicos.devices.generic.ManualSwitch',
+        description = 'which monitor is in use?',
+        states = ['#1','#2','#3'],
+        fmtstr = 'Typ %d',
+        unit = '',
+    ),
+)
