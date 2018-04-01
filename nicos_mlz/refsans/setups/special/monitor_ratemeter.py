77de813b0ac90854f243381bc180e20ddfd78f9c
Matthias Pomm <matthias.pomm@hzg.de>
2018-04-01 17:44:11 +0200

    REFSANS: New statusmonitor ratemeter as part of safe detector system
    Change-Id: I7492784dd5aabdd28fed00d64665bba1d7369f82
    Reviewed-on: https://forge.frm2.tum.de/review/17582
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/refsans/setups/special/monitor_ratemeter.py
@@ -0,0 +1,36 @@
+# created by MP
+# 04.12.2017 07:53:39
+# to call it
+# ssh refsans@refsansctrl01 oder 02
+# INSTRUMENT=nicos_mlz.refsans bin/nicos-monitor -S monitor_mp-PO
+# not perfet but working
+
+description = 'Ratemeter sds'
+group = 'special'
+
+devices = dict(
+    Monitor = device('nicos.services.monitor.qt.Monitor',
+        title = description,
+        loglevel = 'info',
+        cache = 'refsansctrl01.refsans.frm2',
+        prefix = 'nicos/',
+        font = 'Luxi Sans',
+        valuefont = 'Consolas',
+        fontsize = 12,
+        padding = 5,
+        layout = [
+            Row(
+Column(
+    Block('ratemeter', [
+        #BlockRow(Field(name='gonio Theta',    dev='gonio_theta',    width=30, unit='Grad'),),
+        #BlockRow(Field(name='gonio Phi',      dev='gonio_phi',      width=30, unit='Grad'),),
+        #BlockRow(Field(name='gonio Omega',    dev='gonio_omega',    width=30, unit='Grad'),),
+        #BlockRow(Field(name='gonio Y',        dev='gonio_y',        width=30, unit='mm'),),
+        #BlockRow(Field(name='gonio Z',        dev='gonio_z',        width=30, unit='mm'),),
+        BlockRow(Field(name='s d s',      dev='sds',        width=10, unit='cps'),),
+        ],
+    ),
+)            ),
+        ],
+    ),
+)
