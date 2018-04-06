5f80a394df685b46af8d767e2b0daf796be413ca
Jens Krüger <jens.krueger@frm2.tum.de>
2018-04-06 15:33:11 +0200

    RESEDA: All data sinks are lowlevel devices by default
    Change-Id: I0fd6b437c5da79953d672f17bb3295d877e7908a
    Reviewed-on: https://forge.frm2.tum.de/review/17644
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/reseda/setups/filesavers.py
+++ nicos_mlz/reseda/setups/filesavers.py
@@ -11,7 +11,6 @@ sysconfig = dict(
 devices = dict(
     HDF5FileSaver = device('nicos_mlz.reseda.devices.hdf5.ResedaHDF5Sink',
         description = 'Saves scalar and image data for scans in a defined structure into HDF5 files.',
-        lowlevel = True,
         detectors = ['X'],  # effectively disabled, to re-enable: set to ['']
     ),
 )
