5b381ecd8299056de4abb397d654c047042154e8
Jens Krüger <jens.krueger@frm2.tum.de>
2018-04-17 17:49:20 +0200

    FRM2: Adapt stressihtf2 setup
    The stressihtf2 box is now using entangle.
    
    Change-Id: I85e6baccf3eb1fe1987ce6c703533a0542577215
    Reviewed-on: https://forge.frm2.tum.de/review/17750
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/frm2/setups/stressihtf2.py
+++ nicos_mlz/frm2/setups/stressihtf2.py
@@ -7,43 +7,47 @@ includes = ['alias_T']
 
 nethost = setupname
 
-plc_tango_base = 'tango://%s:10000/box/plc/_' % setupname
+tango_base = 'tango://%s:10000/box/' % setupname
 
 devices = {
-    'T_%s' % setupname: device('nicos.devices.taco.TemperatureController',
+    'T_%s' % setupname: device('nicos.devices.tango.TemperatureController',
                                description = 'The sample temperature',
-                               tacodevice = '//%s/box/eurotherm/control' % nethost,
-                               abslimits = (0, 2000),
-                               unit = 'C',
+                               tangodevice = tango_base + 'eurotherm/ctrl',
+                               # abslimits = (0, 2000),
+                               # unit = 'C',
                                fmtstr = '%.1f',
                               ),
-    'T_sample_%s' % setupname: device('nicos.devices.taco.TemperatureSensor',
+    'T_sample_%s' % setupname: device('nicos.devices.tango.Sensor',
                                       description = 'The sample temperature '
                                                     'sensor',
-                                      tacodevice = '//%s/box/eurotherm/sensora' % nethost,
-                                      unit = 'C',
+                                      tangodevice = tango_base + 'eurotherm/sensora',
+                                      # unit = 'C',
                                       fmtstr = '%.1f',
                                      ),
     'n2': device('nicos.devices.tango.NamedDigitalOutput',
-                 description = 'N2 valve control',
-                 tangodevice = plc_tango_base + 'gas1',
-                 mapping = {'closed': 0, 'open': 1}
-                ),
+        description = 'N2 valve control',
+        tangodevice = tango_base + 'plc/_gas1',
+        mapping = {'closed': 0, 'open': 1},
+        unit = '',
+    ),
     'he': device('nicos.devices.tango.NamedDigitalOutput',
-                 description = 'He valve control',
-                 tangodevice = plc_tango_base + 'gas2',
-                 mapping = {'closed': 0, 'open': 1}
-                ),
+        description = 'He valve control',
+        tangodevice = tango_base + 'plc/_gas2',
+        mapping = {'closed': 0, 'open': 1},
+        unit = '',
+    ),
     'lamps': device('nicos.devices.tango.NamedDigitalOutput',
-                 description = 'Halogen lamps control',
-                 tangodevice = plc_tango_base + 'onoff',
-                 mapping = {'off': 0, 'on': 1}
-                ),
+        description = 'Halogen lamps control',
+        tangodevice = tango_base + 'plc/_onoff',
+        mapping = {'off': 0, 'on': 1},
+        unit = '',
+    ),
     'water': device('nicos.devices.tango.NamedDigitalInput',
-                    description = 'Cooling water status',
-                    tangodevice = plc_tango_base + 'waterok',
-                    mapping = {'failed': 0, 'ok': 1},
-                   ),
+        description = 'Cooling water status',
+        tangodevice = tango_base + 'plc/_waterok',
+        mapping = {'failed': 0, 'ok': 1},
+        unit = '',
+    ),
 }
 
 alias_config = {
