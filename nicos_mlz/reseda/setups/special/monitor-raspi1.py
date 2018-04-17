9726f0fdf7544c2d1f618e255cb769494606efa2
Jens Krüger <jens.krueger@frm2.tum.de>
2018-04-17 09:56:28 +0200

    RESEDA: Add cryostats, ccrs, and magnets to status monitor
    Fixes: #3733
    Change-Id: If226e1f8f5dc4bfb23e04d51ba3e4d17b2d450a8
    Reviewed-on: https://forge.frm2.tum.de/review/17774
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

--- nicos_mlz/reseda/setups/special/monitor-raspi1.py
+++ nicos_mlz/reseda/setups/special/monitor-raspi1.py
@@ -73,6 +73,123 @@ _column2 = Column(
      ),
 )
 
+ccrs = []
+for i in range(10, 22 + 1):
+    ccrs.append(Block('CCR%d' % i, [
+        BlockRow(
+            Field(name='Setpoint', key='t_ccr%d/setpoint' % i,
+                   unitkey='t/unit'),
+            Field(name='Target', key='t_ccr%d/target' % i,
+                   unitkey='t/unit'),
+        ),
+        BlockRow(
+            Field(name='Manual Heater Power Stick',
+                  key='t_ccr%d_stick/heaterpower' % i, format='%.3f',
+                  unitkey='t/unit'),
+        ),
+        BlockRow(
+            Field(name='Manual Heater Power Tube',
+                  key='t_ccr%d_tube/heaterpower' % i, format='%.3f',
+                  unitkey='t/unit'),
+        ),
+        BlockRow(
+            Field(name='P1 ', dev='ccr%d_p1' % i, format='%.3f'),
+        ),
+        BlockRow(
+             Field(name='A', dev='T_ccr%d_A' % i),
+             Field(name='B', dev='T_ccr%d_B' % i),
+        ),
+        BlockRow(
+             Field(name='C', dev='T_ccr%d_C' % i),
+             Field(name='D', dev='T_ccr%d_D' % i),
+        ),
+        ],
+        setups='ccr%d' % i,
+    ))
+
+cryos = []
+for cryo in 'cci3he1 cci3he2 cci3he3 ccidu1 ccidu2'.split():
+    cryos.append(Block(cryo.title(), [
+        BlockRow(
+            Field(name='Setpoint', key='t_%s/setpoint' % cryo,
+                   unitkey='t/unit'),
+            Field(name='Target', key='t_%s/target' % cryo,
+                   unitkey='t/unit'),
+        ),
+        BlockRow(
+            Field(name='Manual Heater Power', key='t_%s/heaterpower' % cryo,
+                   unitkey='t/unit'),
+        ),
+        BlockRow(
+             Field(name='A', dev='T_%s_A' % cryo),
+             Field(name='B', dev='T_%s_B' % cryo),
+        ),
+        ],
+        setups=cryo,
+    ))
+
+_ccmsans = Block('SANS-1 5T Magnet', [
+    BlockRow(Field(name='Field', dev='b_ccmsans', width=12)),
+    BlockRow(Field(name='Target', key='b_ccmsans/target', width=12),
+             Field(name='Asymmetry', key='b_ccmsans/asymmetry', width=12),
+    ),
+    BlockRow(Field(name='Power Supply 1', dev='a_ccmsans_left', width=12),
+             Field(name='Power Supply 2', dev='a_ccmsans_right', width=12),
+    ),
+    ],
+    setups='ccmsans',
+)
+
+_miramagnet = Block('MIRA 0.5T Magnet', [
+    BlockRow(Field(name='Field', dev='B_miramagnet', width=12),
+             Field(name='Target', key='B_miramagnet/target', width=12),
+    ),
+    BlockRow(Field(name='Current', dev='I_miramagnet', width=12)),
+    ],
+    setups='miramagnet',
+)
+
+_amagnet = Block('Antares Magnet', [
+    BlockRow(Field(name='Field', dev='B_amagnet', width=12),
+             Field(name='Target', key='B_amagnet/target', width=12),
+    ),
+    BlockRow(
+             Field(name='Current', dev='amagnet_current', width=12),
+             Field(name='ON/OFF', dev='amagnet_onoff', width=12),
+    ),
+    BlockRow(
+             Field(name='Polarity', dev='amagnet_polarity', width=12),
+             Field(name='Connection', dev='amagnet_connection', width=12),
+    ),
+    BlockRow(
+             Field(name='Lambda out', dev='l_out', width=12),
+    ),
+    ],
+    setups='amagnet',
+)
+
+_ccm2a = Block('CCM2a Magnet', [
+    BlockRow(
+             Field(name='Field', dev='B_ccm2a', width=12),
+            ),
+    BlockRow(
+             Field(name='Target', key='B_ccm2a/target', width=12),
+             Field(name='Readback', dev='B_ccm2a_readback', width=12),
+            ),
+    BlockRow(
+             Field(name='T1', dev='ccm2a_T1', width=12),
+             Field(name='T2', dev='ccm2a_T2', width=12),
+            ),
+    BlockRow(
+             Field(name='TA', dev='ccm2a_TA', width=12),
+             Field(name='TB', dev='ccm2a_TB', width=12),
+            ),
+    ],
+    setups='ccm2a',
+)
+
+magnets = [_ccm2a, _ccmsans, _miramagnet, _amagnet]
+
 _column3 = Column(
     Block('Cascade detector', [
         BlockRow(Field(name='ROI',   key='psd_channel[0]', format='%.0f', width=9),
@@ -96,7 +213,7 @@ _column3 = Column(
         ],
         setups='alias_T and ccr'
         ),
-)
+) + Column(*magnets) + Column(*ccrs) + Column(*cryos)
 
 devices = dict(
     Monitor = device('nicos.services.monitor.qt.Monitor',
