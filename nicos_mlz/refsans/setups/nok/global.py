c5366c3635da7c57cc43bed354cdafa590f6a7f5
Matthias Pomm <matthias.pomm@hzg.de>
2018-04-19 11:36:27 +0200

    REFSANS: Create a 'global' configuration setup
    This setup contains some global used values to avoid typing it more than
    ones and to have a single point of control.
    
    Change-Id: Ia92cd08fb818d71c6ce50c5ac2e612dcfc6ac303
    Reviewed-on: https://forge.frm2.tum.de/review/17785
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/refsans/setups/nok/global.py
@@ -0,0 +1,12 @@
+description = 'the only place for global Values'
+group = 'configdata'
+
+GLOBAL_Values = {
+    'ng':    0.0,
+    'rc':  22.5,
+    'vc':  37.5,
+    'fc':  52.5,
+    #showcase
+    'hide_poti': True,
+    'hide_acc' : False,
+}
