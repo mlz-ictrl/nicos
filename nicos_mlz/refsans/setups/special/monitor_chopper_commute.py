251b2f09bfbadd9d5962273a4f264c4a9c95b693
Matthias Pomm <matthias.pomm@hzg.de>
2018-04-19 13:30:43 +0200

    REFSANS: add special chopper setup
    Change-Id: I9a052e9efb72f2184e5f6a8960fdded0ee395d74
    Reviewed-on: https://forge.frm2.tum.de/review/17778
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/refsans/setups/special/monitor_chopper_commute.py
@@ -0,0 +1,106 @@
+# coding: utf-8
+
+# created by Martin Haese, Tel FRM 10763
+# MP zum kommutieren
+# last modified 05.04.2018 10:38:26
+# to call it
+# ssh -X refsans@refsansctrl01 oder 02
+# cd /refsanscontrol/src/nicos-core
+# INSTRUMENT=nicos_mlz.refsans bin/nicos-monitor -S monitor_chopper
+
+description = 'REFSANS chopper monitor'
+group = 'special'
+
+# Legende fuer _chconfigcol
+# dev='...' stellt hier nur die moeglichen Werte dar, keine devices
+
+_chconfigcol = Column(
+    Block(' chopper configuration ', [
+        BlockRow(
+            Field(name='lambda min', dev='chopper_lambda_min', width=14, unit='AA'),
+            Field(name='lambda max', dev='chopper_lambda_max', width=14, unit='AA'),
+            Field(name='resolution', dev='lambda_resolution', width=14),
+            Field(name='flight path', dev='chopper_to_detector', width=14, unit='mm'),
+            ),
+        ],
+    ),
+    Block(' chopper configuration ', [
+        BlockRow(
+            Field(name='Modus', dev='chopper_mode', width=24),
+            Field(name='real', dev='chopper2_pos', width=24),
+            ),
+        ],
+    ),
+)
+
+_disk1col = Column(
+    Block('disk 1', [
+        BlockRow(Field(name='speed',  dev='chopper1',  width=6.5, unit='rpm')),
+        BlockRow(Field(name='gear',   key='chopper1/gear',   width=6.5)),
+        BlockRow(Field(name='mode', key='chopper1/mode', width=6.5)),
+        ],
+    ),
+)
+
+_disk2col = Column(
+    Block('disk 2', [
+        BlockRow(Field(name='phase',  dev='chopper2_phase',  width=6.5, unit='deg')),
+        BlockRow(Field(name='gear',   key='chopper2/gear',   width=6.5)),
+        BlockRow(Field(name='mode', key='chopper2/mode', width=6.5)),
+        ],
+    ),
+)
+
+_disk3col = Column(
+    Block('disk 3', [
+        BlockRow(Field(name='phase',  key='chopper3/phase',  width=6.5, unit='deg')),
+        BlockRow(Field(name='gear',   key='chopper3/gear',   width=6.5)),
+        BlockRow(Field(name='mode', key='chopper3/mode', width=6.5)),
+        ],
+    ),
+)
+
+_disk4col = Column(
+    Block('disk 4', [
+        BlockRow(Field(name='phase',  key='chopper4/phase',  width=6.5, unit='deg')),
+        BlockRow(Field(name='gear',   key='chopper4/gear',   width=6.5)),
+        BlockRow(Field(name='mode', key='chopper4/mode', width=6.5)),
+        ],
+    ),
+)
+
+_disk5col = Column(
+    Block('disk 5', [
+        BlockRow(Field(name='phase',  key='chopper5/phase',  width=6.5, unit='deg')),
+        BlockRow(Field(name='gear',   key='chopper5/gear',   width=6.5)),
+        BlockRow(Field(name='mode', key='chopper5/mode', width=6.5)),
+        ],
+    ),
+)
+
+_disk6col = Column(
+    Block('disk 6', [
+        BlockRow(Field(name='phase',  key='chopper6/phase',  width=6.5, unit='deg')),
+        BlockRow(Field(name='gear',   key='chopper6/gear',   width=6.5)),
+        BlockRow(Field(name='mode', key='chopper6/mode', width=6.5)),
+        ],
+    ),
+)
+
+devices = dict(
+    Monitor = device('nicos.services.monitor.qt.Monitor',
+        title = description,
+        loglevel = 'info',
+        cache = 'refsansctrl01.refsans.frm2',
+        prefix = 'nicos/',
+        font = 'Luxi Sans',
+        valuefont = 'Consolas',
+        fontsize = 12,
+        padding = 5,
+        layout = [
+            # Row(_chconfigcol),
+            Row(_disk1col, _disk2col, _disk3col, _disk4col,
+                _disk5col, _disk6col),
+        ],
+    ),
+)
