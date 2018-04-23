3ba445dc784d312eb86a964b49cd95a358775407
Jens Krüger <jens.krueger@frm2.tum.de>
2018-04-23 11:06:54 +0200

    VPUMA: adopt setups closer to the original PUMA
    Change-Id: I7411dd12d71b4f0a857851c32ae0711869066f68
    Reviewed-on: https://forge.frm2.tum.de/review/17824
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_demo/vpuma/setups/cad.py
+++ nicos_demo/vpuma/setups/cad.py
@@ -2,16 +2,27 @@ description = 'Combined multianalyzer axis setup'
 
 group = 'lowlevel'
 
-includes = ['tas']
+includes = ['analyzer']
 
 devices = dict(
     cad = device('nicos_mlz.puma.devices.coupledaxis.PumaCoupledAxis',
         description = 'cad - combined axis for multianalyzer',
-        tt = 'att',
+        tt = 'att_cad',
         th = 'ath',
         fmtstr = '%.3f',
         unit = 'deg',
         precision = .1,
+    ),
+    att_cad = device('nicos.devices.generic.Axis',
+        description = 'Scattering angle two-theta of analyser',
+        motor = 'st_att',
+        precision = 0.01,
+        offset = 90.,
+        # offset = 0.,
+        jitter = 0.2,
+        dragerror = 1,
+        maxtries = 30,
+	# lowlevel = True,
     ),
 )
 
