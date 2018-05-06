956d920a32c946088e86d44ce8fdeea9bec8cdcd
Matthias Pomm <matthias.pomm@hzg.de>
2018-05-06 21:16:12 +0200

    REFSANS: change autocollimation device and setup name
    Change-Id: I5201a12191587c6caaa7ec430ff43e4db1c3a4a6
    Reviewed-on: https://forge.frm2.tum.de/review/17910
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/autocollimator.py
+++ nicos_mlz/refsans/setups/elements/autocollimator.py
@@ -1,4 +1,4 @@
-description = 'Trioptic autocollimator, water subtracted'
+description = 'autocollimator, water subtracted, vendor Trioptic'
 
 # not included by others
 group = 'optional'
@@ -8,21 +8,21 @@ all_lowlevel = False  # or True
 tango_host = 'tango://refsanshw.refsans.frm2:10000/test/triangle/io'
 
 devices = dict(
-    triangle = device('nicos_mlz.refsans.devices.triangle.TriangleMaster',
+    autocollimator = device('nicos_mlz.refsans.devices.triangle.TriangleMaster',
         description = description,
         tangodevice = tango_host,
         lowlevel = True,  # all_lowlevel,
         unit = '',
     ),
-    triangle_theta = device('nicos_mlz.refsans.devices.triangle.TriangleAngle',
-        description = description + ', triangle Y on PC',
+    autocollimator_theta = device('nicos_mlz.refsans.devices.triangle.TriangleAngle',
+        description = description + ', autocollimator Y on PC',
         lowlevel = all_lowlevel,
         index = 0,
         tangodevice = tango_host,
         unit = '',
     ),
-    triangle_phi = device('nicos_mlz.refsans.devices.triangle.TriangleAngle',
-        description = description + ', triangle X on PC',
+    autocollimator_phi = device('nicos_mlz.refsans.devices.triangle.TriangleAngle',
+        description = description + ', autocollimator X on PC',
         lowlevel = all_lowlevel,
         index = 1,
         tangodevice = tango_host,
