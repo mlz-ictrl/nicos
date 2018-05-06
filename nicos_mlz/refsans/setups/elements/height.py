cd542aafaeff21c5e3ac8c936a69a6584491b0aa
Matthias Pomm <matthias.pomm@hzg.de>
2018-05-06 21:16:13 +0200

    REFSANS: Change device and setup name for the sample height
    Change-Id: I5a39f01885c30e3f1bdfdd1a86f59f775b8f71ef
    Reviewed-on: https://forge.frm2.tum.de/review/17911
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/height.py
+++ nicos_mlz/refsans/setups/elements/height.py
@@ -5,7 +5,7 @@ group = 'optional'
 tango_base = 'tango://refsanshw.refsans.frm2:10000/refsans/'
 
 devices = dict(
-    keyence = device('nicos.devices.tango.Sensor',
+    height = device('nicos.devices.tango.Sensor',
         description = 'Sample surface position',
         tangodevice = tango_base + 'keyence/sensor',
         unit = 'mm',
