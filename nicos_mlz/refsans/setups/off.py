f89e815a9a01c5de3a69bbc3e4c29fe9782a936e
Matthias Pomm <matthias.pomm@hzg.de>
2018-05-06 21:16:11 +0200

    REFSANS: Add setup to read standard devices all the time
    Change-Id: Ic0c1d3a252cfcf5cf4f20f7c46928010c2737b86
    Reviewed-on: https://forge.frm2.tum.de/review/17909
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/refsans/setups/off.py
@@ -0,0 +1,17 @@
+description = 'REFSANS setup for off state just observe pressure and temperature'
+
+group = 'basic'
+
+includes = [
+    'guidehall',
+    'reactor',
+    'fak40',
+    'poti_ref',
+    'vsd',
+    'nl2b',
+    'pumpstand',
+    'memograph',
+]
+
+startupcode = """
+"""
