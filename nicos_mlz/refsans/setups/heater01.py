4456e4f8129e5262ff8ee24572af60006fddd68e
Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
2018-06-09 13:20:18 +0200

    REFSANS: provide setup file for stove
    which is controlled by a raspi-in-a-box and entangle.
    
    Change-Id: I85f8b1018faeab78bfd122981df1148afc8392dd
    Reviewed-on: https://forge.frm2.tum.de/review/18113
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/refsans/setups/heater01.py
@@ -0,0 +1,22 @@
+description = 'Refsans heater01 stove box'
+
+group = 'plugplay'
+
+includes = ['alias_T']
+
+tango_base = 'tango://%s:10000/box/omega/' % setupname
+
+devices = {
+    'T_%s' % setupname : device('nicos.devices.tango.TemperatureController',
+        description = 'Temperature of the stove',
+        tangodevice = tango_base + 'temperature',
+        abslimits = (0, 400),
+        unit = 'degC',
+        fmtstr = '%.1f',
+    ),
+}
+
+alias_config = {
+    'T':  {'T_%s' % setupname: 500},
+    'Ts':  {'T_%s' % setupname: 500},
+}
