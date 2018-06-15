b4bec903d0a6f70cd0dd6b3c475a5b44619b7ee9
Stefan Rainow <s.rainow@fz-juelich.de>
2018-06-15 19:27:35 +0200

    SPHERES: Shutter
    Added Shutterdevice which handles the shutterstate.
    It also displays upstream shutterstates.
    
    Change-Id: Icfc83dd87a787e1487cbddf763c901723a33f5c7
    Reviewed-on: https://forge.frm2.tum.de/review/16276
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Stefan Rainow <s.rainow@fz-juelich.de>

---
+++ nicos_mlz/spheres/devices/shutter.py
@@ -0,0 +1,107 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2018 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Stefan Rainow <s.rainow@fz-juelich.de>
+#
+# *****************************************************************************
+
+"""
+Device related to the shutter at SPHERES
+"""
+
+from nicos.core import status
+from nicos.core.mixins import HasTimeout
+from nicos.core.params import Attach, Param
+from nicos.devices.tango import NamedDigitalInput, NamedDigitalOutput
+
+
+class ShutterCluster( HasTimeout, NamedDigitalOutput):
+    '''
+    Combines the state of different shutters to one overall state.
+    Always displays closed if the instrument shutter is closed.
+    State is set to warning if the instrument shutter is open,
+    but upstream shutters are closed.
+    Status messages are set in the setup.
+    '''
+
+    CLOSED = 'close'
+    UPSTREAMCLOSED = 'upstream closed'
+    CLOSEDSTATES = [CLOSED, UPSTREAMCLOSED]
+
+    attached_devices = {
+        'upstream':
+            Attach('Upstreamshutters',
+                   NamedDigitalInput,
+                   multiple=True),
+    }
+
+    parameters = {
+        'statusmapping':
+            Param('Mapping for the status',
+                   type=dict),
+        'attachedmapping':
+            Param('Mapping for the upstream shutters',
+                  type=dict),
+    }
+
+    def doInit(self, mode):
+        NamedDigitalOutput.doInit(self, mode)
+
+        # just in case the statusmapping is reversed in the setup
+        self._statusmapping = dict((v, k)
+                                   for (k, v) in self.statusmapping.items())
+        self._statusmapping.update(self.statusmapping)
+
+        # 'constant' for comparison if all the shutters are open
+        self._allUpstreamOpen = 2**len(self._attached_upstream)-1
+
+    def getCurrentShutterState(self):
+        upstreamstate = 0
+        for i, shutter in enumerate(self._attached_upstream):
+            upstreamstate += self.attachedmapping[shutter.read()] << i
+
+        instrumentstate = self._dev.value
+
+        return (upstreamstate, instrumentstate)
+
+    def doRead(self, maxage=0):
+        upstream, instrument = self.getCurrentShutterState()
+
+        if not instrument:
+            # upstream state doesn't matter if the instrument shutter is closed
+            return self.CLOSED
+        elif upstream < self._allUpstreamOpen:
+            return self.UPSTREAMCLOSED
+
+        return 'open'
+
+    def doStatus(self, maxage=0):
+        upstream, instrument = self.getCurrentShutterState()
+
+        if instrument and upstream == self._allUpstreamOpen:
+            return status.OK, ''
+        elif not instrument:
+            return status.OK, 'shutter closed'
+
+        return (status.WARN, self._statusmapping[upstream])
+
+    def doReset(self):
+        NamedDigitalOutput.doReset(self)
+        self.move('close')
---
+++ nicos_mlz/spheres/setups/shutter.py
@@ -0,0 +1,31 @@
+description = 'setup for the shutter'
+
+group = 'optional'
+
+includes = ['sps']
+
+nethost = 'phys.spheres.frm2'
+shutter = 'tango://%s:10000/spheres/profibus/' % nethost
+
+devices = dict(
+    shutter = device('nicos_mlz.spheres.devices.shutter.ShutterCluster',
+        description = 'Display whether all the shutters leading to the sample '
+        'are open',
+        tangodevice = shutter + 'instrumentshutter',
+        upstream = ['upstream_shut1', 'upstream_shut2', 'upstream_shut3'],
+        mapping = {'open': 1,
+                   'close': 0},
+        attachedmapping = {'closed': 0,
+                           'open': 1},
+        timeout = 5,
+        statusmapping = {
+            0: 'closed: DNS, fast and 6-fold',
+            1: 'closed: DNS and fast',
+            2: 'closed: DNS and 6-fold',
+            3: 'closed: DNS',
+            4: 'closed: fast and 6-fold',
+            5: 'closed: fast',
+            6: 'closed: 6-fold',
+        },
+    ),
+)
