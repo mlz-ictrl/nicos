7859a0ae60cef15cff637c6d30d8611309c127b1
Matthias Pomm <matthias.pomm@hzg.de>
2018-06-19 10:09:33 +0200

    REFSANS: Fix small (top) goniometer setup
    - use Axis class for top_z to get the offset parameter
    - set defect top_phi device to lowlevel (hiding from user)
    
    Change-Id: I4ee8424b3936fd1112c73261e25826da3246dcc4
    Reviewed-on: https://forge.frm2.tum.de/review/18201
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/gonio_top.py
+++ nicos_mlz/refsans/setups/elements/gonio_top.py
@@ -1,4 +1,4 @@
-description = 'smal goniometer to adjust sample on gonio'
+description = 'small goniometer to adjust sample on gonio'
 
 group = 'optional'
 
@@ -11,14 +11,23 @@ devices = dict(
         tacodevice = '%s/phytron/kanal_10' % tacodev,
         abslimits = (-9.5, 10.5),
     ),
-    gonio_top_z = device('nicos.devices.taco.Motor',
+    gonio_top_z = device('nicos.devices.generic.Axis',
+        description = 'Top Z axis motor with offset',
+        motor = 'gonio_top_z_m',
+        coder = 'gonio_top_z_m',
+        precision = 0.01,
+        offset = 0.0
+    ),
+    gonio_top_z_m = device('nicos.devices.taco.Motor',
         description = 'Top Z axis motor',
         tacodevice = '%s/phytron/kanal_11' % tacodev,
         abslimits = (-0.05, 15),
+        lowlevel = True,
     ),
     gonio_top_phi = device('nicos.devices.taco.Motor',
-        description = 'Top Phi axis motor',
+        description = 'Top Phi axis motor, defect MP 04.06.2018 09:48:28',
         tacodevice = '%s/phytron/kanal_12' % tacodev,
         abslimits = (-10.5, 10.5),
+        lowlevel = True,
     ),
 )
