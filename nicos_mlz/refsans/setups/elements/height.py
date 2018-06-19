44b199c6de9ba0c3349d926acd75925174d5d09e
Matthias Pomm <matthias.pomm@hzg.de>
2018-06-19 10:09:31 +0200

    REFSANS: Add hint for offset of height device
    Change-Id: Ie4ac1580a7e2233e27d02c724df2747f17e3fdbb
    Reviewed-on: https://forge.frm2.tum.de/review/18200
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/height.py
+++ nicos_mlz/refsans/setups/elements/height.py
@@ -6,7 +6,8 @@ tango_base = 'tango://refsanshw.refsans.frm2:10000/refsans/'
 
 devices = dict(
     height = device('nicos.devices.tango.Sensor',
-        description = 'Sample surface position',
+        description = 'Sample surface position, offset in entangle. Access via '
+                      'quango.',
         tangodevice = tango_base + 'keyence/sensor',
         unit = 'mm',
     ),
