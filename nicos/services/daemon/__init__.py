99046d7e0800adbb7aa6fbca8d53d6a910c3c259
Georg Brandl <g.brandl@fz-juelich.de>
2018-06-25 09:04:46 +0200

    daemon: close sockets on "queue full"
    This catches instances where the client is stuck at the
    very beginning of the connection, before authentication.
    
    Fixes: #3840
    Change-Id: Ia842f0ea2fdb9c1b1bc52c5347e61adf74775867

--- nicos/services/daemon/__init__.py
+++ nicos/services/daemon/__init__.py
@@ -226,6 +226,7 @@ class NicosDaemon(Device):
                 self.log.warning('handler %s: queue full, closing socket',
                                  handler.ident)
                 closeSocket(handler.event_sock)
+                closeSocket(handler.sock)
 
     def emit_event_private(self, event, data):
         """Emit an event to only the calling handler."""
@@ -241,6 +242,7 @@ class NicosDaemon(Device):
                 self.log.warning('handler %s: queue full, closing socket',
                                  handler.ident)
                 closeSocket(handler.event_sock)
+                closeSocket(handler.sock)
 
     def clear_handlers(self):
         """Remove all handlers."""
