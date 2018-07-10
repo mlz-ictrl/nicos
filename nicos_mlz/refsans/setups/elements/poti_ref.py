ad049be4a7d1b95b36eacc7132116843e2bb8d49
Jens Krüger <jens.krueger@frm2.tum.de>
2018-07-10 14:04:00 +0200

    REFSANS: Change 'wegboxes' to TANGO devices
    Change-Id: I3355b68c4a530c9046355c03bd9d9ab13353f1fc
    Reviewed-on: https://forge.frm2.tum.de/review/18369
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/poti_ref.py
+++ nicos_mlz/refsans/setups/elements/poti_ref.py
@@ -2,32 +2,31 @@ description = 'reference values for the encoder potiometers'
 
 group = 'lowlevel'
 
-nethost = 'refsanssrv.refsans.frm2'
-tacodev = '//%s/test' % nethost
+tango_host = 'tango://refsanshw:10000/test/'
 
 devices = dict(
-    wegbox_A_1ref = device('nicos.devices.taco.AnalogInput',
+    wegbox_A_1ref = device('nicos.devices.tango.Sensor',
         description = 'wegbox_A_1ref',
-        tacodevice = '%s/WB_A/1_6' % tacodev,
+        tangodevice = tango_host + 'WB_A/1_6',
     ),
-    wegbox_A_2ref = device('nicos.devices.taco.AnalogInput',
+    wegbox_A_2ref = device('nicos.devices.tango.Sensor',
         description = 'wegbox_A_2ref',
-        tacodevice = '%s/WB_A/2_6' % tacodev,
+        tangodevice = tango_host + 'WB_A/2_6',
     ),
-    wegbox_B_1ref = device('nicos.devices.taco.AnalogInput',
+    wegbox_B_1ref = device('nicos.devices.tango.Sensor',
         description = 'wegbox_B_1ref',
-        tacodevice = '%s/WB_B/1_6' % tacodev,
+        tangodevice = tango_host + 'WB_B/1_6',
     ),
-    wegbox_B_2ref = device('nicos.devices.taco.AnalogInput',
+    wegbox_B_2ref = device('nicos.devices.tango.Sensor',
         description = 'wegbox_B_2ref',
-        tacodevice = '%s/WB_B/2_6' % tacodev,
+        tangodevice = tango_host + 'WB_B/2_6',
     ),
-    wegbox_C_1ref = device('nicos.devices.taco.AnalogInput',
+    wegbox_C_1ref = device('nicos.devices.tango.Sensor',
         description = 'wegbox_C_1ref',
-        tacodevice = '%s/WB_C/1_6' % tacodev,
+        tangodevice = tango_host + 'WB_C/1_6',
     ),
-    wegbox_C_2ref = device('nicos.devices.taco.AnalogInput',
+    wegbox_C_2ref = device('nicos.devices.tango.Sensor',
         description = 'wegbox_C_2ref',
-        tacodevice = '%s/WB_C/2_6' % tacodev,
+        tangodevice = tango_host + 'WB_C/2_6',
     ),
 )
