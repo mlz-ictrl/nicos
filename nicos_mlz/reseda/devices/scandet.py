9abd235a79f3ea34b05db1cc66c93a60bc84c3a0
Christian Franz <christian.franz@frm2.tum.de>
2018-07-19 11:14:53 +0200

    RESEDA: Save local changes in scandet implementation
    Change-Id: Ief7fb5bf13ba0b6ee426b89f71437bd78e49310a
    Reviewed-on: https://forge.frm2.tum.de/review/18476
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/reseda/devices/scandet.py
+++ nicos_mlz/reseda/devices/scandet.py
@@ -22,14 +22,15 @@
 #
 # *****************************************************************************
 
-import numpy as np
-
-from nicos.core import Param, Override, Value, floatrange, oneof
-from nicos.utils.fitting import curve_fit
+from nicos.core import Override, Param, Value, floatrange, oneof
 from nicos.devices.generic import ScanningDetector as NicosScanDet
+from nicos.utils.fitting import curve_fit
 
-#from nicos_mlz.mira.devices.cascade_win import CascadeDetector as MiraCascadeDetector
+# from nicos_mlz.mira.devices.cascade_win import CascadeDetector as \
+#     MiraCascadeDetector
 from nicos_mlz.mira.devices.cascade import CascadeDetector as MiraCascadeDetector
+
+import numpy as np
 
 
 def fit_a_sin(x, y):
@@ -39,9 +40,11 @@ def fit_a_sin(x, y):
     if len(y) == 1:
         startpar = [y[0], 0, 1, 0]
     else:
-        startpar = [sum(y)/len(y), len(y)*0.5*(max(y)-min(y))/sum(y), 0.38, 0]
+        startpar = [sum(y)/len(y), len(y)*0.5*(max(y)-min(y))/(sum(y)+1e-6),
+                    0.38, 0]
     if len(y) < 4:
         return startpar, perr, 'not enough data points for a fit, guessing'
+
     def model_sin(x, avg, contrast, freq, phase):
         return avg + avg * contrast * np.sin(freq * x + phase)
     try:
@@ -54,9 +57,10 @@ def fit_a_sin(x, y):
 
 
 class CascadeDetector(MiraCascadeDetector):
-    """Reseda version of the CascadeDetector
+    """Reseda version of the CascadeDetector.
 
-    adds fitting a sin to the timechannels in tof mode"""
+    adds fitting a sin to the timechannels in tof mode
+    """
 
     def doReadArray(self, quality):
         data = super(CascadeDetector, self).doReadArray(quality)
@@ -67,46 +71,61 @@ class CascadeDetector(MiraCascadeDetector):
         # XXX: needs rewrite/refactor of MiraCascadeDetector
 
         # demux timing into foil + timing
-        shaped = data.reshape(8,16,128,128) # foil, time, x, y
-        # note: we have to skip foils 3+4 (not fitted yet)
+
+        # XXX !!!!
+        shaped = data.reshape(8, 16, 128, 128)  # foil, time, x, y
+        # note: signals on foil 2,3,10,11,12,13
+
         x = []
         y = []
         perfoil = 6*[[]]
         # 'time' ranges from 0..15
         for t in range(16):
             _sum = 0
-            for foil in [5]:  # ???? should be [0,1,2,5,6,7] !
+            for foil in [7]:
                 # XXX: roi evaluation?
                 tf = np.sum(shaped[foil][t])
                 _sum += tf
             x.append(t)
             y.append(_sum)
-            for foil in range(6):
-                tf = np.sum(shaped[foil if foil < 3 else foil + 2][t])
-                perfoil[foil].append(tf)
+
+        perfoil = [[float(np.sum(shaped[foil][i])) for i in range(16)]
+                   for foil in [7, 6, 5, 0, 1, 2]]
+
         # ofs, ampl, freq, phase
         self.log.debug('fitting %r on %r' % (y, x))
         popt, perr, msg = fit_a_sin(x, y)
-        self.log.debug('result is %r +/- %r for [avg, contrast, freq, phase]' % (popt, perr))
+        self.log.debug('result is %r +/- %r for [avg, contrast, freq, phase]',
+                       popt, perr)
         if msg:
-            self.log.warning(msg)
-        self.readresult = list(self.readresult) + [abs(popt[1]), perr[1], popt[0], perr[0]]
-        if not msg:
-            # also fit per foil data and pack everything together to be send via cache for display
+            self.log.debug(msg)
+        self.readresult = list(self.readresult) + [abs(popt[1]), perr[1],
+                                                   popt[0], perr[0]]
+        if True:  # pylint: disable=using-constant-test
+            # also fit per foil data and pack everything together to be send
+            # via cache for display
             p = []
-            for foil in range(6):
-                popt, perr, msg = fit_a_sin(x, perfoil[foil])
-                p.append(list([popt[0], perr[0], popt[1], perr[1], popt[2], perr[2], popt[3], perr[3]] + perfoil[foil]))
+            for foil in perfoil:
+                popt, perr, msg = fit_a_sin(x, foil)
+                p.append([float(v) for v in list([popt[0], perr[0],
+                                                  popt[1], perr[1],
+                                                  popt[2], perr[2],
+                                                  popt[3], perr[3]]
+                                                 + foil)])
             self._cache.put(self.name, '_foildata', p, flag='#')
         return data
 
     def valueInfo(self):
         res = super(CascadeDetector, self).valueInfo()
         if self.mode == 'tof':
-            res = res + (Value('fit.contrast', unit='', type='other', errors='next', fmtstr='%.3f'),
-                         Value('fit.contrastErr', unit='', type='error', errors='none', fmtstr='%.3f'),
-                         Value('fit.avg', unit='', type='other', errors='next', fmtstr='%.1f'),
-                         Value('fit.avgErr', unit='', type='error', errors='none', fmtstr='%.1f'))
+            res = res + (Value('fit.contrast', unit='', type='other',
+                               errors='next', fmtstr='%.3f'),
+                         Value('fit.contrastErr', unit='', type='error',
+                               errors='none', fmtstr='%.3f'),
+                         Value('fit.avg', unit='', type='other',
+                               errors='next', fmtstr='%.1f'),
+                         Value('fit.avgErr', unit='', type='error',
+                               errors='none', fmtstr='%.1f'))
         return res
 
 
@@ -122,7 +141,7 @@ class ScanningDetector(NicosScanDet):
                           userparam=True),
         'echostart': Param('starting current value for the phase coil',
                            type=float, default=0, userparam=True,
-                           settable=True)
+                           settable=True),
     }
 
     parameter_overrides = {
@@ -137,7 +156,7 @@ class ScanningDetector(NicosScanDet):
                     roivalues = []
                     scanvalues = []
                     for subset in dataset.subsets:
-                        for i,val in enumerate(subset.detvalueinfo):
+                        for i, val in enumerate(subset.detvalueinfo):
                             if val.name.endswith('.roi'):
                                 roivalues.append(subset.detvaluelist[i])
                                 break
@@ -152,7 +171,7 @@ class ScanningDetector(NicosScanDet):
             x = []
             y = []
             for subset in dataset.subsets:
-                for i,val in enumerate(subset.detvalueinfo):
+                for i, val in enumerate(subset.detvalueinfo):
                     if val.name == ctr.name:
                         y.append(subset.detvaluelist[i])
                         x.append(subset.devvaluelist[0])
@@ -175,14 +194,22 @@ class ScanningDetector(NicosScanDet):
         res = []
         for imgdet in self._attached_detector._attached_images:
             if getattr(imgdet, 'mode', None) in ['image']:
-                return (Value('fit.contrast', unit='', type='other', errors='next', fmtstr='%.3f'),
-                        Value('fit.contrastErr', unit='', type='error', errors='none', fmtstr='%.3f'),
-                        Value('fit.avg', unit='', type='other', errors='next', fmtstr='%.1f'),
-                        Value('fit.avgErr', unit='', type='error', errors='none', fmtstr='%.1f'))
+                return (Value('fit.contrast', unit='', type='other',
+                              errors='next', fmtstr='%.3f'),
+                        Value('fit.contrastErr', unit='', type='error',
+                              errors='none', fmtstr='%.3f'),
+                        Value('fit.avg', unit='', type='other', errors='next',
+                              fmtstr='%.1f'),
+                        Value('fit.avgErr', unit='', type='error',
+                              errors='none', fmtstr='%.1f'))
         res = []
         for ctr in self._attached_detector._attached_counters:
-            res.append(Value('%s.fit.contrast' % ctr.name, unit=ctr.unit, type='other', errors='next', fmtstr='%.3f'))
-            res.append(Value('%s.fit.contrastErr' % ctr.name, unit=ctr.unit, type='error', errors='none', fmtstr='%.3f'))
-            res.append(Value('%s.fit.avg' % ctr.name, unit=ctr.unit, type='other', errors='next', fmtstr='%.1f'))
-            res.append(Value('%s.fit.avgErr' % ctr.name, unit=ctr.unit, type='error', errors='none', fmtstr='%.1f'))
+            res.append(Value('%s.fit.contrast' % ctr.name, unit=ctr.unit,
+                             type='other', errors='next', fmtstr='%.3f'))
+            res.append(Value('%s.fit.contrastErr' % ctr.name, unit=ctr.unit,
+                             type='error', errors='none', fmtstr='%.3f'))
+            res.append(Value('%s.fit.avg' % ctr.name, unit=ctr.unit,
+                             type='other', errors='next', fmtstr='%.1f'))
+            res.append(Value('%s.fit.avgErr' % ctr.name, unit=ctr.unit,
+                             type='error', errors='none', fmtstr='%.1f'))
         return tuple(res)
