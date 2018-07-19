94dfdb8e0d8f64660a8ba51845628367b68fb1e5
Jens Krüger <jens.krueger@frm2.tum.de>
2018-07-19 10:50:57 +0200

    RESEDA: Fix some formatting issues
    Change-Id: I5d0609240937f66879d50e37702fe7963a7919f2
    Reviewed-on: https://forge.frm2.tum.de/review/18475
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/reseda/commands.py
+++ nicos_mlz/reseda/commands.py
@@ -27,16 +27,15 @@
 from nicos import session
 from nicos.commands import usercommand
 from nicos.commands.device import move, stop, wait
-from nicos.commands.scan import manualscan
 from nicos.commands.measure import count
-#from nicos.commands.device import maw
+from nicos.commands.scan import manualscan
 
 __all__ = ['zero', 'setecho', 'set_cascade', 'pol', 'miezescan']
 
 
 @usercommand
 def zero():
-    """Shutting down all (static) power supplies"""
+    """Shutting down all (static) power supplies."""
     ps = ['hrf_0a', 'hrf_0b', 'hrf_1', 'hsf_0a', 'hsf_0b', 'hsf_1', 'sf_0a',
           'sf_0b', 'sf_1', 'gf0', 'gf1', 'gf2', 'gf4', 'gf5', 'gf6', 'gf7',
           'gf8', 'gf9', 'gf10']
@@ -64,7 +63,7 @@ def set_flipper_off():
 
 @usercommand
 def setecho(time):
-    '''Wrapper for setting a echotime'''
+    """Wrap setting of an echotime."""
     echotime = session.getDevice('echotime')
     move(echotime, time)
     set_cascade()
@@ -73,7 +72,7 @@ def setecho(time):
 
 @usercommand
 def set_cascade():
-    """Set Cascade Frequency Generator Freqs and Trigger"""
+    """Set Cascade Frequency Generator Freqs and Trigger."""
     echotime = session.getDevice('echotime')
     psd_chop_freq = session.getDevice('psd_chop_freq')
     psd_timebin_freq = session.getDevice('psd_timebin_freq')
@@ -81,10 +80,11 @@ def set_cascade():
     tau = echotime.target
     f1 = echotime.currenttable[tau]['cbox_0a_fg_freq']
     f2 = echotime.currenttable[tau]['cbox_0b_fg_freq']
-    move(psd_chop_freq, 2*(f2-f1))
-    move(psd_timebin_freq, 32*(f2-f1))
+    move(psd_chop_freq, 2 * (f2 - f1))
+    move(psd_timebin_freq, 32 * (f2 - f1))
     move(fg_burst, 'arm')
     move(fg_burst, 'trigger')
+
 
 @usercommand
 def miezescan(echolist, counttime):
@@ -95,7 +95,8 @@ def miezescan(echolist, counttime):
             set_cascade()
             count(counttime)
 
+
 @usercommand
 def pol(up, down):
-    '''Calculate contrast or polarisation'''
-    return (up-down)/(up+down)
+    """Calculate contrast or polarisation."""
+    return (up - down) / (up + down)
