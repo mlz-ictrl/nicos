140dd0b473e9e549fc6aa5dea5e72c67156ef5bc
Alexander Steffens <a.steffens@fz-juelich.de>
2018-08-09 08:09:08 +0200

    [GALAXI] updates from instrument
    Change-Id: I6484b4d5282e114edef03f80182178fac3cbdbc5
    Reviewed-on: https://forge.frm2.tum.de/review/18598
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos_mlz/galaxi/setups/notifiers.py
+++ nicos_mlz/galaxi/setups/notifiers.py
@@ -1,17 +1,18 @@
-description = 'Email and SMS notifiers'
+description = 'Email notifiers'
 
 group = 'lowlevel'
 
 devices = dict(
     email = device('nicos.devices.notifiers.Mailer',
-        description = 'E-Mail notifier',
+        description = 'Reports via email',
         mailserver = 'mail.fz-juelich.de',
-        sender = 'noreply@fz-juelich.de',
+        sender = 'galaxi@fz-juelich.de',
         copies = [
             ('u.ruecker@fz-juelich.de', 'all'),
             ('e.kentzinger@fz-juelich.de', 'important'),
+            ('a.steffens@fz-juelich.de', 'all'),
             ('l.fleischhauer-fuss@fz-juelich.de', 'important'),
         ],
-        subject = '[NICOS] GALAXI',
+        subject = '[GALAXI]',
     ),
 )
