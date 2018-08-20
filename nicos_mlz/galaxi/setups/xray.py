79c6f2f9e2b1006d286ba005724f042e4964f6c2
Alexander Steffens <a.steffens@fz-juelich.de>
2018-08-20 12:33:49 +0200

    GALAXI: adapt s7 motor names to entangle server configuration
    Depends-On: Iee2f274cf14426cd8eb447c180906e4425c8b600
    Change-Id: If502170ce00f9d05c532240f6defd2a979947db0
    Reviewed-on: https://forge.frm2.tum.de/review/18677
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Alexander Steffens <a.steffens@fz-juelich.de>
    Tested-by: Alexander Steffens <a.steffens@fz-juelich.de>

--- nicos_mlz/galaxi/setups/xray.py
+++ nicos_mlz/galaxi/setups/xray.py
@@ -1,51 +1,52 @@
 # -*- coding: utf-8 -*-
 
-description = 'GALAXI motors'
+description = 'GALAXI x-ray beam alignment motors'
 
 group = 'optional'
 
 display_order = 7
 
 tango_base = 'tango://phys.galaxi.kfa-juelich.de:10000/galaxi/'
+s7_motor = tango_base + 's7_motor/'
 
 devices = dict(
     roy = device('nicos_mlz.jcns.devices.motor.Motor',
         description = 'ROY axis',
-        tangodevice = tango_base + 'fzjs7/ROY',
+        tangodevice = s7_motor + 'roy',
         offset = 0,
         precision = 0.01,
         userlimits = (-5000, 5000),
     ),
     roz = device('nicos_mlz.jcns.devices.motor.Motor',
         description = 'ROZ axis',
-        tangodevice = tango_base + 'fzjs7/ROZ',
+        tangodevice = s7_motor + 'roz',
         offset = 0,
         precision = 0.01,
         userlimits = (-5000, 5000),
     ),
     dofchi = device('nicos_mlz.jcns.devices.motor.Motor',
         description = 'DOFChi axis',
-        tangodevice = tango_base + 'fzjs7/DOFChi',
+        tangodevice = s7_motor + 'dofchi',
         offset = 0,
         precision = 0.01,
         userlimits = (-50, 50),
     ),
     dofom = device('nicos_mlz.jcns.devices.motor.Motor',
         description = 'DOFOm axis',
-        tangodevice = tango_base + 'fzjs7/DOFOm',
+        tangodevice = s7_motor + 'dofom',
         offset = 0,
         precision = 0.01,
         userlimits = (-50, 50),
     ),
     roby = device('nicos_mlz.jcns.devices.motor.Motor',
         description = 'ROBY axis',
-        tangodevice = tango_base + 'fzjs7/ROBY',
+        tangodevice = s7_motor + 'roby',
         offset = 0,
         precision = 0.01,
     ),
     robz = device('nicos_mlz.jcns.devices.motor.Motor',
         description = 'ROBZ axis',
-        tangodevice = tango_base + 'fzjs7/ROBZ',
+        tangodevice = s7_motor + 'robz',
         offset = 0,
         precision = 0.01,
     ),
