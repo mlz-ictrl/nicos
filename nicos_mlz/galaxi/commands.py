4275a885fe81f93ef137bc546b68aa3d42b5af01
Alexander Steffens <a.steffens@fz-juelich.de>
2018-08-31 13:19:35 +0200

    GALAXI: add voice ouput device to welcome new users
    Change-Id: I8d9558b551ee9317985dbbb742c9042a170d1233
    Reviewed-on: https://forge.frm2.tum.de/review/18762
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

--- nicos_mlz/galaxi/commands.py
+++ nicos_mlz/galaxi/commands.py
@@ -18,30 +18,64 @@
 # 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 # Module authors:
+#
+#   Lydia Fleischhauer-Fuss <l.fleischhauer-fuss@fz-juelich.de>
 #   Alexander Steffens <a.steffens@fz-juelich.de>
-#   L.Fleischhauer-Fuss <l.fleischhauer-fuss@fz-juelich.de>
 #
 # *****************************************************************************
 
 """Module for GALAXI specific commands."""
 
 from nicos import session
-from nicos.commands import usercommand, helparglist
-from nicos.commands.measure import count as nicos_count
+from nicos.commands import basic, device, helparglist, measure, usercommand
 from nicos_mlz.galaxi.devices.pilatus import PilatusDetector
 
 
 @usercommand
-@helparglist('number of rewrites, [presets]')
-def count(num=1, **preset):
-    """GALAXI specific command: Count with Pilatus detector and rewrite image"""
+@helparglist('proposal, title, localcontact, ...')
+def NewExperiment(proposal, title='', localcontact='', user='', **parameters):
+    """Start a new experiment with the given proposal number and title and
+    welcome the new user with the GALAXI voice output device.
 
+    You should also give a argument for the local contact and the primary user.
+    More users can be added later with `AddUser`.  Example:
+
+    >>> NewExperiment(541, 'Spin waves', 'L. Contact', 'F. User <user@abc.de>')
+
+    When configured, proposal information will be automatically filled in from
+    the proposal database.
+    """
+    if user:
+        voice_output = session.getDevice('voice_output')
+        device.move(voice_output, voice_output.read(0) ^ 1)
+        session.log.info('Welcome to GALAXI, %s!', user)
+    basic.NewExperiment(proposal, title, localcontact, user, **parameters)
+
+
+@usercommand
+@helparglist('[number of counts], [preset, ...]')
+def count(n=1, **presets):
+    """Perform ``n`` counts with all default detectors set with
+    `SetDetectors()`.
+
+    A temporary filename will be set when using DECTRIS Pilatus detector in
+    order to be able to overwrite the TIFF images.
+
+    Examples:
+
+    >>> count()           # count once with the default preset and detectors
+    >>> count(100)        # perform 100 counts without changing presets
+    >>> count(t=10)       # count once with time preset of 10 seconds
+    >>> count(10, t=60)  # perform 10 counts with time preset of 60 seconds
+
+    :param int num: number of counts to be executed
+    :param presets: presets to be used when counting
+    """
     exp = session.experiment
-    preset['temporary'] = True
-    for _ in range(num):
+    presets['temporary'] = True
+    for _ in range(n):
         for _det in exp.detectors:
             if isinstance(_det, PilatusDetector):
                 _det.nextfilename = 'tmpcount.tif'
-        nicos_count(**preset)
-        # allow daemon to stop here
-        session.breakpoint(2)
+        measure.count(**presets)
+        session.breakpoint(2)  # allow daemon to stop here
