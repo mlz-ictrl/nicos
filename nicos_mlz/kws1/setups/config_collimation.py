f52c33fa2859e9a366c6e563b8324dc2a763ee7f
Georg Brandl <g.brandl@fz-juelich.de>
2018-09-10 09:55:16 +0200

    KWS1: setup updates
    Change-Id: I52246886996904b6276b3f2adcbf1d1a7663f8c7
    Reviewed-on: https://forge.frm2.tum.de/review/18824
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos_mlz/kws1/setups/config_collimation.py
+++ nicos_mlz/kws1/setups/config_collimation.py
@@ -2,15 +2,17 @@ description = 'presets for the collimation guides and apertures'
 group = 'configdata'
 
 COLLIMATION_PRESETS = {
-    '2m (30x30)':   dict(guides=2,  ap_x=30,   ap_y=30),
-    '4m (30x30)':   dict(guides=4,  ap_x=30,   ap_y=30),
+#    '2m (30x30)':   dict(guides=2,  ap_x=30,   ap_y=30),
+#    '4m (30x30)':   dict(guides=4,  ap_x=30,   ap_y=30),
     '4m (50x50)':   dict(guides=4,  ap_x=49.9, ap_y=49.9),
     '8m (30x30)':   dict(guides=8,  ap_x=30,   ap_y=30),
-    '8m (6x6) Tr':  dict(guides=8,  ap_x=6,    ap_y=6),
+    '8m (2x2) Tr':  dict(guides=8,  ap_x=2,    ap_y=2),
     '8m (50x50)':   dict(guides=8,  ap_x=50,   ap_y=50),
-    '14m (30x30)':  dict(guides=14, ap_x=30,   ap_y=30),
-    '20m (30x30)':  dict(guides=20, ap_x=30,   ap_y=30),
-    '2m (50x50)':   dict(guides=2,  ap_x=50,   ap_y=50),
+#    '14m (30x30)':  dict(guides=14, ap_x=30,   ap_y=30),
+    '14m (50x50)':  dict(guides=14, ap_x=50,   ap_y=50),
+#    '20m (30x30)':  dict(guides=20, ap_x=30,   ap_y=30),
+    '20m (50x50)':  dict(guides=20, ap_x=50,   ap_y=50),
+#    '2m (50x50)':   dict(guides=2,  ap_x=50,   ap_y=50),
     '2m (40x1.3) GIWANS': dict(guides=2,  ap_x=40, ap_y=1.3),
     '4m (10x15) GISANS':  dict(guides=4,  ap_x=10, ap_y=15),
     '4m (5x48) GISANS':   dict(guides=4,  ap_x=5,  ap_y=48),
@@ -28,5 +30,5 @@ COLLIMATION_PRESETS = {
     '20m (10x15) GISANS': dict(guides=20, ap_x=10, ap_y=15),
     '20m (5x48) GISANS':  dict(guides=20, ap_x=5,  ap_y=48),
     '20m (2x48) GISANS':  dict(guides=20, ap_x=2,  ap_y=48),
-    '8m (17x17) as C14':  dict(guides=8,  ap_x=17, ap_y=17),
+#    '8m (17x17) as C14':  dict(guides=8,  ap_x=17, ap_y=17),
 }
--- nicos_mlz/kws1/setups/config_selector.py
+++ nicos_mlz/kws1/setups/config_selector.py
@@ -2,13 +2,13 @@ description = 'preset values for the velocity selector'
 group = 'configdata'
 
 SELECTOR_PRESETS = {
-    '5A':  dict(lam=5,  spread=0.1, speed=26730.0),
-    '6A':  dict(lam=6,  spread=0.1, speed=22275.0),
-    '7A':  dict(lam=7,  spread=0.1, speed=19092.6),
-    '8A':  dict(lam=8,  spread=0.1, speed=16706.4),
-    '9A':  dict(lam=9,  spread=0.1, speed=14850.0),
-    '10A': dict(lam=10, spread=0.1, speed=13365.0),
-    '11A': dict(lam=11, spread=0.1, speed=12150.0),
-    '12A': dict(lam=12, spread=0.1, speed=11137.2),
-    '20A': dict(lam=20, spread=0.1, speed=6682.5),
+    '5A':  dict(lam=5,  spread=0.1, speed=26730.0), #speed=26360.5
+    '6A':  dict(lam=6,  spread=0.1, speed=21967.1), #old 22275
+    '7A':  dict(lam=7,  spread=0.1, speed=18828.9), #old 19092.6
+    '8A':  dict(lam=8,  spread=0.1, speed=16475.3), #old 16706.4
+#    '9A':  dict(lam=9,  spread=0.1, speed=14850.0),
+    '10A': dict(lam=10, spread=0.1, speed=13180.2), #old 13365.0
+    '11A': dict(lam=11, spread=0.1, speed=11982.0), #old 12150.0
+    '12A': dict(lam=12, spread=0.1, speed=10983.5), #old 11137.2
+    '20A': dict(lam=20, spread=0.1, speed=6590.1), #old 6682.5
 }
