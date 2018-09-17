e6266c88c352a5d2b34c8b713005b09205e18529
Dominic Oram <dominic.oram@stfc.ac.uk>
2018-09-17 11:52:24 +0100

    Added ISIS specific set up
    Change-Id: If09c41fdcc3b288e768c1c40faba57274086afb7
    Reviewed-on: https://forge.frm2.tum.de/review/18861
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_isis/ibex/setups/special/daemon.py
@@ -0,0 +1,44 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2018 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Dominic Oram <dominic.oram@stfc.ac.uk>
+#
+# *****************************************************************************
+
+description = 'Setup for the Ibex execution daemon'
+group = 'special'
+
+devices = dict(
+    Auth=device('nicos.services.daemon.auth.list.Authenticator',
+                hashing='sha1',
+                # first entry is the user name, second the hashed password, third the user level
+                passwd=[('ibex', 'a2eed0a7fcb214a497052435191b5264cca5b687', 'admin')],
+                ),
+    Daemon=device('nicos.services.daemon.NicosDaemon',
+                  # Set the server to 'localhost' to only allow traffic from the local machine
+                  # Set the server to the machine's IP to allow traffic from other machines
+                  # Default port is 1301
+                  server='127.0.0.1',
+                  authenticators=['Auth'],
+                  loglevel='info',
+                  serializercls='nicos.protocols.daemon.classic.JsonSerializer',
+                  servercls='nicos.services.daemon.proto.zeromq.Server',
+                  ),
+)
---
+++ nicos_isis/ibex/setups/system.py
@@ -0,0 +1,40 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2018 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Dominic Oram <dominic.oram@stfc.ac.uk>
+#
+# *****************************************************************************
+
+description = 'Ibex System setup'
+
+group = 'lowlevel'
+
+modules = ['nicos.commands.basic', 'nicos_isis.ibex.genie_commands']
+
+devices = dict(
+    # NOTE: removing the FreeSpace device doens't hurt the daemon functionality.
+    # However, the daemon seems to die slightly more gracefully on CTRL+C when
+    # this device is present
+    Space = device('nicos.devices.generic.FreeSpace',
+                   description = 'The amount of free space for storing data',
+                   path = None,
+                   minfree = 5,
+                  ),
+)
