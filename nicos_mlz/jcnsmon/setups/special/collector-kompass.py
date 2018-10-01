1ddc15c2ecac22ec6d8600f7e1e5b01273a16a3b
Christian Felder <c.felder@fz-juelich.de>
2018-10-01 16:32:04 +0200

    jcnsmon: Monitor j-nse magnet field sensors and kompass stt movement.
    Change-Id: I78d088bfa2c26358f1e68f36dca3c24308d6439f
    Reviewed-on: https://forge.frm2.tum.de/review/19017
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

---
+++ nicos_mlz/jcnsmon/setups/special/collector-kompass.py
@@ -0,0 +1,14 @@
+description = 'setup for the NICOS collector service'
+group = 'special'
+
+devices = dict(
+    GlobalCache = device('nicos.services.collector.CacheForwarder',
+        cache = 'localhost',
+        prefix = 'nicos/kompass',
+    ),
+    Collector = device('nicos.services.collector.Collector',
+        cache = 'kompassctrl.kompass.frm2',
+        forwarders = ['GlobalCache'],
+        keyfilters = ['stt.*'],
+    ),
+)
