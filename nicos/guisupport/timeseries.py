10d2dc9278d9a60c726c934f8a3e88c16f8a4b63
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2018-10-02 06:51:22 +0200

    guisupport/timeseries: future division
    remove now unneeded explicit casts
    
    Change-Id: If54558e47d32c39258fbc490c25c737e3b0a037c

--- nicos/guisupport/timeseries.py
+++ nicos/guisupport/timeseries.py
@@ -27,7 +27,7 @@
 NICOS value plot widget.
 """
 
-from __future__ import absolute_import
+from __future__ import absolute_import, division
 
 import functools
 import operator
@@ -55,13 +55,13 @@ def buildTickDistAndSubTicks(mintime, maxtime, minticks=3):
     # calculate maxticks, depends on 'good' values
     maxticks = minticks
     for i in range(len(good)-1):
-        if maxticks < minticks * good[i+1]/float(good[i]):
-            maxticks = minticks * good[i+1]/float(good[i])
+        if maxticks < minticks * good[i+1]/good[i]:
+            maxticks = minticks * good[i+1]/good[i]
 
     # determine ticking range
     length = maxtime - mintime
-    maxd = length / float(minticks)
-    mind = length / float(maxticks+1)
+    maxd = length / minticks
+    mind = length / (maxticks+1)
 
     # scale to useful numbers
     scale_ind = 0
@@ -86,7 +86,7 @@ def buildTickDistAndSubTicks(mintime, maxtime, minticks=3):
     subticks = 0
     for i, d in enumerate(good):
         if mind * scale <= d <= maxd * scale:
-            tickdist = d / float(scale)
+            tickdist = d / scale
             subticks = divs[i]
 
     # check ticking
