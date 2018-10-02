1223aecffd627f4464a7272824180565f41e7889
Björn Pedersen <bjoern.pedersen@frm2.tum.de>
2018-10-02 08:58:53 +0200

    frm2/commands: add absolute import
    Change-Id: Icbdbb311247b7ae5c1feae79462d3a795ab03f2e

--- nicos_mlz/frm2/commands/imaging.py
+++ nicos_mlz/frm2/commands/imaging.py
@@ -22,6 +22,8 @@
 #
 # *****************************************************************************
 
+from __future__ import absolute_import
+
 from os.path import relpath
 
 from nicos import session
