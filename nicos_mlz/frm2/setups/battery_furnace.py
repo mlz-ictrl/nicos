3773b3267f9184c031e706ec0adf3b56e87fbf5f
Jens Krüger <jens.krueger@tum.de>
2018-10-05 20:59:31 +0200

    FRM2: move the battery_furnace setup from ANTARES
    Change-Id: Ib623b084eac359761c827269498b7fccea95c323
    Reviewed-on: https://forge.frm2.tum.de/review/19091
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/frm2/setups/battery_furnace.py
+++ nicos_mlz/frm2/setups/battery_furnace.py
@@ -3,17 +3,19 @@ group = 'optional'
 
 includes = ['alias_T']
 
+nethost = 'mpfc01'
+
 devices = dict(
     T_bat_A = device('nicos.devices.taco.TemperatureController',
         description = 'CCR0 temperature regulation',
-        tacodevice = '//172.25.20.210/mpfc/ls340/control1',
+        tacodevice = '//%s/mpfc/ls340/control1' % nethost,
         pollinterval = 1,
         maxage = 6,
         abslimits = (0, 500),
     ),
     T_bat_B = device('nicos.devices.taco.TemperatureController',
         description = 'CCR0 temperature regulation',
-        tacodevice = '//172.25.20.210/mpfc/ls340/control2',
+        tacodevice = '//%s/mpfc/ls340/control2' % nethost,
         pollinterval = 1,
         maxage = 6,
         abslimits = (0, 500),
