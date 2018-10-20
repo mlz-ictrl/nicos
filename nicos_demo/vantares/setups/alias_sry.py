3b1384b44dbec8a1decb6d699cd0e0899618ff6a
Jens Krüger <jens.krueger@tum.de>
2018-10-20 10:50:36 +0200

    DEMO/VANTARES: add the 'sry' alias setting
    The 'sry' device is the default device for the 'tomo' command.
    
    Change-Id: I5a0d9a88fb7fee48a8c28d17d595708ba49f57fd
    Reviewed-on: https://forge.frm2.tum.de/review/19246
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_demo/vantares/setups/alias_sry.py
@@ -0,0 +1,7 @@
+description = 'sry environment alias for tomo rotation device'
+
+group = 'lowlevel'
+
+devices = dict(
+    sry = device('nicos.devices.generic.DeviceAlias'),
+)
--- nicos_demo/vantares/setups/antares_s.py
+++ nicos_demo/vantares/setups/antares_s.py
@@ -1,4 +1,4 @@
 description = 'Basic Setup for Experiments in Experimental Chamber 1'
 group = 'basic'
 
-includes = ['basic', 'sbl', 'huber', 'detector_translation']
+includes = ['basic', 'sbl', 'huber', 'detector_translation', 'alias_sry']
