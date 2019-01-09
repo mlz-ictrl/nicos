57ab4f89d90aa5b76083b151b7d393bfa9d486a4
Jens Krüger <jens.krueger@frm2.tum.de>
2019-01-09 08:50:34 +0100

    VANTARES: add second detector setup (ikonl)
    Change-Id: Ic02708f747bb4d2929d38211179198028913aacb
    Reviewed-on: https://forge.frm2.tum.de/review/19698
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_demo/vantares/setups/detector_ikonl.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+description = 'Andor IKON-L CCD camera emulation'
+
+group = 'optional'
+
+devices = dict(
+    timer_ikonl = device('nicos.devices.generic.VirtualTimer',
+        description = 'The camera\'s internal timer',
+        lowlevel = True,
+    ),
+    ikonl = device('nicos.devices.generic.VirtualImage',
+        description = 'Demo 2D detector',
+        fmtstr = '%d',
+        sizes = (1024, 1024),
+        lowlevel = True,
+    ),
+    temp_ikonl = device('nicos.devices.generic.VirtualTemperature',
+        description = 'The CCD chip temperature',
+        abslimits = (-100, 0),
+        warnlimits = (None, 0),
+        speed = 6,
+        unit = 'degC',
+        maxage = 5,
+        fmtstr = '%.0f',
+    ),
+    det_ikonl = device('nicos.devices.generic.Detector',
+        description = 'The Andor Ikon L CCD camera detector',
+        images = ['ikonl'],
+        timers = ['timer_ikonl'],
+    ),
+)
+
+startupcode = """
+SetDetectors(det_ikonl)
+"""
