778dc9f1623ac7ce76ec0ceb1622064673991e3a
Georg Brandl <g.brandl@fz-juelich.de>
2019-01-15 13:01:50 +0100

    POLI: add special code for nuclear physics setup
    Change-Id: I6ec7fc4dab3aee1f5057bd1927635abf18d89608
    Reviewed-on: https://forge.frm2.tum.de/review/19838
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ nicos_mlz/poli/devices/amplscan.py
@@ -0,0 +1,81 @@
+from nicos import session
+from nicos.core import SubscanMeasurable, Attach, Param, listof, \
+    Measurable, anytype, Value
+from nicos.core.scan import Scan
+from nicos.utils.fitting import CosineFit
+from nicos.commands import usercommand
+from nicos.commands.analyze import fit, _getData
+
+
+@usercommand
+def amplscan(dev1, start1, step1, numpoints1,
+             dev2, start2, step2, numpoints2,
+             t):
+    scandet = session.getDevice('scandet')
+    scandet.scandev = str(dev2)
+    scandet.positions = [start2 + i*step2 for i in range(numpoints2)]
+    dev1pos = [[start1 + i*step1] for i in range(numpoints1)]
+    ds = Scan([session.getDevice(dev1)], dev1pos, None, detlist=[scandet],
+              preset={'t': t}).run()
+    fit(CosineFit, 'A', dataset=ds)
+
+
+class ScanningDetector(SubscanMeasurable):
+    """Generic "super" detector that scans over a configured device and counts
+    with a given detector."""
+
+    attached_devices = {
+        'detector': Attach('Detector to scan', Measurable),
+    }
+
+    parameters = {
+        'scandev':    Param('Current device to scan', type=str, settable=True),
+        'positions':  Param('Positions to scan over', type=listof(anytype),
+                            settable=True),
+        'readresult': Param('Storage for processed results from detector, to'
+                            'be returned from doRead()', type=listof(anytype),
+                            settable=True, userparam=False),
+    }
+
+    fitcls = CosineFit
+
+    def doInit(self, mode):
+        self._preset = None
+        self._nparams = len(self.fitcls.fit_params)
+
+    def doSetPreset(self, **preset):
+        self._preset = preset
+
+    def doStart(self):
+        positions = [[p] for p in self.positions]
+        ds = Scan(
+            [session.getDevice(self.scandev)],
+            positions, None, detlist=[self._attached_detector],
+            preset=self._preset, subscan=True
+        ).run()
+        xs, ys, dys, _, ds = _getData()
+        fit = self.fitcls()
+        res = fit.run(xs, ys, dys)
+        if res._failed:
+            self.log.warning('Fit failed: %s.' % res._message)
+            self.readresult = [0] * (self._nparams * 2)
+        else:
+            session.notifyFitCurve(ds, fit.fit_title, res.curve_x, res.curve_y)
+            readres = []
+            for par, err in zip(res._pars[1], res._pars[2]):
+                readres.append(par)
+                readres.append(err)
+            self.readresult = readres
+
+    def valueInfo(self):
+        res = []
+        for p in self.fitcls.fit_params:
+            res.append(Value(p, type='other', errors='next'))
+            res.append(Value('d' + p, type='error'))
+        return tuple(res)
+
+    def doRead(self, maxage=0):
+        return self.readresult
+
+    def doFinish(self):
+        pass
---
+++ nicos_mlz/poli/nuclear_commands.py
@@ -0,0 +1,38 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2018 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Georg Brandl <g.brandl@fz-juelich.de>
+#
+# *****************************************************************************
+
+"""Nuclear physics specific commands for POLI."""
+
+from nicos.commands import usercommand
+from nicos.commands.analyze import fit
+from nicos.utils.fitting import CosineFit
+
+__all__ = [
+    'sinfit',
+]
+
+# pylint: disable=keyword-arg-before-vararg
+@usercommand
+def sinfit(xcol='ctr1', *args):
+    return fit(CosineFit, xcol, *args)
