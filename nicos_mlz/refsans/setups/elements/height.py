73ed2076e98eac56f7086fbb164a6f1b36cc5f1b
Matthias Pomm <matthias.pomm@hzg.de>
2019-01-18 16:36:05 +0100

    REFSANS: add sample height positioning system
    Change-Id: Ic07b19763f4937ccb02cc839d7e232f3a58c4342
    Reviewed-on: https://forge.frm2.tum.de/review/19779
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/height.py
+++ nicos_mlz/refsans/setups/elements/height.py
@@ -5,10 +5,15 @@ group = 'optional'
 tango_base = 'tango://refsanshw.refsans.frm2:10000/refsans/'
 
 devices = dict(
-    height = device('nicos.devices.tango.Sensor',
-        description = 'Sample surface position, offset in entangle. Access via '
-                      'quango.',
+    height = device('nicos_mlz.refsans.devices.tristate.TriState',
+        description = 'Sample surface position.',
+        unit = 'mm',
+        port = 'height_port',
+    ),
+    height_port = device('nicos.devices.tango.Sensor',
+        description = 'Sample surface position',
         tangodevice = tango_base + 'keyence/sensor',
         unit = 'mm',
+        lowlevel = True,
     ),
 )
