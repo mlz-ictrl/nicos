b0cf0e7daee34e5599a1cbe9ea6e9d9f8f53435c
Matthias Pomm <matthias.pomm@hzg.de>
2019-01-23 08:18:38 +0100

    REFSANS: modify primary monitor setup
    - adding some new monitor (hardware) selections, 'None' for no monitor
    - add device for monitor position in beam direction from source
    
    Change-Id: Id48793b4a62daaa404a979431787bb850c5654f8
    Reviewed-on: https://forge.frm2.tum.de/review/20377
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/prim_monitor.py
+++ nicos_mlz/refsans/setups/elements/prim_monitor.py
@@ -2,22 +2,12 @@ description = 'postion of Monitor: X in beam; Z may be motor'
 
 group = 'optional'
 
-nethost = 'refsanssrv.refsans.frm2'
-tacodev = '//%s/test' % nethost
-
-
 devices = dict(
-    prim_monitor_z = device('nicos.devices.generic.ManualSwitch',
-        description = 'how is the z pos',
-        states = ['const x from b3'],
-        fmtstr = '%s',
-        unit = 'foo',
+    prim_monitor_z = device('nicos.devices.generic.ManualMove',
+        description = 'Monitor axis motor',
+        abslimits = (-10, 300),
+        unit = 'mm',
     ),
-    # prim_monitor_z = device('nicos.devices.taco.Motor',
-    #     description = 'Monitor axis motor',
-    #     tacodevice = '%s/phytron/kanal_09' % tacodev, !bg2
-    #     abslimits = (10, 300),
-    # ),
     prim_monitor_x = device('nicos.devices.generic.ManualMove',
         description = 'pos of monitor in beam',
         abslimits = (0, 500),
@@ -26,7 +16,7 @@ devices = dict(
     ),
     prim_monitor_typ = device('nicos.devices.generic.ManualSwitch',
         description = 'which monitor is in use?',
-        states = ['#1','#2','#3'],
+        states = ['None', '#1', '#2', '#3', '#4', '#5', '#6', '#7'],
         fmtstr = 'Typ %d',
         unit = '',
     ),
