1540be944a342a47db3b2cafd630a62ab4f1f4bd
Andreas Wilhelm <andreas.wilhelm@frm2.tum.de>
2019-03-29 09:55:15 +0100

    SANS-1: add wut boxes
    add new wut boxes
    
    device name matches wut host name
    
    Change-Id: I84f2a6c33d46d77c8ad81d930305c84cd21cb308
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/21811
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/sans1/setups/wut-0-10-02.py
@@ -0,0 +1,30 @@
+description = 'W&T Box * 0-10V * Nr. 2'
+
+includes = []
+_wutbox = 'wut-0-10-02'
+_wutbox_dev = _wutbox.replace('-','_')
+
+devices = {
+    _wutbox_dev +'_1': device('nicos_mlz.sans1.devices.wut.WutValue',
+        hostname = _wutbox + '.sans1.frm2',
+        port = '1',
+        description = 'input 1 voltage',
+        fmtstr = '%.3F',
+        lowlevel = False,
+        loglevel = 'info',
+        pollinterval = 5,
+        maxage = 20,
+        unit = 'V',
+    ),
+    _wutbox_dev +'_2': device('nicos_mlz.sans1.devices.wut.WutValue',
+        hostname = _wutbox + '.sans1.frm2',
+        port = '2',
+        description = 'input 2 voltage',
+        fmtstr = '%.3F',
+        lowlevel = False,
+        loglevel = 'info',
+        pollinterval = 5,
+        maxage = 20,
+        unit = 'V',
+    ),
+}
---
+++ nicos_mlz/sans1/setups/wut-4-20-01.py
@@ -0,0 +1,30 @@
+description = 'W&T Box * 4-20mA * Nr. 1'
+
+includes = []
+_wutbox = 'wut-40-20-01'
+_wutbox_dev = _wutbox.replace('-','_')
+
+devices = {
+    _wutbox_dev +'_1':device('nicos_mlz.sans1.devices.wut.WutValue',
+        hostname = _wutbox + '.sans1.frm2',
+        port = '1',
+        description = 'input 1 current',
+        fmtstr = '%.3F',
+        lowlevel = False,
+        loglevel = 'info',
+        pollinterval = 5,
+        maxage = 20,
+        unit = 'A',
+    ),
+    _wutbox_dev +'_2': device('nicos_mlz.sans1.devices.wut.WutValue',
+        hostname = _wutbox + '.sans1.frm2',
+        port = '2',
+        description = 'input 2 current',
+        fmtstr = '%.3F',
+        lowlevel = False,
+        loglevel = 'info',
+        pollinterval = 5,
+        maxage = 20,
+        unit = 'A',
+    ),
+}
