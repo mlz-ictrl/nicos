6d2d802e607878c72e6879872ec67f4ec5d6724c
Matthias Pomm <matthias.pomm@hzg.de>
2019-04-17 13:30:06 +0200

    REFSANS: introduce a global instrument config
    Change-Id: If5996078d063cb85f52fb2a309c37682a3f01772
    Reviewed-on: https://forge.frm2.tum.de/review/20476
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/refsans/setups/instrument.py
@@ -0,0 +1,21 @@
+description = 'origin'
+
+group = 'configdata'
+
+values = {
+    'nethost' : 'refsanssrv.refsans.frm2',
+    'facility' : 'mlz',
+    'Softwaresytem' : 'nicos',
+    'geografical_place' : 'frm2',
+    'instruments_name' : 'refsans',
+    'pc_hw' : 'refsanshw',
+    'pc_hw_real1' : 'refsansctrl02',
+    'pc_hw_real2' : '',
+    'pc_sw' : 'refsansctrl01',
+    'pc_sw_real1' : 'refsansctrl01',
+    'pc_sw_real2' : '',
+    'pc_tango' : 'refsansctrl01',
+    'pc_tango_real1' : 'refsansctrl01',
+    'pc_tango_real2' : '',
+    'tango_port' : 10000,
+}
