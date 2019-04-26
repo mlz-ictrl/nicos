5e2fd217a75946335567aa079dc629cf78e6b25b
Matthias Pomm <matthias.pomm@hzg.de>
2019-04-26 11:18:26 +0200

    REFSANS add 'hide_temp' flag to showcase configdata
    Change-Id: I9dbbd5e64526bb4b5e1d36002f92bfb78e93d422
    Reviewed-on: https://forge.frm2.tum.de/review/20478
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/cf_showcase.py
+++ nicos_mlz/refsans/setups/cf_showcase.py
@@ -3,12 +3,15 @@ group = 'configdata'
 
 showcase_values = {
     # showcase        | This Line is regular use
-    # 'hide_poti':      True,
-    'hide_poti':     False,
-    'hide_acc' :     False,
-    # 'hide_acc' :     True,
+    # 'hide_poti': True,
+    'hide_poti': False,
+    'hide_acc': False,
+    # 'hide_acc': True,
 
-    'NOreference'  :  True,
-    # 'NOreference'  : False,
+    'hide_temp': True,
+    # 'hide_temp': False,
+
+    'NOreference':  True,
+    # 'NOreference': False,
 
 }
