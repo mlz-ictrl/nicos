76319fde49c8c3c44711c5978a206dd8ce25db9d
Jens Krüger <jens.krueger@tum.de>
2019-05-06 14:31:45 +0200

    REFSANS: use parameterized test routines
    Change-Id: Icc3e544dab1967cb711ba196613dee44e3166e8e
    Reviewed-on: https://forge.frm2.tum.de/review/20628
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- test/nicos_mlz/refsans/test_calculations.py
+++ test/nicos_mlz/refsans/test_calculations.py
@@ -32,65 +32,62 @@ from nicos_mlz.refsans.lib.calculations import chopper_config, \
     chopper_resolution
 
 
-def test_chopper_config():
+class TestBasicCalculations(object):
 
-    def check_results(res, expected):
-        assert res[0] == expected[0]
-        for v, e in zip(res[1], expected[1]):
-            assert v == approx(e, abs=0.01)
+    def test_chopper_config(self):
 
-    check_results(chopper_config(0., 21., D=21.455, disk2_pos=6, gap=0.1),  # 2
-                  (537, (0, 300, 31.12, 0, 141.69, 0)))
-    check_results(chopper_config(0., 21., D=14.38, disk2_pos=6, gap=0.1),  # 3
-                  (852, (0, 300, 49.38, 0, 224.88, 0)))
-    check_results(chopper_config(0.49, 21., D=14.38, disk2_pos=6, gap=0.1),  # 3a
-                  (877, (0, 300, 50.80, 0, 231.32, 6.92)))
-    check_results(chopper_config(0., 21., D=21.455, disk2_pos=5, gap=0.1),  # 4
-                  (509, (0, 0, 33.21, 0, 151.21, 0)))
-    check_results(chopper_config(1.17, 21., D=14.38, disk2_pos=5, gap=0.1),  # 5a
-                  (836, (0, 0, 54.50, 1.75, 248.17, 15.75)))
-    check_results(chopper_config(0, 21., D=14.38, disk2_pos=5, gap=0.1),  # 5
-                  (785, (0, 0, 51.16, 0, 232.97, 0)))
-    check_results(chopper_config(0., 21., D=21.455, disk2_pos=4, gap=0.1),  # 6
-                  (493, (0, 0, 34.40, 0, 156.67, 0)))
-    check_results(chopper_config(0, 21., D=14.38, disk2_pos=4, gap=0.1),  # 7
-                  (748, (0, 0, 52.13, 0, 237.40, 0)))
-    check_results(chopper_config(1.6, 21., D=14.38, disk2_pos=4, gap=0.1),  # 7a
-                  (813, (0, 0, 56.65, 3.53, 257.99, 20.94)))
+        def check_results(res, expected):
+            assert res[0] == expected[0]
+            for v, e in zip(res[1], expected[1]):
+                assert v == approx(e, abs=0.01)
 
-    check_results(chopper_config(3., 10., D=21.455, disk2_pos=6, gap=0.1),  # 9
-                  (1700, (0, 300, 46.90, 0, 213.56, 82.10)))
-    check_results(chopper_config(3., 10., D=14.38, disk2_pos=6, gap=0.1),  # 10
-                  (2789, (0, 300, 76.92, 0, 350.29, 134.66)))
-    check_results(chopper_config(3., 10., D=14.38, disk2_pos=5, gap=0.1),  # 11
-                  (2460, (0, 0, 76.33, 13.19, 347.57, 118.78)))
-    check_results(chopper_config(3., 10., D=21.455, disk2_pos=5, gap=0.1),  # 12
-                  (1572, (0, 0, 48.78, 8.43, 222.13, 75.91)))
-    check_results(chopper_config(3., 10., D=14.38, disk2_pos=4, gap=0.1),  # 13
-                  (2291, (0, 0, 76.02, 18.65, 346.18, 110.64)))
-    check_results(chopper_config(3., 10., D=21.455, disk2_pos=4, gap=0.1),  # 14
-                  (1501, (0, 0, 49.82, 12.22, 226.85, 72.50)))
+        for wlmin, wlmax, D, chopper2_pos, gap, expected in [
+            (0., 21., 21.455, 6, 0.1, (537, (0, 300, 31.12, 0, 141.69, 0))),
+            (0., 21., 14.38, 6, 0.1, (852, (0, 300, 49.38, 0, 224.88, 0))),
+            (0.49, 21., 14.38, 6, 0.1, (877, (0, 300, 50.80, 0, 231.32,
+                                              6.92))),
+            (0., 21., 21.455, 5, 0.1, (509, (0, 0, 33.21, 0, 151.21, 0))),
+            (1.17, 21., 14.38, 5, 0.1, (836, (0, 0, 54.50, 1.75, 248.17,
+                                              15.75))),
+            (0, 21., 14.38, 5, 0.1, (785, (0, 0, 51.16, 0, 232.97, 0))),
+            (0., 21., 21.455, 4, 0.1, (493, (0, 0, 34.40, 0, 156.67, 0))),
+            (0, 21., 14.38, 4, 0.1, (748, (0, 0, 52.13, 0, 237.40, 0))),
+            (1.6, 21., 14.38, 4, 0.1, (813, (0, 0, 56.65, 3.53, 257.99,
+                                             20.94))),
+            (3., 10., 21.455, 6, 0.1, (1700, (0, 300, 46.90, 0, 213.56,
+                                              82.10))),
+            (3., 10., 14.38, 6, 0.1, (2789, (0, 300, 76.92, 0, 350.29,
+                                             134.66))),
+            (3., 10., 14.38, 5, 0.1, (2460, (0, 0, 76.33, 13.19, 347.57,
+                                             118.78))),
+            (3., 10., 21.455, 5, 0.1, (1572, (0, 0, 48.78, 8.43, 222.13,
+                                              75.91))),
+            (3., 10., 14.38, 4, 0.1, (2291, (0, 0, 76.02, 18.65, 346.18,
+                                             110.64))),
+            (3., 10., 21.455, 4, 0.1, (1501, (0, 0, 49.82, 12.22, 226.85,
+                                              72.50))),
+            (9., 21., 21.455, 6, 0.1, (1034, (0, 300, 59.93, 0, 272.89,
+                                              149.86))),
+            (9., 21., 14.38, 6, 0.1, (1745, (0, 300, 101.08, 0, 460.29,
+                                             252.78))),
+            (3., 7., 21.455, 6, 0.1, (3104, (0, 300, 59.93, 0, 272.89,
+                                             149.86))),
+            (3., 6., 21.455, 6, 0.1, (4282, (0, 300, 70.86, 0, 322.69,
+                                             206.75))),
+            (5., 10., 21.455, 6, 0.1, (2569, (0, 300, 70.86, 0, 322.69,
+                                              206.75))),
+        ]:
+            check_results(
+                chopper_config(wlmin, wlmax, D=D, disk2_pos=chopper2_pos,
+                               gap=gap), expected)
 
-    check_results(chopper_config(9., 21., D=21.455, disk2_pos=6, gap=0.1),  # 16
-                  (1034, (0, 300, 59.93, 0, 272.89, 149.86)))
-    check_results(chopper_config(9., 21., D=14.38, disk2_pos=6, gap=0.1),  # 17
-                  (1745, (0, 300, 101.08, 0, 460.29, 252.78)))
-
-    check_results(chopper_config(3., 7., D=21.455, disk2_pos=6, gap=0.1),  # 19
-                  (3104, (0, 300, 59.93, 0, 272.89, 149.86)))
-
-    check_results(chopper_config(3., 6., D=21.455, disk2_pos=6, gap=0.1),  # 20
-                  (4282, (0, 300, 70.86, 0, 322.69, 206.75)))
-
-    check_results(chopper_config(5., 10., D=21.455, disk2_pos=6, gap=0.1),  # 22
-                  (2569, (0, 300, 70.86, 0, 322.69, 206.75)))
-
-
-def test_chopper_resolution():
-    assert chopper_resolution(6, 21.455) == 11.48
-    assert chopper_resolution(5, 21.455) == 6.23
-    assert chopper_resolution(4, 21.455) == 3.08
-
-    assert chopper_resolution(6, 14.38) == 17.63
-    assert chopper_resolution(5, 14.38) == 9.44
-    assert chopper_resolution(4, 14.38) == 4.63
+    def test_chopper_resolution(self):
+        for chopper2_pos, D, expected in [
+            (6, 21.455, 11.48),
+            (5, 21.455, 6.23),
+            (4, 21.455, 3.08),
+            (6, 14.38, 17.63),
+            (5, 14.38, 9.44),
+            (4, 14.38, 4.63),
+        ]:
+            assert chopper_resolution(chopper2_pos, D) == expected
