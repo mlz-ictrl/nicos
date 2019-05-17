170a33a5e28f3890c4a0b3e23d4a3a8ee456c742
Jens Krüger <jens.krueger@frm2.tum.de>
2019-05-17 10:10:42 +0200

    DEMO/VSPODI: add missing LogSpace device
    Change-Id: I251dd4b3a34fb2081b8e99a40492f9f0c5f9a291
    Reviewed-on: https://forge.frm2.tum.de/review/20670
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_demo/vspodi/setups/system.py
+++ nicos_demo/vspodi/setups/system.py
@@ -60,4 +60,10 @@ devices = dict(
         path = 'data',
         minfree = 5,
     ),
+    LogSpace = device('nicos.devices.generic.FreeSpace',
+        description = 'Space on log drive',
+        path = 'log',
+        minfree = 0.5,
+        lowlevel = True,
+    ),
 )
