b575863874b2572fc6140864150c180370e75ba8
Matthias Pomm <matthias.pomm@hzg.de>
2019-05-23 09:50:51 +0200

    REFSANS: Fix shutter setup
    change of mapped values because of change from taco to tango
    
    Change-Id: Ic77cd576067201f40fd05414d59f2b47a63e61ee
    Reviewed-on: https://forge.frm2.tum.de/review/20831
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/shutter.py
+++ nicos_mlz/refsans/setups/elements/shutter.py
@@ -16,8 +16,8 @@ devices = dict(
         description = 'Instrument shutter',
         moveable = 'shutter_m',
         precision = 0.5,
-        mapping = {'closed': 0,
-                   'open': 55},
+        mapping = {'closed': 7,
+                   'open': 65},
         fallback = 'offline',
     ),
 )
