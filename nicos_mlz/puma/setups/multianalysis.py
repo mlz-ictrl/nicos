de73d0678438bf04c4bed26e7645223aa457815e
Jens Krüger <jens.krueger@frm2.tum.de>
2019-05-28 16:56:57 +0200

    PUMA: Fix detector setup for multi analysis
    Change-Id: I5ac7baca7aef675b94a57803ec59148ed05262d9
    Reviewed-on: https://forge.frm2.tum.de/review/20750
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/puma/setups/multianalysis.py
+++ nicos_mlz/puma/setups/multianalysis.py
@@ -10,12 +10,17 @@ excludes = ['tas', 'defcal', 'polarization']
 nethost = 'pumasrv.puma.frm2'
 
 devices = dict(
+    channels = device('nicos.devices.vendor.qmesydaq.taco.MultiCounter',
+        tacodevice = '//%s/puma/qmesydaq/det' % nethost,
+        lowlevel = True,
+        channels = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+    ),
     det = device('nicos.devices.generic.Detector',
         description = 'Puma detector QMesydaq device (11 counters)',
         timers = ['timer'],
         monitors = ['mon1'],
-        counters = [],  # ['channels'],
-        images = ['image'],
+        counters = ['channels'],
+        images = [], # ['image'],
         maxage = 86400,
         pollinterval = None,
     ),
