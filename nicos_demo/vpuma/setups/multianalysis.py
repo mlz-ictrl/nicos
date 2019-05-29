4f05574e8e684beaaf3ffb1c9139067e87e515e1
Jens Krüger <jens.krueger@tum.de>
2019-05-29 15:25:26 +0200

    VPUMA: add multi analysis setup
    Change-Id: Ie576f7b65a02a3def92c8382bbe892a504970335
    Reviewed-on: https://forge.frm2.tum.de/review/20760
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_demo/vpuma/setups/multianalysis.py
@@ -0,0 +1,30 @@
+description = 'Classical multianalysis setup'
+
+group = 'basic'
+
+modules = ['nicos_mlz.puma.commands']
+
+includes = ['pumabase',
+            # 'seccoll', 'collimation', 'ios', 'hv', 'notifiers',
+            'multidetector', 'multianalyzer', 'cad', 'opticalbench',
+            # 'detector',
+           ]
+
+excludes = ['tas', 'defcal', 'polarization']
+
+# devices = dict(
+#     det = device('nicos.devices.generic.Detector',
+#         description = 'Puma detector QMesydaq device (11 counters)',
+#         timers = ['timer'],
+#         monitors = ['mon1'],
+#         counters = [],  # ['channels'],
+#         images = ['image'],
+#         maxage = 86400,
+#         pollinterval = None,
+#     ),
+# )
+
+startupcode = '''
+SetDetectors(det)
+med.opmode = 'multi'
+'''
