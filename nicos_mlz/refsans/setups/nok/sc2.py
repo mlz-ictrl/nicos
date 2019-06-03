3aa045081fc97d4de9614663fd2cd95de6874466
Matthias Pomm <matthias.pomm@hzg.de>
2019-06-03 08:30:10 +0200

    REFSANS: setup sc2 adding comment
    - sc2 has a brake, the motor will move down to mechanic limit if brake
      is relesed and regulation is off
    - if sc2 is in mechanic limit the contoller does not start
    
    Change-Id: I6b45d5677aa14d42bcc7ff104745cd658c9467d8
    Reviewed-on: https://forge.frm2.tum.de/review/20829
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/nok/sc2.py
+++ nicos_mlz/refsans/setups/nok/sc2.py
@@ -2,12 +2,14 @@ description = "sc2 height after nok9"
 
 group = 'lowlevel'
 
-tango_base = 'tango://refsanshw.refsans.frm2.tum.de:10000/test/copley/'
+tango_base = 'tango://refsanshw.refsans.frm2.tum.de:10000/optic/sc2/'
 
 devices = dict(
     sc2 = device('nicos.devices.tango.Motor',
-        description = 'sc2 Motor',
-        tangodevice = tango_base + 'sc2',
+        description = 'sc2 Motor; has brake, will move down to mech. limit if '
+                      'brake and control are off, this will kill the system '
+                      '2019-05-29 photo',
+        tangodevice = tango_base + 'motor',
         abslimits = (-150, 150),
         refpos = -7.2946,
     ),
