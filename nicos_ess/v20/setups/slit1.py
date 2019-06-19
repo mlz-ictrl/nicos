3fb3b840f2c5ab824fbda40196ad5e0f81d1999b
Michael Hart <michael.hart@stfc.ac.uk>
2019-06-19 15:37:05 +0100

    V20: Updated slit1 setup
    Some of the motors were reconfigured in
    CARESS, and so the setup needed to be
    updated to match.
    
    Each motor has its own loadblock now
    instead of sharing a generic one.
    
    Change-Id: I70f92368a273109c4a69732fb95776673df43d36
    Reviewed-on: https://forge.frm2.tum.de/review/20839
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>

--- nicos_ess/v20/setups/slit1.py
+++ nicos_ess/v20/setups/slit1.py
@@ -2,19 +2,6 @@ description = 'Slit 1'
 servername = 'EXV20'
 nameservice = '192.168.1.254'
 
-loadblock = '''start=never,async
-stop=never,async
-read=always,async
-motion_TBG=0.1
-motion_TBK=0.01
-motion_usefloat=true
-motion_autodelete=false
-motion_display=36
-motion_displayformat=%0.3f
-motion_retries=1
-loadoffset=yes
-'''
-
 devices = dict(
     slit1hl = device('nicos.devices.vendor.caress.Motor',
         description = 'Slit 1 Horizontal Left',
@@ -26,9 +13,19 @@ devices = dict(
         objname = '%s' % (servername),
         config = 'MB1HL 500 nist222dh1787.hmi.de:/st222.caress_object '
                  'CopleyStepnet 2 -4000 BeckhoffKL5001 BK5120/63/32/8/0 '
-                 '-4096 232159',
+                 '-4096 -107854',
         lowlevel = True,
-        loadblock = loadblock
+        loadblock = '''start=never,async
+stop = never, async
+read = always, async
+motion_usefloat = true
+motion_autodelete = false
+motion_display = 10
+motion_displayformat = %0.3f
+motion_enc_signedbits = 24
+loadoffset = yes
+'''
+
     ),
     slit1hr = device('nicos.devices.vendor.caress.Motor',
         description = 'Slit 1 Horizontal Right',
@@ -40,9 +37,18 @@ devices = dict(
         objname = '%s' % (servername),
         config = 'MB1HR 500 nist222dh1787.hmi.de:/st222.caress_object '
                  'CopleyStepnet 3 -4000 BeckhoffKL5001 BK5120/63/32/12/0 '
-                 '-4096 227117',
+                 '-4096 230417',
         lowlevel = True,
-        loadblock = loadblock
+        loadblock = '''start=never,async
+stop = never, async
+read = always, async
+motion_usefloat = true
+motion_autodelete = false
+motion_display = 11
+motion_displayformat = %0.3f
+loadoffset = yes
+'''
+
     ),
     slit1vb = device('nicos.devices.vendor.caress.Motor',
         description = 'Slit 1 Vertical Bottom',
@@ -56,7 +62,15 @@ devices = dict(
                  'CopleyStepnet 4 4000 BeckhoffKL5001 BK5120/63/32/16/0 '
                  '4096 368403',
         lowlevel = True,
-        loadblock = loadblock
+        loadblock = '''start = never, async
+stop = never, async
+read = always, async
+motion_usefloat = true
+motion_autodelete = false
+motion_display = 12
+motion_displayformat = %0.3f
+loadoffset = yes
+'''
     ),
     slit1vt = device('nicos.devices.vendor.caress.Motor',
         description = 'Slit 1 Vertical Top',
@@ -68,9 +82,17 @@ devices = dict(
         objname = '%s' % (servername),
         config = 'MB1VT 500 nist222dh1787.hmi.de:/st222.caress_object '
                  'CopleyStepnet 5 4000 BeckhoffKL5001 BK5120/63/32/20/0 '
-                 '4096 371652',
+                 '4096 364514',
         lowlevel = True,
-        loadblock = loadblock
+        loadblock = '''start = never, async
+stop = never, async
+read = always, async
+motion_usefloat = true
+motion_autodelete = false
+motion_display = 13
+motion_displayformat = %0.3f
+loadoffset = yes
+'''
     ),
     slit1 = device('nicos.devices.generic.Slit',
         description = 'Slit 1',
