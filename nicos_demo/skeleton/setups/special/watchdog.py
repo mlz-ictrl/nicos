482a7a9587ddbb46764cb7e4e4a2553d7644dc73
Jens Krüger <jens.krueger@frm2.tum.de>
2019-07-08 16:06:50 +0200

    SKELETON/watchdog: Remove comments and point to doc
    The documentation of the watchlist should not be made in the config
    file itself.
    
    Change-Id: I276a09b326a3173954d4208b0804ee56f0611c1a
    Reviewed-on: https://forge.frm2.tum.de/review/20900
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_demo/skeleton/setups/special/watchdog.py
+++ nicos_demo/skeleton/setups/special/watchdog.py
@@ -2,26 +2,9 @@ description = 'setup for the NICOS watchdog'
 group = 'special'
 
 # watchlist:
-# The entries in this list are dictionaries. Possible keys:
-#
-# 'setup' -- setup that must be loaded (default '' to mean all setups)
-# 'condition' -- condition for warning (a Python expression where cache keys
-#    can be used: t_value stands for t/value etc.
-# 'gracetime' -- time in sec allowed for the condition to be true without
-#    emitting a warning (default 5 sec)
-# 'precondition'
-#   If present, this condition must be fullfiled for at least ``precondtime``,
-#   before condition will trigger. Default is no precondition.
-# 'precondtime'
-#   The time a precondition must be fulfilled. Default is 5 seconds
-# 'message' -- warning message to display
-# 'type' -- for defining different types of warnings; this corresponds to the
-#     configured notifiers (default 'default')
-#     type '' does not emit warnings (useful together with scriptaction)
-# 'scriptaction' -- 'pausecount' to pause the count loop on the condition
-#     or 'stop' or 'immediatestop' to cancel script execution
-#     (default '')
-# 'action' -- code to execute if condition is true (default no code is executed)
+# The entries in this list are dictionaries.
+# For the entry keys and their meaning see:
+# https://forge.frm2.tum.de/nicos/doc/nicos-stable/services/watchdog
 watchlist = [
     # The first 2 entries check the disk space for the data and the log file
     # if there is any underflow in limits the user and/or instrument
