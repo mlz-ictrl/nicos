519e42fd3c77db280dc4fff27425d7a01730684c
Mark Könnecke <Mark.Koennecke@psi.ch>
2019-08-13 21:36:12 +0200

    SINQ: add some commands
    - EPICS
    - Histogram memory
    
    Change-Id: Iaa3219421d2c87c29f5e08998d8bad69f4efee2f
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos-core/+/21010
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_sinq/commands/epicscommands.py
@@ -0,0 +1,48 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2019 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Mark Koennecke <mark.koennecke@psi.ch>
+#
+# *****************************************************************************
+"""
+This defines some commands for interfacing directly with EPICS from NICOS
+This is for us, for debugging
+"""
+import epics
+
+from nicos.commands import helparglist, hiddenusercommand
+
+
+@hiddenusercommand
+@helparglist('PV-name,optionaL as_string')
+def caget(PV, as_string=True):
+    return epics.caget(PV, as_string)
+
+
+@hiddenusercommand
+@helparglist('PV-name,value')
+def caput(PV, value):
+    return epics.caput(PV, value)
+
+
+@hiddenusercommand
+@helparglist('PV-name')
+def cainfo(PV):
+    return epics.cainfo(PV)
---
+++ nicos_sinq/commands/hmcommands.py
@@ -0,0 +1,71 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2019 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Mark Koennecke <mark.koennecke@psi.ch>
+#
+# *****************************************************************************
+"""
+  This defines some general commands for handling PSI-SINQ histogram memories.
+  It is mostly about maintaining the time binning.
+
+"""
+from nicos import session
+from nicos.commands import helparglist, usercommand
+from nicos.core.errors import ConfigurationError
+
+from nicos_sinq.devices.sinqhm.configurator import HistogramConfTofArray
+
+
+@usercommand
+@helparglist('start,step,count')
+def UpdateTimeBinning(start, step, count):
+    """Change the time binning for histogramming the data.
+
+    This is the simple case of defining an equidistant time binning starting at
+    start with count equidistant steps
+    """
+    # Get the configurator device
+    try:
+        configurator = session.getDevice('hm_configurator')
+        for arr in configurator.arrays:
+            if isinstance(arr, HistogramConfTofArray):
+                arr.updateTimeBins(start, step, count)
+        configurator.updateConfig()
+    except ConfigurationError:
+        session.log.error('The configurator device not found. Cannot proceed')
+
+
+@usercommand
+def ShowTimeBinning():
+    """
+    Shows the currently configured time binning
+    """
+    # Get the configurator device
+    try:
+        configurator = session.getDevice('hm_configurator')
+        for arr in configurator.arrays:
+            if isinstance(arr, HistogramConfTofArray):
+                count = len(arr.data)
+                step = arr.data[1] - arr.data[0]
+                start = arr.data[0]
+                session.log.info('Time Binning: start: %d, step %d, count = '
+                                 '%d', start, step, count)
+    except ConfigurationError:
+        session.log.error('The configurator device not found. Cannot proceed')
