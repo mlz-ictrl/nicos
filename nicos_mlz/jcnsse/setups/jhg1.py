b11e021ea810da303b8081e75c03ba6246a837a7
Alexander Weber <al.weber@fz-juelich.de>
2019-08-23 12:15:22 +0200

    jcnsse: add jhg1 box setup
    Currently it is not a general SE box, so setup starts out here.
    
    Change-Id: Ifaa3a2858cd6d8111bba8db6b9bb311e7512abd4
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos-core/+/21068
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>

---
+++ nicos_mlz/jcnsse/setups/jhg1.py
@@ -0,0 +1,35 @@
+description = 'JCNS humidity generator'
+
+group = 'plugplay'
+
+tango_base = 'tango://%s:10000/box/' % setupname
+
+devices = {
+    '%s_flowrate' % setupname: device('nicos.devices.tango.WindowTimeoutAO',
+        description = 'Flow rate through humidity cell',
+        tangodevice = tango_base + 'mhg/flowrate',
+        fmtstr = '%.1f',
+        unit = 'ml',
+        timeout = 600.0,
+        precision = 0.2,
+    ),
+    '%s_humidity' % setupname: device('nicos.devices.tango.WindowTimeoutAO',
+        description = 'Humidity in humidity cell',
+        tangodevice = tango_base + 'mhg/humidity',
+        fmtstr = '%.1f',
+        unit = '%rH',
+        timeout = 600.0,
+        precision = 1,
+    ),
+    'T_%s_cell' % setupname: device('nicos.devices.tango.Sensor',
+        description = 'Temperature in humidity cell',
+        tangodevice = tango_base + 'mhg/temperature',
+        fmtstr = '%.1f',
+        unit = 'degC',
+    ),
+    '%s_standby' % setupname: device('nicos.devices.tango.NamedDigitalOutput',
+        description = 'Switches standby mode on and off',
+        tangodevice = tango_base + 'mhg/standby',
+        mapping = {'on': 1, 'off': 0},
+    ),
+}
