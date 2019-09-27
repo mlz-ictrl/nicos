eedda3680570d5a5482891e3d7efed313355b4ee
Jens Krüger <jens.krueger@tum.de>
2019-09-27 16:03:30 +0200

    GUI/TAS: Create a TAS widget package
    The TAS widget uses the QGraphics module and the guisupport
    package elements to make the drawings easier.
    
    Change-Id: Ia14afcd8ff1075364692f7432e15fec9d21a4250
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/21500
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

---
+++ nicos/guisupport/tas/__init__.py
@@ -0,0 +1,88 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2021 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Georg Brandl <georg.brandl@frm2.tum.de>
+#
+# *****************************************************************************
+
+"""TAS specific display widgets."""
+
+from __future__ import absolute_import, division, print_function
+
+from nicos.guisupport.qt import QSize
+from nicos.guisupport.widget import NicosWidget, PropDef
+
+from .widgets import TasView
+
+
+class TasWidget(NicosWidget, TasView):
+    """Display of the TAS table configuration."""
+
+    designer_description = __doc__
+
+    mthdev = PropDef('mthdev', str, '', 'Monochromator rocking angle device')
+    mttdev = PropDef('mttdev', str, '', 'Monochromator scattering angle device')
+    sthdev = PropDef('sthdev', str, '', 'Sample rotation device')
+    sttdev = PropDef('sttdev', str, '', 'Sample scattering angle device')
+    athdev = PropDef('athdev', str, '', 'Analyzer rocking angle device')
+    attdev = PropDef('attdev', str, '', 'Analyzer scattering angle device')
+    Lmsdev = PropDef('Lmsdev', str, '', 'Distance monochromator->sample device')
+    Lsadev = PropDef('Lsadev', str, '', 'Distance sample->analyzer device')
+    Laddev = PropDef('Laddev', str, '', 'Distance analyzer->detector device')
+    height = PropDef('height', int, 30, 'Widget height in characters')
+    width = PropDef('width', int, 40, 'Widget width in characters')
+
+    def __init__(self, parent, designMode=False):
+        TasView.__init__(self, parent)
+        NicosWidget.__init__(self)
+
+        self._keymap = {}
+        self._statuskeymap = {}
+        self._targetkeymap = {}
+
+    def registerKeys(self):
+        for dev in self.values:
+            devname = str(self.props[dev + 'dev'])
+            if devname:
+                self._keymap[
+                    self._source.register(self, devname + '/value')] = dev
+                self._statuskeymap[
+                    self._source.register(self, devname + '/status')] = dev
+                self._targetkeymap[
+                    self._source.register(self, devname + '/target')] = dev
+
+    def on_keyChange(self, key, value, time, expired):
+        if not expired:
+            if key in self._keymap:
+                # Scale the distances
+                if self._keymap[key] in ('Lms', 'Lsa', 'Lad'):
+                    value /= 10
+                self.values[self._keymap[key]] = value
+            elif key in self._statuskeymap:
+                self.status[self._statuskeymap[key]] = value[0]
+            elif key in self._targetkeymap:
+                self.targets[self._targetkeymap[key]] = value
+            else:
+                return
+            self.update()
+
+    def sizeHint(self):
+        return QSize(self.props['width'] * self._scale + 2,
+                     self.props['height'] * self._scale + 2)
---
+++ nicos/guisupport/tas/widgets.py
@@ -0,0 +1,177 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2021 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Jens Krüger <jens.krueger@frm2.tum.de>
+#
+# *****************************************************************************
+"""Classes to display the TAS instruments."""
+
+from __future__ import absolute_import, division, print_function
+
+from math import cos, radians, sin
+
+from nicos.core import status
+from nicos.guisupport.elements import AnaTable, Beam, DetTable, MonoTable, \
+    SampleTable, TableBase, TableTarget
+from nicos.guisupport.qt import QGraphicsScene, QGraphicsView, QPainter
+
+
+class TasView(QGraphicsView):
+    """Widget to visualise the current positions of the TAS."""
+
+    anaradius = 30
+    monoradius = 40
+    sampleradius = 20
+    detectorradius = 10
+    _beams = []
+
+    def __init__(self, parent=None):
+        scene = QGraphicsScene()
+        QGraphicsView.__init__(self, scene)
+        self.setRenderHints(QPainter.Antialiasing)
+
+        self._mono = MonoTable(0., 0, self.monoradius, scene=scene)
+        self._sample = SampleTable(0, 0, self.sampleradius, scene=scene)
+        self._sample_t = TableTarget(0, 0, self.sampleradius, scene=scene)
+        self._ana = AnaTable(0, 0, self.anaradius, scene=scene)
+        self._ana_t = TableTarget(0, 0, self.anaradius, scene=scene)
+        self._det = DetTable(0, 0, self.detectorradius, scene=scene)
+        self._det_t = TableTarget(0, 0, self.detectorradius, scene=scene)
+        self._src = TableBase(0, 0, 0, scene=scene)
+        self._beams.append(Beam(self._src, self._mono, scene=scene))
+        self._beams.append(Beam(self._mono, self._sample, scene=scene))
+        self._beams.append(Beam(self._sample, self._ana, scene=scene))
+        self._beams.append(Beam(self._ana, self._det, scene=scene))
+
+        # default values (used when no such devices are configured)
+        self.values = {
+            'mth': -45,
+            'mtt': -90,
+            'sth': 30,
+            'stt': 60,
+            'ath': -45,
+            'att': -90,
+            # scale the distances
+            'Lms': 1000 / 10,
+            'Lsa': 580 / 10,
+            'Lad': 400 / 10,
+        }
+        self.targets = self.values.copy()
+        self.status = {
+            'mth': status.OK,
+            'mtt': status.OK,
+            'sth': status.OK,
+            'stt': status.OK,
+            'ath': status.OK,
+            'att': status.OK,
+        }
+        self.update()
+
+    def resizeEvent(self, rsevent):
+        s = self.size()
+        w, h = s.width(), s.height()
+        Lms = self.values['Lms']
+        Lsa = self.values['Lsa']
+        Lad = self.values['Lad']
+
+        maxL = Lms + Lsa + Lad
+        scale = min(w / (2 * (maxL + self.detectorradius)),
+                    h / (maxL + self.monoradius + self.detectorradius))
+        transform = self.transform()
+        transform.reset()
+        transform.scale(scale, scale)
+        self.setTransform(transform)
+        QGraphicsView.resizeEvent(self, rsevent)
+
+    def update(self):
+        Lms = self.values['Lms']
+        Lsa = self.values['Lsa']
+        Lad = self.values['Lad']
+        maxL = Lms + Lsa + Lad
+        mth = self.values['mth']
+
+        # incoming beam
+        self._src.setPos(-(maxL - 3), 0)
+
+        # monochromator
+        self._mono.setState(max(self.status['mth'], self.status['mtt']))
+        self._mono.setPos(0, 0)
+        self._mono.setRotation(mth)
+
+        # sample
+        mtt = self.values['mtt']
+        mttangle = radians(mtt)
+        mttangle_t = radians(self.targets['mtt'])
+        if mth < 0:
+            mttangle = -mttangle
+            mttangle_t = -mttangle_t
+
+        sx, sy = Lms * cos(mttangle), -Lms * sin(mttangle)
+        sx_t, sy_t = Lms * cos(mttangle_t), -Lms * sin(mttangle_t)
+        sth = self.values['sth']
+        stt = self.values['stt']
+
+        self._sample_t.setPos(sx_t, sy_t)
+        self._sample.setState(max(self.status['stt'], self.status['sth']))
+        self._sample.setPos(sx, sy)
+        self._sample.setRotation(sth - mtt + 45.)
+
+        # analyzer
+        sttangle = radians(stt)
+        sttangle_t = radians(self.targets['stt'])
+        if sth < 0:
+            sttangle = mttangle - sttangle
+            sttangle_t = mttangle_t - sttangle_t
+        else:
+            sttangle = mttangle + sttangle
+            sttangle_t = mttangle_t + sttangle_t
+        ax, ay = sx + Lsa * cos(sttangle), sy - Lsa * sin(sttangle)
+        ax_t, ay_t = sx_t + Lsa * cos(sttangle_t), sy_t - Lsa * sin(sttangle_t)
+        ath = self.values['ath']
+
+        self._ana_t.setPos(ax_t, ay_t)
+        self._ana.setState(max(self.status['ath'], self.status['att']))
+        self._ana.setPos(ax, ay)
+        self._ana.setRotation(mtt + ath - stt)
+
+        # detector
+        att = self.values['att']
+        attangle = radians(att)
+        attangle_t = radians(self.targets['att'])
+        if ath < 0:
+            attangle = sttangle - attangle
+            attangle_t = sttangle_t - attangle_t
+        else:
+            attangle = sttangle + attangle
+            attangle_t = sttangle_t + attangle_t
+
+        dx, dy = ax + Lad * cos(attangle), ay - Lad * sin(attangle)
+        dx_t, dy_t = ax_t + Lad * cos(attangle_t), ay_t - Lad * sin(attangle_t)
+
+        self._det_t.setPos(dx_t, dy_t)
+        self._det.setState(self.status['att'])
+        self._det.setPos(dx, dy)
+
+        # This call is needed to refresh drawing of the beams after moving
+        # the tables
+        for b in self._beams:
+            b.update()
+
+        QGraphicsView.update(self)
