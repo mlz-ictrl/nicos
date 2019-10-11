24a4e5c7808fb7e65cb0ce5eac2ca0c9cd65892b
Jens Krüger <jens.krueger@frm2.tum.de>
2019-10-11 10:55:18 +0200

    VTREFF: configure devices with more realistic values
    Since the VTREFF instrument should be used as well for practical
    training for students, it must have better starting values for speed and
    positions of the virtual devices.
    
    Change-Id: I0a5b0fbec158b0fc28580b0fbf3e3984e43e495e
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos-core/+/21397
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_demo/vtreff/setups/beamstop.py
+++ nicos_demo/vtreff/setups/beamstop.py
@@ -8,6 +8,7 @@ devices = dict(
         motor = device('nicos.devices.generic.VirtualMotor',
             abslimits = (-0.1, 44.8586),
             unit = 'mm',
+            speed = 1,
         ),
         precision = 0.01,
         fmtstr = '%.3f',
--- nicos_demo/vtreff/setups/ktable.py
+++ nicos_demo/vtreff/setups/ktable.py
@@ -8,10 +8,12 @@ devices = dict(
         description = 'X axis',
         abslimits = (-1000, 1000),
         unit = 'mm',
+        speed = 1,
     ),
     det_ay = device('nicos.devices.generic.VirtualMotor',
         description = 'Y axis',
         abslimits = (-1000, 1000),
         unit = 'mm',
+        speed = 1,
     ),
 )
