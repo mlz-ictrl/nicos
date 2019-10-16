ee499bed3214ee8d63530dcef00b3dfb780ccf59
Jens Krüger <jens.krueger@frm2.tum.de>
2019-10-16 13:42:18 +0200

    NECTAR: Add thermal collimator setup
    Change-Id: I8516b12c3ca074d2f088c39c546b1c1ccbfb0a22
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos-core/+/21409
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/nectar/setups/tcoll.py
@@ -0,0 +1,35 @@
+description = 'Thermal collimator'
+
+group = 'optional'
+
+tango_base = 'tango://phytron02.nectar.frm2.tum.de:10000/'
+
+devices = dict(
+    drum_c = device('nicos.devices.tango.Sensor',
+        tangodevice = tango_base + 'box/phytron7/enc',
+        lowlevel = True,
+    ),
+    drum_m = device('nicos.devices.tango.Motor',
+        tangodevice = tango_base + 'box/phytron7/mot',
+        abslimits = (-10, 370),
+        unit = 'deg',
+        lowlevel = True,
+    ),
+    drum = device('nicos.devices.generic.Axis',
+        motor = 'drum_m',
+        # coder = 'drum_c',  # coder not working for multiturns (Resolver)
+        precision = 0.05,
+        lowlevel = True,
+    ),
+    tcoll = device('nicos.devices.generic.Switcher',
+        description = 'Thermal collimator',
+        precision = 0.01,
+        moveable = 'drum',
+        mapping = {
+            '1': 0,
+            '2': 90,
+            '3': 180,
+            '4': 270,
+        },
+    ),
+)
