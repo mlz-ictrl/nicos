70b67cc177f16e10204bfc6d66d5947cb8504e98
Jens Krüger <jens.krueger@frm2.tum.de>
2019-10-18 15:32:41 +0200

    VTOFTOF: add cryo setup to simulate temperatures
    Change-Id: I18a04761d5c0fb1433ee398ba92acbc4aa4389ac
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos-core/+/21439
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_demo/vtoftof/setups/cryo.py
@@ -0,0 +1,37 @@
+group = 'optional'
+description = 'virtual temperature device'
+
+devices = dict(
+    T = device('nicos.devices.generic.DeviceAlias'),
+    Ts = device('nicos.devices.generic.DeviceAlias'),
+    T_demo = device('nicos.devices.generic.VirtualRealTemperature',
+        description = 'A virtual (but realistic) temperature controller',
+        abslimits = (2, 1000),
+        warnlimits = (0, 325),
+        ramp = 60,
+        unit = 'K',
+        jitter = 0,
+        precision = 0.1,
+        window = 30.0,
+        lowlevel = True,
+    ),
+    T_sample = device('nicos.devices.generic.ReadonlyParamDevice',
+        parameter = 'sample',
+        device = 'T_demo',
+        description = 'Temperature of virtual sample',
+        lowlevel = True,
+    ),
+)
+
+alias_config = {
+    'T':  {'T_demo': 100},
+    'Ts': {'T_sample': 100},
+}
+
+extended = dict(
+    representative = 'T',
+)
+
+startupcode = """
+AddEnvironment(T, Ts)
+"""
