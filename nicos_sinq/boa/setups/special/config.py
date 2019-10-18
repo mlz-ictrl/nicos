446eac77e113879b3d1ba1f6c7293ae028cf5b7d
koennecke <mark.koennecke@psi.ch>
2019-10-18 10:58:48 +0200

    BOA: initial diretory layout and setup
    Change-Id: I59e8a5c655c95e460de33f0f16712551eb749d98
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos-core/+/21434
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_sinq/boa/setups/special/config.py
@@ -0,0 +1,12 @@
+description = 'Generic configuration settings for BOA'
+
+
+import os
+
+group = 'configdata'
+
+KAFKA_BROKERS = ['ess01:9092']
+HISTOGRAM_MEMORY_URL = os.environ.get('BOAHM','http://localhost:8080/admin')
+HISTOGRAM_MEMORY_ENDIANESS = 'little'
+
+DATA_PATH = os.environ.get('NICOSDUMP','.') + '/boa/'
---
+++ nicos_sinq/boa/setups/system.py
@@ -0,0 +1,52 @@
+description = 'system setup'
+
+group = 'lowlevel'
+
+sysconfig = dict(
+    cache = 'localhost',
+    instrument = 'BOA',
+    experiment = 'Exp',
+    datasinks = ['conssink', 'dmnsink', 'nxsink', 'livesink'],
+)
+
+modules = [
+    'nicos.commands.standard', 'nicos_sinq.commands.sics',
+    'nicos_sinq.commands.hmcommands', 'nicos_sinq.commands.epicscommands'
+]
+
+devices = dict(
+    BOA = device('nicos.devices.instrument.Instrument',
+        description = 'instrument object',
+        instrument = 'SINQ BOA',
+        responsible = 'Patrick Hautle <patrick.hautle@psi.ch>',
+        operators = ['Paul-Scherrer-Institut (PSI)'],
+        facility = 'SINQ, PSI',
+        website = 'https://www.psi.ch/sinq/BOA/',
+    ),
+    Sample = device('nicos.devices.experiment.Sample',
+        description = 'The defaultsample',
+    ),
+    Exp = device('nicos_sinq.devices.experiment.SinqExperiment',
+        description = 'experiment object',
+        dataroot = configdata('config.DATA_PATH'),
+        sendmail = False,
+        serviceexp = 'Service',
+        sample = 'Sample',
+    ),
+    Space = device('nicos.devices.generic.FreeSpace',
+        description = 'The amount of free space for storing data',
+        path = None,
+        minfree = 5,
+    ),
+    conssink = device('nicos.devices.datasinks.ConsoleScanSink'),
+    dmnsink = device('nicos.devices.datasinks.DaemonSink'),
+    nxsink = device('nicos_sinq.nexus.nexussink.NexusSink',
+        description = "Sink for NeXus file writer",
+        filenametemplate = ['boa%(year)sn%(scancounter)06d.hdf'],
+        templatesmodule = 'nicos_sinq.boa.nexus.nexus_templates',
+        templateclass = 'BOATemplateProvider',
+    ),
+    livesink = device('nicos.devices.datasinks.LiveViewSink',
+        description = "Sink for forwarding live data to the GUI",
+    ),
+)
