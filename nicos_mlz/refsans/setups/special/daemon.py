7f1fe3d6f29f2b48fac7a0e469075058be742b0e
Jens Krüger <jens.krueger@frm2.tum.de>
2019-11-19 14:41:14 +0100

    REFSANS/daemon: Remove default admin account
    Change-Id: I104e079cab68bf183111d37dacbbf74be0f8f93f
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/21793
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/special/daemon.py
+++ nicos_mlz/refsans/setups/special/daemon.py
@@ -18,7 +18,6 @@ devices = dict(
         passwd = [
             ('guest', '', 'guest'),
             ('user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user'),
-            ('admin', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
         ],
     ),
     Daemon = device('nicos.services.daemon.NicosDaemon',
