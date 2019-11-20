b26f2f58c4df5a3e396f56ae26b33284a8f7c87e
Jens Krüger <jens.krueger@frm2.tum.de>
2019-11-20 17:27:07 +0100

    STRESSI: add the additional rotation axis setup
    Change-Id: I3f60f4772e9e70f303164521958174c8937685d6
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/21799
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/stressi/setups/addphi.py
@@ -0,0 +1,19 @@
+description = 'Additional rotation table'
+
+group = 'optional'
+
+tango_base = 'tango://motorbox05.stressi.frm2.tum.de:10000/box/'
+
+devices = dict(
+    addphi_m = device('nicos.devices.tango.Motor',
+        tangodevice = tango_base + 'channel4/motor',
+        fmtstr = '%.2f',
+        lowlevel = True,
+        speed = 4,
+    ),
+    addphi = device('nicos.devices.generic.Axis',
+        description = 'Additional rotation table',
+        motor = 'addphi_m',
+        precision = 0.01,
+    ),
+)
