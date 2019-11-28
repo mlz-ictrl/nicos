RESEDA/MiezeDisplay: Fix calculation of the errors
The code for calculation of the errors for each of the measurement points
was not very clearly written (a little bit ugly).

Additionally the Py3 incompatibility of the old code is fixed.

Change-Id: I26b3d9a7c6d754ff9c7f848faf1298cd572c5c04
Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/21925
Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

nicos_mlz/reseda/gui/mieze_display.py
@@ -107,8 +107,8 @@ class FoilWidget(QWidget):
                                for x in fitcurve.x])
         datacurve.x = np.arange(0, 16, 1)
         datacurve.y = np.array(counts)
-        dy = map(np.sqrt, datacurve.y)
-        datacurve.errorBar1 = ErrorBar(datacurve.x, datacurve.y, dy,
+        datacurve.errorBar1 = ErrorBar(datacurve.x, datacurve.y,
+                                       np.sqrt(datacurve.y),
                                        markercolor=datacurve.markercolor)
         datacurve.linetype = None
         self.plotwidget.reset()