8f69efc3b9b2d6b3590dfbe2b3eeceb5dd1ff594
Matthias Pomm <matthias.pomm@hzg.de>
2019-12-02 08:55:17 +0100

    REFSANS: Add regulation of the liquid sample surface height
    - at REFSANS we have a langmuir trough. The height of the surface is measured
    - if the height of the surface is falling (due to evaporation) the Z has to correct this
    - this has to work independent from the measurement
    
    Change-Id: Ie4f0264d8939e0bb2e3111cfe00f5e19e8189535
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/22133
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/height.py
+++ nicos_mlz/refsans/setups/elements/height.py
@@ -2,13 +2,15 @@ description = 'Sample surface position measurement'
 
 group = 'lowlevel'
 
+# includes = ['gonio']
+
 instrument_values = configdata('instrument.values')
 
 tango_base = instrument_values['tango_base']
-code_base = instrument_values['code_base'] + 'tristate.TriState'
+code_base = instrument_values['code_base']
 
 devices = dict(
-    height = device(code_base,
+    height = device(code_base  + 'tristate.TriState',
         description = 'Sample surface position.',
         unit = 'mm',
         port = 'height_port',
@@ -18,5 +20,33 @@ devices = dict(
         tangodevice = tango_base + 'refsans/keyence/sensor',
         unit = 'mm',
         lowlevel = True,
+    ),
+    sim_height = device('nicos.devices.generic.ManualMove',
+        description = 'sim only',
+        abslimits = (-10, 10),
+        default = 0,
+        fmtstr = '%.3f',
+        unit = 'foo',
+        lowlevel = True,
+    ),
+    sim_z = device('nicos.devices.generic.ManualMove',
+        description = 'sim only',
+        abslimits = (-10, 10),
+        default = 0,
+        fmtstr = '%.3f',
+        unit = 'foo',
+        lowlevel = True,
+    ),
+    active_regulator = device('nicos_mlz.reseda.devices.regulator.Regulator',
+        description = 'todo',
+        sensor = 'height',
+        moveable = 'gonio_z',
+        loopdelay = 1.0,
+        maxstep = 0.1,
+        minstep = 0.01,
+        maxage = 11.0,
+        pollinterval = 5.0,
+        stepfactor = 1.0,
+        unit = 'mm',
     ),
 )
