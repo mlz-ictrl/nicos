68465edc480e875ddd21727b0e7a32df44e26713
Christian Felder <c.felder@fz-juelich.de>
2019-12-12 17:06:42 +0100

    Configure shutter for new profinet plc v4
    Change-Id: I742eeb000ac053a95b8d63a87845727ddf657225
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/22010
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Christian Felder <c.felder@fz-juelich.de>

--- nicos_mlz/biodiff/setups/shutter.py
+++ nicos_mlz/biodiff/setups/shutter.py
@@ -7,18 +7,18 @@ tango_base = 'tango://phys.biodiff.frm2:10000/biodiff/'
 
 _MAP_SHUTTER = {
     'open': 1,
-    'closed': 2,
+    'closed': 0,
 }
 
 devices = dict(
     gammashutter = device('nicos_mlz.jcns.devices.shutter.Shutter',
         description = 'Gamma shutter',
-        tangodevice = tango_base + 'fzjdp_digital/expshutter',
+        tangodevice = tango_base + 'plcdigital/gammashutter',
         mapping = _MAP_SHUTTER,
     ),
     photoshutter = device('nicos_mlz.jcns.devices.shutter.Shutter',
         description = 'Photo shutter',
-        tangodevice = tango_base + 'fzjdp_digital/photoexpshutter',
+        tangodevice = tango_base + 'plcdigital/photoshutter',
         mapping = _MAP_SHUTTER,
     ),
 )
