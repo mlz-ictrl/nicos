59b96ef0ea4f7a8cb3091cfe35eaf4135a50c47f
Matthias Pomm <matthias.pomm@hzg.de>
2020-01-09 19:31:01 +0100

    REFSANS: add external and internal sensors for julabo01
    Change-Id: I171f6bc4fedc56e83f4921893ef4ffb23845bda9
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/22150
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/julabo.py
+++ nicos_mlz/refsans/setups/elements/julabo.py
@@ -4,11 +4,22 @@ group = 'optional'
 
 instrument_values = configdata('instrument.values')
 
-tango_base = instrument_values['tango_base'] + 'refsans/julabo01/control'
+tango_base = instrument_values['tango_base'] + 'refsans/julabo01/'
 
 devices = dict(
     julabo_temp = device('nicos.devices.tango.TemperatureController',
-        description = 'julabo01 temperature',
-        tangodevice = tango_base,
+        description = 'Julabo01 temperature control',
+        tangodevice = tango_base + 'control',
+        fmtstr = '%.2f',
+    ),
+    julabo_int = device('nicos.devices.tango.Sensor',
+        description = 'Julabo01 temperature bath',
+        tangodevice = tango_base + 'intsensor',
+        fmtstr = '%.2f',
+    ),
+    julabo_ext = device('nicos.devices.tango.Sensor',
+        description = 'Julabo01 external sensor',
+        tangodevice = tango_base + 'extsensor',
+        fmtstr = '%.2f',
     ),
 )
