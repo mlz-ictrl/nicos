a9504295d2c4051bc900308c74499d9a83ba79c7
Matthias Pomm <matthias.pomm@hzg.de>
2020-01-09 19:31:07 +0100

    REFSANS use config setups 'code_base'
    Change-Id: I5171aecd134f7f6b13187983d4e02cda5804cc49
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/22156
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/cf_optic.py
+++ nicos_mlz/refsans/setups/cf_optic.py
@@ -1,16 +1,14 @@
 description = 'global Values '
 group = 'configdata'
 
-#scale = 1.0
-scale = 0.1 #to debug, shorter distances to save time
+scale = 1.0
 
 optic_values = {
     # Numbers
     'ng':   0.0,
     'rc':  22.5 * scale,
     'vc':  37.5 * scale,
-    # 'fc':  52.5 * scale,
-    'fc':  50.0 * scale,
+    'fc':  50.0 * scale,  # in previous times: 52.5
 
     'gisans_scale': scale,
 
