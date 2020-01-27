6778c5fd19a6aa66afacfe64015ea3edf922aa83
Georg Brandl <g.brandl@fz-juelich.de>
2020-01-27 10:25:33 +0100

    monitor/qt: fix remaining resizing problem on Qt 5
    Change-Id: I6c57d9c3ac3a7f2328e7271cf5dde5534c80aede
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/22310
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Tested-by: Georg Brandl <g.brandl@fz-juelich.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos/services/monitor/qt.py
+++ nicos/services/monitor/qt.py
@@ -101,7 +101,10 @@ class MonitorWindow(QMainWindow):
             # screen window is actually made larger than the full screen
             self.resize(self.sizeHint())
             if self._wantFullScreen:
-                self.showFullScreen()
+                if QT_VER == 5:
+                    self.setGeometry(QApplication.screens()[0].geometry())
+                else:
+                    self.showFullScreen()
         return QMainWindow.event(self, event)
 
     def do_reconfigure(self, emitdict):
