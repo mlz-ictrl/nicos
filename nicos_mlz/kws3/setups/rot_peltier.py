2ea8f1ec9534f8cd9255f9f314ec01ab67d0ab14
Georg Brandl <g.brandl@fz-juelich.de>
2020-01-28 07:04:26 +0100

    KWS3: add setup for rotating peltier
    Change-Id: I0fc40e075a6bf56e493543c67aed0d8e145d5a7f
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/24785
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ nicos_mlz/kws3/setups/rot_peltier.py
@@ -0,0 +1,23 @@
+description = 'Peltier controller for rotating sample environment'
+
+group = 'optional'
+
+includes = ['alias_T']
+
+tango_base = 'tango://phys.kws3.frm2:10000/kws3/'
+
+devices = dict(
+    T_rotpeltier = device('nicos.devices.tango.TemperatureController',
+        description = 'The regulated temperature',
+        tangodevice = tango_base + 'rotpeltier/ctrl',
+        unit = 'degC',
+        fmtstr = '%.1f',
+        precision = 0.5,
+        timeout = 45 * 60.,
+    ),
+)
+
+alias_config = {
+    'T':  {'T_rotpeltier': 100},
+    'Ts': {'T_rotpeltier': 100},
+}
