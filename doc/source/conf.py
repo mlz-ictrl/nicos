0fa68418883e257a61b00a114d8d50f07430087d
Jens Krüger <jens.krueger@tum.de>
2020-02-13 08:55:31 +0100

    DOC: update the copyright (year)
    Change-Id: If49d90ff228c93272241e992f10657fff3170bef
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/22449
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -72,7 +72,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'NICOS'
 author = u'NICOS authors and contributors'
-copyright = u'2009-2019, FRM II / ' + author  # pylint: disable=redefined-builtin
+copyright = u'2009-2020, FRM II / ' + author  # pylint: disable=redefined-builtin
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
