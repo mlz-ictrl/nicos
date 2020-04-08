100d2989fb369cced1ca33d6d340ef789fed3ae0
Matt Clarke <matt.clarke@esss.se>
2020-04-08 13:57:37 +0200

    Initial move from essiip to ymir

---
+++ nicos_ess/ymir/setups/laser_detector.py
@@ -0,0 +1,5 @@
+description = 'The laser detector in the YMIR cave'
+
+devices = dict(
+    laser = device('nicos_ess.devices.datasources.laser_detector.LaserDetector'),
+)
---
+++ nicos_ess/ymir/setups/ymir_motors.py
@@ -0,0 +1,34 @@
+description = "The motion stages in the YMIR cave"
+
+devices = dict(
+    mX=device(
+        "nicos_ess.devices.epics.motor.EpicsMotor",
+        epicstimeout=3.0,
+        precision=0.1,
+        description="Single axis positioner",
+        motorpv="SES-SCAN:MC-MCU-001:m1",
+        errormsgpv="SES-SCAN:MC-MCU-001:m1-MsgTxt",
+        errorbitpv="SES-SCAN:MC-MCU-001:m1-Err",
+        reseterrorpv="SES-SCAN:MC-MCU-001:m1-ErrRst",
+    ),
+    mY=device(
+        "nicos_ess.devices.epics.motor.EpicsMotor",
+        epicstimeout=3.0,
+        precision=0.1,
+        description="Single axis positioner",
+        motorpv="SES-SCAN:MC-MCU-001:m2",
+        errormsgpv="SES-SCAN:MC-MCU-001:m2-MsgTxt",
+        errorbitpv="SES-SCAN:MC-MCU-001:m2-Err",
+        reseterrorpv="SES-SCAN:MC-MCU-001:m2-ErrRst",
+    ),
+    mZ=device(
+        "nicos_ess.devices.epics.motor.EpicsMotor",
+        epicstimeout=3.0,
+        precision=0.1,
+        description="Single axis positioner",
+        motorpv="SES-SCAN:MC-MCU-001:m3",
+        errormsgpv="SES-SCAN:MC-MCU-001:m3-MsgTxt",
+        errorbitpv="SES-SCAN:MC-MCU-001:m3-Err",
+        reseterrorpv="SES-SCAN:MC-MCU-001:m3-ErrRst",
+    ),
+)
