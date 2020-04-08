e46722777a6ca955f687e2020e38629760f1654a
Matt Clarke <matt.clarke@esss.se>
2020-04-08 15:23:19 +0200

    Reverted to non-docker

--- nicos_ess/ymir/setups/special/poller.py
+++ nicos_ess/ymir/setups/special/poller.py
@@ -4,16 +4,18 @@ group = 'special'
 sysconfig = dict(
     # use only 'localhost' if the cache is really running on the same machine,
     # otherwise use the official computer name
-    cache='nicos-cache'
+    cache='localhost'
 )
 
 devices = dict(
     Poller=device('nicos.services.poller.Poller',
-                    alwayspoll = [], # setups that should be polled regardless if loaded
-                    neverpoll = [],  # setups that should not be polled even if loaded
-                    blacklist = [],  # DEVICES that should never be polled
-                                     # (usually detectors or devices that have problems
-                                     # with concurrent access from processes)
-                    loglevel='debug'
-                   ),
+                  alwayspoll=[],
+                  # setups that should be polled regardless if loaded
+                  neverpoll=[],
+                  # setups that should not be polled even if loaded
+                  blacklist=[],  # DEVICES that should never be polled
+                  # (usually detectors or devices that have problems
+                  # with concurrent access from processes)
+                  loglevel='debug'
+                  ),
 )
