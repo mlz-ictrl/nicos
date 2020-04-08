284196c66540b9414ce19d9bd71d2aa98cad57d9
Georg Brandl <g.brandl@fz-juelich.de>
2020-04-08 09:10:30 +0200

    utils: allow more chars in user names in parseConnectionString
    Also make sure that the match ends at the end of the string.
    
    Change-Id: I4d62dfd3c63e40f081913c42a21a26cd693be99c
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/22897
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: JenkinsCodeReview <bjoern_pedersen@frm2.tum.de>

--- nicos/utils/__init__.py
+++ nicos/utils/__init__.py
@@ -553,7 +553,7 @@ def terminalSize():
 
 def parseConnectionString(s, defport):
     """Parse a string in the format 'user:pass@host:port"."""
-    res = re.match(r"(?:(\w+)(?::([^@]*))?@)?([\w.-]+)(?::(\d+))?", s)
+    res = re.match(r"(?:([^:]+)(?::([^@]*))?@)?([\w.-]+)(?::(\d+))?$", s)
     if res is None:
         return None
     return {
