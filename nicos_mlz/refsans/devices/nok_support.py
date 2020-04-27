6fd7f6ecd5780dbabf44786a5ff094d9131ee81c
Matthias Pomm <matthias.pomm@hzg.de>
2020-04-27 10:09:04 +0200

    REFSANS: Add 'absolute' parameter to EncoderDifference device
    - add parameter to select absolute or signed value
    - signed value may be used for debugging
    
    Change-Id: Ib05006593a969de46058002a387d666ec4f78408
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/23472
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/devices/nok_support.py
+++ nicos_mlz/refsans/devices/nok_support.py
@@ -462,9 +462,15 @@ class MotorEncoderDifference(Readable):
         'analog': Attach('analog encoder maybe poti', Readable),
     }
 
+    parameters = {
+        'absolute': Param('Value is absolute or signed.', type=bool,
+                          settable=True, default=True),
+    }
+
     def doRead(self, maxage=0):
-        return abs(self._attached_analog.read(maxage) -
-                   self._attached_motor.read(maxage))
+        dif = self._attached_analog.read(maxage) - \
+            self._attached_motor.read(maxage)
+        return abs(dif) if self.absolute else dif
 
     def doStatus(self, maxage=0):
         return status.OK, ''
