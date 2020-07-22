59bd541ecb10e1f1bcd389ac2d072223b88e9e3e
Jens Krüger <jens.krueger@frm2.tum.de>
2020-07-22 09:41:15 +0200

    Antares: Set the data file to readonly after writing
    The data files at Antares should be set after writing automatically to
    readonly to avoid accidental removing of data files by file system
    operation by instrument scientists.
    
    Fixes: #4255
    Change-Id: I67a031b03a77d83a075b7608cf8e981d1b425766
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/23469
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_demo/vantares/setups/filesavers.py
+++ nicos_demo/vantares/setups/filesavers.py
@@ -12,6 +12,7 @@ devices = dict(
     FITSFileSaver = device('nicos.devices.datasinks.FITSImageSink',
         description = 'Saves image data in FITS format',
         filenametemplate = ['%(pointcounter)08d.fits'],
+        filemode = 0o444,
     ),
     DiObSink = device('nicos_mlz.devices.datasinks.DiObSink'),
 )
