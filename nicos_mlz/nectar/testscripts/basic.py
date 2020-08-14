10fc20cf47edd09332f0c05c214699944da54050
Jens Krüger <jens.krueger@frm2.tum.de>
2020-08-14 16:07:37 +0200

    TEST/DryRun: Activate NECTAR test
    Change-Id: Ia67278dd8b37e70f439cd91d03f5f0e36d6a9f74
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/23716
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

---
+++ nicos_mlz/nectar/testscripts/basic.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# pylint: skip-file
+
+# test: setups = nectar, servostar, detector
+# test: setupcode = SetDetectors(det)
+
+FinishExperiment()
+NewSample('Test_abc')
+Remark('Collimator 40x50mm')
+
+# Not enough space in y direction, also for x required
+maw(sty, 1)
+maw(stx, 1)
+
+# take openbeam
+for i in range(2):
+    openbeamimage(t=1)
+
+# since nectar has no shutter control, this is usually done at the beginning of
+# the measurement without sample in beam
+for i in range(2):
+    darkimage(t=1)
+
+print('OB and DI Finished')
+
+maw(stx, 20)
+maw(sty, 10)
+tomo(10, sry, t=1)
+
+print('Tomo Finished!')
+print('Test finished')
