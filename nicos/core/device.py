b16787199a8d4ddc1e4414b106a73af36513a58f
Georg Brandl <g.brandl@fz-juelich.de>
2020-09-01 15:50:32 +0200

    core: update list of special methods for proxy
    Change-Id: I0e130c86617ff66ef216054f1d78511cdc299057
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/23823
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos/core/device.py
+++ nicos/core/device.py
@@ -2419,20 +2419,20 @@ def make_method(name):
 
 
 for name in [
-        '__abs__', '__add__', '__and__', '__call__', '__cmp__', '__coerce__',
-        '__contains__', '__delitem__', '__delslice__', '__div__', '__divmod__',
-        '__float__', '__floordiv__', '__ge__', '__getitem__',
-        '__getslice__', '__gt__', '__hash__', '__hex__', '__iadd__', '__iand__',
-        '__idiv__', '__idivmod__', '__ifloordiv__', '__ilshift__', '__imod__',
-        '__imul__', '__int__', '__invert__', '__ior__', '__ipow__', '__irshift__',
-        '__isub__', '__iter__', '__itruediv__', '__ixor__', '__le__', '__len__',
-        '__long__', '__lshift__', '__lt__', '__mod__', '__mul__',
-        '__neg__', '__oct__', '__or__', '__pos__', '__pow__', '__radd__',
-        '__rand__', '__rdiv__', '__rdivmod__', '__reduce__', '__reduce_ex__',
-        '__reversed__', '__rfloorfiv__', '__rlshift__', '__rmod__',
-        '__rmul__', '__ror__', '__rpow__', '__rrshift__', '__rshift__', '__rsub__',
-        '__rtruediv__', '__rxor__', '__setitem__', '__setslice__', '__sub__',
-        '__truediv__', '__xor__', 'next',
+        '__abs__', '__add__', '__and__', '__bool__', '__call__',
+        '__contains__', '__delitem__', '__divmod__', '__float__',
+        '__floordiv__', '__ge__', '__getitem__', '__gt__', '__hash__',
+        '__iadd__', '__iand__', '__idivmod__', '__ifloordiv__', '__ilshift__',
+        '__imatmul__', '__imod__', '__imul__', '__index__', '__int__',
+        '__invert__', '__ior__', '__ipow__', '__irshift__', '__isub__',
+        '__iter__', '__itruediv__', '__ixor__', '__le__', '__len__',
+        '__lshift__', '__lt__', '__matmul__', '__mod__', '__mul__', '__neg__',
+        '__next__', '__or__', '__pos__', '__pow__', '__radd__', '__rand__',
+        '__rdivmod__', '__reduce__', '__reduce_ex__', '__reversed__',
+        '__rfloorfiv__', '__rlshift__', '__rmatmul__', '__rmod__', '__rmul__',
+        '__ror__', '__rpow__', '__rrshift__', '__rshift__', '__rsub__',
+        '__rtruediv__', '__rxor__', '__setitem__', '__sub__', '__truediv__',
+        '__xor__',
 ]:
     if hasattr(Device, name):
         setattr(DeviceAlias, name, make_method(name))
