1bebc66bb34335ebd15e3ed7da0fca1010508417
Jens Krüger <jens.krueger@frm2.tum.de>
2020-09-18 16:38:14 +0200

    ICTRL: Reformat monitor setups
    Change-Id: Ida58fd3b93fee480c7083fc3d4ac47f1b311db6e
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/23969
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/ictrl/setups/special/monitor.py
+++ nicos_mlz/ictrl/setups/special/monitor.py
@@ -3,13 +3,14 @@ group = 'special'
 
 _expcolumn = Column(
     Block('Experiment', [
-        BlockRow(Field(name='Proposal', key='exp/proposal', width=7),
-                 Field(name='Title',    key='exp/title',    width=20,
-                       istext=True, maxlen=20),
-                 Field(name='Current status', key='exp/action', width=40,
-                       istext=True, maxlen=40),
-                 Field(name='Last file', key='exp/lastscan'),
-                ),
+        BlockRow(
+            Field(name='Proposal', key='exp/proposal', width=7),
+            Field(name='Title', key='exp/title', width=20, istext=True,
+                  maxlen=20),
+            Field(name='Current status', key='exp/action', width=40,
+                  istext=True, maxlen=40),
+            Field(name='Last file', key='exp/lastscan'),
+        ),
         ],
     ),
 )
