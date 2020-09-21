52222f341041725036b0a3bf3904a8af9f446e71
Jens Krüger <jens.krueger@frm2.tum.de>
2020-09-21 14:34:14 +0200

    SE: Reformat monitor setups
    Change-Id: Iface9de0c35b506acf5dfc64c01c3391a8bba1b8
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/23974
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/se/setups/special/monitor.py
+++ nicos_mlz/se/setups/special/monitor.py
@@ -3,18 +3,21 @@ group = 'special'
 
 _expcolumn = [
     Block('Experiment', [
-        BlockRow(Field(name='Proposal', key='exp/proposal', width=7),
-                 Field(name='Title', key='exp/title', width=30, istext=True,
+        BlockRow(
+            Field(name='Proposal', key='exp/proposal', width=7),
+            Field(name='Title', key='exp/title', width=30, istext=True,
                        maxlen=40),
-                 Field(name='Sample', key='sample/samplename', width=30, istext=True),
-                 Field(name='Remark', key='exp/remark', width=30, istext=True),
-                )
+            Field(name='Sample', key='sample/samplename', width=30, istext=True),
+            Field(name='Remark', key='exp/remark', width=30, istext=True),
+        )
         ],
     ),
 ]
 
 _reactorblock = Block('Reactor', [
-    BlockRow(Field(name='Power', dev='ReactorPower'), ),
+    BlockRow(
+        Field(name='Power', dev='ReactorPower'),
+    ),
     ]
 )
 
