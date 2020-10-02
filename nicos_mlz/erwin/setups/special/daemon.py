fa1fc4182cc5198508c5e27fd03ad693d58f7784
Jens Krüger <jens.krueger@frm2.tum.de>
2020-10-02 17:45:12 +0200

    MLZ: Create the ErWIN instrument
    Change-Id: I74a2ff88f3e861f8af590393e0078cf469f9776d
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/24028
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/erwin/setups/special/daemon.py
@@ -0,0 +1,24 @@
+description = 'setup for the execution daemon'
+
+group = 'special'
+
+import hashlib
+
+devices = dict(
+    Auth = device('nicos.services.daemon.auth.list.Authenticator',
+        hashing = 'sha1',
+        # for the meaning of these entries see
+        # https://forge.frm2.tum.de/nicos/doc/nicos-stable/services/daemon/#nicos.services.daemon.auth.list.Authenticator
+        passwd = [
+            ('guest', '', 'guest'),
+            ('user', hashlib.sha1(b'user').hexdigest(), 'user'),
+            ('admin', hashlib.sha1(b'admin').hexdigest(), 'admin'),
+        ],
+    ),
+    # PamAuth = device('nicos.services.daemon.auth.pam.Authenticator'),
+    Daemon = device('nicos.services.daemon.NicosDaemon',
+        authenticators = ['Auth'],
+        loglevel = 'debug',
+        server = '',
+    ),
+)
