25d65da86f52baa042ffaa49c9e9b7e7f2e84022
Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
2020-10-05 17:07:03 +0200

    adapt ccm5v5 setup
    ccm5v5 got a new ControlBox and a new name (was ccm55v)
    
    Change-Id: I653a62715d58dd5496f05951ebf0c51cc2df9989
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/24027
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Tested-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

--- nicos_mlz/frm2/setups/ccm5v5.py
+++ nicos_mlz/frm2/setups/ccm5v5.py
@@ -4,31 +4,36 @@ group = 'plugplay'
 
 includes = ['alias_B', 'alias_sth']
 
-# setupname is set by nicos before loading this file
-# setupname = filename - '.py' extension
-nethost = setupname
-
+tango_base = 'tango://%s:10000/box/' % setupname
 
 devices = {
-    'B_%s' % setupname: device('nicos.devices.taco.CurrentSupply',
+    'B_%s' % setupname: device('nicos.devices.tango.Actuator',
         description = 'The magnetic field',
-        tacodevice = '//%s/magnet/smc120/t' % nethost,
+        tangodevice = tango_base + 'magnet/field',
         abslimits = (-5.555, 5.555),
     ),
-    'sth_%s' % setupname: device('nicos.devices.taco.Axis',
+    'sth_%s' % setupname: device('nicos.devices.generic.Axis',
         description = 'Cryotstat tube rotation',
-        tacodevice = '//%s/magnet/axis/tube' % nethost,
         abslimits = (-180, 180),
+        motor = device('nicos.devices.tango.Motor',
+            tangodevice = tango_base + 'motor/motor',
+            lowlevel = True,
+            ),
+        coder = device('nicos.devices.tango.Sensor',
+            tangodevice = tango_base + 'motor/encoder',
+            lowlevel = True,
+            ),
+        precision = 0.001,
     ),
 }
 
-# Maximum temeratures for field operation above 80A (6.6T) taken from the manual
+# Maximum temeratures for field operation above 80A taken from the manual
 maxtemps = [None, 4.3, 4.3, 5.1, 4.7, None, None, None, 4.3]
 
 for i in range(1, 9):
-    dev = device('nicos.devices.taco.TemperatureSensor',
+    dev = device('nicos.devices.tango.Sensor',
         description = '5.5T magnet temperature sensor %d' % i,
-        tacodevice = '//%s/magnet/ls218/sens%d' % (nethost, i),
+        tangodevice = tango_base + 'lakeshore/sensor%d' % i,
         warnlimits = (0, maxtemps[i]),
         pollinterval = 30,
         maxage = 90,
