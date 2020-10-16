7c309eb3ed99dfcf8bcb691973ef64fe03b6c931
Matt Clarke <matt.clarke@esss.se>
2020-10-16 13:04:44 +0200

    ESS: Changes to event receiver PVs
    The EPICS PVs for YMIR have changed as YMIR now has its own
    event receiver.
    
    Change-Id: I18a8d08a8558894c19e99e5d227d583a3ea96e7b
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/24172
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@esss.se>

--- nicos_ess/ymir/setups/event_receiver.py
+++ nicos_ess/ymir/setups/event_receiver.py
@@ -1,12 +1,14 @@
 description = 'Event Receiver setup.'
 
-pv_root = 'LabS-Utgard-VIP'
+pv_root = 'Utg-Ymir:TS-EVR-01:'
 
 devices = dict(
-    EVR_time = device('nicos.devices.epics.EpicsStringReadable',
-                readpv = '{}Time:Valid-Sts'.format(pv_root),
-                description='Status of the EVR timing'),
-    EVR_link = device('nicos.devices.epics.EpicsStringReadable',
-                readpv = '{}Link-Sts'.format(pv_root),
-                description='Status of link to EVG'),
+    EVR_time=device(
+        'nicos.devices.epics.EpicsStringReadable',
+        readpv='{}Time-Valid-Sts'.format(pv_root),
+        description='Status of the EVR timing'),
+    EVR_link=device(
+        'nicos.devices.epics.EpicsStringReadable',
+        readpv='{}Link-Sts'.format(pv_root),
+        description='Status of link to EVG'),
 )
