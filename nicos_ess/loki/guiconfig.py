303b490ed7c7d4bbe451f88636e728df95ae92c9
Matt Clarke <matt.clarke@esss.se>
2020-10-19 11:53:05 +0200

    Renamed python file to match name of ui file.

--- nicos_ess/loki/guiconfig.py
+++ nicos_ess/loki/guiconfig.py
@@ -2,7 +2,7 @@ main_window = docked(
     tabbed(
         (
             "Experiment",
-            vsplit(panel("nicos_ess.loki.gui.setup_panel.ExpPanel")),  # vsplit
+            vsplit(panel("nicos_ess.loki.gui.setup_exp.ExpPanel")),  # vsplit
         ),
         (
             "Setup",
