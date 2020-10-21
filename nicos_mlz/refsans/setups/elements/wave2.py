078f6ce8164694b1fb85178edf7bad75c704e160
Jens Krüger <jens.krueger@frm2.tum.de>
2020-10-21 22:17:05 +0200

    REFSANS: Switch frequency generators to TANGO
    Change-Id: Iaebae8b9905926ebc71d2633f24603fa7389f937
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/24194
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/wave2.py
+++ nicos_mlz/refsans/setups/elements/wave2.py
@@ -2,20 +2,20 @@ description = 'Funktionsgenerator HP33220'
 
 group = 'lowlevel'
 
-nethost = 'refsanssrv.refsans.frm2'
+tango_base = 'tango://refsanshw.refsans.frm2.tum.de:10000/test/hp33220a_2/'
 
 devices = dict(
     #
     ## hp33250aserver wave2 exports
     #
-    hp33220a_2_freq = device('nicos.devices.taco.AnalogOutput',
+    hp33220a_2_freq = device('nicos.devices.tango.AnalogOutput',
         description = 'freq of wave2',
-        tacodevice = '//%s/test/hp33220a_2/freq' % nethost,
+        tangodevice = tango_base + 'freq',
         abslimits = (0, 1e6),
     ),
-    hp33220a_2_amp = device('nicos.devices.taco.AnalogOutput',
+    hp33220a_2_amp = device('nicos.devices.tango.AnalogOutput',
         description = 'amp of wave2',
-        tacodevice = '//%s/test/hp33220a_2/amp' % nethost,
+        tangodevice = tango_base + 'amp',
         abslimits = (0, 10),
     ),
 )
