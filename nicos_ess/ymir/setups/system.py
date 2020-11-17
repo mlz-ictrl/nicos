43f63c0c6f8a5ef07754f2322c4cfc2e0599eaf7
Matt Clarke <matt.clarke@esss.se>
2020-11-17 08:40:48 +0000

    Use Freia data for just-bin-it

--- nicos_ess/ymir/setups/system.py
+++ nicos_ess/ymir/setups/system.py
@@ -47,7 +47,7 @@ devices = dict(
 
     det=device('nicos_ess.devices.datasources.just_bin_it.JustBinItDetector',
                description='The just-bin-it histogrammer', hist_topic='nicos1',
-               data_topic='event_data', brokers=['172.30.242.20:9092'],
+               data_topic='FREIA_detector', brokers=['172.30.242.20:9092'],
                unit='evts', command_topic='hist_commands',
                response_topic='hist_responses'),
 
