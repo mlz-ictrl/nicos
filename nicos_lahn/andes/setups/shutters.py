ccc0bb9760220cddcb43dae85745fae7fef7bf7f
Leonardo Ibanez <leoo.davinci@gmail.com>
2020-11-19 17:48:23 -0300

    ANDES: modified setup.
    Change-Id: I2a9b64a5632fea9c21068c8240cf022c7aef69ab
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/24441
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

--- nicos_lahn/andes/setups/shutters.py
+++ nicos_lahn/andes/setups/shutters.py
@@ -3,12 +3,16 @@ description = 'shutter devices'
 group = 'lowlevel'
 
 devices = dict(
+    shutter1 = device('nicos.devices.generic.ManualSwitch',
+        description = 'shutter 1',
+        states = ['closed', 'open'],
+    ),
     shutter2 = device('nicos.devices.generic.ManualSwitch',
         description = 'shutter 2',
-        states = ['open', 'closed'],
+        states = ['closed', 'open'],
     ),
     shutter3 = device('nicos.devices.generic.ManualSwitch',
         description = 'shutter 3',
-        states = ['open', 'closed'],
+        states = ['closed', 'open'],
     ),
 )
