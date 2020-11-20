f4feb8aaa46db97b483fd5b9339a71c1bf837a29
Jens Krüger <jens.krueger@frm2.tum.de>
2020-11-20 10:29:37 +0100

    KOMPASS: Add CAEN FAST power supplies
    Fixes: #4284
    
    Change-Id: Ia1c1c06b5c91a68f75ac5f10ad06c4e929e87678
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/24442
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/kompass/setups/caenels.py
@@ -0,0 +1,18 @@
+description = 'CAEN ELS FAST powersupplies'
+
+group = 'optional'
+
+tango_base = 'tango://kompasshw.kompass.frm2.tum.de:10000/kompass/'
+
+devices = dict(
+    caenels1 = device('nicos.devices.tango.PowerSupply',
+        description = 'FAST power supply 1',
+        tangodevice = tango_base + 'fastps1/ps',
+        fmtstr = '%.3f',
+    ),
+    caenels2 = device('nicos.devices.tango.PowerSupply',
+        description = 'FAST power supply 2',
+        tangodevice = tango_base + 'fastps2/ps',
+        fmtstr = '%.3f',
+    ),
+)
