df5a1c3bf39b738a463d922c6a8eed90e08bbd39
Jens Krüger <jens.krueger@frm2.tum.de>
2020-11-23 15:43:41 +0100

    PGAA: Fix the beam focus device
    Due to the parallel work of TANGO devices the setting of the output
    is not parallel safe.
    
    Change-Id: Iac19fbc4ee428d3ea43aa17920edfd08fca7b462
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/24452
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/pgaa/devices/beamfocus.py
+++ nicos_mlz/pgaa/devices/beamfocus.py
@@ -52,15 +52,20 @@ class BeamFocus(Moveable):
         self._attached_ellipse.move(0)
         self._attached_collimator.move(0)
 
-    def doStart(self, pos):
-        if pos != self.doRead(0):
-            if pos == 'Ell':
-                self._attached_collimator.move(0)
-                self._attached_ellipse.move(1)
-            elif pos == 'Col':
-                self._attached_ellipse.move(0)
-                self._attached_collimator.move(1)
+    def doStart(self, target):
+        if not self.isAtTarget(target=target):
+            devs = [self._attached_ellipse, self._attached_collimator]
+            pos = [1, 0] if target == 'Ell' else [0, 1]
+            for d, p in zip(devs, pos):
+                d.maw(p)
             self._hw_wait()
+
+    def isAtTarget(self, pos=None, target=None):
+        if target is None:
+            target = self.target
+        if pos is None:
+            pos = self.doRead(0)
+        return pos == target
 
     def doRead(self, maxage=0):
         ell = self._attached_ellipse.read(maxage)
