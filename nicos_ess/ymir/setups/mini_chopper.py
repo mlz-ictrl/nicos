cf685a08e45a2997a9f7e22663f37deaebc8af8b
Matt Clarke <matt.clarke@esss.se>
2021-01-26 13:59:49 +0100

    ESS: PV root for mini-chopper has changed and the abs limit was wrong
    Updated the PV to the new value.
    The absolute limits were out by a factor of 10!
    
    Change-Id: Ic43fa53d34320e8684c585a373199ab74c6b4ebc
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/24931
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@esss.se>

--- nicos_ess/ymir/setups/mini_chopper.py
+++ nicos_ess/ymir/setups/mini_chopper.py
@@ -1,6 +1,6 @@
 description = "The mini-chopper for YMIR"
 
-pv_root = "LabS-VIP:Chop-Drv-0101:"
+pv_root = "Utg-Ymir:Chop-Drv-0101:"
 
 devices = dict(
     Chopper_speed=device(
@@ -8,7 +8,7 @@ devices = dict(
         description="The speed setpoint.",
         readpv="{}Spd_Stat".format(pv_root),
         writepv="{}Spd_SP".format(pv_root),
-        abslimits=(0.0, 140),
+        abslimits=(0.0, 14),
     ),
     Chopper_status=device(
         "nicos.devices.epics.EpicsStringReadable",
