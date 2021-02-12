fb55267f75d853bf88a7e10b4e2d06d349aacbfd
Georg Brandl <g.brandl@fz-juelich.de>
2021-02-12 17:40:48 +0100

    demo/mcstas: decode messages from McStas
    Change-Id: Ie1310e475e05914d1729e9173f9958d661fcdd66
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/25024
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos_demo/devices/mcstas.py
+++ nicos_demo/devices/mcstas.py
@@ -188,11 +188,11 @@ class McStasImage(ImageChannelMixin, PassiveChannel):
             if out:
                 self.log.debug('McStas output:')
                 for line in out.splitlines():
-                    self.log.debug('[McStas] %s', line)
+                    self.log.debug('[McStas] %s', line.decode('utf-8', 'ignore'))
             if err:
                 self.log.warning('McStas found some problems:')
                 for line in err.splitlines():
-                    self.log.warning('[McStas] %s', line)
+                    self.log.warning('[McStas] %s', line.decode('utf-8', 'ignore'))
         except OSError as e:
             self.log.error('Execution failed: %s', e)
         if self._process:
