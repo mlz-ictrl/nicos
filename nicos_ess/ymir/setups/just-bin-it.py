568290208032958426443098a5bb436d26eaa539
Matt Clarke <matt.clarke@esss.se>
2021-02-19 09:35:31 +0100

    ESS: Change JustBinItDetector to be a proper "Detector"
    To work properly with the multi-detector view, the JustBinItDetector needs to be a subclass of Detector.
    Correspondingly, some of the histogramming functionality has been moved into a new ImageChannel-based class to enable JustBinItDetector to have multiple images.
    Hopefully this makes sense...
    
    Change-Id: I1aaa579033e116e8cd29431b4f64b89630cd89da
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/25046
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Michele Brambilla <michele.brambilla@psi.ch>

--- nicos_ess/ymir/setups/just-bin-it.py
+++ nicos_ess/ymir/setups/just-bin-it.py
@@ -1,13 +1,37 @@
 description = 'The just-bin-it histogrammer.'
 
 devices = dict(
+    det_image1=device(
+        'nicos_ess.devices.datasources.just_bin_it.JustBinItImage',
+        description='A just-bin-it image channel',
+        hist_topic='hist-topic-1',
+        data_topic='FREIA_detector',
+        brokers=['172.30.242.20:9092'],
+        unit='evts',
+        hist_type='2-D DET',
+        det_width=32,
+        det_height=192,
+        det_range=(1, 6144),
+        ),
+    det_image2=device(
+        'nicos_ess.devices.datasources.just_bin_it.JustBinItImage',
+        description='A just-bin-it image channel',
+        hist_topic='hist-topic-2',
+        data_topic='FREIA_detector',
+        brokers=['172.30.242.20:9092'],
+        unit='evts',
+        hist_type='2-D TOF',
+        det_width=32,
+        det_height=192,
+    ),
     det=device('nicos_ess.devices.datasources.just_bin_it.JustBinItDetector',
-               description='The just-bin-it histogrammer',
-               hist_topic='hist-topic', data_topic='FREIA_detector',
-               brokers=['172.30.242.20:9092'], unit='evts',
-               command_topic='hist_commands', response_topic='hist_responses',
-               hist_type='2-D DET', det_width=32, det_height=192,
-               det_range=(1, 6144)),
+        description='The just-bin-it histogrammer',
+        brokers=['172.30.242.20:9092'],
+        unit='',
+        command_topic='hist_commands',
+        response_topic='hist_responses',
+        images=['det_image1', 'det_image2'],
+    ),
 )
 
 startupcode = '''
