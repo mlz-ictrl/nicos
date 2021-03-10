80a8dafc940e26025096418af236fe71c9407ae7
Matt Clarke <matt.clarke@esss.se>
2021-03-10 11:03:27 +0100

    Copy works, but shortcut clashes

--- nicos_ess/loki/gui/loki_scriptbuilder.py
+++ nicos_ess/loki/gui/loki_scriptbuilder.py
@@ -3,10 +3,10 @@ from functools import partial
 from nicos.clients.gui.panels import Panel
 from nicos.clients.gui.utils import loadUi
 from nicos.guisupport.qt import pyqtSlot, QTableWidgetItem, QHeaderView, \
-    Qt
+    Qt, QShortcut, QKeySequence, QApplication
 from nicos.utils import findResource
 
-TABLE_QSS = "alternate-background-color: aliceblue;"
+TABLE_QSS = 'alternate-background-color: aliceblue;'
 
 
 class LokiScriptBuilderPanel(Panel):
@@ -23,17 +23,17 @@ class LokiScriptBuilderPanel(Panel):
         self.duration_options = ['Mevents', 'seconds', 'frames']
 
         self.permanent_columns = {
-            "position": "Position",
-            "sample": "Sample",
-            "thickness": "Thickness\n(mm)",
-            "trans_duration": "TRANS Duration",
-            "sans_duration": "SANS Duration"
+            'position': 'Position',
+            'sample': 'Sample',
+            'thickness': 'Thickness\n(mm)',
+            'trans_duration': 'TRANS Duration',
+            'sans_duration': 'SANS Duration'
         }
 
         self.optional_columns = {
-            "temperature": ("Temperature", self.chkShowTempColumn),
-            "pre-command": ("Pre-command", self.chkShowPreCommand),
-            "post-command": ("Post-command", self.chkShowPostCommand)
+            'temperature': ('Temperature', self.chkShowTempColumn),
+            'pre-command': ('Pre-command', self.chkShowPreCommand),
+            'post-command': ('Post-command', self.chkShowPostCommand)
         }
 
         self.columns_in_order = [name for name in self.permanent_columns.keys()]
@@ -59,9 +59,9 @@ class LokiScriptBuilderPanel(Panel):
             self._hide_column(name)
 
         # Configure duration type combo-boxes
-        self._link_duration_combobox_to_column("sans_duration",
+        self._link_duration_combobox_to_column('sans_duration',
                                                self.comboSansDurationType)
-        self._link_duration_combobox_to_column("trans_duration",
+        self._link_duration_combobox_to_column('trans_duration',
                                                self.comboTransDurationType)
 
         # Set up trans order combo-box
@@ -77,6 +77,72 @@ class LokiScriptBuilderPanel(Panel):
 
         self.tableScript.setRowCount(num_rows)
 
+        QShortcut(QKeySequence.Paste, self.tableScript).activated.connect(
+            self._handle_table_paste)
+
+        temp = QShortcut(QKeySequence.Copy, self.tableScript)
+
+        temp.activated.connect(self._handle_copy_cells)
+        temp.setContext(Qt.WidgetWithChildrenShortcut)
+        print(temp)
+
+    def _handle_copy_cells(self):
+        if len(self.tableScript.selectedRanges()) != 1:
+            # Can only select one continuous region to copy
+            return
+        selected_data = self._extract_selected_data()
+        QApplication.instance().clipboard().setText('\n'.join(selected_data))
+        print('\n'.join(selected_data))
+
+    def _extract_selected_data(self):
+        selected_data = []
+        row_data = []
+        curr_row = -1
+        for index in self.tableScript.selectionModel().selectedIndexes():
+            if curr_row != index.row():
+                if row_data:
+                    selected_data.append('\t'.join(row_data))
+                    row_data.clear()
+                curr_row = index.row()
+            cell_text = self._get_cell_text(index.row(), index.column())
+            row_data.append(cell_text)
+        if row_data:
+            selected_data.append('\t'.join(row_data))
+            row_data.clear()
+        return selected_data
+
+    def _get_cell_text(self, row, column):
+        cell = self.tableScript.item(row, column)
+        if cell:
+            return cell.text()
+        return ''
+
+    def _handle_table_paste(self):
+        indices = []
+        for index in self.tableScript.selectionModel().selectedIndexes():
+            indices.append((index.row(), index.column()))
+        top_left = indices[0]
+
+        clipboard_text = QApplication.instance().clipboard().text()
+        data_type = QApplication.instance().clipboard().mimeData()
+
+        if not data_type.hasText():
+            # Don't paste images etc.
+            return
+
+        copied_table = [[x for x in row.strip().split('\t')]
+                        for row in clipboard_text.split('\n')]
+
+        if len(copied_table) == 1 and len(copied_table[0]) == 1:
+            # Only one value, so put it in all selected cells
+            self._do_bulk_update(copied_table[0][0])
+            return
+
+        # Copied data is tabular so insert at top-left most position
+        for y, row in enumerate(copied_table):
+            for x, value in enumerate(row):
+                self._update_cell(top_left[0] + y, top_left[1] + x, value)
+
     def _link_duration_combobox_to_column(self, column_name, combobox):
         combobox.addItems(self.duration_options)
         combobox.currentTextChanged.connect(
@@ -86,8 +152,11 @@ class LokiScriptBuilderPanel(Panel):
 
     @pyqtSlot()
     def on_bulkUpdateButton_clicked(self):
+        self._do_bulk_update(self.txtValue.text())
+
+    def _do_bulk_update(self, value):
         for index in self.tableScript.selectionModel().selectedIndexes():
-            self._update_cell(index.row(), index.column(), self.txtValue.text())
+            self._update_cell(index.row(), index.column(), value)
 
     @pyqtSlot()
     def on_clearTableButton_clicked(self):
