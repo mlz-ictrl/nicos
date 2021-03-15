d0a1c14e7dd6fb3f147e0942ee2e6320b71ba720
Andreas Wilhelm <andreas.wilhelm@frm2.tum.de>
2021-03-15 09:51:14 +0100

    SANS-1: add software instrument shutter
    added software instrument shutter (we have two shutters; one only movable via manual key and one with software)
    
    tested at machine
    
    Change-Id: I143e7a1967f970ca5cdd96f078d7223fce0bee36
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/25775
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/sans1/devices/shutter.py
@@ -0,0 +1,43 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2021 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
+#
+# *****************************************************************************
+
+from nicos.core import HasTimeout, Override, status
+from nicos.devices.generic import MultiSwitcher
+
+
+class Shutter(HasTimeout, MultiSwitcher):
+    """Combine the MultiSwitcher with HasTimeout."""
+
+    parameter_overrides = {
+        'timeout': Override(default=13),
+        'fallback': Override(default='?'),
+    }
+    hardware_access = False
+    relax_mapping = True
+
+    def doStatus(self, maxage=0):
+        r = self.read(maxage)
+        if r == self.fallback:
+            return status.BUSY, ''
+        return status.OK, ''
