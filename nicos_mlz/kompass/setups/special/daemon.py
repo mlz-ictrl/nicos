a3924ca8c3755423c51b205c73afcb7172ac4fb4
Jens Krüger <jens.krueger@tum.de>
2021-03-18 21:12:37 +0100

    KOMPASS: drop list authenticator support
    Change-Id: I5846d05a43da5d405bb1a5cbb5a11afbe44b133e
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/25231
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/kompass/setups/special/daemon.py
+++ nicos_mlz/kompass/setups/special/daemon.py
@@ -2,13 +2,6 @@ description = 'setup for the execution daemon'
 group = 'special'
 
 devices = dict(
-    Auth = device('nicos.services.daemon.auth.list.Authenticator',
-        hashing = 'md5',
-        passwd = [('guest', '', 'guest'),
-                  # ('user', 'd3bde5ce3e546626df42771c58986d4e', 'user'),
-                  # ('admin', 'f3309476bdb36550aa8fb90ae748c9cc', 'admin'),
-                 ],
-    ),
     UserDBAuth = device('nicos_mlz.devices.ghost.Authenticator',
          description = 'FRM II user office authentication',
          instrument = 'KOMPASS',
@@ -29,7 +22,7 @@ devices = dict(
     ),
     Daemon = device('nicos.services.daemon.NicosDaemon',
         server = '',
-        authenticators = ['UserDBAuth', 'LDAPAuth', 'Auth',],
+        authenticators = ['UserDBAuth', 'LDAPAuth',],
         loglevel = 'info',
     ),
 )
