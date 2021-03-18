d11e7734c0fe3d0d2abb2f51b28d53a8fb667bd4
Jens Krüger <jens.krueger@tum.de>
2021-03-18 21:13:45 +0100

    NECTAR: Drop list authenticator support
    Change-Id: I7ae8a9890ac3bfc4491764427cf203e09963bc28
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/25232
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/nectar/setups/special/daemon.py
+++ nicos_mlz/nectar/setups/special/daemon.py
@@ -20,17 +20,9 @@ devices = dict(
             'se': 'user',
         }
     ),
-    Auth = device('nicos.services.daemon.auth.list.Authenticator',
-        hashing = 'md5',
-        passwd = [
-            ('guest', '', 'guest'),
-            # ('user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user'),
-            # ('admin', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
-        ],
-    ),
     Daemon = device('nicos.services.daemon.NicosDaemon',
         server = '0.0.0.0',
-        authenticators = ['UserDBAuth', 'LDAPAuth', 'Auth'],
+        authenticators = ['UserDBAuth', 'LDAPAuth'],
         loglevel = 'info',
     ),
 )
