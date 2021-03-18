664ee1824704d00775d00b641ef43ca20284bcac
Jens Krüger <jens.krueger@frm2.tum.de>
2021-03-18 19:11:49 +0100

    ANTARES: Drop simple authenication support
    Change-Id: If62b02fc571b0c081174c25e62d70d4f10b3d74c
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/25225
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/antares/setups/special/daemon.py
+++ nicos_mlz/antares/setups/special/daemon.py
@@ -12,6 +12,7 @@ devices = dict(
         grouproles = {
             'antares': 'admin',
             'ictrl': 'admin',
+            'se': 'guest',
         }
     ),
     UserDBAuth = device('nicos_mlz.devices.ghost.Authenticator',
@@ -23,20 +24,13 @@ devices = dict(
          },
          loglevel = 'info',
     ),
-    Auth = device('nicos.services.daemon.auth.list.Authenticator',
-        description = 'Authentication device',
-        hashing = 'md5',
-        # first entry is the user name, second the hashed password, third the user level
-        passwd = [
-            ('guest', '', 'guest'),
-            ('user', 'ee11cbb19052e40b07aac0ca060c23ee', 'user'),
-            ('admin', '21232f297a57a5a743894a0e4a801fc3', 'admin'),
-        ],
-    ),
     Daemon = device('nicos.services.daemon.NicosDaemon',
         description = 'Daemon, executing commands and scripts',
         server = '0.0.0.0',
-        authenticators = ['LDAPAuth', 'UserDBAuth', 'Auth'],
+        authenticators = [
+            'LDAPAuth',
+            'UserDBAuth',
+        ],
         loglevel = 'debug',
     ),
 )
