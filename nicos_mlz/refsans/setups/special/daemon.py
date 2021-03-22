85f8aa9ffa786574b4bc4d18bf0a93ea89cdb35b
Jens Krüger <jens.krueger@tum.de>
2021-03-22 09:26:18 +0100

    REFSANS: Add GhOST user database login support
    Change-Id: Iee0f2fb75b49267796690884badd9a496fc46c4c
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/25247
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/special/daemon.py
+++ nicos_mlz/refsans/setups/special/daemon.py
@@ -2,6 +2,14 @@ description = 'setup for the execution daemon'
 group = 'special'
 
 devices = dict(
+    UserDBAuth = device('nicos_mlz.devices.ghost.Authenticator',
+         description = 'FRM II user office authentication',
+         instrument = 'REFSANS',
+         ghosthost = 'ghost.mlz-garching.de',
+         aliases = {
+         },
+         loglevel = 'info',
+    ),
     LDAPAuth = device('nicos.services.daemon.auth.ldap.Authenticator',
         uri = 'ldap://phaidra.admin.frm2',
         userbasedn = 'ou=People,dc=frm2,dc=de',
@@ -14,6 +22,6 @@ devices = dict(
     ),
     Daemon = device('nicos.services.daemon.NicosDaemon',
         server = '',
-        authenticators = ['LDAPAuth'],
+        authenticators = ['UserDBAuth', 'LDAPAuth'],
     ),
 )
