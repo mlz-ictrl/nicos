56ff36ee2faff811c99815d9438f26956c5af23b
AÜC Hardal <umit.hardal@ess.eu>
2021-04-12 12:04:18 +0200

    Remove class level docstring

--- nicos_ess/loki/gui/loki_panel.py
+++ nicos_ess/loki/gui/loki_panel.py
@@ -26,9 +26,6 @@ from nicos.clients.gui.panels import Panel
 
 
 class LokiPanelBase(Panel):
-    """
-    This is the base class for LoKI panel.
-    """
     def __init__(self, parent, client, options):
         Panel.__init__(self, parent, client, options)
 
