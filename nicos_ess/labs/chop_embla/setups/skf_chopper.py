ddad6b55379ffbd76de669eb9572ddb7482f9b50
Matt Clarke <matt.clarke@esss.se>
2021-04-14 13:17:09 +0200

    Wrong name!

--- nicos_ess/labs/chop_embla/setups/skf_chopper.py
+++ nicos_ess/labs/chop_embla/setups/skf_chopper.py
@@ -3,7 +3,7 @@ description = 'Some kind of SKF chopper'
 pv_root = 'LabS-Embla:Chop-Drv-0601:'
 
 devices = dict(
-    ls155_mode=device('nicos_ess.devices.epics.pva.EpicsReadable',
+    skf_drive_temp=device('nicos_ess.devices.epics.pva.EpicsReadable',
         description='Drive temperature',
         readpv='{}DrvTmp_Stat'.format(pv_root),
     ),
