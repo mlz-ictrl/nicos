351d68a17acacc5a227aa9bedf8cad6413d368c8
Matt Clarke <matt.clarke@esss.se>
2021-04-14 13:41:19 +0200

    Add more pvs

--- nicos_ess/labs/chop_embla/setups/skf_chopper.py
+++ nicos_ess/labs/chop_embla/setups/skf_chopper.py
@@ -7,4 +7,29 @@ devices = dict(
         description='Drive temperature',
         readpv='{}DrvTmp_Stat'.format(pv_root),
     ),
+    skf_status=device(
+        'nicos.devices.epics.EpicsStringReadable',
+        description='The chopper status.',
+        readpv='{}Chop_Stat'.format(pv_root),
+    ),
+    skf_control=device(
+        'nicos_ess.devices.epics.extensions.EpicsMappedMoveable',
+        description='Used to start and stop the chopper.',
+        readpv='{}Cmd'.format(pv_root),
+        writepv='{}Cmd'.format(pv_root),
+        requires={'level': 'admin'},
+        lowlevel=True,
+        mapping={'Start chopper': 6,
+                 'Stop chopper': 3,
+                 'Reset chopper': 1,
+        },
+    ),
+    skf_speed=device(
+        'nicos_ess.devices.epics.pva.EpicsAnalogMoveable',
+        description='The current speed.',
+        readpv='{}Spd_Stat'.format(pv_root),
+        writepv='{}Spd_SP'.format(pv_root),
+        abslimits=(0.0, 14),
+    ),
+
 )
