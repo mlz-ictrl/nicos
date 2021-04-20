948e88b3a87fe0651f9c2b5c0f5f47e87ba905b6
Georg Brandl <g.brandl@fz-juelich.de>
2021-04-20 11:18:16 +0200

    live: make gamma filter dependency optional
    This new dependency is not needed for many instruments and belongs
    to optional.
    
    Change-Id: I0c524d4391957e1fe4a8c487ffd5053b405ae32e
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/25588
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos/clients/gui/panels/live.py
+++ nicos/clients/gui/panels/live.py
@@ -48,7 +48,11 @@ from nicos.guisupport.qt import QActionGroup, QByteArray, QListWidgetItem, \
 from nicos.guisupport.qtgr import MouseEvent
 from nicos.protocols.cache import cache_load
 from nicos.utils import BoundedOrderedDict, ReaderRegistry
-from nicos.utils.gammafilter import gam_rem_adp_log
+
+try:
+    from nicos.utils.gammafilter import gam_rem_adp_log
+except ImportError:
+    gam_rem_adp_log = None
 
 COLORMAPS = OrderedDict(GR_COLORMAPS)
 
@@ -1047,6 +1051,11 @@ class ImagingControls(QWidget):
         self.maxSlider.sliderReleased.connect(self.showData)
         self.minSlider.sliderReleased.connect(self.showData)
 
+        if not gam_rem_adp_log:
+            self.despeckleBox.hide()
+            self.despeckleValues.hide()
+        else:
+            self.despeckleWarningLabel.hide()
         self.despeckleBox.toggled.connect(self.showData)
         self.darkfieldBox.toggled.connect(self.showData)
         self.normalizeBox.toggled.connect(self.showData)
