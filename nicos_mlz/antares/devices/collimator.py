9ef4b7cb2ca9562dc3f6694762ed1f8d412ed9c7
Michael Schulz <michael.schulz@frm2.tum.de>
2021-04-21 16:10:35 +0200

    antares: add device to calculate geometric blur
    Change-Id: If849a4fb12f932762ebc28627570b58a18f6c540
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/25626
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/antares/devices/collimator.py
+++ nicos_mlz/antares/devices/collimator.py
@@ -26,7 +26,7 @@
 
 from nicos.core.device import Readable
 from nicos.core.errors import ConfigurationError
-from nicos.core.params import Attach
+from nicos.core.params import Attach, Override
 
 
 class CollimatorLoverD(Readable):
@@ -51,3 +51,44 @@ class CollimatorLoverD(Readable):
         except ValueError:
             ret = 0
         return ret
+
+
+class GeometricBlur(Readable):
+    """Special device to calculate geometric blur.
+
+    Calculated from collimation and sample to detector distance."""
+
+    attached_devices = {
+        'l': Attach('Distance device', Readable),
+        'd': Attach('Pinhole', Readable),
+        'sdd': Attach('Sample Detector Distance', Readable),
+    }
+
+    parameter_overrides = {
+        'unit': Override(volatile=True, mandatory=False),
+    }
+
+    def doInit(self, mode):
+        units = set(d.unit for d in self._adevs.values())
+        if len(units) != 1:
+            raise ConfigurationError(self, 'different units for L, d and sdd '
+                                     '(%s vs %s vs %s)' %
+                                     (self._attached_l.unit,
+                                      self._attached_d.unit,
+                                      self._attached_sdd.unit))
+        if 'mm' not in units:
+            raise ConfigurationError(self, "attached devices units have to be "
+                                     "'mm'")
+
+    def doRead(self, maxage=0):
+        try:
+            ret = float(self._attached_sdd.read(maxage)) * \
+                float(self._attached_d.read(maxage)) / \
+                float(self._attached_l.read(maxage))
+            return 1000 * ret  # convert to um
+        except ValueError:
+            ret = 0
+        return ret
+
+    def doReadUnit(self):
+        return 'um'
