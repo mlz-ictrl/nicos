d423e075700e111f237de6d737abdd7c8480a9a6
Jens Krüger <jens.krueger@frm2.tum.de>
2021-04-22 11:26:11 +0200

    KOMPASS: add reactor power to basic setups
    Make the 'ReactorPower' device available for all basic setups.
    
    Change-Id: I9cc4315b5c0985cb6965d9bd1c9b9de95cab3e1d
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/25651
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/kompass/setups/diff.py
+++ nicos_mlz/kompass/setups/diff.py
@@ -2,6 +2,7 @@ description = 'KOMPASS diffraction mode setup'
 group = 'basic'
 
 includes = ['mono', 'guidefocus', 'selector', 'astrium', 'sample', 'alias_sth',
+            'reactor',
             'detector',
            ]
 
