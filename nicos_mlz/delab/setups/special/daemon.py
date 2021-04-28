7d09462f669160d6c3bacb41d699768b81dc1e54
Local Administrator <localadmin@mesyctrl.del.frm2.tum.de>
2021-04-28 23:57:40 +0200

    DELAB: Switch user authentication to LDAP
    Change-Id: Ibf4bc6edfd182010219fef307fde7ce6b3f1fbe7
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/25827
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/delab/setups/special/daemon.py
+++ nicos_mlz/delab/setups/special/daemon.py
@@ -2,18 +2,18 @@ description = 'setup for the daemon'
 group = 'special'
 
 devices = dict(
-    Auth = device('nicos.services.daemon.auth.list.Authenticator',
-        hashing = 'sha1',
-        # first entry is the user name, second the hashed password, third the user level
-        passwd = [
-            ('guest', '', 'guest'),
-            ('user', '12dea96fec20593566ab75692c9949596833adc9', 'user'),
-            ('admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin'),
-        ],
+    LDAPAuth = device('nicos.services.daemon.auth.ldap.Authenticator',
+        uri = 'ldap://phaidra.admin.frm2',
+        userbasedn = 'ou=People,dc=frm2,dc=de',
+        groupbasedn = 'ou=Group,dc=frm2,dc=de',
+        grouproles = {
+            'del': 'admin',
+            'ictrl': 'admin',
+        }
     ),
     Daemon = device('nicos.services.daemon.NicosDaemon',
         server = '0.0.0.0',
         loglevel = 'info',
-        authenticators = ['Auth'],
+        authenticators = ['LDAPAuth'],
     ),
 )
