e984892d1b533eca6f4c55c4d599e4e098b7ad56
Jens Krüger <jens.krueger@tum.de>
2021-04-30 10:18:32 +0200

    Demo/Antares: Add ILL specific detector
    Change-Id: I93ff80ae9220b140827cf93589f3599196ed2140
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/28332
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_demo/vantares/setups/detector_ill.py
@@ -0,0 +1,26 @@
+description = 'External ILL camera'
+
+group = 'optional'
+
+devices = dict(
+    trigger_hw = device('nicos.devices.generic.ManualSwitch',
+        states = [0, 1],
+        lowlevel = True,
+    ),
+    trigger = device('nicos.devices.generic.Pulse',
+        description = 'Camera trigger',
+        onvalue = 1,
+        offvalue = 0,
+        ontime = 0.1,
+        moveable = 'trigger_hw',
+    ),
+    timer_ill = device('nicos_mlz.antares.devices.TriggerTimer',
+        description = 'Software timer sending trigger signal',
+        trigger = 'trigger',
+    ),
+    det_ill = device('nicos.devices.generic.Detector',
+        description = 'The ILL detector',
+        timers = ['timer_ill'],
+    ),
+)
+
