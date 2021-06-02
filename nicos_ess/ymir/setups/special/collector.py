7123d28081d051bb96ee8e21d0538b1e09ae0dbc
Matt Clarke <matt.clarke@esss.se>
2021-06-02 08:19:30 +0200

    ESS: Use correct broker and topic for collector
    Added the correct broker address and topic for YMIR
    
    Change-Id: I322aa380e373599b4e9b33e59502327454aff0cf
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/26151
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@esss.se>

--- nicos_ess/ymir/setups/special/collector.py
+++ nicos_ess/ymir/setups/special/collector.py
@@ -5,8 +5,8 @@ devices = dict(
     CacheKafka=device(
         'nicos_ess.devices.cache_kafka_forwarder.CacheKafkaForwarder',
         dev_ignore=['space', 'sample'],
-        brokers=['localhost:9092'],
-        output_topic="from_nicos",
+        brokers=['172.30.242.20:9092'],
+        output_topic='ymir_nicos_devices',
     ),
     Collector=device('nicos.services.collector.Collector',
         cache='localhost:14869',
