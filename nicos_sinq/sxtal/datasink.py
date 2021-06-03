cf2d3542f115c3288818e104b132e3eb01390b34
Jens Krüger <jens.krueger@tum.de>
2021-06-03 10:20:49 +0200

    SINQ: Update copyright (fix year)
    Change-Id: Id1837944b715bc90a1131cf31559a389f163d721
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/26156
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_sinq/sxtal/datasink.py
+++ nicos_sinq/sxtal/datasink.py
@@ -1,7 +1,7 @@
 #  -*- coding: utf-8 -*-
 # *****************************************************************************
 # NICOS, the Networked Instrument Control System of the MLZ
-# Copyright (c) 2009-2020 by the NICOS contributors (see AUTHORS)
+# Copyright (c) 2009-2021 by the NICOS contributors (see AUTHORS)
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -75,10 +75,10 @@ class CCLSinkHandler(SINQAsciiSinkHandler):
 
     def begin(self):
         if not self._ccl_file:
-            self._ccl_file = open(self.dataset.filepaths[0], 'w')
+            self._ccl_file = open(self.dataset.filepaths[0], 'w')  # pylint: disable=consider-using-with
             base = os.path.splitext(self.dataset.filepaths[0])[0]
             rflfile = base + '.rfl'
-            self._rfl_file = open(rflfile, 'w')
+            self._rfl_file = open(rflfile, 'w')  # pylint: disable=consider-using-with
             self._rfl_file.write('%s\n' % rflfile)
 
     def addSubset(self, point):
