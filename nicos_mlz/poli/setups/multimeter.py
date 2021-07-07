5e93503f93c6d5e8667ba43e2a1ad5156d8c3d64
Georg Brandl <g.brandl@fz-juelich.de>
2021-07-07 08:05:05 +0200

    POLI: setup cleanup
    Change-Id: I69927def5753705da1785c6b42bbb8b09f1c6571
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/26348
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos_mlz/poli/setups/multimeter.py
+++
@@ -1,9 +0,0 @@
-description = 'HP34420 multimeter readout'
-group = 'optional'
-
-devices = dict(
-    R = device('nicos.devices.taco.AnalogInput',
-        description = 'multimeter readout',
-        tacodevice = '//heidi22/heidi2/fluke/resistance',
-    ),
-)
