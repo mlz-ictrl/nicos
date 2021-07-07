3f3499adea894e7b113797572f868be1d92bf645
Georg Brandl <g.brandl@fz-juelich.de>
2021-07-07 08:10:12 +0200

    PANDA: setup cleanup
    Change-Id: Iada572fc8a3f02d10638e4da8847d12ff7586d5e
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/26351
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos_mlz/panda/setups/multimeter.py
+++
@@ -1,9 +0,0 @@
-description = 'HP34420 multimeter readout'
-group = 'optional'
-
-devices = dict(
-    R = device('nicos.devices.taco.AnalogInput',
-        description = 'multimeter readout',
-        tacodevice = '//phys.panda.frm2/panda/hp34420/resistance',
-    ),
-)
