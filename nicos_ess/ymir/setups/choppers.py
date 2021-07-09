3b1ceb34aba1da1dbfa55ffde710b8e6c91229b9
Matt Clarke <matt.clarke@esss.se>
2021-07-09 07:34:13 +0200

    ESS: rename choppers setup and devices
    Based on feedback from demo.
    
    Change-Id: I38470523d60e8eaeef2b8bb5f58a560ca8e7d57b
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/26363
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@esss.se>

--- nicos_ess/ymir/setups/choppers.py
+++ nicos_ess/ymir/setups/choppers.py
@@ -3,12 +3,12 @@ description = 'The mini-chopper for YMIR'
 pv_root = 'Utg-Ymir:Chop-Drv-0101:'
 
 devices = dict(
-    Chopper_status=device(
+    mini_chopper_status=device(
         'nicos.devices.epics.EpicsStringReadable',
         description='The chopper status.',
         readpv='{}Chop_Stat'.format(pv_root),
     ),
-    Chopper_control=device(
+    mini_chopper_control=device(
         'nicos_ess.devices.epics.extensions.EpicsMappedMoveable',
         description='Used to start and stop the chopper.',
         readpv='{}Cmd'.format(pv_root),
@@ -20,14 +20,14 @@ devices = dict(
                  'Reset chopper': 1,
         },
     ),
-    Chopper_speed=device(
+    mini_chopper_speed=device(
         'nicos.devices.epics.EpicsAnalogMoveable',
         description='The current speed.',
         readpv='{}Spd_Stat'.format(pv_root),
         writepv='{}Spd_SP'.format(pv_root),
         abslimits=(0.0, 14),
     ),
-    Chopper_delay=device(
+    mini_chopper_delay=device(
         'nicos.devices.epics.EpicsAnalogMoveable',
         description='The current delay.',
         readpv='{}Chopper-Delay-SP'.format(pv_root),
