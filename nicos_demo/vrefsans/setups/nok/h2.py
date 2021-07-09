9df041d9f4ecfae63bb02eeccc194b045039114e
Jens Krüger <jens.krueger@frm2.tum.de>
2021-07-09 09:42:04 +0200

    Refsans: Add gap device
    Change-Id: Icbc6e1bdd1aa6b629ccfa0e23e6df8b4eaeaf1fd
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/26370
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_demo/vrefsans/setups/nok/h2.py
+++ nicos_demo/vrefsans/setups/nok/h2.py
@@ -3,16 +3,29 @@ description = 'Slit H2 using Beckhoff controllers'
 group = 'lowlevel'
 
 devices = dict(
-    h2_center = device('nicos.devices.generic.VirtualMotor',
-        description = 'Horizontal slit system: offset of the slit-center to the beam',
+    h2l = device('nicos.devices.generic.VirtualMotor',
+        description = 'left slit motor',
+        abslimits = (-69.5, 0),
         unit = 'mm',
-        abslimits = (-69.5, 69.5),
-        speed = 1.,
+        lowlevel = True,
     ),
-    h2_width = device('nicos.devices.generic.VirtualMotor',
-        description = 'Horizontal slit system: opening of the slit',
+    h2r = device('nicos.devices.generic.VirtualMotor',
+        description = 'right slit motor',
+        abslimits = (0, 69.5),
         unit = 'mm',
-        abslimits = (0.05, 69.5),
-        speed = 1.,
+        lowlevel = True,
     ),
+    h2 = device('nicos_mlz.refsans.devices.slits.Gap',
+        description = 'Horizontal slit system',
+        left = 'h2l',
+        right = 'h2r',
+        opmode = 'offcentered',
+    ),
+    h2_center = device('nicos.devices.generic.DeviceAlias'),
+    h2_width = device('nicos.devices.generic.DeviceAlias'),
 )
+
+alias_config = {
+    'h2_width':  {'h2.width': 100},
+    'h2_center': {'h2.center': 100},
+}
