fd3be452266b4b8e9384bae8e7647e576c4d3fd3
Jens Krüger <jens.krueger@frm2.tum.de>
2021-07-14 09:10:53 +0200

    SINQ/SANS: Fix GUI config
    Change-Id: I0b5ad120af5903b73245a2d531d8d56ac0d80f57
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/26389
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Mark Koennecke <mark.koennecke@psi.ch>

--- nicos_sinq/sans/guiconfig.py
+++ nicos_sinq/sans/guiconfig.py
@@ -37,33 +37,33 @@ main_window = docked(
                   positions=22, setups='sans and sc2')
               ),
              ('Instrument',
-              panel('nicos_ess.gui.panels.setup_panel.SetupsPanel')),
+              panel('nicos.clients.flowui.panels.setup_panel.SetupsPanel')),
          ),
         ),
-        ('  ', panel('nicos_ess.gui.panels.empty.EmptyPanel')),
+        ('  ', panel('nicos.clients.flowui.panels.empty.EmptyPanel')),
         ('Instrument interaction',
          hsplit(
              vbox(
                  panel(
-                     'nicos_ess.gui.panels.cmdbuilder.CommandPanel',
+                     'nicos.clients.flowui.panels.cmdbuilder.CommandPanel',
                       modules=['nicos.clients.gui.cmdlets'],
                     ),
                  tabbed(
                      ('Output',
-                      panel('nicos_ess.gui.panels.console.ConsolePanel',
+                      panel('nicos.clients.flowui.panels.console.ConsolePanel',
                             hasinput=False)),
                      ('Scan Plot',
-                      panel('nicos_ess.gui.panels.scans.ScansPanel')),
+                      panel('nicos.clients.flowui.panels.scans.ScansPanel')),
                      ('Detector Image',
-                      panel('nicos_ess.gui.panels.live.LiveDataPanel')),
+                      panel('nicos.clients.flowui.panels.live.LiveDataPanel')),
                      ('Script Status',
-                      panel('nicos_ess.gui.panels.status.ScriptStatusPanel',
+                      panel('nicos.clients.flowui.panels.status.ScriptStatusPanel',
                             eta=True)),
                  ),
 
              ), # vsplit
              panel(
-                 'nicos_ess.gui.panels.devices.DevicesPanel',
+                 'nicos.clients.flowui.panels.devices.DevicesPanel',
                  dockpos='right',
                  param_display={'tas': 'scanmode',
                                 'Exp': ['lastpoint', 'lastscan']},
@@ -77,14 +77,14 @@ main_window = docked(
             'Batch file generation',
             vsplit(
                 panel('nicos.clients.gui.panels.scriptbuilder.CommandsPanel'),
-                panel('nicos_ess.gui.panels.editor.EditorPanel',
+                panel('nicos.clients.flowui.panels.editor.EditorPanel',
                       tools=None),
             ), # vsplit
         ),
-        ('Detector Image', panel('nicos_ess.gui.panels.live.LiveDataPanel')),
+        ('Detector Image', panel('nicos.clients.flowui.panels.live.LiveDataPanel')),
         (
             'History',
-            panel('nicos_ess.gui.panels.history.HistoryPanel'),
+            panel('nicos.clients.flowui.panels.history.HistoryPanel'),
         ),
         ('Logs',
             tabbed(
@@ -95,10 +95,10 @@ main_window = docked(
          ),
 
 
-        ('  ', panel('nicos_ess.gui.panels.empty.EmptyPanel')),
+        ('  ', panel('nicos.clients.flowui.panels.empty.EmptyPanel')),
 
         ('Finish Experiment',
-         panel('nicos_ess.gui.panels.setup_panel.FinishPanel')),
+         panel('nicos.clients.flowui.panels.setup_panel.FinishPanel')),
 
         position='left',
     ), # tabbed
