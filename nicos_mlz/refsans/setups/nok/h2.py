437adeb07f4e46324a77835d886a460364d39595
Matthias Pomm <matthias.pomm@hereon.de>
2021-07-29 08:52:31 +0200

    REFSANS: Change h2 setup
    use aliases for the h2_width and h2_height devices
    
    Change-Id: I29267266a898f152c6b4973eb48c447cc2d4b2f7
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/28141
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/nok/h2.py
+++ nicos_mlz/refsans/setups/nok/h2.py
@@ -1,22 +1,48 @@
-description = 'Slit H2 using Beckhoff controllers using PILS'
+description = 'Slit H2 using Beckhoff controllers'
 
 group = 'lowlevel'
 
 instrument_values = configdata('instrument.values')
-
 tango_base = instrument_values['tango_base'] + 'h2/plc/'
+code_base = instrument_values['code_base']
 
 devices = dict(
-    h2_center = device('nicos.devices.entangle.Actuator',
-        description = 'Horizontal slit system: offset of the slit-center to the beam. towards TOFTOF is plus',
-        tangodevice = tango_base + '_centeraxis',
+    h2_ctrl1 = device('nicos.devices.entangle.Actuator',
+        description = 'Horizontal slit: KWS side. towards TOFTOF is plus',
+        tangodevice = tango_base + '_axis1',
         unit = 'mm',
-        abslimits = (-69.5, 69.5),
+        visibility = (),
     ),
-    h2_width = device('nicos.devices.entangle.Actuator',
-        description = 'Horizontal slit system: opening of the slit',
-        tangodevice = tango_base + '_widthaxis',
+    h2_ctrl2 = device('nicos.devices.entangle.Actuator',
+        description = 'Horizontal slit: TOFTOF side',
+        tangodevice = tango_base + '_axis2',
         unit = 'mm',
-        abslimits = (0.05, 69.5),
+        visibility = (),
     ),
+    h2_motor1 = device(code_base + 'analogencoder.AnalogMove',
+        description = 'Beam right; dir and offset',
+        device = 'h2_ctrl1',
+        unit = 'mm',
+        poly = [61.6156,  -1], #2021-07-23 MP von LH
+        visibility = (),
+    ),
+    h2_motor2 = device(code_base + 'analogencoder.AnalogMove',
+        description = 'Beam left; dir and offset',
+        device = 'h2_ctrl2',
+        unit = 'mm',
+        poly = [60.3869,  -1], #2021-07-23 MP von LH
+        visibility = (),
+    ),
+    h2 = device('nicos_mlz.refsans.devices.slits.Gap',
+        description = 'Horizontal slit system',
+        left = 'h2_motor1',
+        right = 'h2_motor2',
+        opmode = 'offcentered',
+    ),
+    h2_center = device('nicos.devices.generic.DeviceAlias'),
+    h2_width = device('nicos.devices.generic.DeviceAlias'),
 )
+alias_config = {
+    'h2_width':  {'h2.width': 100},
+    'h2_center': {'h2.center': 100},
+}
