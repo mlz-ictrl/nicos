0ccb59cc5b4c08958f919ab40cb78e0284256ff1
Andreas Wilhelm <andreas.wilhelm@frm2.tum.de>
2021-08-06 06:41:24 +0200

    SANS-1: remove a device
    remove a unused device
    
    Change-Id: I77ee4fdb01642fc46a4466225d87ad3d05eff989
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/26500
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

--- nicos_mlz/sans1/setups/special/monitor-sans1pi2.py
+++ nicos_mlz/sans1/setups/special/monitor-sans1pi2.py
@@ -483,7 +483,7 @@ _spinflipper = Block('Spin Flipper', [
         Field(name='Reverse', key='P_spinflipper/reverse', unitkey='W'),
     ),
     BlockRow(
-        Field(name='Temperature', dev='T_spinflipper'),
+        #Field(name='Temperature', dev='T_spinflipper'),
         Field(name='Voltage', dev='U_spinflipper'),
     ),
     BlockRow(
