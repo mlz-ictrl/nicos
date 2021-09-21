44a6e044998d12439fd7d29a4f59582cc270a774
Arno Frank <arno.frank@tuwien.ac.at>
2021-09-21 17:25:08 +0200

    XCCM: add setups for virtual clone
    Change-Id: I37c5b68ef163539d7cdac23e986115d139df5be9
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/26727
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_tuw/xccm/setups/virtual_xccm.py
@@ -0,0 +1,13 @@
+description = 'XCCM'
+
+group = 'basic'
+
+includes = [
+#    'source',
+#    'table',
+#    'detector',
+    'virtual_sample_motors',
+#    'detector_motors',
+    'virtual_optic_motors',
+#    'IOcard'
+]
