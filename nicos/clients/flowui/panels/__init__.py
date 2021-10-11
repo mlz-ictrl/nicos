d1893a4a44ea5c657c967701080754e96734c070
Jens Krüger <jens.krueger@frm2.tum.de>
2021-10-11 11:56:24 +0200

    FLOWUI: Fix root path detection
    The used routine from nicostools/setupfiletool package isn't available
    at the installed system. The nicostools directory will not be installed.
    
    Change-Id: Iff9eadc19ced69ebd35da42e0b9805a13a125405
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/26821
    Reviewed-by: Matt Clarke <matt.clarke@esss.se>
    Reviewed-by: Michele Brambilla <michele.brambilla@psi.ch>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

--- nicos/clients/flowui/panels/__init__.py
+++ nicos/clients/flowui/panels/__init__.py
@@ -1,10 +1,33 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2021 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Michele Brambilla <michele.brambilla@psi.ch>
+#
+# *****************************************************************************
+
 from os import path
 
+from nicos import config
 from nicos.guisupport.qt import QIcon
 
-from nicostools.setupfiletool.utilities.utilities import getNicosDir
-
-root_path = getNicosDir()
+root_path = config.nicos_root
 icons_path = path.join(root_path, 'resources', 'material', 'icons')
 
 
