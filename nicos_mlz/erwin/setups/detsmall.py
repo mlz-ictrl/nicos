7fe841fe90e255b66f6075e6c6abd46a987c1d31
Jens Krüger <jens.krueger@frm2.tum.de>
2021-10-12 16:56:17 +0200

    ERWIN: add setup for small CHARM detector
    Change-Id: I1793839f23168d0ca71618058ae151992bcfb6e9
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/26838
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/erwin/setups/detsmall.py
@@ -0,0 +1,34 @@
+description = 'Small charm detector'
+
+group = 'optional'
+
+tango_host = 'qmesydaq-test.del.frm2.tum.de'
+
+tango_base = 'tango://%s:10000/qm/qmesydaq/' % tango_host
+
+devices = dict(
+    ds_mon1 = device('nicos.devices.vendor.qmesydaq.tango.CounterChannel',
+        description = 'Monitor 1 at small charm detector',
+        tangodevice = tango_base + 'counter0',
+        type = 'monitor',
+    ),
+    ds_events = device('nicos.devices.vendor.qmesydaq.tango.CounterChannel',
+        description = 'Event counter at small charm detector',
+        tangodevice = tango_base + 'events',
+        type = 'other',
+    ),
+    ds_image = device('nicos.devices.vendor.qmesydaq.tango.ImageChannel',
+        description = 'Image at small charm detector',
+        tangodevice = tango_base + 'image',
+    ),
+    ds_timer = device('nicos.devices.vendor.qmesydaq.tango.TimerChannel',
+        description = 'Timer at small charm detector',
+        tangodevice = tango_base + 'timer',
+    ),
+    ds_det = device('nicos.devices.generic.Detector',
+        description = 'Small charm detector',
+        images = ['ds_image'],
+        monitors = ['ds_mon1'],
+        timers = ['ds_timer'],
+    ),
+)
