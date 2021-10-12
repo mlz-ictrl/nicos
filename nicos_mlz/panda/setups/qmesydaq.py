ba85d85c6f6c5d6a0c27a9cb2ccdcb3170f91e02
Georg Brandl <g.brandl@fz-juelich.de>
2021-10-12 08:19:55 +0200

    PANDA: setup updates
    Change-Id: Ia12faa928e088bbd2b6a4a3557a0984e5b5ad3f5
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/27830
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos_mlz/panda/setups/qmesydaq.py
+++ nicos_mlz/panda/setups/qmesydaq.py
@@ -3,31 +3,26 @@ description = 'qmesydaq channel devices'
 group = 'lowlevel'
 display_order = 70
 
-qm_base = 'tango://phys.panda.frm2:10000/panda/qmesydaq/'
+qm_base = 'tango://phys.panda.frm2:10000/qm/qmesydaq/'
 
 devices = dict(
-    mon1 = device('nicos.devices.entangle.CounterChannel',
+    mon1 = device('nicos.devices.vendor.qmesydaq.tango.CounterChannel',
         description = 'QMesyDAQ Counter0',
         tangodevice = qm_base + 'counter0',
         type = 'monitor',
     ),
-    mon2 = device('nicos.devices.entangle.CounterChannel',
+    mon2 = device('nicos.devices.vendor.qmesydaq.tango.CounterChannel',
         description = 'QMesyDAQ Counter1',
         tangodevice = qm_base + 'counter1',
         type = 'monitor',
     ),
-    # events = device('nicos.devices.entangle.CounterChannel',
-    #     description = 'QMesyDAQ Events channel',
-    #     tangodevice = qm_base + 'events',
-    #     type = 'counter',
-    # ),
-    timer = device('nicos.devices.entangle.TimerChannel',
+    timer = device('nicos.devices.vendor.qmesydaq.tango.TimerChannel',
         description = 'QMesyDAQ Timer',
         tangodevice = qm_base + 'timer',
     ),
     data = device('nicos.devices.vendor.qmesydaq.tango.MultiCounter',
         description = 'QMesyDAQ Channels',
-        tangodevice = qm_base + 'det',
+        tangodevice = qm_base + 'image',
     ),
     det = device('nicos.devices.generic.Detector',
         description = 'QMesyDAQ MultiChannel Detector',
