a69389e79920d131a30825e953636288446ff841
Kenan Muric <kenan.muric@ess.eu>
2021-10-21 10:15:28 +0200

    ESS: Update EVR setup for YMIR
    Minor update to event receiver setup file for YMIR
    
    Change-Id: I08d755665de4e37cecc4acabffb988fb077f7786
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/26929
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@esss.se>

--- nicos_ess/ymir/setups/event_receiver.py
+++ nicos_ess/ymir/setups/event_receiver.py
@@ -4,11 +4,20 @@ pv_root = 'Utg-Ymir:TS-EVR-01:'
 
 devices = dict(
     EVR_time=device(
-        'nicos.devices.epics.EpicsStringReadable',
+        'nicos.devices.epics.pva.EpicsStringReadable',
         readpv='{}Time-Valid-Sts'.format(pv_root),
-        description='Status of the EVR timing'),
+        description='Status of the EVR timing',
+        monitor=True),
     EVR_link=device(
-        'nicos.devices.epics.EpicsStringReadable',
+        'nicos.devices.epics.pva.EpicsStringReadable',
         readpv='{}Link-Sts'.format(pv_root),
-        description='Status of link to EVG'),
+        description='Status of link to EVG',
+        monitor=True),
+    NTP_DIFF=device(
+        'nicos.devices.epics.pva.EpicsReadable',
+        readpv='LABS-VIP:time-fs725-01:NSDiffNTPEVR'.format(pv_root),
+        unit='ns',
+        description='The difference between the Utgård EVR and the NTP client',
+        monitor=True),
+
 )
