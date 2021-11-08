7b55bbcf017fe54bbe83a44cd1170fee5c931dcc
Jens Krüger <jens.krueger@tum.de>
2021-11-08 10:16:58 +0100

    TEST/PUMA: Create test classes for devices
    Change-Id: I28c84afc3861fc42cbda492d1db14c518fab7640
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/27107
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- test/nicos_mlz/puma/test_devices.py
+++ test/nicos_mlz/puma/test_devices.py
@@ -26,38 +26,49 @@
 
 import pytest
 
-from nicos.core.errors import LimitError, PositionError  # , MoveError
+from nicos.core.errors import LimitError, PositionError
 
 from test.utils import raises, approx
 
 session_setup = 'puma'
 
 
-def test_comb_axis(session, log):
-    phi = session.getDevice('phi')
-    assert phi.iscomb is False
+class TestCombAxis:
 
-    # test in normal operation mode
-    assert phi.read(0) == 0
-    for t in [10, 0]:
-        phi.maw(t)
-        assert phi.read(0) == t
+    @pytest.fixture(scope='function')
+    def combaxis(self, session):
+        phi = session.getDevice('phi')
+        assert phi.iscomb is False
 
-    assert phi._attached_fix_ax.read(0) == 0
+        yield phi
 
-    # test in combined mode
-    assert phi._fixpos is None
+        phi.maw(0)
+        phi.iscomb = False
+        session.destroyDevice(phi)
 
-    phi.iscomb = True
-    assert phi.iscomb is True
-    assert phi._fixpos == 0
+    def test_normal(self, combaxis):
+        # test in normal operation mode
+        assert combaxis.read(0) == 0
+        for t in [10, 0]:
+            combaxis.maw(t)
+            assert combaxis.read(0) == t
 
-    phi.maw(10)
-    assert phi.read(0) == 10
-    assert phi._attached_fix_ax.read(0) == -10
+        assert combaxis._attached_fix_ax.read(0) == 0
 
-    with log.allow_errors():
-        assert raises(LimitError, phi.move, 20)
+    def test_combined_mode(self, combaxis, session, log):
+        # test in combined mode
+        assert combaxis._fixpos is None
+
+        combaxis.iscomb = True
+        assert combaxis.iscomb is True
+        assert combaxis._fixpos == 0
+
+        combaxis.maw(10)
+        assert combaxis.read(0) == 10
+        assert combaxis._attached_fix_ax.read(0) == -10
+
+        with log.allow_errors():
+            assert raises(LimitError, combaxis.move, 20)
 
 
 class TestFocusAxis:
@@ -88,41 +99,45 @@ class TestFocusAxis:
             assert af.read(0) == approx(c, abs=af.precision)
 
 
-def test_mtt_axis(session, log):
-    ax = session.getDevice('mtt')
+class TestMttAxis:
 
-    assert ax.read(0) == 0
+    @pytest.fixture(scope='function', autouse=True)
+    def mtt(self, session):
+        mtt = session.getDevice('mtt')
+        assert mtt.read(0) == 0
+        mtt.maw(0)
+        assert mtt.read(0) == 0
 
-    ax.maw(0)
-    assert ax.read(0) == 0
-    for t in [-10, 0, ax.polypos + 1]:
-        ax.maw(t)
-        assert ax.read(0) == t
+        yield mtt
 
-    # The sequence [ax.polypos - 1, ax.polypos + 1] could only be tested if
-    # a simulation of a switch depending on a position of an other device is
-    # available
-    #
-    # This test will check the fail due to this missing device
-    # with log.allow_errors():
-    #     assert raises(MoveError, ax.maw, (ax.polypos - 1))
+    def test_moves(self, mtt):
+        for t in [-10, 0, mtt.polypos + 1]:
+            mtt.maw(t)
+            assert mtt.read(0) == t
+
+        # The sequence [ax.polypos - 1, ax.polypos + 1] could only be tested if
+        # a simulation of a switch depending on a position of an other device
+        # is available
+        #
+        # This test will check the fail due to this missing device
+        # with log.allow_errors():
+        #     assert raises(MoveError, ax.maw, (ax.polypos - 1))
 
 
 class TestCad:
     """Test class for the PUMA coupled axis device."""
 
-    @pytest.fixture(scope='function', autouse=True)
-    def prepare(self, session):
+    @pytest.fixture(scope='function')
+    def cad(self, session):
         cad = session.getDevice('cad')
         assert cad.read(0) == 0
 
-        yield
+        yield cad
 
         cad.maw(0)
         assert cad.read(0) == 0
 
-    def test_reset(self, session):
-        cad = session.getDevice('cad')
+    def test_reset(self, cad):
         cad.reset()
 
         # test reset if there is a small mismatch between both axes
@@ -130,22 +145,18 @@ class TestCad:
         cad.reset()
         assert cad.read(0) == 0
 
-    def test_internals(self, session):
-        cad = session.getDevice('cad')
+    def test_internals(self, cad):
         assert cad._checkReachedPosition(None) is False
 
-    def test_move(self, session):
-        cad = session.getDevice('cad')
+    def test_move(self, cad):
         # test move in both directions and smaller than single step
         for p in [-10, -2, 0]:
             cad.maw(p)
             assert cad.read(0) == p
 
-    def test_fails(self, session):
+    def test_fails(self, cad, session):
         th = session.getDevice('ath')
         th.maw(10)
-
-        cad = session.getDevice('cad')
 
         assert raises(PositionError, cad.reset)
 
