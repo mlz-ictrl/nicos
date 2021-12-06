eba534cf1bd46f4f25daf027bd4e35e7a332f03c
Jens Krüger <jens.krueger@frm2.tum.de>
2021-12-06 15:03:33 +0100

    KOMPASS: Put the sy2 device to an optional setup
    Change-Id: I3082b2e0fba8748ce45c5a6f8285d07fa762fed9
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/27258
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/kompass/setups/sy2.py
@@ -0,0 +1,19 @@
+description = 'Additional sample table devices'
+
+group = 'optional'
+
+tango_base = 'tango://kompasshw.kompass.frm2:10000/kompass/'
+
+devices = dict(
+    sy2_m = device('nicos.devices.entangle.Motor',
+        tangodevice = tango_base + 'sample/sy2_m',
+        fmtstr = '%.1f',
+        lowlevel = True,
+    ),
+    sy2 = device('nicos.devices.generic.Axis',
+        description = 'Additional sample table Y translation',
+        motor = 'sy2_m',
+        fmtstr = '%.1f',
+        precision = 0.01,
+    ),
+)
