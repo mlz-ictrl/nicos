6dc93911880d0a259dd55301660b70a983d24852
Jens Krüger <jens.krueger@frm2.tum.de>
2022-01-12 20:09:05 +0100

    Labs/Antares: fix tango->entangle
    Change-Id: I1122a365efcee67efb5907eb5781116c1d13060f
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/27431
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/labs/antares/ill/setups/lambdas.py
+++ nicos_mlz/labs/antares/ill/setups/lambdas.py
@@ -4,7 +4,7 @@ group = 'optional'
 tango_base = 'tango://127.0.0.1:10000/antares/'
 
 devices = dict(
-    I_lambda1 = device('nicos.devices.tango.PowerSupply',
+    I_lambda1 = device('nicos.devices.entangle.PowerSupply',
         description = 'Current 1',
         tangodevice = tango_base + 'lambda1/current',
         precision = 0.04,
