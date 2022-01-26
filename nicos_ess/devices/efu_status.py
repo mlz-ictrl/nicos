46077600710c554af572632d0cd28e0e951ab50e
Kenan Muric <kenan.muric@ess.eu>
2022-01-26 11:45:07 +0100

    ESS: Change to host parameter usage
    A minor change to handling of the host parameter.
    Specifying DEFAULT_PORT.
    
    Change-Id: If5499daa3a7390ab4a88cd764af3c3aa54b62316
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/27503
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@esss.se>

--- nicos_ess/devices/efu_status.py
+++ nicos_ess/devices/efu_status.py
@@ -21,10 +21,12 @@
 #   Kenan Muric <kenan.muric@ess.eu>
 #
 # *****************************************************************************
-from subprocess import check_output
+from subprocess import CalledProcessError, check_output
 
 from nicos.core import Override, Param, Readable, host, status
 from nicos.utils import parseHostPort
+
+DEFAULT_EFU_PORT = 8888
 
 
 class EFUStatus(Readable):
@@ -32,8 +34,9 @@ class EFUStatus(Readable):
     This device reports Event Formation Unit status.
     """
     parameters = {
-        'ipconfig': Param('IP and port configuration', type=host,
-                           mandatory=True, userparam=False),
+        'ipconfig': Param('IP and port configuration',
+                          type=host(defaultport=DEFAULT_EFU_PORT),
+                          mandatory=True, userparam=False),
     }
 
     parameter_overrides = {
@@ -53,8 +56,7 @@ class EFUStatus(Readable):
     }
 
     def doInit(self, mode):
-        efu_host, efu_port = parseHostPort(self.ipconfig.defaulthost,
-                                           self.ipconfig.defaultport)
+        efu_host, efu_port = parseHostPort(self.ipconfig, DEFAULT_EFU_PORT)
         self._command = f'echo "RUNTIMESTATS" | nc {efu_host} {efu_port}'
 
     def doRead(self, maxage=0):
@@ -68,7 +70,7 @@ class EFUStatus(Readable):
             raw_stat = check_output(self._command, shell=True)
             stat = int(raw_stat.split()[-1])
             return self._stat_to_status.get(stat, unknown_status)
-        except (ValueError, IndexError) as e:
+        except (ValueError, IndexError, CalledProcessError) as e:
             self.log.error('Could not correctly access EFU status. '
                            'Error was: %s', e)
             return unknown_status
