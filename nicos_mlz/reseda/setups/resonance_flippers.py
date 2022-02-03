ce10ce1c7d2be12c2fe58b2a387be74490ad21e6
Jens Krüger <jens.krueger@frm2.tum.de>
2022-02-03 18:52:15 +0100

    Reseda: Add sensor devices for the Magnet monitors
    Change-Id: I082ead6348a2edf6ade788e9dee341b8757ebe30
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/27643
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/reseda/setups/resonance_flippers.py
+++ nicos_mlz/reseda/setups/resonance_flippers.py
@@ -17,6 +17,10 @@ devices = dict(
         unit = 'A',
         precision = 0.02,
     ),
+    hrf_0a_current = device('nicos.devices.entangle.Sensor',
+        description = 'Read back of the current in arm 0 - A coil',
+        tangodevice = tango_base + '/mss1/current',
+    ),
     hrf_0b = device('nicos.devices.entangle.PowerSupply',
         description = 'Helmholtz coils for resonant flipper arm 0 - B',
         tangodevice = '%s/spf2/current' % tango_base,
@@ -26,6 +30,10 @@ devices = dict(
         maxage = 120,
         unit = 'A',
         precision = 0.02,
+    ),
+    hrf_0b_current = device('nicos.devices.entangle.Sensor',
+        description = 'Read back of the current in arm 0 - B coil',
+        tangodevice = tango_base + '/mss2/current',
     ),
     hrf_1a = device('nicos.devices.entangle.PowerSupply',
         description = 'Helmholtz coils for resonant flipper arm 1 - A',
@@ -48,3 +56,13 @@ devices = dict(
         precision = 0.02,
     ),
 )
+
+for i in range(1, 4):
+    devices['T_hrf_0a_%d' % i] = device('nicos.devices.entangle.Sensor',
+                                        description = 'Temperature %d in superconduting coil 0a' % i,
+                                        tangodevice = tango_base + '/mss1/temp%d' % i,
+                                        )
+    devices['T_hrf_0b_%d' % i] = device('nicos.devices.entangle.Sensor',
+                                        description = 'Temperature %d in superconduting coil 0b' % i,
+                                        tangodevice = tango_base + '/mss2/temp%d' % i,
+                                        )
