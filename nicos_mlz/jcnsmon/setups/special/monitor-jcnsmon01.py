f2b2109aae8c496d9f47fbb580e4389b91fa740d
Georg Brandl <g.brandl@fz-juelich.de>
2022-02-17 08:16:25 +0100

    JCNSMON: setup updates
    Change-Id: I164d361e4fd5e22f27b53afef62ce7fb107d8d44
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/28614
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos_mlz/jcnsmon/setups/special/monitor-jcnsmon01.py
+++ nicos_mlz/jcnsmon/setups/special/monitor-jcnsmon01.py
@@ -6,32 +6,83 @@ group = 'special'
 ##################################
 
 ## 8T Magnet ccm8v ##
-ccm8v = SetupBlock('ccm8v', 'se')
+ccm8v = Block('ccm8v - 8T MAGNET', [
+    BlockRow(Field(name='Field', dev='se/B_ccm8v',unit='T'),
+             Field(name='Hall', dev='se/ccm8v_Bhall', unit='T'),
+                 Field(name='P_sample', dev='se/ccm8v_psample',unit='mbar'),
+            ),
+    '---',
+    BlockRow(Field(name='T_VTI', dev='se/T_ccm8v_vti'),
+             Field(name='VTI heat', dev='se/ccm8v_vti_heater'),
+             Field(name='VTI NV', dev='se/ccm8v_vti_nv'),
+             Field(name='P_VTI', dev='se/ccm8v_pvti'),
+            ),
+    '---',
+    BlockRow(Field(name='LT-Stick', dev='se/T_ccm8v_stick'),
+             Field(name='LT-heat', dev='se/ccm8v_stick_heater'),
+                 Field(name='HT-Stick', dev='se/T_ccm8v_htstick'),
+             Field(name='HT-heat', dev='se/ccm8v_htstick_heater'),
+            ),
+    '---',
+    BlockRow(Field(name='Coils', dev='se/ccm8v_Tmag'),
+             Field(name='Dewar', dev='se/ccm8v_pdewar', unit=' '),
+             Field(name='Coldhead', dev='se/ccm8v_Tcoldhead'),
+             Field(name='P_iso', dev='se/ccm8v_piso',unit='mbar'),
+            ),
+    BlockRow(Field(name='LHe', dev='se/ccm8v_LHe'),
+             Field(name='LN2', dev='se/ccm8v_LN2'),
+            ),
+
+    BlockRow(Field(plot='p_ccm8v', name='LHe', key='se/ccm8v_LHe/value',
+                plotwindow=36000, width=40, height=30, fontsize=20),
+                Field(plot='p_ccm8v', name='LN2', key='se/ccm8v_LN2/value')
+            ),
+])
 
 ## HTS 3T Magnet ##
 ccm3a_1 = Block('ccm3a - HTS 3T', [
-        BlockRow(Field(name='B', dev='se/B_ccm3a_readback', unit='T')),
+        BlockRow(Field(name='B', dev='se/B_ccm3a_readback', unit='T')
+                ),
         BlockRow(Field(name='T1', dev='se/ccm3a_T1', unit='K'),
-                 Field(name='T2', dev='se/ccm3a_T2', unit='K')),
-        BlockRow(Field(name='T3', dev='se/ccm3a_TA', unit='K'),
-                 Field(name='T4', dev='se/ccm3a_TB', unit='K')
-                 )
+                 Field(name='T2', dev='se/ccm3a_T2', unit='K'),
+                 Field(name='T3', dev='se/ccm3a_TA', unit='K'),
+                 Field(name='T4', dev='se/ccm3a_TB', unit='K'),
+                ),
+        BlockRow(Field(plot='ccm3a_plot', name='T1', key='se/ccm3a_T1/value',
+                       plotwindow=3600, width=40, height=30, fontsize=20),
+                  Field(plot='ccm3a_plot', name='T2',key='se/ccm3a_T2/value'),
+                  Field(plot='ccm3a_plot', name='T3',key='se/ccm3a_TA/value'),
+                  Field(plot='ccm3a_plot', name='T4',key='se/ccm3a_TB/value'),
+                ),
 ])
 
 ## SPHERES CRYO PLOT ##
 cct6_1 = Block('cct6 - SPHERES CRYO', [
-        BlockRow(Field(name='pressure', dev='se/cct6_pressure', units='mbar'),
-                 Field(name='T tube', dev='se/cct6_T_tube', unit='K'),
-                 Field(name='T_sample', dev='se/cct6_T_sample', units='K')
+        BlockRow(Field(name='T tube',   dev='se/cct6_T_tube',     unit='K'),
+                 Field(name='T sample', dev='se/cct6_T_sample',   unit='K'),
+                 Field(name='T sample 2', dev='se/cct6_sample2',  unit='K'),
                 ),
-#        BlockRow(Field(plot='temp', name='T sample', key='se/jlc22_T_sample/value', plotwindow=3600, width=30, height=20),
-#                 Field(plot='temp', name='T tube', key='se/jlc22_T_tube/value')),
+        '---',
+        BlockRow(Field(name='tube SP',  dev='se/cct6_T_tube/setpoint',   unit='K'),
+                 Field(name='sample SP',dev='se/cct6_setpoint', unit='K'),
+                 Field(name='pressure', dev='se/cct6_c_pressure', unit='mbar'),
+                ),
+        BlockRow(Field(name='heater tube',   dev='se/cct6_T_tube/heateroutput',   unit='%'),
+                 Field(name='heater sample', dev='se/cct6_T_sample/heateroutput', unit='%'),
+                ),
+        BlockRow(Field(plot='temp_cct6', name='T sample', key='se/cct6_T_sample/value',
+                       plotwindow=3600, width=30, height=30, fontsize=60),
+                 Field(plot='temp_cct6', name='T tube', key='se/cct6_T_tube/value'),
+                 Field(plot='temp_cct6', name='T sample2', key='se/cct6_T_c_pressure/value'),
+                ),
 ])
 
 ##################################
 #### define layout on monitor ####
 ##################################
-layout = [Row(Column(ccm8v), Column(cct6_1,ccm3a_1))]
+layout1 = [Row(Column(ccm8v), Column(cct6_1,ccm3a_1))]
+layout2 = [Row(Column(ccm8v), Column(cct6_1), Column(ccm3a_1))]
+layout3 = [Row(Column(cct6_1))]
 
 
 ##################################
@@ -39,14 +90,13 @@ layout = [Row(Column(ccm8v), Column(cct6_1,ccm3a_1))]
 ##################################
 devices = dict(
     Monitor = device('nicos.services.monitor.qt.Monitor',
-        title = 'h',
+        title = '',
         loglevel = 'info',
         cache = 'jcnsmon.jcns.frm2',
         font = 'Droid Sans',
         valuefont = 'Droid Sans Mono',
-        fontsize = 30,
-        padding = 15,
-        layout = layout,
+        fontsize = 16,
+        padding = 2,
+        layout = layout2,
         expectmaster = False,
-    ),
-)
+    ))
--- nicos_mlz/jcnsmon/setups/special/poller.py
+++ nicos_mlz/jcnsmon/setups/special/poller.py
@@ -9,7 +9,7 @@ sysconfig = dict(
 
 devices = dict(
     Poller = device('nicos.services.poller.Poller',
-        alwayspoll = [],  # setups that should be polled regardless if loaded
+        alwayspoll = ['all_memographs'],
         neverpoll = [],  # setups that should not be polled even if loaded
         blacklist = [],  # DEVICES that should never be polled
         # (usually detectors or devices that have problems
