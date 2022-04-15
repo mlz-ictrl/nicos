9ef3f667199c730239e242a759aeed77d6228b24
Jens Krüger <jens.krueger@tum.de>
2022-04-15 09:30:25 +0200

    VSPODI: Add sample changer setup
    Change-Id: I095c244148d775b66aa8293a462ea08c34fbafd0
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/28174
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_demo/vspodi/setups/samplechanger.py
@@ -0,0 +1,48 @@
+description = 'Sample changer and rotation'
+
+group = 'optional'
+
+devices = dict(
+    samr = device('nicos.devices.generic.ManualSwitch',
+        description = '(de-)activates the sample rotation',
+        states = ('off', 'on'),
+    ),
+    sams_m = device('nicos.devices.generic.VirtualMotor',
+        description = 'Motor position of the sample change selection wheel',
+        abslimits = (-10, 370),
+        speed = 5,
+        unit = 'deg',
+        visibility = (),
+    ),
+    sams_a = device('nicos.devices.generic.Axis',
+        description = 'Position of sample selection wheel',
+        motor = 'sams_m',
+        precision = 0.05,
+        visibility = (),
+    ),
+    sams = device('nicos.devices.generic.Switcher',
+        description = 'Sample Changer drum',
+        moveable = 'sams_a',
+        mapping = {
+            'S1': -3.04,
+            'S2': 33.11,
+            'S3': 68.8,
+            'S4': 104.95,
+            'S5': 140.93,
+            'S6': 177.20,
+            'S7': 212.91,
+            'S8': 249.07,
+            'S9': 285.11,
+            'S10': 321.03,
+        },
+        fallback = '?',
+        fmtstr = '%s',
+        precision = 0.1,
+        blockingmove = False,
+        unit = '',
+    ),
+)
+display_order = 60
+
+alias_config = {
+}
