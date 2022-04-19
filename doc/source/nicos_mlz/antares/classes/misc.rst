4af8e4e92facefc3ca0229738343cb870789b9c0
Jens Krüger <jens.krueger@frm2.tum.de>
2022-04-19 09:56:27 +0200

    ANTARES: Switch HGM09 gaussmeter to entangle
    Change-Id: I776786567b45a1884b7aef542b0ad61b9420cf7a
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/28181
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- doc/source/nicos_mlz/antares/classes/misc.rst
+++ doc/source/nicos_mlz/antares/classes/misc.rst
@@ -1,8 +1,5 @@
 Miscellaneous devices
 ---------------------
 
-.. module:: nicos_mlz.antares.devices.hgm09
-.. autoclass:: HGM09()
-
 .. module:: nicos_mlz.antares.devices.selector
 .. autoclass:: SelectorTilt()
--- nicos_mlz/antares/setups/hgm09.py
+++ nicos_mlz/antares/setups/hgm09.py
@@ -5,8 +5,8 @@ group = 'optional'
 tango_base = 'tango://antareshw.antares.frm2:10000/antares/'
 
 devices = dict(
-    hgm09 = device('nicos_mlz.antares.devices.hgm09.HGM09',
+    hgm09 = device('nicos.devices.entangle.Sensor',
         description = 'HGM09 Hand Gauss Meter',
-        tangodevice = tango_base + 'rs232/hgm09',
+        tangodevice = tango_base + 'hgm09/sensor',
     ),
 )
