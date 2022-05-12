f5b3b0e47c8c7c3cb7ccb8a9cdb08c01b062fc67
Matt Clarke <matt.clarke@esss.se>
2022-05-12 13:18:55 +0200

    ESS: add second Lakeshore device for E03
    Turns out that there were two to control
    
    Change-Id: If10ae47ef271692fab8718b10e019450520d26c6
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/28407
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@esss.se>

--- nicos_ess/labs/e03/setups/lakeshore336_001.py
+++ nicos_ess/labs/e03/setups/lakeshore336_001.py
@@ -3,7 +3,7 @@ description = 'The Lakeshore 336 temperature controller.'
 pv_root = 'SE-SEE:SE-LS336-004:'
 
 devices = dict(
-    T_ls336_A=device(
+    T_ls336_A_001=device(
         'nicos.devices.epics.pva.EpicsAnalogMoveable',
         description='Channel A temperature',
         readpv='{}KRDG0'.format(pv_root),
@@ -12,7 +12,7 @@ devices = dict(
         pva=True,
         monitor=True,
     ),
-    S_ls336_A=device(
+    S_ls336_A_001=device(
         'nicos.devices.epics.pva.EpicsReadable',
         description='Channel A raw sensor reading',
         readpv='{}SRDG0'.format(pv_root),
@@ -20,7 +20,7 @@ devices = dict(
         pva=True,
         monitor=True,
     ),
-    T_ls336_B=device(
+    T_ls336_B_001=device(
         'nicos.devices.epics.pva.EpicsAnalogMoveable',
         description='Channel B temperature',
         readpv='{}KRDG1'.format(pv_root),
@@ -29,7 +29,7 @@ devices = dict(
         pva=True,
         monitor=True,
     ),
-    S_ls336_B=device(
+    S_ls336_B_001=device(
         'nicos.devices.epics.pva.EpicsReadable',
         description='Channel B raw sensor reading',
         readpv='{}SRDG1'.format(pv_root),
@@ -37,7 +37,7 @@ devices = dict(
         pva=True,
         monitor=True,
     ),
-    T_ls336_C=device(
+    T_ls336_C_001=device(
         'nicos.devices.epics.pva.EpicsAnalogMoveable',
         description='Channel C temperature',
         readpv='{}KRDG2'.format(pv_root),
@@ -46,7 +46,7 @@ devices = dict(
         pva=True,
         monitor=True,
     ),
-    S_ls336_C=device(
+    S_ls336_C_001=device(
         'nicos.devices.epics.pva.EpicsReadable',
         description='Channel C raw sensor reading',
         readpv='{}SRDG2'.format(pv_root),
@@ -54,7 +54,7 @@ devices = dict(
         pva=True,
         monitor=True,
     ),
-    T_ls336_D=device(
+    T_ls336_D_001=device(
         'nicos.devices.epics.pva.EpicsAnalogMoveable',
         description='Channel D temperature',
         readpv='{}KRDG3'.format(pv_root),
@@ -63,7 +63,7 @@ devices = dict(
         pva=True,
         monitor=True,
     ),
-    S_ls336_D=device(
+    S_ls336_D_001=device(
         'nicos.devices.epics.pva.EpicsReadable',
         description='Channel D raw sensor reading',
         readpv='{}SRDG3'.format(pv_root),
---
+++ nicos_ess/labs/e03/setups/lakeshore336_002.py
@@ -0,0 +1,74 @@
+description = 'The Lakeshore 336 temperature controller.'
+
+pv_root = 'SE-SEE:SE-LS336-003:'
+
+devices = dict(
+    T_ls336_A_002=device(
+        'nicos.devices.epics.pva.EpicsAnalogMoveable',
+        description='Channel A temperature',
+        readpv='{}KRDG0'.format(pv_root),
+        writepv='{}SETP_S1'.format(pv_root),
+        targetpv='{}SETP1'.format(pv_root),
+        pva=True,
+        monitor=True,
+    ),
+    S_ls336_A_002=device(
+        'nicos.devices.epics.pva.EpicsReadable',
+        description='Channel A raw sensor reading',
+        readpv='{}SRDG0'.format(pv_root),
+        visibility=(),
+        pva=True,
+        monitor=True,
+    ),
+    T_ls336_B_002=device(
+        'nicos.devices.epics.pva.EpicsAnalogMoveable',
+        description='Channel B temperature',
+        readpv='{}KRDG1'.format(pv_root),
+        writepv='{}SETP_S2'.format(pv_root),
+        targetpv='{}SETP2'.format(pv_root),
+        pva=True,
+        monitor=True,
+    ),
+    S_ls336_B_002=device(
+        'nicos.devices.epics.pva.EpicsReadable',
+        description='Channel B raw sensor reading',
+        readpv='{}SRDG1'.format(pv_root),
+        visibility=(),
+        pva=True,
+        monitor=True,
+    ),
+    T_ls336_C_002=device(
+        'nicos.devices.epics.pva.EpicsAnalogMoveable',
+        description='Channel C temperature',
+        readpv='{}KRDG2'.format(pv_root),
+        writepv='{}SETP_S3'.format(pv_root),
+        targetpv='{}SETP3'.format(pv_root),
+        pva=True,
+        monitor=True,
+    ),
+    S_ls336_C_002=device(
+        'nicos.devices.epics.pva.EpicsReadable',
+        description='Channel C raw sensor reading',
+        readpv='{}SRDG2'.format(pv_root),
+        visibility=(),
+        pva=True,
+        monitor=True,
+    ),
+    T_ls336_D_002=device(
+        'nicos.devices.epics.pva.EpicsAnalogMoveable',
+        description='Channel D temperature',
+        readpv='{}KRDG3'.format(pv_root),
+        writepv='{}SETP_S4'.format(pv_root),
+        targetpv='{}SETP4'.format(pv_root),
+        pva=True,
+        monitor=True,
+    ),
+    S_ls336_D_002=device(
+        'nicos.devices.epics.pva.EpicsReadable',
+        description='Channel D raw sensor reading',
+        readpv='{}SRDG3'.format(pv_root),
+        visibility=(),
+        pva=True,
+        monitor=True,
+    ),
+)
