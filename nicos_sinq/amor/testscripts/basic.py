023117b982f337818484f47683c03d08cfd45f07
Jens Krüger <jens.krueger@frm2.tum.de>
2022-06-28 18:50:11 +0200

    TEST: Extend dependencies for AMOR
    The serialise_hs01 module is not included in older versions of the
    streaming_data_types package
    
    Change-Id: Ie39def314cce2cd60136add59941a4e0f03abe30
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/28781
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_sinq/amor/testscripts/basic.py
+++ nicos_sinq/amor/testscripts/basic.py
@@ -3,6 +3,7 @@
 # test: needs = kafka
 # test: needs = epics
 # test: needs = streaming_data_types
+# test: needs = streaming_data_types.serialise_hs01
 # test: subdirs = amor
 # test: setups = deflector
 
