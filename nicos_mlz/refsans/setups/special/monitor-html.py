167d3b63d1fe7fb396e43aca917966866ba6623a
Matthias Pomm <matthias.pomm@hereon.de>
2022-08-30 07:55:38 +0200

    REFSANS: Add HTML status monitor
    Change-Id: I40994353584f3e183b4338bc0c9ad61d819827c6
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/29085
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/refsans/setups/special/monitor-html.py
@@ -0,0 +1,32 @@
+#  -*- coding: utf-8 -*-
+
+description = 'setup for the HTML status monitor'
+group = 'special'
+
+_experimentcol = Column(
+    Block('Experiment', [
+        BlockRow(
+            Field(name='Remark', key='exp/remark', width=80, istext=True, maxlen=80),
+        ),
+        ],
+    ),
+)
+
+devices = dict(
+    Monitor = device('nicos.services.monitor.html.Monitor',
+        description = 'Status Display',
+        title = 'REFSANS Status Monitor',
+        filename = '/refsanscontrol/webroot/index.html',
+        loglevel = 'info',
+        interval = 10,
+        cache = 'refsansctrl.refsans.frm2',
+        prefix = 'nicos/',
+        font = 'Luxi Sans',
+        valuefont = 'Consolas',
+        fontsize = 15,
+        padding = 5,
+        layout = [[_experimentcol],
+                  ],
+        noexpired = True,
+    ),
+)
