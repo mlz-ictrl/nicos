e837d43c3da1738c7b79b360b4a9363ad39b3254
Jens Krüger <jens.krueger@tum.de>
2022-11-16 09:00:16 +0100

    NeXuS: add special element for the NICOS program info
    The entry 'program_name' in the NXentry structure can be filled up with
    the 'NicosProgramDataset', which includes the version and loaded setups.
    
    Change-Id: I794cfbd3b66c5509afe9ce59d9a970db04857f46
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/29745
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Mark Koennecke <mark.koennecke@psi.ch>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos/nexus/specialelements.py
@@ -0,0 +1,38 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2022 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Mark Koennecke <Mark.Koennecke@psi.ch>
+#
+# *****************************************************************************
+
+from nicos import nicos_version, session
+from .elements import ConstDataset, NXAttribute
+
+
+class NicosProgramDataset(ConstDataset):
+    """Place holder for the NICOS program info.
+
+    This elememt can be used for the `program_name` entry in the NXentry group.
+    """
+    def __init__(self):
+        ConstDataset.__init__(self, 'NICOS', 'string',
+                              version=NXAttribute(nicos_version, 'string'),
+                              configuration=NXAttribute(
+                                  session.explicit_setups, 'string'))
