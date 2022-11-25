fd9bd486d1ea52bce1343b42c53183f4e43a7d44
Georg Brandl <g.brandl@fz-juelich.de>
2022-11-25 16:37:52 +0100

    j_nse: update setup for counter card
    Change-Id: I2e4c594756b9f353ff8308814947d7bf67230713
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/29834
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos_mlz/j_nse/setups/counter.py
+++ nicos_mlz/j_nse/setups/counter.py
@@ -1,27 +1,23 @@
 # -*- coding: utf-8 -*-
 
-description = "ZEA-2 counter card setup"
-group = "lowlevel"
+description = 'ZEA-2 counter card setup'
+group = 'lowlevel'
 
-tango_base = "tango://phys.j-nse.frm2:10000/j-nse/"
+tango_base = 'tango://phys.j-nse.frm2:10000/j-nse/'
 
 devices = dict(
-    timer = device("nicos_mlz.jcns.devices.fpga.FPGATimerChannel",
-        description = "ZEA-2 counter card timer channel",
-        tangodevice = tango_base + 'count/0',
+    timer = device('nicos_mlz.jcns.devices.fpga_new.FPGATimerChannel',
+        description = 'Counter card timer channel',
+        tangodevice = tango_base + 'count/timer',
     ),
-    mon0 = device("nicos_mlz.jcns.devices.fpga.FPGACounterChannel",
-        description = "Monitor 0",
-        tangodevice = tango_base + 'count/0',
+    mon0 = device('nicos.devices.entangle.CounterChannel',
+        description = 'Monitor 0',
+        tangodevice = tango_base + 'count/mon0',
         type = 'monitor',
-        fmtstr = '%d',
-        channel = 0,
     ),
-    mon1 = device("nicos_mlz.jcns.devices.fpga.FPGACounterChannel",
-        description = "Monitor 1",
-        tangodevice = tango_base + 'count/0',
+    mon1 = device('nicos.devices.entangle.CounterChannel',
+        description = 'Monitor 1',
+        tangodevice = tango_base + 'count/mon1',
         type = 'monitor',
-        fmtstr = '%d',
-        channel = 1,
     ),
 )
