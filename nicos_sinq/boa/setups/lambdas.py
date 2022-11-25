5230f0a033010ce4772bb487b936317e46dbbd2c
Jens Krüger <jens.krueger@frm2.tum.de>
2022-11-25 18:16:17 +0100

    BOA/Fix: Add port number to TANGO URI
    The port number of the TANGO system is needed in TANGO URI
    
    Change-Id: I423827bbe2d2a6b79d748848097e7f4b5820c222
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/29842
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_sinq/boa/setups/lambdas.py
+++ nicos_sinq/boa/setups/lambdas.py
@@ -2,7 +2,7 @@ description = 'Lambda power supplies'
 group = 'optional'
 
 #tango_base = 'tango://10.167.30.89:10000/antares/'
-tango_base = 'tango://antaresopc10.psi.ch/antares/'
+tango_base = 'tango://antaresopc10.psi.ch:10000/antares/'
 devices = dict(
     I_lambda1 = device('nicos.devices.entangle.PowerSupply',
         description = 'Current 1',
