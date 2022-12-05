a93928b509c751d00e789056e53fe4a47d688ae4
Jens Krüger <jens.krueger@frm2.tum.de>
2022-12-05 17:40:17 +0100

    SPODI: Fix setup formatting
    Change-Id: I81a3484fc4c35202aebb8a7748fd2f4931abdc8f
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/29935
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/spodi/setups/notifiers.py
+++ nicos_mlz/spodi/setups/notifiers.py
@@ -7,8 +7,8 @@ devices = dict(
     email    = device('nicos.devices.notifiers.Mailer',
         sender = 'spodi@frm2.tum.de',
         copies = [('markus.hoelzel@frm2.tum.de', 'all'),   # gets all messages
-                                ('anatoliy.senyshyn@frm2.tum.de', 'all'),   # gets all messages
-                                ('josef.pfanzelt@frm2.tum.de', 'important')], # gets only important messages
+                  ('anatoliy.senyshyn@frm2.tum.de', 'all'),   # gets all messages
+                  ('josef.pfanzelt@frm2.tum.de', 'important')], # gets only important messages
         subject = 'SPODI',
         mailserver ='mailhost.frm2.tum.de',
     ),
