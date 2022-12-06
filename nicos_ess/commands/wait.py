811b924ef7df378cf60271309615fb3d69c84948
Jens Krüger <jens.krueger@frm2.tum.de>
2022-12-06 18:21:16 +0100

    ESS/Commands: Fix doc
    Change-Id: Id38602b68f9b97a5ba19578cb8e588f6cf596051
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/29944
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_ess/commands/wait.py
+++ nicos_ess/commands/wait.py
@@ -1,3 +1,26 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2022 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Matt Clarke <matt.clarke@ess.eu>
+#
+# *****************************************************************************
 import time
 
 from nicos import session
@@ -17,7 +40,7 @@ def waitfor_stable(device, target, accuracy, time_stable, timeout=3600):
 
     >>> waitfor_stable(dev1, 10, 1, 30, 600)
 
-    will wait until the device postion is between 9 and 10 for a continous
+    will wait until the device position is between 9 and 11 for a continous
     period of 30 seconds, but will exit after 10 minutes if
     stability is not reached.
     """
