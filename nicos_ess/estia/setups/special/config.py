940aa644e71db67c896465e23667061a441941a5
Jens Krüger <jens.krueger@frm2.tum.de>
2022-12-06 17:18:02 +0100

    ESTIA: Add the definition of ESTIA_FILEWRITER_ROOT
    Change-Id: I2a70448d16657cf70fd30f0099350ce79a10594b
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/29943
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Kenan Muric <kenan.muric@ess.eu>

--- nicos_ess/estia/setups/special/config.py
+++ nicos_ess/estia/setups/special/config.py
@@ -1,5 +1,6 @@
 description = 'Generic configuration settings for ESTIA'
 group = 'configdata'
 
-ESTIA_DATA_ROOT = '/opt/nicos-data/estia'
+ESTIA_DATA_ROOT = '/opt/nicos-data'
+ESTIA_FILEWRITER_ROOT = '/opt/nicos-data/estia'
 KAFKA_BROKERS = ["localhost:9092"]
