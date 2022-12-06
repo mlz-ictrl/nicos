b5cb4a319bb2daa26a077dcc0af4ef49318ac751
Jens Krüger <jens.krueger@tum.de>
2022-12-06 21:18:10 +0100

    Core/data/manager: Fix formatting issue
    Change-Id: Ia6ecea51933e7411c01d9f29a65f9f6787d53c5b
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/29945
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos/core/data/manager.py
+++ nicos/core/data/manager.py
@@ -179,8 +179,7 @@ class DataManager:
                     dataset.handlers.extend(handlers)
             # Sorting handlers for right execution order
             dataset.handlers = sorted(
-                dataset.handlers,
-                key=lambda _handler: _handler.ordering)
+                dataset.handlers, key=lambda _handler: _handler.ordering)
         if self._current:
             self._current.subsets.append(dataset)
             dataset.number = len(self._current.subsets)
