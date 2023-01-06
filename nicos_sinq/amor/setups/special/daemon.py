b470c98a0770391951434f8bb602d4608997d77c
koennecke <mark.koennecke@psi.ch>
2023-01-06 11:07:01 +0100

    SINQ: changed NICOS password for 2023
    Change-Id: Ic2ac925a59fdc575f6b000a5fa3c0b040b7b0ff5
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/30068
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Mark Koennecke <mark.koennecke@psi.ch>

--- nicos_sinq/amor/setups/special/daemon.py
+++ nicos_sinq/amor/setups/special/daemon.py
@@ -6,9 +6,9 @@ devices = dict(
         hashing = 'sha1',
         passwd = [
             ('spy', '15346b593c4d0cf05fb6e67a5669d852e6550481', 'guest'),
-            ('user', '0c42722568e3d5471031c3be7d029f9b41123552', 'user'),
+            ('user', '0ecb08958ccdf087111e54131906b08a8b89be9a', 'user'),
             ('admin', '76702e9ada292df094a875e5f72e9f778099d477', 'admin'),
-            ('lnsadmin', '76702e9ada292df094a875e5f72e9f778099d477', 'admin')
+            ('lnsadmin', '76702e9ada292df094a875e5f72e9f778099d477', 'admin'),
         ],
     ),
     Daemon = device('nicos.services.daemon.NicosDaemon',
--- nicos_sinq/icon/setups/special/daemon.py
+++ nicos_sinq/icon/setups/special/daemon.py
@@ -19,7 +19,7 @@ devices = dict(
                 hashing='sha1',
                 passwd=[('spy', '15346b593c4d0cf05fb6e67a5669d852e6550481',
                          'guest'),
-                        ('user', '88380931080d6a2a097133d0704ea789814c8dea',
+                        ('user', '0ecb08958ccdf087111e54131906b08a8b89be9a',
                          'user'),
                         ('admin', '76702e9ada292df094a875e5f72e9f778099d477',
                          'admin'),
