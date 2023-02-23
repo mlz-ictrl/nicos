a66f48a0ebcc960f2f1e11497e0a3441a6819ffb
stefanosathanasopoul <stefanos.athanasopoulos@ess.eu>
2023-02-23 09:54:21 +0100

    ESS: Adding ODIN directory
    Change-Id: Ie3a7e9f805ef74c3d3e349e94333870e131f513c
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/30504
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@ess.eu>

---
+++ nicos_ess/odin/guiconfig.py
@@ -0,0 +1,77 @@
+"""NICOS GUI default configuration."""
+
+main_window = docked(
+    tabbed(
+        ('Experiment', panel('nicos_ess.gui.panels.exp_panel.ExpPanel')),
+        ('Setup',
+         panel('nicos.clients.flowui.panels.setup_panel.SetupsPanel')),
+        ('  ', panel('nicos.clients.flowui.panels.empty.EmptyPanel')),
+        (
+            'Instrument interaction',
+            hsplit(
+                vbox(
+                    panel(
+                        'nicos.clients.flowui.panels.cmdbuilder.CommandPanel',
+                        modules=['nicos.clients.gui.cmdlets'],
+                    ),
+                    tabbed(
+                        ('Output',
+                         panel(
+                             'nicos.clients.flowui.panels.console.ConsolePanel',
+                             hasinput=False)),
+                        ('Scan Plot',
+                         panel('nicos.clients.flowui.panels.scans.ScansPanel')
+                         ),
+                        ('Detector Image',
+                         panel(
+                             'nicos.clients.flowui.panels.live.MultiLiveDataPanel'
+                         )),
+                        ('Script Status',
+                         panel(
+                             'nicos.clients.flowui.panels.status.ScriptStatusPanel',
+                             eta=True)),
+                    ),
+                ),  # vsplit
+                panel(
+                    'nicos.clients.flowui.panels.devices.DevicesPanel',
+                    dockpos='right',
+                    param_display={'Exp': ['lastpoint', 'lastscan']},
+                    filters=[],
+                ),
+            ),  # hsplit
+        ),
+        (
+            'Batch file generation',
+            panel('nicos.clients.flowui.panels.editor.EditorPanel',
+                  tools=None),
+        ),
+        (
+            'History',
+            panel('nicos.clients.flowui.panels.history.HistoryPanel'),
+        ),
+        (
+            'Logs',
+            tabbed(
+                ('Errors',
+                 panel('nicos.clients.gui.panels.errors.ErrorPanel')),
+                ('Log files',
+                 panel('nicos.clients.gui.panels.logviewer.LogViewerPanel')),
+            ),
+        ),
+        position='left',
+        margins=(0, 0, 0, 0),
+        textpadding=(30, 20),
+    ),  # tabbed
+)  # docked
+
+windows = []
+
+tools = [
+    tool('Report NICOS bug or request enhancement',
+         'nicos.clients.gui.tools.bugreport.BugreportTool'),
+]
+
+options = {
+    'facility': 'ess',
+    'mainwindow_class': 'nicos.clients.flowui.mainwindow.MainWindow',
+}
---
+++ nicos_ess/odin/setups/system.py
@@ -0,0 +1,42 @@
+description = 'system setup'
+
+group = 'lowlevel'
+
+sysconfig = dict(
+    cache='localhost',
+    instrument='ODIN',
+    experiment='Exp',
+    datasinks=['conssink', 'daemonsink', 'liveview'],
+)
+
+modules = ['nicos.commands.standard', 'nicos_ess.commands']
+
+devices = dict(
+    ODIN=device(
+        'nicos.devices.instrument.Instrument',
+        description='instrument object',
+        instrument='ODIN',
+        responsible='Manuel Morgano <manuel.morgano@ess.eu>',
+        website='https://europeanspallationsource.se/instruments/odin'),
+    Sample=device(
+        'nicos.devices.sample.Sample',
+        description='The currently used sample',
+    ),
+    Exp=device(
+        'nicos_ess.devices.experiment.EssExperiment',
+        description='experiment object',
+        dataroot='/opt/nicos-data',
+        filewriter_root='/opt/nicos-data/odin',
+        sample='Sample',
+        cache_filepath='/opt/nicos-data/odin/cached_proposals.json'),
+    conssink=device(
+        'nicos_ess.devices.datasinks.console_scan_sink.ConsoleScanSink'),
+    daemonsink=device('nicos.devices.datasinks.DaemonSink', ),
+    liveview=device('nicos.devices.datasinks.LiveViewSink', ),
+    Space=device(
+        'nicos.devices.generic.FreeSpace',
+        description='The amount of free space for storing data',
+        path=None,
+        minfree=5,
+    ),
+)
