82376bf82720c79e2a2fc3a6d4999eb111047ba7
Matt Clarke <matt.clarke@ess.eu>
2023-04-18 07:26:25 +0200

    ESS: add collector setup for ODIN
    Change-Id: I6f717f21612e163594396e9f6b17954327a57315
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/30906
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@ess.eu>

---
+++ nicos_ess/odin/setups/special/collector.py
@@ -0,0 +1,16 @@
+description = 'setup for the NICOS collector'
+group = 'special'
+
+devices = dict(
+    CacheKafka=device(
+        'nicos_ess.devices.cache_kafka_forwarder.CacheKafkaForwarder',
+        dev_ignore=['space', 'sample'],
+        brokers=['10.100.1.19:9092'],
+        output_topic='odin_nicos_devices',
+    ),
+    Collector=device(
+        'nicos.services.collector.Collector',
+        cache='localhost:14869',
+        forwarders=['CacheKafka'],
+    ),
+)
