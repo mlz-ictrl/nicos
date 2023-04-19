2e33b7117f70800f19f96a651796a961a4e99931
Matt Clarke <matt.clarke@ess.eu>
2023-04-19 11:48:45 +0200

    ESS: switch bifrost to new live view
    Change-Id: Ie0c57624b7db5195d7e57cfa448194c21baeb4d6
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/30926
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@ess.eu>

--- nicos_ess/bifrost/guiconfig.py
+++ nicos_ess/bifrost/guiconfig.py
@@ -24,7 +24,7 @@ main_window = docked(
                          ),
                         ('Detector Image',
                          panel(
-                             'nicos.clients.flowui.panels.live.MultiLiveDataPanel'
+                             'nicos_ess.gui.panels.live.MultiLiveDataPanel'
                          )),
                         ('Script Status',
                          panel(
