cb5d3e8ff11e88f2d4b893581b487896ba0be3bf
Jonas Petersson <jonas.petersson@ess.eu>
2023-04-20 07:44:28 +0200

    ESS: Add auto-enable PV for motors
    A PV which dictates if the motor will be auto-enabled
    if it is moved has been added to the motor to make
    warnings and status messages more informative and
    less cluttered.
    
    Change-Id: I4ef41b54ef77402277980e6cb257fc26f0204fe5
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/30930
    Reviewed-by: Matt Clarke <matt.clarke@ess.eu>
    Tested-by: Matt Clarke <matt.clarke@ess.eu>

--- nicos_ess/ymir/setups/rotation_stage.py
+++ nicos_ess/ymir/setups/rotation_stage.py
@@ -5,6 +5,7 @@ devices = dict(rotation_stage=device(
     'nicos_ess.devices.epics.pva.motor.EpicsMotor',
     description='Rotation stage in YMIR',
     motorpv=f'{pv_root}m',
+    powerautopv=f'{pv_root}m-PwrAuto',
     errormsgpv=f'{pv_root}m-MsgTxt',
     errorbitpv=f'{pv_root}m-Err',
     reseterrorpv=f'{pv_root}m-ErrRst',
