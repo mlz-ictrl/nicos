a97f2f51dffb4dbf5c1820e4b8fedda28f7df94b
Matthias Pomm <matthias.pomm@hzg.de>
2023-04-25 08:31:25 +0200

    REFSANS: setup julabo: adding flow from ana4gpio01_ch2
    There is a panel for the media of the julabo, with some devices.
    One device is a flowmeter with an analog readout for the flow 0-10 V.
    This will be read by the raspi ana4gpio01.
    
    Change-Id: I9a7ea45c3c56c6ad79b081f3a8ff1411ee3f1d50
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31483
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/julabo.py
+++ nicos_mlz/refsans/setups/elements/julabo.py
@@ -2,9 +2,12 @@ description = 'REFSANS setup for julabo01 Presto A40'
 
 group = 'optional'
 
+includes = ['ana4gpio01']
+
 instrument_values = configdata('instrument.values')
 
 tango_base = instrument_values['tango_base'] + 'refsans/julabo01/'
+code_base = instrument_values['code_base']
 
 devices = dict(
     julabo_temp = device('nicos.devices.entangle.TemperatureController',
@@ -21,5 +24,11 @@ devices = dict(
         description = 'Julabo01 external sensor',
         tangodevice = tango_base + 'extsensor',
         fmtstr = '%.2f',
+    ),
+    julabo_flow = device(code_base + 'analogencoder.AnalogEncoder',
+        description = 'flow of julabo at PO by Sensor',
+        device = 'ana4gpio01_ch2',
+        poly = [0, 30.10],  #10V = 100L/min Spannungsteiler 10V/(5110+2200)*2200=3,01V
+        unit = 'L/min',
     ),
 )
