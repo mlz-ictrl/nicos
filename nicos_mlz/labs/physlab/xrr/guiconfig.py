df92eefad78b2a4513164ec8f2d066643da0bbdb
Jens Krüger <jens.krueger@tum.de>
2023-05-18 08:09:39 +0200

    XRR: Add new instrument in the MLZ physics labs
    Change-Id: I77482b2bc072ab55a70f60ea2f416f88ed8c13cb
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31135
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/labs/physlab/xrr/guiconfig.py
@@ -0,0 +1,48 @@
+"""NICOS GUI default configuration."""
+
+main_window = docked(
+    vsplit(
+        panel('nicos.clients.gui.panels.cmdbuilder.CommandPanel',
+              modules=['nicos.clients.gui.cmdlets'],
+        ),
+        panel('nicos.clients.gui.panels.status.ScriptStatusPanel'),
+        panel('nicos.clients.gui.panels.console.ConsolePanel',
+              hasinput=False),
+    ),
+    ('NICOS devices',
+     panel('nicos.clients.gui.panels.devices.DevicesPanel', icons=True,
+           dockpos='right'),
+    ),
+    ('Experiment Information and Setup',
+     panel('nicos.clients.gui.panels.expinfo.ExpInfoPanel'),
+    ),
+)
+
+windows = [
+    window('Editor', 'editor',
+        vsplit(
+           panel('nicos.clients.gui.panels.scriptbuilder.CommandsPanel'),
+           panel('nicos.clients.gui.panels.editor.EditorPanel',
+           ))),
+    window('Scans', 'plotter',
+           panel('nicos.clients.gui.panels.scans.ScansPanel')),
+    window('History', 'find',
+           panel('nicos.clients.gui.panels.history.HistoryPanel')),
+    window('Logbook', 'table',
+           panel('nicos.clients.gui.panels.elog.ELogPanel')),
+    window('Log files', 'table',
+           panel('nicos.clients.gui.panels.logviewer.LogViewerPanel')),
+    window('Errors', 'errors',
+           panel('nicos.clients.gui.panels.errors.ErrorPanel')),
+    window('Live data', 'live',
+           panel('nicos_mlz.labs.physlab.xresd.gui.live.TThetaLiveDataPanel')),
+]
+
+tools = [
+    tool('Calculator', 'nicos.clients.gui.tools.calculator.CalculatorTool'),
+    tool('Report NICOS bug or request enhancement',
+         'nicos.clients.gui.tools.bugreport.BugreportTool'),
+    tool('Emergency stop button',
+         'nicos.clients.gui.tools.estop.EmergencyStopTool',
+         runatstartup=False),
+]
---
+++ nicos_mlz/labs/physlab/xrr/setups/system.py
@@ -0,0 +1,69 @@
+# A detailed description of the setup file structure and it's elements is
+# available here: https://forge.frm2.tum.de/nicos/doc/nicos-stable/setups/
+#
+# Please remove these lines after copying this file.
+
+description = 'system setup'
+
+group = 'lowlevel'
+
+sysconfig = dict(
+    cache = 'localhost',
+    # Adapt this name to your instrument's name (also below).
+    instrument = 'XRR',
+    experiment = 'Exp',
+    datasinks = ['conssink', 'filesink', 'daemonsink', 'livesink'],
+    notifiers = [],  # ['email'],
+)
+
+modules = ['nicos.commands.standard']
+
+includes = [
+    # 'notifiers',
+    # 'incident',
+    # 'shutter',
+    # 'dectris',
+    # 'sample',
+    # 'd8',
+    # 'hv',
+]
+
+devices = dict(
+    XRR = device('nicos.devices.instrument.Instrument',
+        description = 'X-ray reflectormeter',
+        instrument = 'XRR',
+        responsible = 'Bastian Veltel <bastian.veltel@frm2.tum.de>',
+        website = 'https://mlz-garching.de/physics-lab',
+        operators = ['MLZ'],
+        facility = 'mlz',
+    ),
+    Sample = device('nicos.devices.sample.Sample',
+        description = 'The currently used sample',
+    ),
+
+    # Configure dataroot here (usually /data).
+    Exp = device('nicos.devices.experiment.Experiment',
+        description = 'experiment object',
+        dataroot = '/data',
+        sendmail = True,
+        serviceexp = 'service',
+        sample = 'Sample',
+    ),
+    filesink = device('nicos.devices.datasinks.AsciiScanfileSink'),
+    conssink = device('nicos.devices.datasinks.ConsoleScanSink'),
+    daemonsink = device('nicos.devices.datasinks.DaemonSink'),
+    livesink = device('nicos.devices.datasinks.LiveViewSink'),
+    Space = device('nicos.devices.generic.FreeSpace',
+        description = 'The amount of free space for storing data',
+        path = '/data',
+        warnlimits = (5., None),
+        minfree = 5,
+    ),
+    LogSpace = device('nicos.devices.generic.FreeSpace',
+        description = 'Space on log drive',
+        path = 'log',
+        warnlimits = (.5, None),
+        minfree = 0.5,
+        visibility = (),
+    ),
+)
