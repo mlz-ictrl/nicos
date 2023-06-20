dd88c42fb46698d234e40dca448046c422a8888b
Matt Clarke <matt.clarke@ess.eu>
2023-06-20 10:05:02 +0200

    ESS: set LoKI to use Kafka auth
    Change-Id: I0cd89e28a30ac234d406d590a56a70238d93229d
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31382
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@ess.eu>

---
+++ nicos_ess/loki/setups/special/collector.py
@@ -0,0 +1,16 @@
+description = 'setup for the NICOS collector'
+group = 'special'
+
+devices = dict(
+    CacheKafka=device(
+        'nicos_ess.devices.cache_kafka_forwarder.CacheKafkaForwarder',
+        dev_ignore=['space', 'sample'],
+        brokers=['10.100.1.19:8093'],
+        output_topic='loki_nicos_devices',
+    ),
+    Collector=device(
+        'nicos.services.collector.Collector',
+        cache='localhost:14869',
+        forwarders=['CacheKafka'],
+    ),
+)
--- nicos_ess/loki/setups/system.py
+++ nicos_ess/loki/setups/system.py
@@ -11,6 +11,8 @@ sysconfig = dict(
 
 modules = ['nicos.commands.standard', 'nicos_ess.commands']
 
+KAFKA_BROKERS = ["10.100.1.19:8093"]
+
 devices = dict(
     LoKI=device(
         'nicos.devices.instrument.Instrument',
@@ -22,14 +24,26 @@ devices = dict(
         'nicos_ess.devices.sample.EssSample',
         description='The currently used sample',
     ),
-    Exp=device('nicos_ess.devices.experiment.EssExperiment',
-               description='experiment object',
-               dataroot='/opt/nicos-data',
-               filewriter_root='/opt/nicos-data/loki',
-               sample='Sample',
-               cache_filepath='/opt/nicos-data/loki/cached_proposals.json'),
+    Exp=device(
+        'nicos_ess.devices.experiment.EssExperiment',
+        description='experiment object',
+        dataroot='/opt/nicos-data',
+        filewriter_root='/ess/data/loki',
+        sample='Sample',
+        cache_filepath='/opt/nicos-data/loki/cached_proposals.json'),
     conssink=device(
         'nicos_ess.devices.datasinks.console_scan_sink.ConsoleScanSink'),
     daemonsink=device('nicos.devices.datasinks.DaemonSink', ),
     liveview=device('nicos.devices.datasinks.LiveViewSink', ),
+    KafkaForwarderStatus=device(
+        'nicos_ess.devices.forwarder.EpicsKafkaForwarder',
+        description='Monitors the status of the Forwarder',
+        statustopic="loki_forwarder_status",
+        brokers=KAFKA_BROKERS,
+    ),
+    SciChat=device(
+        'nicos_ess.devices.scichat.ScichatBot',
+        description='Sends messages to SciChat',
+        brokers=KAFKA_BROKERS,
+    ),
 )
