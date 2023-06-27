ed8d3a363f17e52f358ab8ab733b726884ef57dc
Matt Clarke <matt.clarke@ess.eu>
2023-06-27 11:17:18 +0200

    ESS: add setup for MacGyver relays
    Change-Id: I2feec526ecf72349d570865454581821ddc54f6c
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31464
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@ess.eu>

---
+++ nicos_ess/ymir/setups/macgyver_relay.py
@@ -0,0 +1,46 @@
+description = 'The solid-state relays of the MacGyver box.'
+
+pv_root = 'se-macgyver-001:'
+
+devices = dict(
+    macgyver_relay_1=device(
+        'nicos.devices.epics.pva.EpicsMappedMoveable',
+        description='MacGyver box relay 1',
+        readpv='{}relay_1-R'.format(pv_root),
+        writepv='{}relay_1-S'.format(pv_root),
+        pva=True,
+        monitor=True,
+        pollinterval=0.5,
+        maxage=None,
+    ),
+    macgyver_relay_2=device(
+        'nicos.devices.epics.pva.EpicsMappedMoveable',
+        description='MacGyver box relay 2',
+        readpv='{}relay_2-R'.format(pv_root),
+        writepv='{}relay_2-S'.format(pv_root),
+        pva=True,
+        monitor=True,
+        pollinterval=0.5,
+        maxage=None,
+    ),
+    macgyver_relay_3=device(
+        'nicos.devices.epics.pva.EpicsMappedMoveable',
+        description='MacGyver box relay 3',
+        readpv='{}relay_3-R'.format(pv_root),
+        writepv='{}relay_3-S'.format(pv_root),
+        pva=True,
+        monitor=True,
+        pollinterval=0.5,
+        maxage=None,
+    ),
+    macgyver_relay_4=device(
+        'nicos.devices.epics.pva.EpicsMappedMoveable',
+        description='MacGyver box relay 4',
+        readpv='{}relay_4-R'.format(pv_root),
+        writepv='{}relay_4-S'.format(pv_root),
+        pva=True,
+        monitor=True,
+        pollinterval=0.5,
+        maxage=None,
+    ),
+)
