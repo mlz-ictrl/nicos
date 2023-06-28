f9bca6720272e672a1e973bdbb0d4191fc3845ed
Jens Krüger <jens.krueger@frm2.tum.de>
2023-06-28 13:30:41 +0200

    REFSANS: Remove additional date information
    There is no need to add date information to the module authors list
    
    Change-Id: Ifc0f55c6d7cdc5c2a2745e27e96c42fe21146cce
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31478
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/devices/converters.py
+++ nicos_mlz/refsans/devices/converters.py
@@ -18,7 +18,7 @@
 # 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 # Module authors:
-#   Matthias Pomm <matthias.pomm@hzg.de> 2018-08-06 11:18:31
+#   Matthias Pomm <matthias.pomm@hzg.de>
 #
 # *****************************************************************************
 
--- nicos_mlz/refsans/devices/pivot.py
+++ nicos_mlz/refsans/devices/pivot.py
@@ -18,7 +18,7 @@
 # 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 # Module authors:
-#   Matthias Pomm <matthias.pomm@hzg.de> 2018-08-08 08:33:38
+#   Matthias Pomm <matthias.pomm@hzg.de>
 #
 # *****************************************************************************
 
