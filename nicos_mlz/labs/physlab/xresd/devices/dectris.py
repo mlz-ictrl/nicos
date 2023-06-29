76129013f15642e9c9a9d3dfcbd2fc1c2a754c44
Jens Krüger <jens.krueger@frm2.tum.de>
2023-06-29 13:29:44 +0200

    XRESD: Change the image reading device
    The image reading device is derived from entangle's ImageChannel to
    avoid a lot of code overhead.
    
    Change-Id: Id52b30cd37f108c523303d97fe0f393308a58f55
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31510
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/labs/physlab/xresd/devices/dectris.py
@@ -0,0 +1,42 @@
+#  -*- coding: utf-8 -*-
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2023 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Alexander Book <alexander.book@frm2.tum.de>
+#
+# *****************************************************************************
+
+
+from nicos.core.params import Param
+from nicos.devices.entangle import ImageChannel
+
+
+class Detector(ImageChannel):
+
+    parameters = {
+        'pixel_size': Param('Size of a single pixel (in mm)',
+                            type=float, volatile=False, settable=False,
+                            category='instrument'),
+        'pixel_count': Param('Number of detector pixels',
+                             type=int, volatile=True, settable=False,
+                             category='instrument'),
+    }
+
+    def doReadPixel_Count(self):
+        return self.arraydesc.shape[0]
