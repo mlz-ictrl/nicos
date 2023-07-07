f30b580580aee0620a4198538026da4a4ea817c1
Jens Krüger <jens.krueger@frm2.tum.de>
2023-07-07 08:36:06 +0200

    REFSANS: Fix setup description
    Change-Id: I173e30f0b10c54fcf8761264c0910391f5ae70e8
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31597
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/attenuators.py
+++ nicos_mlz/refsans/setups/elements/attenuators.py
@@ -1,14 +1,14 @@
-description = 'Alphai alias device'
+description = 'Attenuators in beam guide chamber'
 
 group = 'lowlevel'
 
 devices = dict(
     attenuator1 = device('nicos.devices.generic.ManualSwitch',
         description = 'attenuator at zb0',
-        states = ['out','in'],
+        states = ['out', 'in'],
     ),
     attenuator2 = device('nicos.devices.generic.ManualSwitch',
         description = 'attenuator at zb1',
-        states = ['out','in'],
+        states = ['out', 'in'],
     ),
 )
