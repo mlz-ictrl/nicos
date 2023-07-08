bd163d0f5d6c1c79093610378af8353484dc008f
Jens Krüger <jens.krueger@tum.de>
2023-07-08 09:19:03 +0200

    Test: Fix authorship of Refsans specific tests
    Change-Id: I3772a000e885371d154dbaf1b42900adad0a1da7
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31615
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- test/nicos_mlz/refsans/test_devices.py
+++ test/nicos_mlz/refsans/test_devices.py
@@ -18,7 +18,7 @@
 # 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 # Module authors:
-#   Jens Krüger <jens.krueger@frm2.tum.de>
+#   Matthias Pomm <matthias.pomm@hereon.de>
 #
 # *****************************************************************************
 
