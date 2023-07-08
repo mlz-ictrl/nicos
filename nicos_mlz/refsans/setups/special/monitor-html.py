555fc2c73067a55b43c17fa2369868344f5a81a3
Matthias Pomm <matthias.pomm@hzg.de>
2023-07-08 22:21:09 +0200

    REFSANS: Add some devices to the HTML monitor
    Change-Id: I48c66e6829d8ecaa2bc824666336c530a77fc611
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31672
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/special/monitor-html.py
+++ nicos_mlz/refsans/setups/special/monitor-html.py
@@ -10,6 +10,47 @@ _experimentcol = Column(
     ),
 )
 
+Elemente1 = [
+    'chamber_CB',
+    'chamber_SFK',
+    'chamber_SR',
+]
+
+wi=100
+he=50
+Time_selection1 = [1] + ['day', 24*60*60]
+
+_plot_Block = Block('History motor %d%s' % (Time_selection1[0], Time_selection1[1]) , [
+        BlockRow(
+            Field(dev=Elemente1[0], plot=Elemente1[0],
+                  plotwindow=Time_selection1[0]*Time_selection1[2],
+                  width=wi, height=he),
+            ),
+        BlockRow(
+            Field(dev=Elemente1[1], plot=Elemente1[1],
+                  plotwindow=Time_selection1[0]*Time_selection1[2],
+                  width=wi, height=he),
+            ),
+        BlockRow(
+            Field(dev=Elemente1[2], plot=Elemente1[2],
+                  plotwindow=Time_selection1[0]*Time_selection1[2],
+                  width=wi, height=he),
+            ),
+        ],
+    )
+
+_plot_col = Column(_plot_Block)
+
+_pumpstandcol = Column(
+    Block('pumpstand chamber 2', [
+        BlockRow(
+            Field(name='CB', dev='chamber_CB'),
+            Field(name='SFK', dev='chamber_SFK'),
+            Field(name='SR', dev='chamber_SR'),
+        ),
+    ],),
+)
+
 devices = dict(
     Monitor = device('nicos.services.monitor.html.Monitor',
         description = 'Status Display',
