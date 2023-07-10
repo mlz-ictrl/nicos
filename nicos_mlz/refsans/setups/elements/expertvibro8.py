4846faeaae6367cc7dd1f6e3ce83ee86950463c3
Jens Krüger <jens.kruege@frm2.tum.de>
2023-07-10 14:18:21 +0200

    REFSANS: Fix typo introduced in I448f5e19a1808b816ab1412869cd6e88afebad55
    https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31593
    
    Change-Id: I48cbf8a081672a8a41880f6d891113433a1846b1
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31625
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/expertvibro8.py
+++ nicos_mlz/refsans/setups/elements/expertvibro8.py
@@ -11,22 +11,22 @@ devices = dict(
 )
 
 for i in range(1, 5):
-    devices[f'{setupname}_trigger_{i}'] : device(code_base + 'AnalogValue',
+    devices[f'{setupname}_trigger_{i}'] = device(code_base + 'AnalogValue',
         description = f'{setupname} Trigger {i}',
         tangodevice = tango_base,
         channel = 0 + (i -1) * 4,
     )
-    devices[f'{setupname}_Amplitude_{i}'] : device(code_base + 'AnalogValue',
+    devices[f'{setupname}_Amplitude_{i}'] = device(code_base + 'AnalogValue',
         description = f'{setupname} Amplitude {i}',
         tangodevice = tango_base,
         channel = 1 + (i - 1) * 4,
     )
-    devices[f'{setupname}_Phase_{i}'] : device(code_base + 'AnalogValue',
+    devices[f'{setupname}_Phase_{i}'] = device(code_base + 'AnalogValue',
         description = f'{setupname} Phase {i}',
         tangodevice = tango_base,
         channel = 2 + (i - 1) * 4,
     )
-    devices[f'{setupname}_Frequenz_{i}'] : device(code_base + 'AnalogValue',
+    devices[f'{setupname}_Frequenz_{i}'] = device(code_base + 'AnalogValue',
         description = f'{setupname} Frequenz {i}',
         tangodevice = tango_base,
         channel = 3 + (i - 1) * 4,
