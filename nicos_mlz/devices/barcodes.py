bbba6ff8509852e5a80550050a46c4ffbc3453f0
Jens Krüger <jens.krueger@tum.de>
2023-07-17 21:45:37 +0200

    BarcodeReader: Refactor code
    Switch to event based system:
    
    - Register change event on the 'availableLines'
    - After receiving the event and the number of available lines is not 0,
      a line will be read
    - Analyze and execute the received code
    
    Change-Id: I2577e38a0785b19e0bdb7fa67a4d5f90861c9f0a
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31704
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Tested-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/devices/barcodes.py
+++ nicos_mlz/devices/barcodes.py
@@ -25,131 +25,16 @@
 """Device to receive and interpret barcode input at instruments."""
 
 import re
-import time
 import urllib
+from collections import namedtuple
+
+import tango
 
 from nicos import session
 from nicos.core import SIMULATION, Param, dictof
 from nicos.core.utils import USER, User
 from nicos.devices.entangle import StringIO
 from nicos.services.daemon.script import RequestError, ScriptRequest
-from nicos.utils import createThread
-
-
-class BarcodeInterpreter(StringIO):
-    """Receives input from a connected barcode reader Tango device and executes
-    corresponding actions as scripts in the NICOS daemon.
-
-    - Currently recognized barcodes must be in QR-Code format and should
-      contain an URI of this format::
-
-         nicos+cmd://Command/arg1/arg2/...
-
-      For example, ::
-
-          nicos+cmd://maw/shutter1/"open"
-
-      to execute `maw(shutter1, "open")`.
-
-    - This device supports assigning shortcuts in the `commandmap` parameter.
-      The `Command` in the URI is looked up in the map, and if found, the
-      corresponding code is executed, with URI path arguments represented
-      by `$0`, `$1` and so forth.
-
-      For example, for this command map ::
-
-         { 'SE': 'NewSetup("$0"); printinfo("Loaded $1 $0")' }
-
-      and this QR-Code content ::
-
-         nicos+cmd://SE/ccr0/cryostat
-
-      NICOS will execute `NewSetup("ccr0"); printinfo("Loaded cryostat ccr0")`.
-
-    Executed commands are run at `USER` level by a special user named like this
-    device is called in the setup.
-    """
-
-    parameters = {
-        'commandmap': Param('Mapping of short commands to Python code',
-                            type=dictof(str, str), mandatory=True),
-    }
-
-    _thread = None
-    _stoprequest = False
-
-    def doInit(self, mode):
-        if mode != SIMULATION:
-            daemon = getattr(session, 'daemon_device', None)
-            if daemon:
-                self._thread = createThread('barcode receiver',
-                                            self._thread_func, (daemon,))
-
-    def doShutdown(self):
-        self._stoprequest = True
-
-    def _thread_func(self, daemon):
-        user = User(name=self.name, level=USER)
-        controller = daemon._controller
-        error_occurred = False
-        while not self._stoprequest:
-            try:
-                barcode = self.readLine()
-                if not barcode:
-                    # The Tango call will do a blocking poll with typically 2s
-                    # timeout, so we don't need another sleep here.
-                    continue
-                script = self._convert_code(*barcode.split(',', 1))
-                if not script:
-                    # Let the user know that the code was not recognized.
-                    self._dev.Beep(12)
-                    continue
-                try:
-                    controller.new_request(ScriptRequest(
-                        script, '<barcode request>', user))
-                except RequestError:
-                    self.log.warning('could not initiate request from barcode',
-                                     exc=1)
-                    self._dev.Beep(12)
-                else:
-                    # Acknowledge receipt and execution.
-                    self._dev.Beep(25)
-                error_occurred = False
-            except Exception:
-                # Happens e.g. when the Tango server is restarted during a
-                # readLine call.  Log only once, to avoid continuous spamming.
-                if not error_occurred:
-                    self.log.warning('error waiting for barcode', exc=1)
-                    error_occurred = True
-                # Also leave some time inbetween attempts; if the Tango server
-                # is not coming back we don't want to busy-loop.
-                time.sleep(3)
-
-    def _convert_code(self, codetype, content):
-        """Convert a barcode of given type and content to Python code to
-        execute.
-        """
-        if codetype == 'QR':
-            try:
-                res = urllib.parse.urlparse(content)
-                if not res.scheme.startswith('nicos+'):
-                    return
-            except ValueError:
-                return
-            if res.scheme == 'nicos+cmd':
-                cmd = res.netloc
-                args = res.path.split('/')
-                if cmd in self.commandmap:
-                    code = re.sub(r'\$(\d)', lambda m: args[int(m.group(1))],
-                                  self.commandmap[cmd])
-                else:
-                    code = '%s(%s)' % (cmd, ', '.join(args[1:]))
-                return code
-        elif codetype == 'Code 128' and content.isdigit():
-            # shortcut for samples from sample database
-            # XXX: proposals use the same barcode type without distinction
-            # return 'NewSampleFromDatabase(%s)' % content
-            pass
 
 
 class BarcodeInterpreterMixin:
@@ -256,3 +141,96 @@ class BarcodeInterpreterMixin:
             # XXX: proposals use the same barcode type without distinction
             # return 'NewSampleFromDatabase(%s)' % content
             pass
+
+
+class TangoAttrCb:
+    """An base callback class for Tango events"""
+
+    def __init__(self, device):
+        self._device = device
+        self._tango_device = device._dev
+
+
+class TangoAvailableLinesCb(TangoAttrCb):
+    """Callback class for Tango events of the AvailableLines attributes."""
+
+    def __init__(self, device, daemon):
+        TangoAttrCb.__init__(self, device)
+        self._user = User(name=device.name, level=USER)
+        self._controller = daemon._controller if daemon else None
+
+    def push_event(self, *args, **kwargs):
+        """callback method receiving the event"""
+        event_data = args[0]
+        if event_data.attr_value and event_data.attr_value.value:
+            try:
+                barcode = self._tango_device.readLine()
+                self._device.on_update(namedtuple('Item', ['value'])(value=barcode))
+            except Exception:
+                pass
+
+
+class BarcodeInterpreter(BarcodeInterpreterMixin, StringIO):
+    """Receives input from a connected barcode reader Tango device and executes
+    corresponding actions as scripts in the NICOS daemon.
+
+    - Currently recognized barcodes must be in QR-Code format and should
+      contain an URI of this format::
+
+         nicos+cmd://Command/arg1/arg2/...
+
+      For example, ::
+
+          nicos+cmd://maw/shutter1/"open"
+
+      to execute `maw(shutter1, "open")`.
+
+    - This device supports assigning shortcuts in the `commandmap` parameter.
+      The `Command` in the URI is looked up in the map, and if found, the
+      corresponding code is executed, with URI path arguments represented
+      by `$0`, `$1` and so forth.
+
+      For example, for this command map ::
+
+         { 'SE': 'NewSetup("$0"); printinfo("Loaded $1 $0")' }
+
+      and this QR-Code content ::
+
+         nicos+cmd://SE/ccr0/cryostat
+
+      NICOS will execute `NewSetup("ccr0"); printinfo("Loaded cryostat ccr0")`.
+
+    Executed commands are run at `USER` level by a special user named like this
+    device is called in the setup.
+    """
+
+    parameters = {
+        'commandmap': Param('Mapping of short commands to Python code',
+                            type=dictof(str, str), mandatory=True),
+    }
+
+    _availablelines_id = None
+
+    def register_callback(self, mode, cb):
+        self._registerAvailableLines()
+
+    def doShutdown(self):
+        self._unregisterAvailableLines()
+
+    def _registerAvailableLines(self):
+        """Register 'availableLines' events"""
+        al = self._dev.get_attribute_config('availableLines')
+        al.events.ch_event.abs_change = '1'
+        self._dev.set_attribute_config(al)
+        self._availablelines_id = self._dev.subscribe_event(
+            'availableLines',
+            tango.EventType.CHANGE_EVENT,
+            TangoAvailableLinesCb(self, self._daemon))
+
+    def _unregisterAvailableLines(self):
+        """Unregister 'availableLines' events"""
+        if self._availablelines_id:
+            self._dev.unsubscribe_event(self._availablelines_id)
+
+    def beep(self, pattern):
+        self._dev.Beep(pattern)
