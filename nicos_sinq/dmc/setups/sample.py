a92040c82143ce74ea3e47ac0f0e66e19591f6d1
Mark Koennecke <mark.koennecke@psi.ch>
2023-07-21 11:42:46 +0200

    SINQ: DMC received more motors and fixes to setups and data file format
    Change-Id: I50d34304cdc61bd4d8e6bd375415ecd6649c3db9
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31740
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Mark Koennecke <mark.koennecke@psi.ch>

--- nicos_sinq/dmc/setups/sample.py
+++ nicos_sinq/dmc/setups/sample.py
@@ -1,17 +1,18 @@
-description = 'Sample devices in the SINQ DMC.'
+description = 'Sample devices for SINQ DMC.'
 
 pvprefix = 'SQ:DMC:mcu2:'
 
 devices = dict(
-    a3s=device('nicos_sinq.devices.epics.motor.EpicsMotor',
-               description='Sample omega motor',
-               motorpv=f'{pvprefix}A3',
-               errormsgpv=f'{pvprefix}A3-MsgTxt',
-               can_disable=True,
-               auto_enable=True,
-               ),
-    a3=device('nicos.core.device.DeviceAlias',
-              description='Alias for sample rotation',
-              devclass='nicos.core.device.Moveable'),
+    a3s = device('nicos_sinq.devices.epics.motor.EpicsMotor',
+        description = 'Sample omega motor',
+        motorpv = f'{pvprefix}A3',
+        errormsgpv = f'{pvprefix}A3-MsgTxt',
+        can_disable = True,
+        auto_enable = True,
+    ),
+    a3 = device('nicos.core.device.DeviceAlias',
+        description = 'Alias for sample rotation',
+        devclass = 'nicos.core.device.Moveable'
+    ),
 )
-alias_config = {'a3': {'a3s': 10}}  # , 'se_om': 20}}
+alias_config = {'a3': {'a3s': 10,}}
