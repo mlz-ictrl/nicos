8b39477a42ca09a5af338cf7d17dd1f5278eb212
Matt Clarke <matt.clarke@ess.eu>
2023-08-04 13:52:25 +0200

    ESS: move the last few devices over to PVA
    Change-Id: I4fb9f3309e2072669faa0fce6e4669b787bea27f
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31903
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@ess.eu>

--- nicos_ess/ymir/setups/syringe_pump_ne1600.py
+++ nicos_ess/ymir/setups/syringe_pump_ne1600.py
@@ -4,10 +4,14 @@ pv_root = 'E04-SEE-FLUCO:NE1600-001:'
 
 devices = dict(
     pump_status_1600=device(
-        'nicos.devices.epics.EpicsStringReadable',
+        'nicos.devices.epics.pva.EpicsStringReadable',
         description='The current pump status',
         readpv='{}STATUS_TEXT'.format(pv_root),
         visibility=(),
+        pva=True,
+        monitor=True,
+        pollinterval=0.5,
+        maxage=None,
     ),
     syringe_pump_1600=device(
         'nicos_ess.devices.epics.syringe_pump.SyringePumpController',
