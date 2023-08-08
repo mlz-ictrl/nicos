f122ed1363223a38af9597d54a0e00b171ec6ea5
Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
2023-08-08 16:13:33 +0200

    MLZ/KOMPASS: add setup for spherical pa
    Change-Id: I9cfdedecce1c1cdac12b361d03691ea5c3676c9b
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31918
    Tested-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

---
+++ nicos_mlz/kompass/setups/spherical_pa.py
@@ -0,0 +1,65 @@
+description = 'Kompass setup for longitudinal polarisation analysis mode'
+
+group = 'optional'
+
+tango_base = 'tango://kompasshw.kompass.frm2:10000/kompass/'
+
+devices = dict(
+    radial_coil1 = device('nicos.devices.entangle.PowerSupply',
+        description = "kepco power supply 1",
+        tangodevice = tango_base + 'kepco/current1',
+        fmtstr = '%.3f',
+    ),
+    axial_coil1 = device('nicos.devices.entangle.PowerSupply',
+        description = "kepco power supply 2",
+        tangodevice = tango_base + 'kepco/current2',
+        fmtstr = '%.3f',
+    ),
+    radial_coil2 = device('nicos.devices.entangle.PowerSupply',
+        description = "kepco power supply 3",
+        tangodevice = tango_base + 'kepco/current3',
+        fmtstr = '%.3f',
+    ),
+    axial_coil2 = device('nicos.devices.entangle.PowerSupply',
+        description = "kepco power supply 4",
+        tangodevice = tango_base + 'kepco/current4',
+        fmtstr = '%.3f',
+    ),
+    nutator1 = device('nicos.devices.generic.Axis',
+        description = 'nutator 1 (facing sample)',
+        motor = device('nicos.devices.entangle.Motor',
+            tangodevice = tango_base + 'aircontrol/plc_nutator1_mot',
+            fmtstr = '%.4f',
+            visibility = (),
+        ),
+        coder = device('nicos.devices.entangle.Sensor',
+            tangodevice = tango_base + 'aircontrol/plc_nutator1_enc',
+            fmtstr = '%.4f',
+            visibility = (),
+        ),
+        fmtstr = '%.3f',
+        precision = 0.001,
+    ),
+    nutator2 = device('nicos.devices.generic.Axis',
+        description = 'nutator 2 (facing analyser)',
+        motor = device('nicos.devices.entangle.Motor',
+            tangodevice = tango_base + 'aircontrol/plc_nutator2_mot',
+            fmtstr = '%.4f',
+            visibility = (),
+        ),
+        coder = device('nicos.devices.entangle.Sensor',
+            tangodevice = tango_base + 'aircontrol/plc_nutator2_enc',
+            fmtstr = '%.4f',
+            visibility = (),
+        ),
+        fmtstr = '%.3f',
+        precision = 0.001,
+    ),
+)
+
+startupcode="""
+radial_coil1.userlimits=(-2.1, 2.1)
+axial_coil1.userlimits=(-2.1, 2.1)
+radial_coil2.userlimits=(-2.1, 2.1)
+axial_coil2.userlimits=(-2.1, 2.1)
+"""
