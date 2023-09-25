d5901bde070c6b21bbcfb94726fc28c7f538204a
Matt Clarke <matt.clarke@ess.eu>
2023-09-25 13:20:41 +0200

    ESS: add setup for NIDO camera box
    Change-Id: I2a87d7272b2904b43a189dc55be3a71729181d7c
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/32202
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Matt Clarke <matt.clarke@ess.eu>

---
+++ nicos_ess/nido/setups/camera_box.py
@@ -0,0 +1,23 @@
+description = 'The motor for controlling the camera position'
+
+devices = dict(
+    camera_box_motor=device(
+        'nicos_ess.devices.epics.pva.motor.EpicsMotor',
+        description='The camera box motor',
+        motorpv='YMIR-4004:MC-MCU-02:Mtr4',
+        pollinterval=0.5,
+        maxage=None,
+        monitor=True,
+        pva=True,
+    ),
+    brake=device(
+        'nicos.devices.epics.pva.EpicsMappedMoveable',
+        description='The brake for the camera box motor',
+        readpv='YMIR-4004:MC-MCU-02:Mtr4-ReleaseBrake',
+        writepv='YMIR-4004:MC-MCU-02:Mtr4-ReleaseBrake',
+        pva=True,
+        monitor=True,
+        pollinterval=0.5,
+        maxage=None,
+    )
+)
