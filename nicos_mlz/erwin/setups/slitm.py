96c9da15da8f42ad483fc01b282f12cbbf21b267
Jens Krüger <jens.krueger@tum.de>
2023-11-27 20:29:40 +0100

    ErwiN: Adapt slits to newer information
    Change-Id: Ibd451e54e9af698d0de2bce82c5a677baf844c1b
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/32652
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/erwin/setups/slitm.py
@@ -0,0 +1,37 @@
+description = 'Monochromator slit'
+
+group = 'optional'
+
+devices = dict(
+    slitm = device('nicos.devices.generic.Slit',
+        description = 'B4C slit',
+        bottom = device('nicos.devices.generic.Axis',
+            motor = device('nicos.devices.generic.VirtualMotor',
+                abslimits = (0, 75),
+                unit = 'mm',
+            ),
+            precision = 0.01,
+        ),
+        top = device('nicos.devices.generic.Axis',
+            motor = device('nicos.devices.generic.VirtualMotor',
+                abslimits = (0, 75),
+                unit = 'mm',
+            ),
+            precision = 0.01,
+        ),
+        left = device('nicos.devices.generic.Axis',
+            motor = device('nicos.devices.generic.VirtualMotor',
+                abslimits = (0, 25),
+                unit = 'mm',
+            ),
+            precision = 0.01,
+        ),
+        right = device('nicos.devices.generic.Axis',
+            motor = device('nicos.devices.generic.VirtualMotor',
+                abslimits = (0, 25),
+                unit = 'mm',
+            ),
+            precision = 0.01,
+        ),
+    ),
+)
