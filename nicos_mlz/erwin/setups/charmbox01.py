c100d315ea8b264256d13177af26927a05194b12
Jens Krüger <jens.krueger@frm2.tum.de>
2023-11-30 17:23:36 +0100

    ErwiN: Change pressure device names for charm boxes
    According to the wishes of the detector electronics people the names are
    changed.
    
    Change-Id: I6145baaadb85071966b65a6c31b1caf5549774ae
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/32687
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/erwin/setups/charmbox01.py
+++ nicos_mlz/erwin/setups/charmbox01.py
@@ -2,21 +2,25 @@ description = 'Montoring devices for the CHARM detector'
 
 group = 'lowlevel'
 
-tango_base = 'tango://erwinhw.erwin.frm2.tum.de:10000/erwin/charmbox01/'
+tango_base = 'tango://erwinhw.erwin.frm2.tum.de:10000/erwin/charmbox01/_'
 
 devices = dict(
     charm1_flow = device('nicos.devices.entangle.Sensor',
         tangodevice = tango_base + 'flow',
         visibility = (),
     ),
+    charm1_pdet = device('nicos.devices.entangle.Sensor',
+        tangodevice = tango_base + 'p1',
+        visibility = ()
+    ),
+    charm1_ppump1 = device('nicos.devices.entangle.Sensor',
+        tangodevice = tango_base + 'p2',
+        visibility = ()
+    ),
 )
 
 for i in range(1, 4):
     devices[f'charm1_t{i}'] = device('nicos.devices.entangle.Sensor',
         tangodevice = tango_base + f't{i}',
-        visibility = ()
-    )
-    devices[f'charm1_p{i}'] = device('nicos.devices.entangle.Sensor',
-        tangodevice = tango_base + f'p{i}',
         visibility = ()
     )
--- nicos_mlz/erwin/setups/charmbox02.py
+++ nicos_mlz/erwin/setups/charmbox02.py
@@ -2,21 +2,29 @@ description = 'Montoring devices for the CHARM detector'
 
 group = 'lowlevel'
 
-tango_base = 'tango://erwinhw.erwin.frm2.tum.de:10000/erwin/charmbox02/'
+tango_base = 'tango://erwinhw.erwin.frm2.tum.de:10000/erwin/charmbox02/_'
 
 devices = dict(
     charm2_flow = device('nicos.devices.entangle.Sensor',
         tangodevice = tango_base + 'flow',
         visibility = (),
     ),
+    charm2_pdet = device('nicos.devices.entangle.Sensor',
+        tangodevice = tango_base + 'p1',
+        visibility = ()
+    ),
+    charm2_ppump1 = device('nicos.devices.entangle.Sensor',
+        tangodevice = tango_base + 'p2',
+        visibility = ()
+    ),
+    charm2_ppump2 = device('nicos.devices.entangle.Sensor',
+        tangodevice = tango_base + 'p3',
+        visibility = ()
+    ),
 )
 
 for i in range(1, 4):
     devices[f'charm2_t{i}'] = device('nicos.devices.entangle.Sensor',
         tangodevice = tango_base + f't{i}',
-        visibility = ()
-    )
-    devices[f'charm2_p{i}'] = device('nicos.devices.entangle.Sensor',
-        tangodevice = tango_base + f'p{i}',
         visibility = ()
     )
