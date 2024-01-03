002fb64258c512ab4ec89277fb69b0646e8f165e
Jens Krüger <jens.krueger@tum.de>
2024-01-03 10:29:30 +0100

    SXTal: Fix some pylint issues
    - Use of dunder methods
    
    Change-Id: I2c9803b67bb0dbe28305468ef8513571ff488144
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/32835
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos/devices/sxtal/goniometer/base.py
+++ nicos/devices/sxtal/goniometer/base.py
@@ -1,6 +1,6 @@
 # *****************************************************************************
 # NICOS, the Networked Instrument Control System of the MLZ
-# Copyright (c) 2009-2023 by the NICOS contributors (see AUTHORS)
+# Copyright (c) 2009-2024 by the NICOS contributors (see AUTHORS)
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -103,7 +103,7 @@ class PositionBase:
 
     def asType(self, newtype, wavelength=None):
         if newtype.lower() in typelist:
-            return self.__getattribute__('as%s' % newtype.upper())(wavelength)
+            return getattr(self, 'as%s' % newtype.upper())(wavelength)
         raise TypeError("unknown position type")
 
 
