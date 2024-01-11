01a079c7e6b973a9cff58cb6339daa25d45ce5f5
Andreas Wilhelm <andreas.wilhelm@frm2.tum.de>
2024-01-11 16:07:56 +0100

    FRM2/SANS1: setup changes
    switch samplechanger from motor to actuator to disable broken and dangerous reference method.
    
    Change-Id: If516049ecb605673084682d9d4c05c8e81a28e93
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/32855
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@tum.de>
    Tested-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

--- nicos_mlz/sans1/setups/sample_changer.py
+++ nicos_mlz/sans1/setups/sample_changer.py
@@ -5,7 +5,8 @@ group = 'lowlevel'
 tango_base = 'tango://hw.sans1.frm2.tum.de:10000/sample/changer/'
 
 devices = dict(
-    sc_y = device('nicos.devices.entangle.Motor',
+    # use an Actuator as referencing is broken on this hw
+    sc_y = device('nicos.devices.entangle.Actuator',
         description = 'Sample Changer Axis motor',
         tangodevice = tango_base + 'sc',
         fmtstr = '%.2f',
