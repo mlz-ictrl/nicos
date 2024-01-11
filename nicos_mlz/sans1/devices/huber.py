b6147c6ac165177128c0b7b06829e88a1f7bf49f
Andreas Wilhelm <andreas.wilhelm@frm2.tum.de>
2024-01-11 16:07:30 +0100

    FRM2/SANS1: sample table: block setPosition
    as the huber axes have an absolute encoder, setPosition makes no sense
    and would be harmful.
    
    using nicos offset mechanism for those in need to shift coordinate
    systems...
    
    Change-Id: I20c24953ea6ebd84b8ecb4717d81b495e236f676
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/32854
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

---
+++ nicos_mlz/sans1/devices/huber.py
@@ -0,0 +1,44 @@
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2023 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
+#
+# *****************************************************************************
+
+"""Special device for Huber absolute encoded axis"""
+
+
+from nicos.core import HasOffset
+from nicos.devices.entangle import Actuator
+
+
+class HuberAxis(HasOffset, Actuator):
+
+    def doRead(self, maxage=0):
+        return Actuator.doRead(self, maxage) - self.offset
+
+    def doStart(self, target):
+        return Actuator.doStart(self, target + self.offset)
+
+    def doAdjust(self, oldvalue, newvalue):
+        return HasOffset.doAdjust(self, oldvalue, newvalue)
+
+    def doSetPosition(self, pos):
+        # just adjust the offset, do not interact with HW!
+        self.doAdjust(self.read(0), pos)
