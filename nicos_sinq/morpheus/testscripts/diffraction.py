a6d5c9b6423e95ac77c4c1a9f006c333b5c90687
Jens Krüger <jens.krueger@tum.de>
2024-01-22 06:26:11 +0100

    Test: Add testscript for the MORPHEUS diffraction
    Change-Id: I64f2329bd8ad741b4be79578004a855927bb09b3
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/33482
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Mark Koennecke <mark.koennecke@psi.ch>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_sinq/morpheus/testscripts/diffraction.py
+++ nicos_sinq/morpheus/testscripts/diffraction.py
@@ -7,5 +7,20 @@
 # test: subdirs = morpheus
 # test: setups = diffraction
 
-# read()
+# read all device values and statuses
+read()
 status()
+
+# test the AldiMotor
+d1t.startBack()
+maw(d1t, 10)
+d1t.startBack()
+maw(d1t, 0)
+d1t.startBack()
+maw(slit1_width, 10)
+maw(slit1_width, 0)
+
+# test special command
+ScanOmega((0, 0, 0))
+move(slit1_width, 0)
+move(slit1_width, 10)
