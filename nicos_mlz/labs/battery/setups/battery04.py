1e6b40e2fb5ce09dfda2d5f53be4e3eb942bbfe8
Jens Krüger <jens.krueger@frm2.tum.de>
2024-01-24 17:52:34 +0100

    Labs/Battery: Switch to f-strings in setups
    Change-Id: I2f57ff04ce23689bb4c7413212ef0b633ddb5f39
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/33036
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/labs/battery/setups/battery04.py
+++ nicos_mlz/labs/battery/setups/battery04.py
@@ -2,7 +2,7 @@ description = 'Battery temperature sensors'
 
 # Devices run on a raspberry as entangle devices
 
-tangobase = 'tango://%s:10000/box/sensor/' % setupname
+tangobase = f'tango://{setupname}:10000/box/sensor/'
 
 group = 'plugplay'
 
@@ -10,9 +10,9 @@ devices = dict(
 )
 
 for i in range(1, 9):
-    devices['T_%s_%d' % (setupname, i)] = device('nicos.devices.entangle.Sensor',
-        description = '%s temperature %d' % (setupname.capitalize(), i),
-        tangodevice = tangobase + '%d' % i,
+    devices[f'T_{setupname}_{i}'] = device('nicos.devices.entangle.Sensor',
+        description = f'{setupname.capitalize()} temperature {i}',
+        tangodevice = f'{tangobase}{i}',
         pollinterval = 30,
         maxage = 35,
     )
