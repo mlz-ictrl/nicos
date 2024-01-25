11c62d372f413415c7bdb51e3db9408aedb22937
Jens Krüger <jens.krueger@frm2.tum.de>
2024-01-25 08:50:56 +0100

    VirtualImage: Unify the readresult calculation
    Change-Id: Id05ef8a0423d68d6959d6ff5876a0f4d6826673d
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/32925
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos/devices/generic/virtual.py
+++ nicos/devices/generic/virtual.py
@@ -793,12 +793,12 @@ class VirtualImage(ImageChannelMixin, PassiveChannel):
         self.arraydesc = ArrayDesc(self.name, self.size[::-1], '<u4')
 
     def doPrepare(self):
-        self.readresult = [0]
         if self._mythread and self._mythread.is_alive():
             self._stopflag = True
             self._mythread.join()
         self._mythread = None
         self._buf = self._generate(0).astype('<u4')
+        self.readresult = [self._buf.sum()]
 
     def doStart(self):
         self._last_update = 0
