b6e0fcd07288ca7ae303d289ee689235d7ba8a2b
Jens Krüger <jens.krueger@frm2.tum.de>
2024-01-25 09:17:22 +0100

    SPODI/BIO: Add ROI to the detector
    This ROI is designed to visualise the current fitting region of the
    capillary adjustment algorithm
    
    Change-Id: I404340d85a0e310308e5d4faa876fddfa1777722
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/32946
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/labs/spodi/bio/setups/detector.py
+++ nicos_mlz/labs/spodi/bio/setups/detector.py
@@ -9,10 +9,18 @@ devices = dict(
     tim1 = device('nicos.devices.generic.VirtualTimer',
         visibility = {'metadata', 'namespace'},
     ),
+    roi = device('nicos.devices.generic.RectROIChannel',
+        description = 'ROI',
+        roi = (750, 500, 500, 1000),
+    ),
     det = device('nicos.devices.generic.Detector',
         description = 'Detector',
         images = ['keyence'],
         timers = ['tim1'],
+        counters = ['roi'],
+        postprocess = [
+            ('roi', 'keyence'),
+        ],
     ),
 )
 
