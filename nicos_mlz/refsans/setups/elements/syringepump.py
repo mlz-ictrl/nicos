bb21244f89405f171431e904f48c0cea7e3fbdbc
Jens Krüger <jens.krueger@frm2.tum.de>
2024-01-30 13:46:05 +0100

    Refsans: Switch syringepump setup to plug'n play
    Change-Id: I2e8b69bd4f132056029f38ffb5f317dcd9ee3d27
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/32978
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/refsans/setups/elements/syringepump.py
+++ nicos_mlz/refsans/setups/elements/syringepump.py
@@ -1,38 +1,34 @@
 description = 'Setup for New Era Syringe pumps'
 
-group = 'optional'
+group = 'plugplay'
 
-instrument_values = configdata('instrument.values')
-
-tango_base = instrument_values['tango_base']
-code_base = instrument_values['code_base'] + 'syringepump.'
+tango_base = 'tango://syring01.refsans.frm2.tum.de:10000/box/syringe/'
 
 devices = dict(
     pump0_diameter = device('nicos.devices.entangle.AnalogOutput',
         description = 'Syringe diameter for first pump',
-        tangodevice = tango_base + 'refsans/syringe/pump0diameter',
+        tangodevice = tango_base + 'pump0diameter',
     ),
     pump0_rate = device('nicos.devices.entangle.AnalogOutput',
         description = 'Infusion rate for first pump',
-        tangodevice = tango_base + 'refsans/syringe/pump0rate',
+        tangodevice = tango_base + 'pump0rate',
     ),
-    pump0_run = device(code_base + 'PumpAnalogOutput',
+    pump0_run = device('nicos_mlz.refsans.devices.syringepump.PumpAnalogOutput',
         description = 'Move to volume to infuse (positive) or withdraw (negative)',
-        tangodevice = tango_base + 'refsans/syringe/pump0infuse',
+        tangodevice = tango_base + 'pump0infuse',
         precision = 0.0001,
     ),
-
     pump1_diameter = device('nicos.devices.entangle.AnalogOutput',
         description = 'Syringe diameter for second pump',
-        tangodevice = tango_base + 'refsans/syringe/pump1diameter',
+        tangodevice = tango_base + 'pump1diameter',
     ),
     pump1_rate = device('nicos.devices.entangle.AnalogOutput',
         description = 'Infusion rate for second pump',
-        tangodevice = tango_base + 'refsans/syringe/pump1rate',
+        tangodevice = tango_base + 'pump1rate',
     ),
-    pump1_run = device(code_base + 'PumpAnalogOutput',
+    pump1_run = device('nicos_mlz.refsans.devices.syringepump.PumpAnalogOutput',
         description = 'Move to volume to infuse (positive) or withdraw (negative)',
-        tangodevice = tango_base + 'refsans/syringe/pump1infuse',
+        tangodevice = tango_base + 'pump1infuse',
         precision = 0.0001,
     ),
 )
