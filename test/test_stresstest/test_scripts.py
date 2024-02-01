a24a2432aea9cd3abc6fc863bbd5fdbd4497732c
Jens Krüger <jens.krueger@tum.de>
2024-02-01 21:14:54 +0100

    test_stresstest: shut up pylint complaint
    Calling __init__ is needed in this case.
    
    Change-Id: Ic44399e8bba9d7233ea472a9a159b486cfac179e
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/32996
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- test/test_stresstest/test_scripts.py
+++ test/test_stresstest/test_scripts.py
@@ -1,6 +1,6 @@
 # *****************************************************************************
 # NICOS, the Networked Instrument Control System of the MLZ
-# Copyright (c) 2009-2023 by the NICOS contributors (see AUTHORS)
+# Copyright (c) 2009-2024 by the NICOS contributors (see AUTHORS)
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -85,6 +85,7 @@ def session(request):
 
     from nicos import session
     session.__class__ = ScriptSessionTest
+    # pylint: disable=unnecessary-dunder-call
     session.__init__('TestScriptSession')
     yield session
     session.shutdown()
