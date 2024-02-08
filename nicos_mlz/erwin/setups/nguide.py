2c8369c4051abc011dbf8549a27d29b0f2d9dd75
Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
2024-02-08 11:16:26 +0100

    mlz/erwin: add setup for monitoring nguide pressure sensor(s)
    Change-Id: I6b4f1ebec42a76072732549f39bb5aaf05003c1e
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/33028
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/erwin/setups/nguide.py
@@ -0,0 +1,41 @@
+description = 'Erwin neutron guide sensors'
+group = 'optional'
+
+tango_base = 'tango://hw.erwin.frm2.tum.de:10000/erwin/center3/'
+
+devices = dict(
+    p1_nguide = device('nicos.devices.entangle.Sensor',
+        description = 'Pressure 1',
+        tangodevice = tango_base + 'sensor1',
+        fmtstr = '%.2f',
+        maxage = 65,
+        pollinterval = 30,
+    ),
+    p2_nguide = device('nicos.devices.entangle.Sensor',
+        description = 'Pressure 2',
+        tangodevice = tango_base + 'sensor2',
+        fmtstr = '%.2f',
+        maxage = 65,
+        pollinterval = 30,
+    ),
+    p3_nguide = device('nicos.devices.entangle.Sensor',
+        description = 'Pressure 3',
+        tangodevice = tango_base + 'sensor3',
+        fmtstr = '%.2f',
+        maxage = 65,
+        pollinterval = 30,
+    ),
+)
+
+display_order = 100
+
+monitor_blocks = [
+    Block('Neutron Guide', [
+        BlockRow(
+            Field(name='p1 N-Guide', dev='p1_nguide'),
+            Field(name='p2 N-Guide', dev='p2_nguide'),
+            Field(name='p3 N-Guide', dev='p3_nguide'),
+            ),
+        ],
+    ),
+]
