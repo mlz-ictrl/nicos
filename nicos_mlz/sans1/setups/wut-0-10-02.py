e2240a2cb5a9f6f862c757feaa0271ddb8490c67
Jens Krüger <jens.krueger@frm2.tum.de>
2024-02-13 08:56:10 +0100

    SANS1: Rewwork W&T setups
    - use 'setupname' variable
    - use f-strings
    
    Change-Id: Ic8d26213c36f59dfd908987ec11ff970b47a7d96
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/33042
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/sans1/setups/wut-0-10-02.py
+++ nicos_mlz/sans1/setups/wut-0-10-02.py
@@ -1,12 +1,10 @@
 description = 'W&T Box * 0-10V * Nr. 2'
 
-includes = []
-_wutbox = 'wut-0-10-02'
-_wutbox_dev = _wutbox.replace('-','_')
+_wutbox_dev = setupname.replace('-','_')
 
 devices = {
-    _wutbox_dev +'_1': device('nicos_mlz.sans1.devices.wut.WutReadValue',
-        hostname = _wutbox + '.sans1.frm2.tum.de',
+    _wutbox_dev + '_1': device('nicos_mlz.sans1.devices.wut.WutReadValue',
+        hostname = f'{setupname}.sans1.frm2.tum.de',
         port = 1,
         description = 'input 1 voltage',
         fmtstr = '%.3F',
@@ -15,8 +13,8 @@ devices = {
         maxage = 20,
         unit = 'V',
     ),
-    _wutbox_dev +'_2': device('nicos_mlz.sans1.devices.wut.WutReadValue',
-        hostname = _wutbox + '.sans1.frm2.tum.de',
+    _wutbox_dev + '_2': device('nicos_mlz.sans1.devices.wut.WutReadValue',
+        hostname = f'{setupname}.sans1.frm2.tum.de',
         port = 2,
         description = 'input 2 voltage',
         fmtstr = '%.3F',
