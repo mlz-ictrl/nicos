328187b6287653cf259d816914a1e36f121cebb0
Georg Brandl <g.brandl@fz-juelich.de>
2024-02-14 09:56:05 +0100

    VTOFTOF: remove monitor setup parts copy-pasted from SPODI
    Change-Id: I160dac4a40aa2f230c8ac70489d98e358b9e4a57
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/33060
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Georg Brandl <g.brandl@fz-juelich.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos_demo/vtoftof/setups/special/monitor-html.py
+++ nicos_demo/vtoftof/setups/special/monitor-html.py
@@ -4,27 +4,13 @@ group = 'special'
 _expcolumn = Column(
     Block('Experiment', [
         BlockRow(
-                 Field(name='Current status', key='exp/action', width=40,
-                       istext=True, maxlen=40),
-                 Field(name='Data file', key='exp/lastpoint'))]),
-)
-
-_sampletable = Column(
-    Block('Sample table', [
-        BlockRow(Field(dev='omgs')),
-        BlockRow(Field(dev='tths')),
-    ],
-    ),
-)
-
-_instrument = Column(
-    Block('Instrument', [
-        BlockRow(Field(dev='wav')),
-        BlockRow(Field(dev='slits')),
-        BlockRow(Field(dev='mon'),
-                 Field(name='Resosteps', key='adet/resosteps'),
-                 Field(name='Step', key='adet/value[0]'),),
-    ],
+            Field(name='Proposal', key='exp/proposal', width=7),
+            Field(name='Title',    key='exp/title',    width=20,
+                  istext=True, maxlen=20),
+            Field(name='Current status', key='exp/action', width=40,
+                  istext=True, maxlen=40),
+            Field(name='Last file', key='exp/lastscan'),),
+        ],
     ),
 )
 
@@ -35,71 +21,20 @@ _frm = Column(
     ),
 )
 
-_htf = Column(
-    Block('HTF', [
-        BlockRow(Field(dev='T'),
-                 Field(name='Power', key='T/heaterpower'),)
-    ],
-    setups='htf*',
-    ),
-)
-
-_magnet = Column(
-    Block('Magnet', [
-        BlockRow(Field(dev='B')),
-    ],
-    setups='ccm*',
-    ),
-)
-
-_sc = Column(
-    Block('Sample Changer', [
-        BlockRow(Field(dev='sams'),),
-    ],
-    setups='samplechanger',
-    ),
-)
-
-_e = Column(
-    Block('E field', [
-        BlockRow(Field(dev='E')),
-    ],
-    setups='efield',
-    ),
-)
-
-_tension = Column(
-    Block('Tension rack', [
-        BlockRow(Field(dev='teload'),
-                 Field(dev='tepos'),
-                 Field(dev='teext'),
-                 Field(dev='topos'),
-                 Field(dev='tomom'),),
-    ],
-    setups='tensile',
-    ),
-)
-
-
 devices = dict(
     Monitor = device('nicos.services.monitor.html.Monitor',
-        title = 'SPODI status monitor',
+        title = 'NICOS status monitor',
         loglevel = 'info',
         interval = 10,
         filename = 'webroot/index.html',
         cache = 'localhost',
         font = 'Luxi Sans',
         valuefont = 'Consolas',
-        prefix = 'nicos/',
         padding = 0,
         fontsize = 24,
         layout = [
             Row(_expcolumn),
-            Row(_frm, _instrument, _sampletable),
-            Row(_htf,),
-            Row(_tension),
-            Row(_magnet, _e,),
-            Row(_sc),
+            Row(_frm),
         ],
     ),
 )
