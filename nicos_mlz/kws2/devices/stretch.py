ffd51f5d305a7aff74ed8aa25fa2482647cdf9c0
Alexander Steffens <a.steffens@fz-juelich.de>
2024-02-16 11:15:28 +0100

    kws2: add stretch4 testing setup
    Change-Id: If372203de0964d5b7280d599281c06d19c518ca4
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/33077
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

---
+++ nicos_mlz/kws2/devices/stretch.py
@@ -0,0 +1,44 @@
+# *****************************************************************************
+# NICOS, the Networked Instrument Control System of the MLZ
+# Copyright (c) 2009-2024 by the NICOS contributors (see AUTHORS)
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Module authors:
+#   Georg Brandl <g.brandl@fz-juelich.de>
+#
+# *****************************************************************************
+
+"""Specialized slit device for the 4-fold stretch apparatus."""
+
+from nicos.core import Param
+from nicos.devices.generic.slit import VerticalGap
+
+
+class StretchGap(VerticalGap):
+    """A vertical gap where the speed can be set for both blades."""
+
+    parameters = {
+        'speed': Param('Combined speed of the gap opening or closing',
+                       type=float, settable=True, volatile=True,
+                       unit='main/s'),
+    }
+
+    def doReadSpeed(self):
+        return self._attached_bottom.speed + self._attached_top.speed
+
+    def doWriteSpeed(self, value):
+        self._attached_bottom.speed = value / 2
+        self._attached_top.speed = value / 2
