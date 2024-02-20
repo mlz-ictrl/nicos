b936d28f4713c804156aa6812b06836d57a3036f
Jens Krüger <jens.krueger@frm2.tum.de>
2024-02-20 18:00:33 +0100

    RESI: Fix monitor setup
    Remove all devices, which are not configured in the setup(s)
    
    Change-Id: I4da34f93951d71fdd3065b3602a69569f90d6c84
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/33102
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/resi/setups/special/monitor.py
+++ nicos_mlz/resi/setups/special/monitor.py
@@ -9,34 +9,42 @@ _reactorBlock = Block('Reactor', [
 )
 
 _shutterBlock = Block('Shutter', [
-    BlockRow(Field(name='Prim. shutter', dev='prim_shutt'),
-             Field(name='Sec. sutter', dev='sec_shutt'),
-             )
+    BlockRow(
+        # Field(name='Prim. shutter', dev='prim_shutt'),
+        # Field(name='Sec. sutter', dev='sec_shutt'),
+        ),
     ],
 )
 _hoverBlock = Block('Hover feet', [
-    BlockRow(Field(name='Gonio/opt.Bench', dev='hover_mono'),
-             Field(name='Detector', dev='hover_theta'),
-             )
+    BlockRow(
+        # Field(name='Gonio/opt.Bench', dev='hover_mono'),
+        # Field(name='Detector', dev='hover_theta'),
+        ),
     ],
 )
 _counterBlock = Block('Counter', [
-    BlockRow(Field(name='Counter', dev='det1'),
-             Field(name='Monitor', dev='mon1'),
-             Field(name='Time', dev='timer'),
-             ),
-    BlockRow(Field(name='Counter rate', dev='det1rate'),
-             Field(name='Monitor rate', dev='mon1rate'),
-             )
+    BlockRow(
+        # Field(name='Counter', dev='det1'),
+        # Field(name='Monitor', dev='mon1'),
+        # Field(name='Time', dev='timer'),
+        ),
+    BlockRow(
+        # Field(name='Counter rate', dev='det1rate'),
+        # Field(name='Monitor rate', dev='mon1rate'),
+        )
     ],
 )
 
 
 _ls340Block = Block('LakeShore', [
-    BlockRow(Field(name='Temperature', dev='T_ls340'),
-             Field(name='Target', key='T_ls340/setpoint', unit='K')),
-    BlockRow(Field(name='Sensor A', dev='T_ls340_A'),
-             Field(name='Sensor B', dev='T_ls340_B')),
+    BlockRow(
+        # Field(name='Temperature', dev='T_ls340'),
+        # Field(name='Target', key='T_ls340/setpoint', unit='K'),
+        ),
+    BlockRow(
+        # Field(name='Sensor A', dev='T_ls340_A'),
+        # Field(name='Sensor B', dev='T_ls340_B'),
+        ),
     ],
     setups='',
 )
