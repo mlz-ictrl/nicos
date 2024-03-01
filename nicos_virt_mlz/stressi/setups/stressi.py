bd3b9e7b811d425556c5ad942dceb7125e77818c
Jens Krüger <jens.krueger@frm2.tum.de>
2024-03-01 14:17:47 +0100

    Virt/Stressi: Add different samples
    For the simulation it's needed to define a sample which gives the McStas
    simulation different choices. This is a convenient way to do this. The
    user has to use the 'SelectSample' command.
    
    Change-Id: Ibf809bc5c59a95a298a426cdb38b78f7726c1fa6
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/33179
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_virt_mlz/stressi/setups/stressi.py
+++ nicos_virt_mlz/stressi/setups/stressi.py
@@ -29,4 +29,10 @@ devices = dict(
 
 startupcode = '''
 SetDetectors(adet)
+ClearSamples()
+SetSample(1, 'Absorbtion experiment FoPra', sampletype=1)
+SetSample(2, 'Strain experiment FoPra E-Mod 211', sampletype=2)
+SetSample(3, 'Strain experiment FoPra E-Mod 200', sampletype=3)
+SetSample(4, 'fully flexible sample', sampletype=4)
+SelectSample(1)
 '''
