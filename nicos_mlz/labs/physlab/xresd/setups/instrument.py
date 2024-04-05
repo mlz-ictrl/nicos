fb9d512ea14eee3b3a9ce5cd1b0ef05e46e4e57e
Jens Krüger <jens.krueger@frm2.tum.de>
2024-04-05 16:06:48 +0200

    Physlab: Cleanup the basic config data
    Change-Id: I2a71fcc929938fda27359debe12f80822e4730cf
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/33438
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/labs/physlab/xresd/setups/instrument.py
+++ nicos_mlz/labs/physlab/xresd/setups/instrument.py
@@ -2,21 +2,7 @@ description = 'origin'
 
 group = 'configdata'
 
-instrument_name = 'xresd'
-hw_uri = 'rsxrd.physlab.frm2.tum.de'
-tango_port = 10000
-facility = 'mlz'
-software_system = 'nicos'
-
 values = {
-    'instrument_name': instrument_name,
-    'tango_base': 'tango://%s:%d/' % (
-        hw_uri,
-        tango_port,
-    ),
-    'code_base': '%s_%s.%s.devices.' % (
-        software_system,
-        facility,
-        instrument_name,
-    ),
+    'instrument_name': 'xresd',
+    'tango_base': 'tango://rsxrd.physlab.frm2.tum.de:10000/',
 }
--- nicos_mlz/labs/physlab/xrr/setups/instrument.py
+++ nicos_mlz/labs/physlab/xrr/setups/instrument.py
@@ -2,21 +2,7 @@ description = 'origin'
 
 group = 'configdata'
 
-instrument_name = 'xrr'
-hw_uri = 'rsxrr.physlab.frm2.tum.de'
-tango_port = 10000
-facility = 'mlz'
-software_system = 'nicos'
-
 values = {
-    'instrument_name': instrument_name,
-    'tango_base': 'tango://%s:%d/' % (
-        hw_uri,
-        tango_port,
-    ),
-    'code_base': '%s_%s.%s.devices.' % (
-        software_system,
-        facility,
-        instrument_name,
-    ),
+    'instrument_name': 'xrr',
+    'tango_base': 'tango://rsxrr.physlab.frm2.tum.de:10000/'
 }
