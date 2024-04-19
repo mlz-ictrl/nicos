66f066806be64bc1253d4a845c7cd7978a81fe4f
Jens Krüger <jens.krueger@frm2.tum.de>
2024-04-19 16:56:47 +0200

    XRR: Add output to the electronic logbook (WorkBench)
    Change-Id: I72f1e608bb0d5fa00f2a4e5dc0067e0f688320cb
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/33513
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/labs/physlab/xrr/setups/special/elog.py
+++ nicos_mlz/labs/physlab/xrr/setups/special/elog.py
@@ -4,8 +4,16 @@ group = 'special'
 devices = dict(
     LogbookHtml = device('nicos.services.elog.handler.html.Handler'),
     LogbookText = device('nicos.services.elog.handler.text.Handler'),
+    LogbookWorkbench = device('nicos.services.elog.handler.eworkbench.workbench_rabbit_writer.Handler',
+        rabbit_url = 'localhost',
+        rabbit_port = 5672,
+        rabbit_virtual_host = '/',
+        rabbit_username = 'guest',
+        rabbit_password = 'guest',
+        rabbit_static_queue = 'xrr-workbench',
+    ),
     Logbook = device('nicos.services.elog.Logbook',
-        handlers = ['LogbookHtml', 'LogbookText'],
+        handlers = ['LogbookHtml', 'LogbookText', 'LogbookWorkbench'],
         cache = 'localhost',
     ),
 )
