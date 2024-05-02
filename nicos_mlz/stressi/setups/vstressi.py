849b48fb14e7383cbf1015214175f93f119d6104
Jens Krüger <jens.krueger@frm2.tum.de>
2024-05-02 17:35:24 +0200

    Stressi: Fix typo in sample description (XR)
    In the extended reality (XR) of the STRESS-SPEC instrument the number of
    possible samples is limited.
    
    Change-Id: Ic95cc5dd3653bb0a837a7517ec6467d2be4c5700
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/33619
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/stressi/setups/vstressi.py
+++ nicos_mlz/stressi/setups/vstressi.py
@@ -16,7 +16,7 @@ devices = dict(
     Sample = device('nicos_virt_mlz.stressi.devices.sample.Sample',
         description = 'Simulation sample',
         samples = {
-            1: {'name': 'Absorbtion experiment FoPra', 'sampletype': 1},
+            1: {'name': 'Absorption experiment FoPra', 'sampletype': 1},
             2: {'name': 'Strain experiment FoPra E-Mod 211', 'sampletype': 2},
             3: {'name': 'Strain experiment FoPra E-Mod 200', 'sampletype': 3},
             4: {'name': 'Fully flexible sample', 'sampletype': 4},
