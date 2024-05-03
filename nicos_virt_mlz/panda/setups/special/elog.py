b5887ddd87a5c0015c699c6563b8e2ccaaa97793
Christian Felder <c.felder@fz-juelich.de>
2024-05-03 14:01:08 +0200

    virt/panda: add workbench
    Change-Id: I66c6641f5b062e0fa0cd6a92fa9f1c0d8591c53f
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/31532
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

--- nicos_virt_mlz/panda/setups/special/elog.py
+++ nicos_virt_mlz/panda/setups/special/elog.py
@@ -4,8 +4,17 @@ group = 'special'
 devices = dict(
     LogbookHtml = device('nicos.services.elog.handler.html.Handler'),
     LogbookText = device('nicos.services.elog.handler.text.Handler'),
+    LogbookWorkbench = device(
+        'nicos.services.elog.handler.eworkbench.workbench_rabbit_writer.Handler',
+        url='infra01.dev.cluster.jcns.frm2.tum.de',
+        port=32672,
+        virtual_host='/',
+        username='workbench',
+        password=secret('workbench'),
+        static_queue='vpanda-workbench',
+    ),
     Logbook = device('nicos.services.elog.Logbook',
-        handlers = ['LogbookHtml', 'LogbookText'],
+        handlers = ['LogbookHtml', 'LogbookText', 'LogbookWorkbench'],
         cache = 'localhost',
     ),
 )
