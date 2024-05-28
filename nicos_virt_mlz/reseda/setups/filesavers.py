eb5e5120e7eaa4d2f7a5b8b81286faf181dd03d0
Jens Krüger <jens.krueger@tum.de>
2024-05-28 22:36:01 +0200

    Reseda: Fix comment in filesavers
    The description leads to a wrong configuration if following the advice.
    
    Change-Id: If132cabace62095cc26a8d95d8bbdc1a1063ae40
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/33797
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_virt_mlz/reseda/setups/filesavers.py
+++ nicos_virt_mlz/reseda/setups/filesavers.py
@@ -8,6 +8,6 @@ sysconfig = dict(
 devices = dict(
     HDF5FileSaver = device('nicos_mlz.reseda.datasinks.HDF5Sink',
         description = 'Saves scalar and image data for scans in a defined structure into HDF5 files.',
-        detectors = ['X'],  # effectively disabled, to re-enable: set to ['']
+        detectors = ['X'],  # effectively disabled, to re-enable: set to []
     ),
 )
