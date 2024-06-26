f4b1f0e4a3378a404e1ad5a0f16c818c54cf6845
Jens Krüger <jens.krueger@frm2.tum.de>
2024-06-26 17:29:38 +0200

    Stressi: Add motor devices used in hybrid mode
    Hybrid mode: Real hardware, instead of real detector use McStas
    simulated detector.
    
    Change-Id: I488cfdb98bc0d255ac703bfef5b63a824e00deae
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/34042
    Reviewed-by: Jens Krueger <jens.krueger@tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

--- nicos_mlz/stressi/setups/vstressi.py
+++ nicos_mlz/stressi/setups/vstressi.py
@@ -11,6 +11,8 @@ sysconfig = dict(
     datasinks = ['caresssink'],
 )
 
+tango_base = 'tango://motorbox06.stressi.frm2.tum.de:10000/box/'
+
 devices = dict(
     Sample = device('nicos_virt_mlz.stressi.devices.sample.Sample',
         description = 'Simulation sample',
@@ -20,6 +22,39 @@ devices = dict(
             3: {'name': 'Strain experiment FoPra E-Mod 200', 'sampletype': 3},
             4: {'name': 'Fully flexible sample', 'sampletype': 4},
         },
+    ),
+    chis_m = device('nicos.devices.entangle.Motor',
+        tangodevice = tango_base + 'channel1/motor',
+        fmtstr = '%.2f',
+        visibility = (),
+    ),
+    chis_c = device('nicos.devices.entangle.Sensor',
+        tangodevice = tango_base + 'channel1/coder',
+        fmtstr = '%.2f',
+        visibility = (),
+    ),
+    chis = device('nicos.devices.generic.Axis',
+        description = 'Eulerian Huber CHIS',
+        motor = 'chis_m',
+        coder = 'chis_c',
+        precision = 0.01,
+    ),
+    phis_m = device('nicos.devices.entangle.Motor',
+        tangodevice = tango_base + 'channel2/motor',
+        fmtstr = '%.2f',
+        userlimits = (-700, 700),
+        visibility = (),
+    ),
+    phis_c = device('nicos.devices.entangle.Sensor',
+        tangodevice = tango_base + 'channel2/coder',
+        fmtstr = '%.2f',
+        visibility = (),
+    ),
+    phis = device('nicos.devices.generic.Axis',
+        description = 'Eulerian Huber PHIS',
+        motor = 'phis_m',
+        coder = 'phis_c',
+        precision = 0.01,
     ),
 )
 
