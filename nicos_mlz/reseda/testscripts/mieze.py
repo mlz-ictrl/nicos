8836e60c3d1b1d4d459c6cb1942e40f015fede61
Jens Krüger <jens.krueger@frm2.tum.de>
2024-06-28 17:17:47 +0200

    Reseda: Add mieze setup specific test script
    Change-Id: Iaadc5fb9230bbbbba84eb229799062a598496f63
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/34052
    Reviewed-by: Jens Krueger <jens.krueger@tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

---
+++ nicos_mlz/reseda/testscripts/mieze.py
@@ -0,0 +1,16 @@
+# pylint: skip-file
+
+# test: subdirs = frm2
+# test: setups = mieze
+
+move(selector_lambda, 6.0)
+move(cbox_0a_fg_freq, 4500, cbox_0b_fg_freq, 6200)
+move(hrf_0a, 0.114293, hrf_0b, 0.157471, nse0, 0)
+move(stx, 0, sty, 0, stz, 0, sgx, 0, sgy, 0, srz, 0)
+move(arm2_rot, 0.0)
+move(gf0, 0, gf1, 0, gf2, 0,
+     # gf3, 0,
+     gf4, 0, gf5, 0, gf6, 0, gf7, 0, gf8, 0, gf9, 0, gf10, 0)
+wait()
+
+read(echotime)
