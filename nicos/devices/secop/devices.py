46dc100f68e553de1100cc636a7e65224a87d7b0
Markus Zolliker <markus.zolliker@psi.ch>
2024-08-30 14:07:21 +0200

    SECoP: fix status on SecopReadable
    SecopReadable.status must inherit Readable.status instead of
    SecopDevice.status
    
    Change-Id: I6e9fd4a884e8e9784a40508dfbf59590e70512d4
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/34532
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Markus Zolliker <markus.zolliker@psi.ch>

--- nicos/devices/secop/devices.py
+++ nicos/devices/secop/devices.py
@@ -1166,8 +1166,8 @@ class SecopDevice(Device):
     def status(self, maxage=0):
         """simple replacement for Readable.status
 
-        we have to implement it here, as it is called below even when
-        not inheriting from Readable
+        not inherited from SecopReadable. we have to implement it here,
+        as it is called in SecopDevice._cache_update
         """
         if self._sim_intercept:
             return status.OK, 'simulated ok'
@@ -1224,6 +1224,10 @@ class SecopReadable(SecopDevice, Readable):
         # calculated to be slightly higher than maxage on the attached SecNodeDevice
         'maxage': Override(default=3600, userparam=False, settable=False, volatile=True),
     }
+
+    def status(self):
+        # do not use SecopDevice.status here
+        return Readable.status(self)
 
     def doRead(self, maxage=0):
         try:
