d0485a397658ac674fae585eb8840328377ce86a
Georg Brandl <g.brandl@fz-juelich.de>
2024-09-09 10:00:46 +0200

    ciscripts: disable Numpy/Scipy multithreading in test suite
    Change-Id: Ibfb556039ce9dc4c7ce0da093bf533528d18cdab
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/34584
    Reviewed-by: Bjoern Pedersen <bjoern.pedersen@frm2.tum.de>
    Tested-by: Georg Brandl <g.brandl@fz-juelich.de>
    (cherry picked from commit 5e96e11cd24be0a1fa1d413db820a150e10cf839)
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/34590
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- ciscripts/run_pytest.sh
+++ ciscripts/run_pytest.sh
@@ -5,6 +5,10 @@ set +x
 . $venv/bin/activate
 set -x
 
+# Do not try to run simple fits in scipy using parallelism
+# (makes a difference on Rocky Linux, see issue #4871).
+export OMP_NUM_THREADS=1
+
 mkdir -p ~/test/testroot
 export NICOS_TEST_ROOT=~/test/testroot
 export TZ=Europe/Berlin
