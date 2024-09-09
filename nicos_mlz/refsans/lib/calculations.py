refsans: fix calculation function
The function "angles" did not use its arguments.

The code worked by coincidence because it was always called
with t_SC2c and t_SC2o as arguments anyway.

Change-Id: Ifc401415ab5edfa17e30d12a32a33f79c6a1bf0d
Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/34595
Reviewed-by: Jens Krueger <jens.krueger@tum.de>
Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

nicos_mlz/refsans/lib/calculations.py
@@ -810,7 +810,7 @@ def practical_SC2(wl_min=0,
         closing-times (in seconds) of the SC2 discs
         """
 
-        ang_SC2 = np.array([t_SC2c, t_SC2o]) * freq * 360
+        ang_SC2 = np.array([t_SCc, t_SCo]) * freq * 360
         SC2_opening = ang_SC2[0] - ang_SC2[1]
         return ang_SC2, SC2_opening
         # was return s.mod(ang_SC2, 360), SC2_opening butr this can lead to