e611346d6c624e74fb7b354ec176f84214110946
Jens Krüger <jens.krueger@tum.de>
2024-10-24 21:08:04 +0200

    BOA: Add test scripts
    Change-Id: Ic53dff6ae2788031218292693614360c8fdc38bd
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/34877
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@tum.de>

---
+++ nicos_sinq/boa/testscripts/tomo.py
@@ -0,0 +1,11 @@
+# pylint: skip-file
+
+# test: needs = kafka
+# test: needs = epics
+# test: needs = streaming_data_types>=0.16.0
+# test: needs = confluent_kafka
+# test: subdirs = boa
+# test: setups = andor
+
+status()
+read()
