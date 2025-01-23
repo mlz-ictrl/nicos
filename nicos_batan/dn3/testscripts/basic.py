90330e12c8e205e48944432d07d53d10fa18f64a
Jens Krüger <jens.krueger@tum.de>
2025-01-23 20:05:38 +0100

    BATAN: Add tests for dry-run and devices
    Change-Id: I7b63f4cddd3b92220654abc056a7e303b73ba784
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/35487
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

---
+++ nicos_batan/dn3/testscripts/basic.py
@@ -0,0 +1,8 @@
+# pylint: skip-file
+
+# test: needs = dataparser
+# test: subdirs = dn3
+# test: setups = dn3
+
+read()
+status()
---
+++ nicos_batan/sn1/testscripts/basic.py
@@ -0,0 +1,7 @@
+# pylint: skip-file
+
+# test: subdirs = sn1
+# test: setups = sn1
+
+read()
+status()
