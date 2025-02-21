c905e3dc3344846558fee3e8aada7b91f8fa7db1
Jens Krüger <jens.krueger@tum.de>
2025-02-21 16:54:58 +0100

    VKOMPASS: Add missed files
    Make the instrument completely running via the nicos-demo script
    
    Change-Id: I5a8ebc344db8bd244eda0e0fa26eb1c454f1e46a
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/35665
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_demo/vkompass/guiconfig.py
@@ -0,0 +1,59 @@
+"""NICOS GUI default configuration."""
+
+main_window = docked(
+    vsplit(
+        panel('nicos.clients.gui.panels.cmdbuilder.CommandPanel',
+              modules=['nicos.clients.gui.cmdlets.qscan'],
+              ),
+        panel('nicos.clients.gui.panels.status.ScriptStatusPanel'),
+        # panel('nicos.clients.gui.panels.watch.WatchPanel'),
+        panel('nicos.clients.gui.panels.console.ConsolePanel',
+              hasinput=False),
+    ),
+    ('NICOS devices',
+     panel('nicos.clients.gui.panels.devices.DevicesPanel', icons=True,
+           dockpos='right',)
+    ),
+    ('Experiment Information and Setup',
+     panel('nicos.clients.gui.panels.expinfo.ExpInfoPanel',
+           sample_panel=panel('nicos.clients.gui.panels.setup_panel.TasSamplePanel'),
+          )
+    ),
+)
+
+windows = [
+    window('Editor', 'editor',
+           panel('nicos.clients.gui.panels.editor.EditorPanel')),
+    window('Scans', 'plotter',
+           panel('nicos.clients.gui.panels.scans.ScansPanel')),
+    window('History', 'find',
+           panel('nicos.clients.gui.panels.history.HistoryPanel')),
+    window('Logbook', 'table',
+           panel('nicos.clients.gui.panels.elog.ELogPanel')),
+    window('Log files', 'table',
+           panel('nicos.clients.gui.panels.logviewer.LogViewerPanel')),
+    window('Errors', 'errors',
+           panel('nicos.clients.gui.panels.errors.ErrorPanel')),
+    window('Live data', 'live',
+           panel('nicos.clients.gui.panels.live.LiveDataPanel',
+                 instrument='poli')),
+]
+
+tools = [
+    tool('Downtime report', 'nicos.clients.gui.tools.downtime.DownTimeTool',
+         sender='kompass@frm2.tum.de',
+        ),
+    tool('Calculator', 'nicos.clients.gui.tools.calculator.CalculatorTool'),
+    tool('Neutron cross-sections',
+         'nicos.clients.gui.tools.website.WebsiteTool',
+         url='http://www.ncnr.nist.gov/resources/n-lengths/'),
+    tool('Neutron activation', 'nicos.clients.gui.tools.website.WebsiteTool',
+         url='https://webapps.frm2.tum.de/intranet/activation/'),
+    tool('Neutron calculations', 'nicos.clients.gui.tools.website.WebsiteTool',
+         url='https://webapps.frm2.tum.de/intranet/neutroncalc/'),
+    tool('Report NICOS bug or request enhancement',
+         'nicos.clients.gui.tools.bugreport.BugreportTool'),
+    tool('Emergency stop button',
+         'nicos.clients.gui.tools.estop.EmergencyStopTool',
+         runatstartup=False),
+]
