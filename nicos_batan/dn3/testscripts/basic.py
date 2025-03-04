230ea2f95725f0943d96c04c6232931b838f19ae
Jens Krueger <jens.krueger@frm2.tum.de>
2025-03-04 22:04:53 +0100

    Revert "BATAN: Add tests for dry-run and devices"
    This reverts commit 90330e12c8e205e48944432d07d53d10fa18f64a.
    
    Reason for revert: The tests setups at wrong place
    
    Change-Id: I72a59d30012a3e92e0c40f656a3bb837728c457a
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/35770
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_batan/dn3/testscripts/basic.py
+++
@@ -1,8 +0,0 @@
-# pylint: skip-file
-
-# test: needs = dataparser
-# test: subdirs = dn3
-# test: setups = dn3
-
-read()
-status()
--- nicos_batan/sn1/testscripts/basic.py
+++
@@ -1,7 +0,0 @@
-# pylint: skip-file
-
-# test: subdirs = sn1
-# test: setups = sn1
-
-read()
-status()
