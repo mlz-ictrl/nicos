1fe20dc04b44ab6cda177d5b86a3fc10f31d34f1
localadmin <localadmin@erwinctrl.erwin.frm2.tum.de>
2025-04-11 16:18:54 +0200

    Erwin: Change user running the daemon
    Change-Id: Ib909b31f8a0d3df9e48c414f32d45374aabb9508
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/36050
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/erwin/setups/system.py
+++ nicos_mlz/erwin/setups/system.py
@@ -42,7 +42,7 @@ devices = dict(
             enableFileMode = 0o644,
             disableDirMode = 0o550,
             disableFileMode = 0o440,
-            owner = 'erwin',
+            owner = 'nicd',
             group = 'erwin'
         ),
     ),
