43b42c527b7f66bd27c59ec452b9430d9219cdf3
smathis <stefan.mathis@psi.ch>
2025-06-10 08:30:53 +0200

    SINQ: Added isConnected property to SinqMotor
    Our new controller generation (which we also control via the SinqMotor
    interface) allows for motors being disconnected during operation (e.g.
    while NICOS & EPICS are running). The controller then also reports that
    the axis is disconnected and we forward this information via the
    "Connected" PV. Therefore, we also need to reflect this in NICOS, which
    is done in this patch.
    
    Additionally, the extension PV handling was reworked and is now based on
    a dictionary instead of a long elif chain. Lastly, optional PVs
    (errormsgpv, errorbitpv, reseterrorpv) now have dynamically generated
    default values.
    
    Change-Id: Ie9f38b8ee30d62034faa7b7d0dc7fc6d77908be2
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/36249
    Reviewed-by: Stefan Mathis <stefan.mathis@psi.ch>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>

--- nicos_sinq/devices/epics/motor.py
+++ nicos_sinq/devices/epics/motor.py
@@ -23,7 +23,6 @@
 # *****************************************************************************
 
 from nicos.core import Param, status
-from nicos.core.constants import MASTER
 from nicos.core.errors import UsageError
 from nicos.core.params import Override, none_or, oneof, pvname
 from nicos.devices.epics.pyepics.motor import EpicsMotor as CoreEpicsMotor
@@ -39,69 +38,41 @@ class SinqMotor(DynamicUserlimits, CoreEpicsMotor):
         'encoder_type': Param('Encoder type', default=None, settable=False,
                               type=none_or(oneof('absolute', 'incremental')),
                               userparam=True, volatile=True),
+        'connected': Param('Whether the motor is connected or not.', type=bool,
+                           mandatory=False, settable=False, userparam=True,
+                           volatile=True),
     }
 
     parameter_overrides = {
-        'errormsgpv': Override(settable=True),
-        'errorbitpv': Override(settable=True),
-        'reseterrorpv': Override(settable=True),
         'precision': Override(volatile=True),
         # Necessary since DynamicUserlimits overrides the override in
         # CoreEpicsMotor
         'abslimits': Override(volatile=True, mandatory=False),
     }
 
-    def doInit(self, mode):
-        if mode == MASTER:
-            # The PVs for the error message, the error bit and the error reset
-            # are standardised for SINQ motors. Hence, if no custom PV names
-            # have been set, the standard names are used.
-            if not self.errormsgpv:
-                self.errormsgpv = pvname(self.motorpv + '-MsgTxt')
-            if not self.errorbitpv:
-                self.errorbitpv = pvname(self.motorpv + ':StatusProblem')
-            if not self.reseterrorpv:
-                self.reseterrorpv = pvname(self.motorpv + ':Reset')
-
-            DynamicUserlimits.doInit(self, mode)
-
-        return CoreEpicsMotor.doInit(self, mode)
+    # Additional SINQ-specific record names which extend the basic motor record
+    # defined in nicos.devices.epics.pyepics.motor.EpicsMotor
+    _extension_records = {
+        'enable': ':Enable',
+        'enable_rbv': ':EnableRBV',
+        'can_disable': ':CanDisable',
+        'connected_rbv': ':Connected',
+        'encoder_type': ':EncoderType',
+        'errorbitpv': ':StatusProblem',
+        'reseterrorpv': ':Reset',
+        'errormsgpv': '-MsgTxt',
+    }
 
     def _get_pv_parameters(self):
         pvs = CoreEpicsMotor._get_pv_parameters(self)
-        pvs.add('can_disable')
-        pvs.add('enable')
-        pvs.add('enable_rbv')
-        pvs.add('encoder_type')
-        pvs.add('resolution')
-
-        # Since the doInit function provides default PV names for these
-        # parameters, they are added by default (instead of optionally as in
-        # the parent EpicsMotor class)
-        pvs.add('errormsgpv')
-        pvs.add('errorbitpv')
-        pvs.add('reseterrorpv')
+        for param in self._extension_records.keys():
+            pvs.add(param)
         return pvs
 
     def _get_pv_name(self, pvparam):
-        if pvparam == 'enable':
-            return self.motorpv + ':Enable'
-        elif pvparam == 'enable_rbv':
-            return self.motorpv + ':EnableRBV'
-        elif pvparam == 'can_disable':
-            return self.motorpv + ':CanDisable'
-        elif pvparam == 'encoder_type':
-            return self.motorpv + ':EncoderType'
-        elif pvparam == 'resolution':
-            return self.motorpv + '.MRES'
-        elif pvparam == 'errormsgpv' and not self.errormsgpv:
-            return self.motorpv + '-MsgTxt'
-        elif pvparam == 'errorbitpv' and not self.errorbitpv:
-            return self.motorpv + ':StatusProblem'
-        elif pvparam == 'reseterrorpv' and not self.reseterrorpv:
-            return self.motorpv + ':Reset'
-        else:
-            return CoreEpicsMotor._get_pv_name(self, pvparam)
+        if pvext := self._extension_records.get(pvparam):
+            return self.motorpv + pvext
+        return CoreEpicsMotor._get_pv_name(self, pvparam)
 
     def _register_pv_callbacks(self):
         CoreEpicsMotor._register_pv_callbacks(self)
@@ -114,14 +85,19 @@ class SinqMotor(DynamicUserlimits, CoreEpicsMotor):
 
         self._pvs['errormsgpv'].add_callback(update_status)
         self._pvs['enable_rbv'].add_callback(update_status)
+        self._pvs['connected_rbv'].add_callback(update_status)
         self._pvs['readpv'].add_callback(update_position)
 
     def doStatus(self, maxage=0):
         (stat, msg) = CoreEpicsMotor.doStatus(self, maxage)
 
+        # Ignore errors if the motor is disconnected
+        if not self.connected:
+            return status.DISABLED, 'Motor is disconnected'
+
         # If the motor reports an error, report the error rather than the fact
         # that the motor is disabled.
-        if not self.isEnabled:
+        if not self.enabled:
             if stat == status.ERROR:
                 return (stat, 'Motor is disabled - ' + msg)
             return status.DISABLED, 'Motor is disabled'
@@ -140,7 +116,28 @@ class SinqMotor(DynamicUserlimits, CoreEpicsMotor):
         # cache, which is not what we want!
         return self._get_pv('can_disable', use_monitor=False)
 
+    # Provide SINQ-specific default PV names in case no explicit PV name has
+    # been given in the setup file
+    def doReadErrormsgpv(self):
+        if pv := self._config.get('errormsgpv'):
+            return pv
+        return pvname(self.motorpv + '-MsgTxt')
+
+    def doReadErrorbitpv(self):
+        if pv := self._config.get('errorbitpv'):
+            return pv
+        return pvname(self.motorpv + ':StatusProblem')
+
+    def doReadReseterrorpv(self):
+        if pv := self._config.get('reseterrorpv'):
+            return pv
+        return pvname(self.motorpv + ':Reset')
+
     def doEnable(self, on):
+        if not self.connected:
+            raise UsageError('Motor cannot be enabled / disabled because it is '
+                             'disconnected from the controller!')
+
         if self.can_disable:
             done_moving = self._get_pv('donemoving')
             moving = self._get_pv('moving')
@@ -159,17 +156,23 @@ class SinqMotor(DynamicUserlimits, CoreEpicsMotor):
                 self._cache.put(self, 'status', (status.BUSY,
                                 f'{"En" if on else "Dis"}abling'))
             else:
-                self.log.warning('This motor cannot be disabled')
+                raise UsageError('Motor cannot be disabled!')
+
+    def doReadConnected(self, maxage=0):
+        """Shows if the motor is connected or not"""
+        if not self._sim_intercept:
+            return self._get_pv('connected_rbv') != 0
+        return True
 
     @property
-    def isEnabled(self):
+    def enabled(self):
         """Shows if the motor is enabled or not"""
         if not self._sim_intercept:
             return self._get_pv('enable_rbv') != 0
         return True
 
     def doIsAllowed(self, target):
-        if not self.isEnabled:
+        if not self.enabled:
             return False, 'Motor disabled'
         return True, ''
 
