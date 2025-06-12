073f1e1f17561ce39dc45a50bcad0d761c3fc433
Jens Krüger <jens.krueger@tum.de>
2025-06-12 17:56:30 +0200

    FirePOD: Add radial collimator device
    Change-Id: I2fea162e11c6fd83f39f7f8c8bc6c7bc3f82fb19
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/36559
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/firepod/setups/rc.py
+++ nicos_mlz/firepod/setups/rc.py
@@ -3,14 +3,13 @@ description = 'Radial collimator devices'
 group = 'lowlevel'
 
 devices = dict(
-    # rc = device('nicos_mlz.erwin.devices.rc.RadialCollimator',
-    rc = device('nicos.devices.generic.ManualSwitch',
+    rc = device('nicos_mlz.erwin.devices.rc.RadialCollimator',
         description = 'Radial collimator',
-        states = ['off', 'on'],
-    #     motor = 'rc_motor',
+        motor = 'rc_motor',
     ),
-    # rc_motor = device('nicos.devices.entangle.Motor',
-    #     description = 'Radial collimator motor',
-    #     tangodevice = 'tango://motorbox02.firepod.frm2.tum.de:10000/box/channel7/motor',
-    # ),
+    rc_motor = device('nicos.devices.entangle.Motor',
+        description = 'Radial collimator motor',
+        tangodevice = 'tango://motorbox01.firepod.frm2.tum.de:10000/box/rc/motor',
+        visibility = (),
+    ),
 )
