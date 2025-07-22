191634fa467dc7c158a482f7c4bcb0b945cc5d93
Jens Krüger <jens.krueger@tum.de>
2025-07-23 00:23:25 +0200

    MLZ: Add elog handler to the EWorkBench
    Change-Id: I89f36168207f10269aa7a3e3c8051044b862e337
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/36831
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

--- nicos_mlz/erwin/setups/special/elog.py
+++ nicos_mlz/erwin/setups/special/elog.py
@@ -4,8 +4,16 @@ group = 'special'
 devices = dict(
     LogbookHtml = device('nicos.services.elog.handler.html.Handler'),
     LogbookText = device('nicos.services.elog.handler.text.Handler'),
+    LogbookWorkbench = device('nicos_mlz.devices.elog.eworkbench.Handler',
+        url = 'localhost',
+        port = 5672,
+        virtual_host = '/',
+        username = 'guest',
+        password = secret('eln_rabbitmq_password', default='guest'),
+        static_queue = 'erwin-workbench',
+    ),
     Logbook = device('nicos.services.elog.Logbook',
-        handlers = ['LogbookHtml', 'LogbookText'],
+        handlers = ['LogbookHtml', 'LogbookText', 'LogbookWorkbench'],
         cache = 'localhost',
     ),
 )
--- nicos_mlz/reseda/setups/special/elog.py
+++ nicos_mlz/reseda/setups/special/elog.py
@@ -4,8 +4,16 @@ group = 'special'
 devices = dict(
     LogbookHtml = device('nicos.services.elog.handler.html.Handler'),
     LogbookText = device('nicos.services.elog.handler.text.Handler'),
+    LogbookWorkbench = device('nicos_mlz.devices.elog.eworkbench.Handler',
+        url = 'localhost',
+        port = 5672,
+        virtual_host = '/',
+        username = 'guest',
+        password = secret('eln_rabbitmq_password', default='guest'),
+        static_queue = 'reseda-workbench',
+    ),
     Logbook = device('nicos.services.elog.Logbook',
-        handlers = ['LogbookHtml', 'LogbookText'],
+        handlers = ['LogbookHtml', 'LogbookText', 'LogbookWorkbench'],
         cache = 'resedactrl.reseda.frm2.tum.de',
     ),
 )
