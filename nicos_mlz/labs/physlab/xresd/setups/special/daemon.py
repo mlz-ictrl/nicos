3a2d55a16202431fe59e97bd5d3826f309446d03
Jens Krüger <jens.krueger@frm2.tum.de>
2025-08-19 20:51:44 +0200

    PhysLab: Cleanup daemon setups
    Remove useless comments
    
    Change-Id: I4cd1ac70d9119ba860c6d7a62e3db076054e4d2a
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/37219
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

--- nicos_mlz/labs/physlab/xresd/setups/special/daemon.py
+++ nicos_mlz/labs/physlab/xresd/setups/special/daemon.py
@@ -11,20 +11,15 @@ devices = dict(
     ),
     Auth = device('nicos.services.daemon.auth.list.Authenticator',
         hashing = 'md5',
-        # for the meaning of these entries see
-        # https://forge.frm2.tum.de/nicos/doc/nicos-stable/services/daemon/#nicos.services.daemon.auth.list.Authenticator
         passwd = [
             ('guest', '', 'guest'),
-            # The entries for these password hashes are generated from randomized
-            # passwords and not reproducible, please don't forget to create new
-            # ones.
             ('user', 'd3bde5ce3e546626df42771c58986d4e', 'user'),
             ('admin', 'f3309476bdb36550aa8fb90ae748c9cc', 'admin'),
         ],
     ),
     Daemon = device('nicos.services.daemon.NicosDaemon',
         server = 'localhost',
-        authenticators = ['NemoAuth'], # 'Auth'],
+        authenticators = ['NemoAuth'],
         loglevel = 'info',
     ),
 )
