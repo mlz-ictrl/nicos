c99a25d44c2f70aa6eb037b63b85f37d415cbfe2
Jens Krüger <jens.krueger@tum.de>
2025-10-26 14:36:11 +0100

    MLZ/Virt/Biodiff: Fix double defined device
    Change-Id: Ie12d6913b760515f54e25ba92e17fceb7bd07ae9
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/37691
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

--- nicos_virt_mlz/biodiff/setups/counter.py
+++ nicos_virt_mlz/biodiff/setups/counter.py
@@ -2,9 +2,6 @@ description = 'ZEA-2 counter card setup'
 group = 'lowlevel'
 
 devices = dict(
-    timer = device('nicos.devices.generic.VirtualTimer',
-        description = 'ZEA-2 counter card timer channel',
-    ),
     mon1 = device('nicos.devices.generic.VirtualCounter',
         description = 'Monitor 1',
         type = 'monitor',
