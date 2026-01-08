012f1781dee5254002880e5ab8b5f8074aff684e
Jens Krüger <jens.krueger@frm2.tum.de>
2026-01-08 10:46:41 +0100

    SINQ/BOA: Fix double defined device in setups
    Change-Id: I4432391de0c9d156fd4b85cc61b47d5c63319137
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/38203
    Reviewed-by: Stefan Mathis <stefan.mathis@psi.ch>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

--- nicos_sinq/boa/setups/sim_ad.py
+++ nicos_sinq/boa/setups/sim_ad.py
@@ -11,7 +11,7 @@ detector_channel = 'cam1:'
 motprefix = 'SQ:BOA:turboPmac1:DCCDATZ'
 counterprefix = 'SQ:BOA:counter'
 
-excludes = ['andorccd', 'embl', 'andorccd-l', 'andor', 'fastcomtec', 'camini', 'el737']
+excludes = ['andorccd', 'embl', 'andorccd-l', 'andor', 'fastcomtec', 'camini', 'el737', 'single_el737']
 
 devices = dict(
     dccdatz = device('nicos_sinq.devices.epics.sinqmotor_deprecated.SinqMotor',
