964a00928dd8b481225cb9c3590a91275224a7fb
Georg Brandl <g.brandl@fz-juelich.de>
2026-02-12 13:51:54 +0100

    POLI: update SPS device names
    Change-Id: I0be433629eb147219c3bdef703539a6e52360ee1
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/38440
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Georg Brandl <g.brandl@fz-juelich.de>

--- nicos_mlz/poli/setups/shutter.py
+++ nicos_mlz/poli/setups/shutter.py
@@ -5,11 +5,11 @@ tango_base = 'tango://phys.poli.frm2:10000/poli/'
 
 devices = dict(
     ShutterOut = device('nicos.devices.entangle.DigitalOutput',
-        tangodevice = tango_base + 's7_digital/shutter',
+        tangodevice = tango_base + 's7_io/shutter',
         visibility = (),
     ),
     ShutterIn = device('nicos.devices.entangle.DigitalOutput',
-        tangodevice = tango_base + 's7_digital/shutter_in',
+        tangodevice = tango_base + 's7_io/shutter_in',
         visibility = (),
     ),
     Shutter = device('nicos_mlz.poli.devices.shutter.Shutter',
