07126d7f28f3dcbd03cbcd9858bdc0538e9f4834
Enrico Faulhaber <enrico.faulhaber@frm2.tum.de>
2026-03-12 17:34:26 +0100

    MLZ/FIREPOD: add setup for neutron optic devices
    Change-Id: Ia478fe08c91efac24c53dcf103b054905c25614e
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/38772
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

---
+++ nicos_mlz/firepod/setups/optic.py
@@ -0,0 +1,33 @@
+description = 'Optic devices'
+
+group = 'lowlevel'
+
+tango_base = 'tango://firepodhw.firepod.frm2.tum.de:10000/beckhoff/optic/_'
+
+devices = dict(
+    bs = device('nicos.devices.entangle.NamedDigitalOutput',
+        description = 'Beamstop selection device',
+        tangodevice = tango_base + 'beamstop',
+        mapping = {'down': 0, 'up': 1},
+    ),
+    collimator = device('nicos.devices.entangle.NamedDigitalOutput',
+        description = 'Collimator insert device',
+        tangodevice = tango_base + 'collimator',
+        mapping = {'out': 0, 'in': 1},
+    ),
+    filter = device('nicos.devices.entangle.NamedDigitalOutput',
+        description = 'Graphite filter device',
+        tangodevice = tango_base + 'pg_filter',
+        mapping = {'out': 0, 'in': 1},
+    ),
+    laser = device('nicos.devices.entangle.NamedDigitalOutput',
+        description = 'Adjustment laser device',
+        tangodevice = tango_base + 'laser',
+        mapping = {'out': 0, 'in': 1},
+    ),
+    shutter = device('nicos.devices.entangle.NamedDigitalOutput',
+        description = 'Instrument fast shutter',
+        tangodevice = tango_base + 'shutter',
+        mapping = {'close': 0, 'open': 1},
+    ),
+)
