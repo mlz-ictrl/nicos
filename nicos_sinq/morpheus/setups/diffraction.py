f4d23b5f43ac584a0bd8f2cfa052180dff115bf8
Edward Wall <edward.wall@psi.ch>
2026-03-13 08:29:40 +0100

    SINQ/MORPHEUS: Removes decommissioned instrument
    Change-Id: I712c8990feaf06ff49605cb4c91136a21a62ccdb
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/38775
    Reviewed-by: Stefan Mathis <stefan.mathis@psi.ch>
    Reviewed-by: Edward Wall <edward.wall@psi.ch>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Alexander Söderqvist <alexander.soederqvist@psi.ch>

--- nicos_sinq/morpheus/setups/diffraction.py
+++
@@ -1,7 +0,0 @@
-description = 'Single-crystal diffraction on MORPHEUS'
-
-group = 'basic'
-
-includes = ['morpheus', 'slits', 'euler', 'monochromator']
-
-excludes = ['pnr']
--- nicos_sinq/morpheus/setups/special/config.py
+++
@@ -1,9 +0,0 @@
-description = 'Generic configuration settings for MORPHEUS'
-
-import os
-
-group = 'configdata'
-
-DATA_PATH = os.environ.get('NICOSDUMP', '.') + '/morpheus/'
-
-KAFKA_BROKERS = ['localhost:9092']
