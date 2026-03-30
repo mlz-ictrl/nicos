276b4f21eb3cc6f76f28c766b9f842fc8b480de6
Jens Krüger <jens.krueger@frm2.tum.de>
2026-03-30 18:06:52 +0200

    MLZ/Antares: Disable InfluxDB cache
    Change-Id: Id33788bcc3c9acb445ffc6c76d34889530f3e5f8
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/38912
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/antares/setups/special/cache-backup.py
+++ nicos_mlz/antares/setups/special/cache-backup.py
@@ -1,12 +1,16 @@
-description = 'setup for the flat file cache server for back up'
+description = 'setup for the cache server'
 group = 'special'
 
 devices = dict(
-    DB = device('nicos.services.cache.database.FlatfileCacheDatabase',
-        storepath = '/data/cache',
+    DB = device('nicos.services.cache.database.influxdb.InfluxDB2CacheDatabase',
+        url = 'http://app.antares.frm2.tum.de:8086',
+        org = 'mlz',
+        bucket = 'nicos-cache',
+        bucket_latest = 'nicos-cache-latest-values',
         loglevel = 'info',
     ),
     Server = device('nicos.services.cache.server.CacheServer',
+        description = 'Value caching server',
         db = 'DB',
         server = '0.0.0.0:14870',
         loglevel = 'info',
--- nicos_mlz/antares/setups/special/cache.py
+++ nicos_mlz/antares/setups/special/cache.py
@@ -2,17 +2,14 @@ description = 'setup for the cache server'
 group = 'special'
 
 devices = dict(
-    DB = device('nicos.services.cache.database.influxdb.InfluxDB2CacheDatabase',
-        url = 'http://app.antares.frm2.tum.de:8086',
-        org = 'mlz',
-        bucket = 'nicos-cache',
-        bucket_latest = 'nicos-cache-latest-values',
+    DB = device('nicos.services.cache.database.FlatfileCacheDatabase',
+        storepath = '/data/cache',
         loglevel = 'info',
     ),
     Server = device('nicos.services.cache.server.CacheServer',
         description = 'Value caching server',
         db = 'DB',
-        server = '',
+        server = '0.0.0.0:14869',
         loglevel = 'info',
     ),
 )
