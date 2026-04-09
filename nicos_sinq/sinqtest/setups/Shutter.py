7d972a0addf74cc0d854f2b40de3ec4c851c225e
Jens Krüger <jens.krueger@tum.de>
2026-04-09 21:37:49 +0200

    SINQ/SinqTest: Fix setup
    - not existing parameter in configuration
    
    Change-Id: Idb9fd559215452a3d268dab5c60916b09a480766
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/39009
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

--- nicos_sinq/sinqtest/setups/Shutter.py
+++ nicos_sinq/sinqtest/setups/Shutter.py
@@ -3,6 +3,6 @@ description = 'Shutter Control'
 devices = dict(
     Shutter = device('nicos_sinq.devices.epics.shutter.Shutter',
         description = 'Used to control the shutter',
-        shutterpvprefix = 'SQ:SINQTEST:SPS-SHUTTER',
+        pvprefix = 'SQ:SINQTEST:SPS-SHUTTER',
     ),
 )
