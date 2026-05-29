1be9d4971e481a86f52e128e94ddb123861f01e3
smathis <stefan.mathis@psi.ch>
2026-05-29 14:27:02 +0200

    CORE/EPICS: Use floatrange for non-negative values
    position_deadband and startdelay should obviously not be negative.
    
    Change-Id: If48df45c3c62db4c6778a43979644bb05dab0acd
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/39417
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Reviewed-by: Alexander Söderqvist <alexander.soederqvist@psi.ch>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Stefan Mathis <stefan.mathis@psi.ch>

--- nicos/devices/epics/motor.py
+++ nicos/devices/epics/motor.py
@@ -31,7 +31,7 @@ from nicos.core import Override, Param, UsageError, oneof, pvname, \
 from nicos.core.constants import MASTER
 from nicos.core.errors import LimitError
 from nicos.core.mixins import CanDisable, HasOffset
-from nicos.core.params import anytype, limits
+from nicos.core.params import anytype, floatrange, limits
 from nicos.devices.abstract import CanReference, Motor
 from nicos.devices.epics import EpicsAnalogMoveable
 from nicos.devices.epics.status import SEVERITY_TO_STATUS
@@ -195,7 +195,7 @@ class EpicsMotor(CanReference, HasOffset, CanDisable, EpicsAnalogMoveable, Motor
         'position_deadband':
             Param('Only move if the distance between target and current ' \
                   'position is larger than this value',
-                  type=float,
+                  type=floatrange(0),
                   category='precisions',
                   settable=False,
                   mandatory=False,
@@ -204,7 +204,7 @@ class EpicsMotor(CanReference, HasOffset, CanDisable, EpicsAnalogMoveable, Motor
         'startdelay':
             Param('Maximum time in seconds before starting a movement is ' \
                   'considered as failed',
-                  type=float,
+                  type=floatrange(0),
                   category='general',
                   settable=False,
                   mandatory=False,
