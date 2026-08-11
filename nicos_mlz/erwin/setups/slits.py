fea21df6709747a1107e694e0787a566ae76ac8f
Jens Krüger <jens.krueger@frm2.tum.de>
2026-08-11 21:22:37 +0200

    MLZ/Erwin: Move slit devices to real devices
    Change-Id: I378ee1f0cee63b4bad4ceffea2a741a7c6d77707
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/39972
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>

--- nicos_mlz/erwin/setups/slits.py
+++ nicos_mlz/erwin/setups/slits.py
@@ -2,19 +2,23 @@ description = 'Sample slits'
 
 group = 'lowlevel'
 
+tango_base = 'tango://motorbox01.erwin.frm2.tum.de:10000/box/'
+
 devices = dict(
     ssy = device('nicos.devices.generic.Axis',
         description = 'Y position of center of sample slit',
-        motor = device('nicos.devices.generic.VirtualMotor',
-            abslimits = (-25, 25),
+        motor = device('nicos.devices.entangle.Motor',
+            tangodevice = tango_base + 'channel1/motor',
+            # abslimits = (-25, 25),
             unit = 'mm',
         ),
         precision = 0.01,
     ),
     ssz = device('nicos.devices.generic.Axis',
         description = 'Z position of center of sample slit',
-        motor = device('nicos.devices.generic.VirtualMotor',
-            abslimits = (-25, 25),
+        motor = device('nicos.devices.entangle.Motor',
+            tangodevice = tango_base + 'channel2/motor',
+            # abslimits = (-25, 25),
             unit = 'mm',
         ),
         precision = 0.01,
