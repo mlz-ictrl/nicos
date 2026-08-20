11f4596fb0d65d0c8c8c07ef49e4880c1877d175
Jens Krüger <jens.krueger@frm2.tum.de>
2026-08-20 14:55:50 +0200

    MLZ/FirePOD: Fix sample detector distance
    The distance between sample and detector is changed to the "official"
    data taken from
    "https://mlz-garching.de/englisch/instruments/structure/firepod.html"
    
    Change-Id: I02e9e73b814b44c8f24ffe81cc03fd7a414b9119
    Reviewed-on: https://forge.frm2.tum.de/review/c/frm2/nicos/nicos/+/40055
    Tested-by: Jenkins Automated Tests <pedersen+jenkins@frm2.tum.de>
    Reviewed-by: Jens Krueger <jens.krueger@frm2.tum.de>

--- nicos_mlz/firepod/setups/distances.py
+++ nicos_mlz/firepod/setups/distances.py
@@ -5,8 +5,8 @@ group = 'lowlevel'
 devices = dict(
     detsampledist = device('nicos.devices.generic.ManualMove',
         description = 'Distance between sample and detector',
-        default = 1.117,
-        abslimits = (1.117, 1.117),
+        default = 1.375,
+        abslimits = (1.375, 1.375),
         unit = 'm',
     ),
 )
